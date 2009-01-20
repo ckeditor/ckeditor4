0803a34a0f82b637d0e34777afa7ee2e627ce396
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-01-20 16:53:17 +0000

    Copied default skin and _samples from prototype to trunk.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@2959 da63caf2-3823-0410-a1e8-d69ccee00dfb

---
+++ _samples/sample.js
@@ -0,0 +1,111 @@
+﻿/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+// Firebug has been presented some bugs with console. It must be "initialized"
+// before the page load to work.
+// FIXME: Remove the following in the future, if Firebug gets fixed.
+if ( typeof console != 'undefined' )
+	console.log();
+
+// %REMOVE_START%
+(function() {
+	// The sample##.html files also include this script. We don't want them to
+	// run, so we redirect to the samples.html page, passing the correct
+	// querystring parameter to load the desired page.
+
+	var sampleMatch = window.location.pathname.match( /[\/\\]([^\/\\]+).html/ );
+	if ( sampleMatch && sampleMatch[ 1 ] != 'sample' )
+		window.location = 'sample.html?sample=' + sampleMatch[ 1 ] + ( location.search.length > 1 ? '&' + location.search.substr( 1 ) : '' );
+})();
+// %REMOVE_END%
+
+if ( window.CKEDITOR ) {
+	CKEDITOR.samples = (function() {
+		var ajax = CKEDITOR.ajax;
+
+		// Default values for the CKEDITOR.samples properties.
+		var samples = {
+			htmlData: '<p>No HTML data available.</p>',
+			codeData: '<p>No code data available.</p>'
+		};
+
+		if ( /[?&]sample=[^&]+/.test( document.location.search ) ) {
+			var currentSample = document.location.search.match( /[?&]sample=([^&]+)/ )[ 1 ];
+			var sampleData = ajax.loadXml( CKEDITOR.getUrl( '_samples/' + currentSample + '.html' ) );
+
+			if ( sampleData ) {
+				var getNodeHtml = function( id ) {
+						return sampleData.getInnerXml( '//*[@id="' + id + '"]' );
+					};
+
+				samples.headScript = getNodeHtml( 'headscript' ) || '';
+				samples.styles = getNodeHtml( 'styles' ) || '';
+				samples.htmlData = getNodeHtml( 'html' ) || '';
+				samples.codeData = getNodeHtml( 'code' ) || '';
+
+				if ( samples.headScript )
+					samples.headScript = '<script id="headscript" type="text/javascript">' + samples.headScript + '</script>';
+
+				if ( samples.styles )
+					samples.styles = '<style id="styles" type="text/css">' + samples.styles + '</style>';
+
+				// The '//*/*/*' XPath is the only way to make it work with
+				// xmlns="http://www.w3.org/1999/xhtml" without workarounds. It
+				// means that we must always have <html><head><title> in this
+				// precise order.
+				document.title = sampleData.getInnerXml( '//*/*/*' );
+			}
+		}
+
+		return samples;
+	})();
+
+	(function() {
+		var showCompatibilityMsg = function() {
+				var env = CKEDITOR.env;
+
+				var html = '<p><strong>Your browser is not compatible with CKEditor.</strong>';
+
+				var browsers = {
+					gecko: 'Firefox 1.5',
+					ie: 'Internet Explorer 6.0',
+					opera: 'Opera 9.5',
+					webkit: 'Safari 3.0'
+				};
+
+				var alsoBrowsers = '';
+
+				for ( var key in env ) {
+					if ( browsers[ key ] ) {
+						if ( env[ key ] )
+							html += ' CKEditor is compatible with ' + browsers[ key ] + ' or higher.';
+						else
+							alsoBrowsers += browsers[ key ] + '+, ';
+					}
+				}
+
+				alsoBrowsers = alsoBrowsers.replace( /\+,([^,]+), $/, '+ and $1' );
+
+				html += ' It is also compatible with ' + alsoBrowsers + '.';
+
+				html += '</p><p>With non compatible browsers, you should still be able to see and edit the contents (HTML) in a plain text field.</p>';
+
+				document.getElementById( 'alerts' ).innerHTML = html;
+			};
+
+		var onload = function() {
+				// Show a friendly compatibility message as soon as the page is loaded,
+				// for those browsers that are not compatible with CKEditor.
+				if ( !CKEDITOR.env.isCompatible )
+					showCompatibilityMsg();
+			};
+
+		// Register the onload listener.
+		if ( window.addEventListener )
+			window.addEventListener( 'load', onload, false );
+		else if ( window.attachEvent )
+			window.attachEvent( 'onload', onload );
+	})();
+}
---
+++ _source/skins/default/skin.js
@@ -0,0 +1,98 @@
+﻿/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.skins.add( 'default', ( function() {
+	var preload = [];
+	var dialogJs = [];
+
+	if ( CKEDITOR.env.ie && CKEDITOR.env.version <= 6 ) {
+		// For IE6, we need to preload some images, otherwhise they will be
+		// downloaded several times (CSS background bug).
+		preload.push( 'icons.gif', 'images/sprites.gif', 'images/dialog.sides.gif' );
+
+		// The dialog must be fixed by code in IE6, as it doesn't support
+		// several CSS features (absolute positioning).
+		// dialogJs.push( 'dialog_ie6.js' );
+	}
+
+	return {
+		preload: preload,
+		editor: { css: [ 'editor.css' ] },
+		dialog: { css: [ 'dialog.css' ], js: dialogJs }
+	};
+})() );
+
+(function() {
+	// Define the function for resizing dialog parts at load to speed up
+	// the actual resize operation.
+	var skinName = 'default',
+		setSize = function( dialog, partName, width, height ) {
+			var element = partName ? dialog.parts[ partName ] : dialog._.element.getFirst();
+			if ( width )
+				element.setStyle( 'width', width + 'px' );
+			if ( height )
+				element.setStyle( 'height', height + 'px' );
+		};
+
+	CKEDITOR.dialog.setMargins( 0, 14, 18, 14 );
+
+	CKEDITOR.dialog.on( 'resize', function( evt ) {
+		var data = evt.data,
+			width = data.width,
+			height = data.height,
+			dialog = data.dialog,
+			standardsMode = ( CKEDITOR.document.$.compatMode == 'CSS1Compat' );
+		if ( data.skin != skinName )
+			return;
+
+		// Dialog parts dimensions.
+		//  16x16  |  ?x16  |  16x16
+		//  16x?   |  ?x?   |  16x?
+		//  30x51  |  ?x51  |  30x51
+		setSize( dialog, 't', width - 32, 16 );
+		setSize( dialog, 't_resize', width - 32, null );
+		setSize( dialog, 'l', 16, height - 67 );
+		setSize( dialog, 'l_resize', null, height - 22 );
+		setSize( dialog, 'c', width - 32, height - 67 );
+		setSize( dialog, 'r', 16, height - 67 );
+		setSize( dialog, 'r_resize', null, height - 22 );
+		setSize( dialog, 'b', width - 60, 51 );
+		setSize( dialog, 'b_resize', width - 32, null );
+		setSize( dialog, 'tabs_table', width - 32, null );
+
+		/*
+		 * Although the following fix seems to be for IE6 only, it's also for IE7.
+		 * While IE7 can render DIV nodes with left: and right: defined, it cannot
+		 * put 100% height TABLES correctly inside those DIVs. Unless the width is
+		 * set as well.
+		 */
+		if ( CKEDITOR.env.ie ) {
+			var contentWidth = width - 34,
+				contentHeight = dialog.getPageCount() > 1 ? height - 106 : height - 84,
+				contentsLength = dialog.parts.contents.getChildCount();
+
+			if ( !standardsMode ) {
+				contentWidth += 2;
+				contentHeight += 2;
+				dialog.parts.tabs.setStyle( 'top', '33px' );
+			}
+
+			setSize( dialog, 'title', standardsMode ? width - 52 : width - 32, standardsMode ? null : 31 );
+			setSize( dialog, 'contents', contentWidth, contentHeight );
+			setSize( dialog, 'footer', width - 32 );
+
+			for ( var i = 0; i < contentsLength; i++ ) {
+				var child = dialog.parts.contents.getChild( i );
+				if ( ( child instanceof CKEDITOR.dom.element ) && ( child.$.className || '' ).search( 'cke_dialog_page_contents' ) > -1 )
+					child.setStyles({
+					width: contentWidth - ( standardsMode ? 20 : 0 ) + 'px',
+					height: contentHeight - ( standardsMode ? 20 : 0 ) + 'px'
+				});
+			}
+		}
+
+		setSize( dialog, null, width, height );
+	});
+})();
