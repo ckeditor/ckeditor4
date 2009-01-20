93fd42db04b89a4c87e7bbfee8b998ded7043c4a
wwalc <wwalc@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-01-20 18:41:02 +0000

    Added run.bat and ckeditor.release
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@2971 da63caf2-3823-0410-a1e8-d69ccee00dfb

---
+++ _dev/releaser/run.bat
@@ -0,0 +1,12 @@
+:: Copyright (c) 2003-2008, Frederico Caldeira Knabben. All rights reserved.
+:: For licensing, see LICENSE.html or http://ckeditor.com/license
+
+@ECHO OFF
+
+CLS
+ECHO.
+
+:: rmdir /S /Q release
+
+cd ckreleaser
+java -cp js.jar;tools/javatar/tar.jar;tools/tartool/tartool.jar;tools/jaf/activation.jar org.mozilla.javascript.tools.shell.Main ckreleaser.js ../ckeditor.release ../../.. ../release 0.1 ckeditor_0.1.zip ckeditor_0.1.tar.gz
