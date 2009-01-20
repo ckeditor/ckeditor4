7da903c3a61f484e6120fa5cdd7e34492113d242
wwalc <wwalc@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-01-20 18:51:56 +0000

    corrected release.bat file
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@2973 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _dev/releaser/release.bat
+++ _dev/releaser/release.bat
@@ -9,4 +9,4 @@ ECHO.
 :: rmdir /S /Q release
 
 cd ckreleaser
-java -cp js.jar;tools/javatar/tar.jar;tools/tartool/tartool.jar;tools/jaf/activation.jar org.mozilla.javascript.tools.shell.Main ckreleaser.js ../ckeditor.release ../../.. ../release 0.1 ckeditor_0.1.zip ckeditor_0.1.tar.gz
+java -cp js.jar;tools/javatar/tar.jar;tools/tartool/tartool.jar;tools/jaf/activation.jar org.mozilla.javascript.tools.shell.Main ckreleaser.js ../ckreleaser.release ../../.. ../release 0.1 ckeditor_0.1.zip ckeditor_0.1.tar.gz
