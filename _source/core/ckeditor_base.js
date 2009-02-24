86f3397be40630cde0187462084781a47af96be7
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-02-24 14:59:04 +0000

    #2940 : Introduced ways to override CKEDITOR.basePath and CKEDITOR.getUrl.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@3091 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/ckeditor_base.js
+++ _source/core/ckeditor_base.js
@@ -12,7 +12,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 // Must be updated on changes in the script, as well as updated in the
 // ckeditor_source.js and ckeditor_basic_source.js files.
 
-// if (!window.CKEDITOR){window.CKEDITOR=(function(){return/**@lends CKEDITOR*/{_:{},status:'unloaded',timestamp:'',basePath:(function(){var A='';var B=document.getElementsByTagName('script');for (var i=0;i<B.length;i++){var C=B[i].src.match(/(^|.*[\\\/])ckeditor(?:_basic)?(?:_source)?.js(?:\?.*)?$/i);if (C){A=C[1];break;}};if (A.indexOf('://')==-1){if (A.indexOf('/')===0) A=location.href.match(/^.*?:\/\/[^\/]*/)[0]+A;else A=location.href.match(/^[^\?]*\//)[0]+A;};return A;})(),getUrl:function(resource){if (resource.indexOf('://')==-1&&resource.indexOf('/')!==0) resource=this.basePath+resource;if (this.timestamp) resource+=(resource.indexOf('?')>=0?'&':'?')+'t='+this.timestamp;return resource;}};})();};
+// if(!window.CKEDITOR)window.CKEDITOR=(function(){var a={timestamp:'',_:{},status:'unloaded',basePath:(function(){var d=window.CKEDITOR_BASEPATH||'';if(!d){var e=document.getElementsByTagName('script');for(var f=0;f<e.length;f++){var g=e[f].src.match(/(^|.*[\\\/])ckeditor(?:_basic)?(?:_source)?.js(?:\?.*)?$/i);if(g){d=g[1];break;}}}if(d.indexOf('://')==-1)if(d.indexOf('/')===0)d=location.href.match(/^.*?:\/\/[^\/]*/)[0]+d;else d=location.href.match(/^[^\?]*\/(?:)/)[0]+d;return d;})(),getUrl:function(d){if(d.indexOf('://')==-1&&d.indexOf('/')!==0)d=this.basePath+d;if(this.timestamp)d+=(d.indexOf('?')>=0?'&':'?')+('t=')+this.timestamp;return d;}},b=window.CKEDITOR_GETURL;if(b){var c=a.getUrl;a.getUrl=function(d){return b.call(a,d)||c.call(a,d);};}return a;})();
 
 // #### Raw code
 // ATTENTION: read the above "Compressed Code" notes when changing this code.
@@ -25,7 +25,8 @@ if ( !window.CKEDITOR ) {
 	 * @example
 	 */
 	window.CKEDITOR = (function() {
-		return /** @lends CKEDITOR */ {
+		var CKEDITOR =
+		/** @lends CKEDITOR */ {
 
 			/**
 			 * A constant string unique for each release of CKEditor. Its value
@@ -71,6 +72,9 @@ if ( !window.CKEDITOR ) {
 
 			/**
 			 * Contains the full URL for the CKEditor installation directory.
+			 * It's possible to manually provide the base path by setting a
+			 * global variable named CKEDITOR_BASEPATH. This global variable
+			 * must be set "before" the editor script loading.
 			 * @type String
 			 * @example
 			 * alert( <b>CKEDITOR.basePath</b> );  // "http://www.example.com/ckeditor/" (e.g.)
@@ -80,15 +84,18 @@ if ( !window.CKEDITOR ) {
 				// var basePath in "core/loader.js".
 
 				// Find out the editor directory path, based on its <script> tag.
-				var path = '';
-				var scripts = document.getElementsByTagName( 'script' );
+				var path = window.CKEDITOR_BASEPATH || '';
 
-				for ( var i = 0; i < scripts.length; i++ ) {
-					var match = scripts[ i ].src.match( /(^|.*[\\\/])ckeditor(?:_basic)?(?:_source)?.js(?:\?.*)?$/i );
+				if ( !path ) {
+					var scripts = document.getElementsByTagName( 'script' );
 
-					if ( match ) {
-						path = match[ 1 ];
-						break;
+					for ( var i = 0; i < scripts.length; i++ ) {
+						var match = scripts[ i ].src.match( /(^|.*[\\\/])ckeditor(?:_basic)?(?:_source)?.js(?:\?.*)?$/i );
+
+						if ( match ) {
+							path = match[ 1 ];
+							break;
+						}
 					}
 				}
 
@@ -110,6 +117,11 @@ if ( !window.CKEDITOR ) {
 			 * Gets the full URL for CKEditor resources. By default, URLs
 			 * returned by this function contains a querystring parameter ("t")
 			 * set to the {@link CKEDITOR.timestamp} value.
+			 * It's possible to provide a custom implementation to this
+			 * function by setting a global variable named CKEDITOR_GETURL.
+			 * This global variable must be set "before" the editor script
+			 * loading. If the custom implementation returns nothing, the
+			 * default implementation is used.
 			 * @returns {String} The full URL.
 			 * @example
 			 * // e.g. http://www.example.com/ckeditor/skins/default/editor.css?t=87dm
@@ -132,6 +144,18 @@ if ( !window.CKEDITOR ) {
 				return resource;
 			}
 		};
+
+		// Make it possible to override the getUrl function with a custom
+		// implementation pointing to a global named CKEDITOR_GETURL.
+		var newGetUrl = window.CKEDITOR_GETURL;
+		if ( newGetUrl ) {
+			var originalGetUrl = CKEDITOR.getUrl;
+			CKEDITOR.getUrl = function( resource ) {
+				return newGetUrl.call( CKEDITOR, resource ) || originalGetUrl.call( CKEDITOR, resource );
+			}
+		}
+
+		return CKEDITOR;
 	})();
 }
 
--- ckeditor_basic_source.js
+++ ckeditor_basic_source.js
@@ -5,36 +5,43 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 // Compressed version of core/ckeditor_base.js. See original for instructions.
 /*jsl:ignore*/
-if ( !window.CKEDITOR ) {
+if ( !window.CKEDITOR )
 	window.CKEDITOR = (function() {
-		return /**@lends CKEDITOR*/ {
-			_: {},
-			status: 'unloaded', timestamp: '', basePath: (function() {
-				var A = '';
-				var B = document.getElementsByTagName( 'script' );
-				for ( var i = 0; i < B.length; i++ ) {
-					var C = B[ i ].src.match( /(^|.*[\\\/])ckeditor(?:_basic)?(?:_source)?.js(?:\?.*)?$/i );
-					if ( C ) {
-						A = C[ 1 ];
+	var a = {
+		timestamp: '', _: {},
+		status: 'unloaded', basePath: (function() {
+			var d = window.CKEDITOR_BASEPATH || '';
+			if ( !d ) {
+				var e = document.getElementsByTagName( 'script' );
+				for ( var f = 0; f < e.length; f++ ) {
+					var g = e[ f ].src.match( /(^|.*[\\\/])ckeditor(?:_basic)?(?:_source)?.js(?:\?.*)?$/i );
+					if ( g ) {
+						d = g[ 1 ];
 						break;
 					}
-				};
-				if ( A.indexOf( '://' ) == -1 ) {
-					if ( A.indexOf( '/' ) === 0 )
-						A = location.href.match( /^.*?:\/\/[^\/]*/ )[ 0 ] + A;
-					else
-						A = location.href.match( /^[^\?]*\// )[ 0 ] + A;
-				};
-				return A;
-			})(), getUrl: function( resource ) {
-				if ( resource.indexOf( '://' ) == -1 && resource.indexOf( '/' ) !== 0 )
-					resource = this.basePath + resource;
-				if ( this.timestamp )
-					resource += ( resource.indexOf( '?' ) >= 0 ? '&' : '?' ) + 't=' + this.timestamp;
-				return resource;
-			} };
-	})();
-};
+				}
+			}
+			if ( d.indexOf( '://' ) == -1 )
+				if ( d.indexOf( '/' ) === 0 )
+				d = location.href.match( /^.*?:\/\/[^\/]*/ )[ 0 ] + d;
+			else
+				d = location.href.match( /^[^\?]*\/(?:)/ )[ 0 ] + d;
+			return d;
+		})(), getUrl: function( d ) {
+			if ( d.indexOf( '://' ) == -1 && d.indexOf( '/' ) !== 0 )
+				d = this.basePath + d;
+			if ( this.timestamp )
+				d += ( d.indexOf( '?' ) >= 0 ? '&' : '?' ) + ( 't=' ) + this.timestamp;
+			return d;
+		} },
+		b = window.CKEDITOR_GETURL; if ( b ) {
+		var c = a.getUrl;
+		a.getUrl = function( d ) {
+			return b.call( a, d ) || c.call( a, d );
+		};
+	}
+	return a;
+})();
 /*jsl:end*/
 
 // Uncomment the following line to have a new timestamp generated for each
