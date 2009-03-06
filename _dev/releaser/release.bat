ecad152c0ab9f157f43680c44ca494278e8d38f6
wwalc <wwalc@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-03-06 18:56:06 +0000

    Corrected batch files, changed external property
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@3155 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _dev/releaser/release.bat
+++ _dev/releaser/release.bat
@@ -1,5 +1,7 @@
-:: Copyright (c) 2003-2009, Frederico Caldeira Knabben. All rights reserved.
+::
+:: Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
 :: For licensing, see LICENSE.html or http://ckeditor.com/license
+::
 
 @ECHO OFF
 
@@ -8,6 +10,4 @@ ECHO.
 
 :: rmdir /S /Q release
 
-cd ckreleaser
-java -cp js.jar;tools/javatar/tar.jar;tools/tartool/tartool.jar;tools/jaf/activation.jar org.mozilla.javascript.tools.shell.Main ckreleaser.js ../ckreleaser.release ../../.. ../release 0.1 ckeditor_0.1.zip ckeditor_0.1.tar.gz
-cd ..
+java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release 0.1 ckeditor_0.1.zip ckeditor_0.1.tar.gz
