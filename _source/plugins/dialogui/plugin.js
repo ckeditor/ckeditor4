020f4401364cac1174981fb9f4735e94b079eeea
martinkou <martinkou@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-03-27 19:47:47 +0000

    Removed debug message from [3295].
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@3304 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/dialogui/plugin.js
+++ _source/plugins/dialogui/plugin.js
@@ -411,9 +411,6 @@ CKEDITOR.plugins.add( 'dialogui' );
 					element.on( 'click', function( evt ) {
 						me.fire( 'click', { dialog: me.getDialog() } );
 					});
-					element.on( 'keypress', function( evt ) {
-						console.log( evt.data.$.keyCode );
-					});
 				})();
 
 				element.unselectable();
