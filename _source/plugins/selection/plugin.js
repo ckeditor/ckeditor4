933f18e919529f4efe390e81b192338e924b5923
arczi <arczi@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-04-04 10:31:44 +0000

    #3267 Fixed bug in selection plugin occurring when editor is hidden
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@3346 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/selection/plugin.js
+++ _source/plugins/selection/plugin.js
@@ -437,9 +437,12 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 				// CKEDITOR.dom.range instances.
 
 				var sel = this.getNative(),
-					nativeRange = sel.createRange(),
+					nativeRange = sel && sel.createRange(),
 					type = this.getType(),
 					range;
+
+				if ( !sel )
+					return [];
 
 				if ( type == CKEDITOR.SELECTION_TEXT ) {
 					range = new CKEDITOR.dom.range( this.document );
@@ -486,6 +489,9 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 			var ranges = [];
 			var sel = this.getNative();
+
+			if ( !sel )
+				return [];
 
 			for ( var i = 0; i < sel.rangeCount; i++ ) {
 				var nativeRange = sel.getRangeAt( i );
