fccbbc4d3e783f35c561ef62e3c079738f09f77d
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-07-13 08:25:02 +0000

    Fixed #3928: Regression of [3818].
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@3864 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/htmldataprocessor/plugin.js
+++ _source/plugins/htmldataprocessor/plugin.js
@@ -104,7 +104,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 						savedAttributeName;
 					for ( var i = 0; i < attributeNames.length; i++ ) {
 						savedAttributeName = '_cke_saved_' + attributeNames[ i ];
-						savedAttributeName in attribs && ( delete attribs[ savedAttributeName ] );
+						savedAttributeName in attribs && ( delete attribs[ attributeNames[ i ] ] );
 					}
 				}
 			},
