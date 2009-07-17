888889f7cb31f0367b5856bf230e56ae60decff2
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-07-17 11:12:27 +0000

    Fixing #3972: eliminate duplicate variable definition which cause value overlapping.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@3925 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/domiterator/plugin.js
+++ _source/plugins/domiterator/plugin.js
@@ -66,8 +66,8 @@ CKEDITOR.plugins.add( 'domiterator' );
 					testRange.moveToPosition( this._.lastNode, CKEDITOR.POSITION_AFTER_END );
 					if ( testRange.checkEndOfBlock() ) {
 						var path = new CKEDITOR.dom.elementPath( testRange.endContainer );
-						var block = path.block || path.blockLimit;
-						this._.lastNode = block.getNextSourceNode( true );
+						var lastBlock = path.block || path.blockLimit;
+						this._.lastNode = lastBlock.getNextSourceNode( true );
 					}
 				}
 
