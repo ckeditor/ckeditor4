203fd3bd628622cb5296ab4e6949230bf98de966
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-07-31 12:38:37 +0000

    #4167 : Removed the API docs from the release.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@4040 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _dev/docs_build/docs_build.bat
+++ _dev/docs_build/docs_build.bat
@@ -3,15 +3,15 @@
 :: Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
 :: For licensing, see LICENSE.html or http://ckeditor.com/license
 ::
-:: Build the _docs/api documentation files.
+:: Builds the documentation files.
 ::
 
-ECHO Building the API document at _docs/api...
+ECHO Building the API document into the api_docs directory...
 
-del /F /Q "../../_docs/api/*.*"
+del /F /Q "api_docs/*.*"
 
 java -jar ../_thirdparty/jsdoc-toolkit/jsrun.jar ../_thirdparty/jsdoc-toolkit/app/run.js -c=docs_build.conf
 
-:: php ../fixlineends/fixlineends.php --eolstripwhite --eofnewline --eofstripwhite --nohidden --nosystem ../../_docs/api/
+:: php ../fixlineends/fixlineends.php --eolstripwhite --eofnewline --eofstripwhite --nohidden --nosystem api_docs/
 
 ECHO Finished!
