6ed4129e32c97eec69eb6ee0a56db894b00be03b
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-08-05 13:46:00 +0000

    Fixed the releaser settings into trunk.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@4060 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _dev/releaser/release.sh
+++ _dev/releaser/release.sh
@@ -3,4 +3,4 @@
 # Copyright (c) 2003-2009, Frederico Caldeira Knabben. All rights reserved.
 # For licensing, see LICENSE.html or http://ckeditor.com/license
 
-java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "CKEditor 3.0 SVN" ckeditor_3.0_svn
+java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.0 SVN" ckeditor_3.0_svn
