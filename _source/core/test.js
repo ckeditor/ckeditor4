90f13efececc45652f90300709eb874c0b8a0922
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-08-20 09:10:41 +0000

    Fixing #3010: Adding testing APIs for easy selection range setup.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@4144 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/test.js
+++ _source/core/test.js
@@ -9,5 +9,60 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
  *
  */
 
-CKEDITOR.test = {};
+CKEDITOR.test = {
+	/**
+	 * Retrieve the value of an element as normalized HTML.
+	 * @param id {String} Id of the element.
+	 */
+	getValueAsHtml: function( id ) {
+		return CKEDITOR.test.fixHtml( CKEDITOR.document.getById( id ).getValue(), false );
+	},
+
+	/**
+	 * Fill the the target element with both html and the denoted selection range.
+	 * @param element {CKEDITOR.dom.element} The element to fill.
+	 * @param html {String} String presentation of the HTML along with the
+	 * selection range marker to load.
+	 * @example
+	 * <p>single ^ selection</p>
+	 * <p>[multiple]<span>[selection]</span></p>
+	 */
+	setHtmlWithSelection: function( element, html ) {
+		if ( !CKEDITOR.dom.selection )
+			throw new Error( 'selection plugin is required to use this method.' );
+
+		var rangeCount = 0,
+			replaceWithBookmark = function( match ) {
+				var bookmark;
+				switch ( match ) {
+					case ']':
+						bookmark = '<span id="E' + rangeCount + '"></span>';
+						rangeCount++;
+						break;
+					case '^':
+						bookmark = '<span id="S' + rangeCount + '"></span>';
+						rangeCount++;
+						break;
+					case '[':
+						bookmark = '<span id="S' + rangeCount + '"></span>';
+				}
+				return bookmark;
+			};
+
+		html = html.replace( /\^|\[|\]/g, replaceWithBookmark );
+		element.setHtml( html );
+		var doc = element.getDocument(),
+			ranges = [],
+			sel = doc.getSelection();
+
+		for ( var i = 0; i < rangeCount; i++ ) {
+			var range = new CKEDITOR.dom.range( doc );
+			range.moveToBookmark({ startNode: 'S' + i, endNode: 'E' + i, serializable: true } );
+			ranges.push( range );
+		}
+
+		sel.selectRanges( ranges );
+	}
+
+};
 YAHOO.lang.augmentObject( CKEDITOR.test, CKTESTER.tools );
