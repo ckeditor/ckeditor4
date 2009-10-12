aa488d0647d7ba96fb9475f95c48dbfac508fbf2
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-10-12 14:47:12 +0000

    Fixing #4351: Dash and dot could appear in attribute names.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@4349 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/htmlparser.js
+++ _source/core/htmlparser.js
@@ -15,7 +15,7 @@ CKEDITOR.htmlParser = function() {
 };
 
 (function() {
-	var attribsRegex = /([\w:]+)(?:(?:\s*=\s*(?:(?:"([^"]*)")|(?:'([^']*)')|([^\s>]+)))|(?=\s|$))/g,
+	var attribsRegex = /([\w\-:.]+)(?:(?:\s*=\s*(?:(?:"([^"]*)")|(?:'([^']*)')|([^\s>]+)))|(?=\s|$))/g,
 		emptyAttribs = { checked:1,compact:1,declare:1,defer:1,disabled:1,ismap:1,multiple:1,nohref:1,noresize:1,noshade:1,nowrap:1,readonly:1,selected:1 };
 
 	CKEDITOR.htmlParser.prototype = {
