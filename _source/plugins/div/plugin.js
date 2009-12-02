8a0afe3f12786469d602a446475252b2bf666443
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2009-12-02 17:42:10 +0000

    Post fixing #2885.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@4565 da63caf2-3823-0410-a1e8-d69ccee00dfb

---
+++ _source/plugins/div/plugin.js
@@ -0,0 +1,112 @@
+/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+/**
+ * @fileOverview The "div" plugin. It wraps the selected block level elements with a 'div' element with specified styles and attributes.
+ * 
+ */
+
+(function() {
+	CKEDITOR.plugins.add( 'div', {
+		requires: [ 'editingblock', 'domiterator' ],
+
+		init: function( editor ) {
+			var lang = editor.lang.div;
+
+			editor.addCommand( 'creatediv', new CKEDITOR.dialogCommand( 'creatediv' ) );
+			editor.addCommand( 'editdiv', new CKEDITOR.dialogCommand( 'editdiv' ) );
+			editor.addCommand( 'removediv', {
+				exec: function( editor ) {
+					var selection = editor.getSelection(),
+						ranges = selection && selection.getRanges(),
+						range,
+						bookmarks = selection.createBookmarks(),
+						walker,
+						toRemove = [];
+
+					function findDiv( node ) {
+						var path = new CKEDITOR.dom.elementPath( node ),
+							blockLimit = path.blockLimit,
+							div = blockLimit.is( 'div' ) && blockLimit;
+
+						if ( div && !div.getAttribute( '_cke_div_added' ) ) {
+							toRemove.push( div );
+							div.setAttribute( '_cke_div_added' );
+						}
+					}
+
+					for ( var i = 0; i < ranges.length; i++ ) {
+						range = ranges[ i ];
+						if ( range.collapsed )
+							findDiv( selection.getStartElement() );
+						else {
+							walker = new CKEDITOR.dom.walker( range );
+							walker.evaluator = findDiv;
+							walker.lastForward();
+						}
+					}
+
+					for ( var i = 0; i < toRemove.length; i++ )
+						toRemove[ i ].remove( true );
+
+					selection.selectBookmarks( bookmarks );
+				}
+			});
+
+			editor.ui.addButton( 'CreateDiv', {
+				label: lang.toolbar,
+				command: 'creatediv'
+			});
+
+			if ( editor.addMenuItems ) {
+				editor.addMenuItems({
+					editdiv: {
+						label: lang.edit,
+						command: 'editdiv',
+						group: 'div',
+						order: 1
+					},
+
+					removediv: {
+						label: lang.remove,
+						command: 'removediv',
+						group: 'div',
+						order: 5
+					}
+				});
+
+				if ( editor.contextMenu ) {
+					editor.contextMenu.addListener( function( element, selection ) {
+						if ( !element )
+							return null;
+
+						var elementPath = new CKEDITOR.dom.elementPath( element ),
+							blockLimit = elementPath.blockLimit;
+
+						if ( blockLimit && blockLimit.getAscendant( 'div', true ) ) {
+							return {
+								editdiv: CKEDITOR.TRISTATE_OFF,
+								removediv: CKEDITOR.TRISTATE_OFF
+							}
+						}
+
+						return null;
+					});
+				}
+			}
+
+			CKEDITOR.dialog.add( 'creatediv', this.path + 'dialogs/div.js' );
+			CKEDITOR.dialog.add( 'editdiv', this.path + 'dialogs/div.js' );
+		}
+	});
+})();
+
+/*
+ * @name CKEDITOR.config.div_wrapTable
+ * Whether to wrap the whole table instead of indivisual cells when created 'div' in table cell.
+ * @type Boolean
+ * @default false
+ * @example config.div_wrapTable = true;
+ */
