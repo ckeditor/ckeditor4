652c7eee965bb51bc6e3ea6a975ec9f6c32471a1
alfonsoml <alfonsoml@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-01-12 18:39:31 +0000

    #4637 nowrap attribute in table cells were not loaded
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@4916 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/tabletools/dialogs/tableCell.js
+++ _source/plugins/tabletools/dialogs/tableCell.js
@@ -164,6 +164,10 @@ CKEDITOR.dialog.add( 'cellProperties', function( editor ) {
 							[ langCell.yes, 'yes' ],
 							[ langCell.no, 'no' ]
 							],
+						setup: function( selectedCell ) {
+							if ( selectedCell.getAttribute( 'noWrap' ) )
+								this.setValue( 'no' );
+						},
 						commit: function( selectedCell ) {
 							if ( this.getValue() == 'no' )
 								selectedCell.setAttribute( 'noWrap', 'nowrap' );
