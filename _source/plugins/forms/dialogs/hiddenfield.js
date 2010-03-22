07673ae90aaa521d5400a0152c0f4893b12cbfa8
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-03-22 15:57:04 +0000

    Reverting wrong changes been included in [5263].
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5264 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/forms/dialogs/hiddenfield.js
+++ _source/plugins/forms/dialogs/hiddenfield.js
@@ -1,45 +1,35 @@
-/*
+﻿/*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 CKEDITOR.dialog.add( 'hiddenfield', function( editor ) {
 	return {
 		title: editor.lang.hidden.title,
-		hiddenField: null,
 		minWidth: 350,
 		minHeight: 110,
 		onShow: function() {
 			delete this.hiddenField;
 
-			var editor = this.getParentEditor(),
-				selection = editor.getSelection(),
-				element = selection.getSelectedElement();
-
-			if ( element && element.getAttribute( '_cke_real_element_type' ) && element.getAttribute( '_cke_real_element_type' ) == 'hiddenfield' ) {
+			var element = this.getParentEditor().getSelection().getSelectedElement();
+			if ( element && element.getName() == "input" && element.getAttribute( 'type' ) == "checkbox" ) {
 				this.hiddenField = element;
-				element = editor.restoreRealElement( this.hiddenField );
 				this.setupContent( element );
-				selection.selectElement( this.hiddenField );
 			}
 		},
 		onOk: function() {
-			var name = this.getValueOf( 'info', '_cke_saved_name' ),
-				value = this.getValueOf( 'info', 'value' ),
-				editor = this.getParentEditor(),
-				element = CKEDITOR.env.ie ? editor.document.createElement( '<input name="' + CKEDITOR.tools.htmlEncode( name ) + '">' ) : editor.document.createElement( 'input' );
+			var editor,
+				element = this.hiddenField,
+				isInsertMode = !element;
 
-			element.setAttribute( 'type', 'hidden' );
-			this.commitContent( element );
-			var fakeElement = editor.createFakeElement( element, 'cke_hidden', 'hiddenfield' );
-
-			if ( !this.hiddenField )
-				editor.insertElement( fakeElement );
-			else {
-				fakeElement.replace( this.hiddenField );
-				editor.getSelection().selectElement( fakeElement );
+			if ( isInsertMode ) {
+				editor = this.getParentEditor();
+				element = editor.document.createElement( 'input' );
+				element.setAttribute( 'type', 'hidden' );
 			}
 
-			return true;
+			if ( isInsertMode )
+				editor.insertElement( element );
+			this.commitContent( element );
 		},
 		contents: [
 			{
@@ -54,13 +44,13 @@ CKEDITOR.dialog.add( 'hiddenfield', function( editor ) {
 				'default': '',
 				accessKey: 'N',
 				setup: function( element ) {
-
 					this.setValue( element.getAttribute( '_cke_saved_name' ) || element.getAttribute( 'name' ) || '' );
 				},
 				commit: function( element ) {
 					if ( this.getValue() )
-						element.setAttribute( 'name', this.getValue() );
+						element.setAttribute( '_cke_saved_name', this.getValue() );
 					else {
+						element.removeAttribute( '_cke_saved_name' );
 						element.removeAttribute( 'name' );
 					}
 				}
