e8a7d2e2cf939add46b4ff0a798242e3bce85685
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-04-02 08:18:34 +0000

    In-code documentation updates.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5333 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/basicstyles/plugin.js
+++ _source/plugins/basicstyles/plugin.js
@@ -46,43 +46,50 @@ CKEDITOR.plugins.add( 'basicstyles', {
  * config.coreStyles_bold = { element : 'span', attributes : {'class': 'Bold'} };
  */
 CKEDITOR.config.coreStyles_bold = { element: 'strong', overrides: 'b' };
+
 /**
  * The style definition to be used to apply the italic style in the text.
  * @type Object
  * @default { element : 'em', overrides : 'i' }
  * @example
- * CKEDITOR.config.coreStyles_italic		= { element : 'span', attributes : {'class': 'Italic'} };
+ * config.coreStyles_bold = { element : 'i', overrides : 'em' };
+ * @example
+ * CKEDITOR.config.coreStyles_italic = { element : 'span', attributes : {'class': 'Italic'} };
  */
 CKEDITOR.config.coreStyles_italic = { element: 'em', overrides: 'i' };
+
 /**
  * The style definition to be used to apply the underline style in the text.
  * @type Object
  * @default { element : 'u' }
  * @example
- * CKEDITOR.config.coreStyles_underline	= { element : 'span', attributes : {'class': 'Underline'}};
+ * CKEDITOR.config.coreStyles_underline = { element : 'span', attributes : {'class': 'Underline'}};
  */
 CKEDITOR.config.coreStyles_underline = { element: 'u' };
+
 /**
  * The style definition to be used to apply the strike style in the text.
  * @type Object
  * @default { element : 'strike' }
  * @example
- * CKEDITOR.config.coreStyles_strike		= { element : 'span', attributes : {'class': 'StrikeThrough'}, overrides : 'strike' };
+ * CKEDITOR.config.coreStyles_strike = { element : 'span', attributes : {'class': 'StrikeThrough'}, overrides : 'strike' };
  */
 CKEDITOR.config.coreStyles_strike = { element: 'strike' };
+
 /**
  * The style definition to be used to apply the subscript style in the text.
  * @type Object
  * @default { element : 'sub' }
  * @example
- * CKEDITOR.config.coreStyles_subscript	= { element : 'span', attributes : {'class': 'Subscript'}, overrides : 'sub' };
+ * CKEDITOR.config.coreStyles_subscript = { element : 'span', attributes : {'class': 'Subscript'}, overrides : 'sub' };
  */
 CKEDITOR.config.coreStyles_subscript = { element: 'sub' };
+
 /**
  * The style definition to be used to apply the superscript style in the text.
  * @type Object
  * @default { element : 'sup' }
  * @example
- * CKEDITOR.config.coreStyles_superscript	= { element : 'span', attributes : {'class': 'Superscript'}, overrides : 'sup' };
+ * CKEDITOR.config.coreStyles_superscript = { element : 'span', attributes : {'class': 'Superscript'}, overrides : 'sup' };
  */
 CKEDITOR.config.coreStyles_superscript = { element: 'sup' };
