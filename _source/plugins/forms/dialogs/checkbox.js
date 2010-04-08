4db4ad9dd74293cd92730181b9be9f99bb1849cc
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-04-08 16:26:50 +0000

    #5499: Fixing JS error thrown when change checkbox value attribute since we re-create an element without owning document.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5359 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/forms/dialogs/checkbox.js
+++ _source/plugins/forms/dialogs/checkbox.js
@@ -80,7 +80,7 @@ CKEDITOR.dialog.add( 'checkbox', function( editor ) {
 					else {
 						if ( CKEDITOR.env.ie ) {
 							// Remove attribute 'value' of checkbox #4721.
-							var checkbox = new CKEDITOR.dom.element( 'input' );
+							var checkbox = new CKEDITOR.dom.element( 'input', element.getDocument() );
 							element.copyAttributes( checkbox, { value:1 } );
 							checkbox.replace( element );
 							editor.getSelection().selectElement( checkbox );
