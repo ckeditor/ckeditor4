eadc0de82d00560cf1d076c8a5ccb158a8b504cf
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-04-09 13:23:04 +0000

    #5326 : Updates to the Catalan language file. Thanks to mcus21.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5363 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/lang/ca.js
+++ _source/lang/ca.js
@@ -30,15 +30,15 @@ CKEDITOR.lang[ 'ca' ] = {
 	 * Screenreader titles. Please note that screenreaders are not always capable
 	 * of reading non-English words. So be careful while translating it.
 	 */
-	editorTitle: 'Rich text editor, %1, press ALT 0 for help.', // MISSING
+	editorTitle: 'Editor de text enriquit, %1, prem ALT 0 per obtenir ajuda.',
 
 	// ARIA descriptions.
-	toolbar: 'Toolbar', // MISSING
-	editor: 'Rich Text Editor', // MISSING
+	toolbar: 'Barra d\'eines',
+	editor: 'Editor de text enriquit',
 
 	// Toolbar buttons without dialogs.
 	source: 'Codi font',
-	newPage: 'Nova Pàgina',
+	newPage: 'Nova pàgina',
 	save: 'Desa',
 	preview: 'Visualització prèvia',
 	cut: 'Retalla',
@@ -90,19 +90,19 @@ CKEDITOR.lang[ 'ca' ] = {
 		cssStyle: 'Estil',
 		ok: 'D\'acord',
 		cancel: 'Cancel·la',
-		close: 'Close', // MISSING
-		preview: 'Preview', // MISSING
+		close: 'Tanca',
+		preview: 'Previsualitza',
 		generalTab: 'General',
 		advancedTab: 'Avançat',
 		validateNumberFailed: 'Aquest valor no és un número.',
 		confirmNewPage: 'Els canvis en aquest contingut que no es desin es perdran. Esteu segur que voleu carregar una pàgina nova?',
 		confirmCancel: 'Algunes opcions s\'han canviat. Esteu segur que voleu tancar la finestra de diàleg?',
-		options: 'Options', // MISSING
-		target: 'Target', // MISSING
-		targetNew: 'New Window (_blank)', // MISSING
-		targetTop: 'Topmost Window (_top)', // MISSING
-		targetSelf: 'Same Window (_self)', // MISSING
-		targetParent: 'Parent Window (_parent)', // MISSING
+		options: 'Opcions',
+		target: 'Destí',
+		targetNew: 'Nova finestra (_blank)',
+		targetTop: 'Finestra major (_top)',
+		targetSelf: 'Mateixa finestra (_self)',
+		targetParent: 'Finestra pare (_parent)',
 
 		// Put the voice-only part of the label in the span.
 		unavailable: '%1<span class="cke_accessibility">, no disponible</span>'
@@ -130,7 +130,7 @@ CKEDITOR.lang[ 'ca' ] = {
 		upload: 'Puja',
 		advanced: 'Avançat',
 		type: 'Tipus d\'enllaç',
-		toUrl: 'URL', // MISSING
+		toUrl: 'URL',
 		toAnchor: 'Àncora en aquesta pàgina',
 		toEmail: 'Correu electrònic',
 		targetFrame: '<marc>',
@@ -214,7 +214,7 @@ CKEDITOR.lang[ 'ca' ] = {
 		width: 'Amplada',
 		widthPx: 'píxels',
 		widthPc: 'percentatge',
-		widthUnit: 'width unit', // MISSING
+		widthUnit: 'unitat d\'amplada',
 		height: 'Alçada',
 		cellSpace: 'Espaiat de cel·les',
 		cellPad: 'Encoixinament de cel·les',
@@ -264,7 +264,7 @@ CKEDITOR.lang[ 'ca' ] = {
 			invalidHeight: 'L\'alçada de cel·la ha de ser un nombre.',
 			invalidRowSpan: 'L\'expansió de files ha de ser un nombre enter.',
 			invalidColSpan: 'L\'expansió de columnes ha de ser un nombre enter.',
-			chooseColor: 'Choose' // MISSING
+			chooseColor: 'Trieu'
 		},
 
 		row: {
@@ -366,7 +366,7 @@ CKEDITOR.lang[ 'ca' ] = {
 		width: 'Amplada',
 		height: 'Alçada',
 		lockRatio: 'Bloqueja les proporcions',
-		unlockRatio: 'Unlock Ratio', // MISSING
+		unlockRatio: 'Desbloqueja el ràtio',
 		resetSize: 'Restaura la mida',
 		border: 'Vora',
 		hSpace: 'Espaiat horit.',
@@ -378,12 +378,12 @@ CKEDITOR.lang[ 'ca' ] = {
 		linkTab: 'Enllaç',
 		button2Img: 'Voleu transformar el botó d\'imatge seleccionat en una simple imatge?',
 		img2Button: 'Voleu transformar la imatge seleccionada en un botó d\'imatge?',
-		urlMissing: 'Image source URL is missing.', // MISSING
-		validateWidth: 'Width must be a whole number.', // MISSING
-		validateHeight: 'Height must be a whole number.', // MISSING
-		validateBorder: 'Border must be a whole number.', // MISSING
-		validateHSpace: 'HSpace must be a whole number.', // MISSING
-		validateVSpace: 'VSpace must be a whole number.' // MISSING
+		urlMissing: 'Falta la URL de la imatge.',
+		validateWidth: 'L\'amplada ha de ser un nombre enter.',
+		validateHeight: 'L\'alçada ha de ser un nombre enter.',
+		validateBorder: 'La vora ha de ser un nombre enter.',
+		validateHSpace: 'HSpace ha de ser un nombre enter.',
+		validateVSpace: 'VSpace ha de ser un nombre enter.'
 	},
 
 	// Flash Dialog
@@ -466,7 +466,7 @@ CKEDITOR.lang[ 'ca' ] = {
 	},
 
 	elementsPath: {
-		eleLabel: 'Elements path', // MISSING
+		eleLabel: 'Elements path',
 		eleTitle: '%1 element'
 	},
 
@@ -486,18 +486,18 @@ CKEDITOR.lang[ 'ca' ] = {
 
 	clipboard: {
 		title: 'Enganxa',
-		cutError: 'La seguretat del vostre navegador no permet executar automàticament les operacions de retallar. Si us plau, utilitzeu el teclat (Ctrl/Cmd+X).',
-		copyError: 'La seguretat del vostre navegador no permet executar automàticament les operacions de copiar. Si us plau, utilitzeu el teclat (Ctrl/Cmd+C).',
-		pasteMsg: 'Si us plau, enganxeu dins del següent camp utilitzant el teclat (<STRONG>Ctrl/Cmd+V</STRONG>) i premeu <STRONG>OK</STRONG>.',
+		cutError: 'La seguretat del vostre navegador no permet executar automàticament les operacions de retallar. Si us plau, utilitzeu el teclat (Ctrl+X).',
+		copyError: 'La seguretat del vostre navegador no permet executar automàticament les operacions de copiar. Si us plau, utilitzeu el teclat (Ctrl+C).',
+		pasteMsg: 'Si us plau, enganxeu dins del següent camp utilitzant el teclat (<STRONG>Ctrl+V</STRONG>) i premeu <STRONG>OK</STRONG>.',
 		securityMsg: 'A causa de la configuració de seguretat del vostre navegador, l\'editor no pot accedir al porta-retalls directament. Enganxeu-ho un altre cop en aquesta finestra.',
-		pasteArea: 'Paste Area' // MISSING
+		pasteArea: 'Àrea d\'enganxat'
 	},
 
 	pastefromword: {
 		confirmCleanup: 'El text que voleu enganxar sembla provenir de Word. Voleu netejar aquest text abans que sigui enganxat?',
 		toolbar: 'Enganxa des del Word',
 		title: 'Enganxa des del Word',
-		error: 'It was not possible to clean up the pasted data due to an internal error' // MISSING
+		error: 'No ha estat possible netejar les dades enganxades degut a un error intern'
 	},
 
 	pasteText: {
@@ -518,7 +518,7 @@ CKEDITOR.lang[ 'ca' ] = {
 
 	stylesCombo: {
 		label: 'Estil',
-		panelTitle: 'Formatting Styles', // MISSING
+		panelTitle: 'Estils de format',
 		panelTitle1: 'Estils de bloc',
 		panelTitle2: 'Estils incrustats',
 		panelTitle3: 'Estils d\'objecte'
@@ -541,19 +541,19 @@ CKEDITOR.lang[ 'ca' ] = {
 	},
 
 	div: {
-		title: 'Create Div Container', // MISSING
-		toolbar: 'Create Div Container', // MISSING
-		cssClassInputLabel: 'Stylesheet Classes', // MISSING
-		styleSelectLabel: 'Style', // MISSING
-		IdInputLabel: 'Id', // MISSING
-		languageCodeInputLabel: ' Language Code', // MISSING
-		inlineStyleInputLabel: 'Inline Style', // MISSING
-		advisoryTitleInputLabel: 'Advisory Title', // MISSING
-		langDirLabel: 'Language Direction', // MISSING
-		langDirLTRLabel: 'Left to Right (LTR)', // MISSING
-		langDirRTLLabel: 'Right to Left (RTL)', // MISSING
-		edit: 'Edit Div', // MISSING
-		remove: 'Remove Div' // MISSING
+		title: 'Crea un contenidor Div',
+		toolbar: 'Crea un contenidor Div',
+		cssClassInputLabel: 'Classes de la fulla d\'estils',
+		styleSelectLabel: 'Estil',
+		IdInputLabel: 'Id',
+		languageCodeInputLabel: ' Codi d\'idioma',
+		inlineStyleInputLabel: 'Estil en línia',
+		advisoryTitleInputLabel: 'Títol de guia',
+		langDirLabel: 'Direcció de l\'idioma',
+		langDirLTRLabel: 'D\'esquerra a dreta (LTR)',
+		langDirRTLLabel: 'De dreta a esquerra (RTL)',
+		edit: 'Edita Div',
+		remove: 'Elimina Div'
 	},
 
 	font: {
@@ -571,52 +571,52 @@ CKEDITOR.lang[ 'ca' ] = {
 	colorButton: {
 		textColorTitle: 'Color de Text',
 		bgColorTitle: 'Color de Fons',
-		panelTitle: 'Colors', // MISSING
+		panelTitle: 'Colors',
 		auto: 'Automàtic',
 		more: 'Més colors...'
 	},
 
 	colors: {
-		'000': 'Black', // MISSING
-		'800000': 'Maroon', // MISSING
-		'8B4513': 'Saddle Brown', // MISSING
-		'2F4F4F': 'Dark Slate Gray', // MISSING
-		'008080': 'Teal', // MISSING
-		'000080': 'Navy', // MISSING
-		'4B0082': 'Indigo', // MISSING
-		'696969': 'Dim Gray', // MISSING
-		'B22222': 'Fire Brick', // MISSING
-		'A52A2A': 'Brown', // MISSING
-		'DAA520': 'Golden Rod', // MISSING
-		'006400': 'Dark Green', // MISSING
-		'40E0D0': 'Turquoise', // MISSING
-		'0000CD': 'Medium Blue', // MISSING
-		'800080': 'Purple', // MISSING
-		'808080': 'Gray', // MISSING
-		'F00': 'Red', // MISSING
-		'FF8C00': 'Dark Orange', // MISSING
-		'FFD700': 'Gold', // MISSING
-		'008000': 'Green', // MISSING
-		'0FF': 'Cyan', // MISSING
-		'00F': 'Blue', // MISSING
-		'EE82EE': 'Violet', // MISSING
-		'A9A9A9': 'Dark Gray', // MISSING
-		'FFA07A': 'Light Salmon', // MISSING
-		'FFA500': 'Orange', // MISSING
-		'FFFF00': 'Yellow', // MISSING
-		'00FF00': 'Lime', // MISSING
-		'AFEEEE': 'Pale Turquoise', // MISSING
-		'ADD8E6': 'Light Blue', // MISSING
-		'DDA0DD': 'Plum', // MISSING
-		'D3D3D3': 'Light Grey', // MISSING
-		'FFF0F5': 'Lavender Blush', // MISSING
-		'FAEBD7': 'Antique White', // MISSING
-		'FFFFE0': 'Light Yellow', // MISSING
-		'F0FFF0': 'Honeydew', // MISSING
-		'F0FFFF': 'Azure', // MISSING
-		'F0F8FF': 'Alice Blue', // MISSING
-		'E6E6FA': 'Lavender', // MISSING
-		'FFF': 'White' // MISSING
+		'000': 'Negre',
+		'800000': 'Granat',
+		'8B4513': 'Marró sella',
+		'2F4F4F': 'Gris pissarra fosca',
+		'008080': 'Blau xarxet',
+		'000080': 'Blau marí',
+		'4B0082': 'Indi',
+		'696969': 'Gris intens',
+		'B22222': 'Maó',
+		'A52A2A': 'Marró (web)',
+		'DAA520': 'Solidago',
+		'006400': 'Verd fosc',
+		'40E0D0': 'Turquesa',
+		'0000CD': 'Atzur',
+		'800080': 'Lila',
+		'808080': 'Gris',
+		'F00': 'Vermell',
+		'FF8C00': 'Taronja fosc',
+		'FFD700': 'Or',
+		'008000': 'Verd',
+		'0FF': 'Cian',
+		'00F': 'Blau',
+		'EE82EE': 'Lavanda rosat',
+		'A9A9A9': 'Gris clar',
+		'FFA07A': 'Salmó clar',
+		'FFA500': 'Taronja',
+		'FFFF00': 'Groc',
+		'00FF00': 'Verd llima',
+		'AFEEEE': 'Blau pàlid',
+		'ADD8E6': 'Blau clar',
+		'DDA0DD': 'Pruna',
+		'D3D3D3': 'Gris clar',
+		'FFF0F5': 'Lavanda rosat',
+		'FAEBD7': 'Blanc antic',
+		'FFFFE0': 'Groc clar',
+		'F0FFF0': 'Verd pàlid',
+		'F0FFFF': 'Blau cel pàlid',
+		'F0F8FF': 'Cian pàlid',
+		'E6E6FA': 'Lavanda',
+		'FFF': 'Blanc'
 	},
 
 	scayt: {
@@ -645,8 +645,8 @@ CKEDITOR.lang[ 'ca' ] = {
 		copy: 'Copyright &copy; $1. All rights reserved.'
 	},
 
-	maximize: 'Maximiza',
-	minimize: 'Minimize', // MISSING
+	maximize: 'Maximitza',
+	minimize: 'Minimitza',
 
 	fakeobjects: {
 		anchor: 'Àncora',
@@ -658,12 +658,12 @@ CKEDITOR.lang[ 'ca' ] = {
 	resize: 'Arrossegueu per redimensionar',
 
 	colordialog: {
-		title: 'Select color', // MISSING
-		highlight: 'Highlight', // MISSING
-		selected: 'Selected', // MISSING
-		clear: 'Clear' // MISSING
+		title: 'Selecciona el color',
+		highlight: 'Destacat',
+		selected: 'Seleccionat',
+		clear: 'Neteja'
 	},
 
-	toolbarCollapse: 'Collapse Toolbar', // MISSING
-	toolbarExpand: 'Expand Toolbar' // MISSING
+	toolbarCollapse: 'Redueix la barra d\'eines',
+	toolbarExpand: 'Amplia la barra d\'eines'
 };
