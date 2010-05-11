393341fb3cb0271e413be7fcd605bcceaa9131bc
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-05-11 15:49:25 +0000

    #5237: Deprecate the unicode-bidi:override style for dialog title.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5454 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/lang/ar.js
+++ _source/lang/ar.js
@@ -677,7 +677,7 @@ CKEDITOR.lang[ 'ar' ] = {
 
 	about: {
 		title: 'عن CKEditor',
-		dlgTitle: 'عن rotidEKC',
+		dlgTitle: 'عن CKEditor',
 		moreInfo: 'للحصول على معلومات الترخيص ، يرجى زيارة موقعنا على شبكة الانترنت:',
 		copy: 'حقوق النشر &copy; $1. جميع الحقوق محفوظة.'
 	},
