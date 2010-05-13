3ae2fd0179c27f8b83cea5a4f9e9434744c5ac0d
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-05-13 11:47:00 +0000

    Remove duplicate change entry of [5466].
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5467 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/pastetext/dialogs/pastetext.js
+++ _source/plugins/pastetext/dialogs/pastetext.js
@@ -45,7 +45,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 						'height:170px;' +
 						'resize: none;' +
 						'dir=' + editor.config.contentsLangDirection + ';' +
-						'dir=' + editor.config.contentsLangDirection + ';' +
 						'border:1px solid black;' +
 						'background-color:white">' +
 						'</textarea>',
