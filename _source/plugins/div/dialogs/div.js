e0d662a9008279911a01d1add58f803020339080
wwalc <wwalc@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-09-21 14:33:23 +0000

    Merged log:CKEditor/branches/versions/3.4.x@5888:5890
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5891 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/div/dialogs/div.js
+++ _source/plugins/div/dialogs/div.js
@@ -453,7 +453,9 @@
 				this.hide();
 			},
 			onHide: function() {
-				this._element.removeCustomData( 'elementStyle' );
+				// Remove style only when editing existing DIV. (#6315)
+				if ( command == 'editdiv' )
+					this._element.removeCustomData( 'elementStyle' );
 				delete this._element;
 			}
 		};
