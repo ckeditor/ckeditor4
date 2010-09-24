037d4a02c09f94cf923357d0708514e8d76e5ffe
Saare <Saare@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-09-24 09:49:46 +0000

    #6346: Fixing `colorButton_enableMore`'s documentation.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5898 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/colorbutton/plugin.js
+++ _source/plugins/colorbutton/plugin.js
@@ -168,7 +168,7 @@ CKEDITOR.plugins.add( 'colorbutton', {
 
 /**
  * Whether to enable the "More Colors..." button in the color selectors.
- * @default false
+ * @default true
  * @type Boolean
  * @example
  * config.colorButton_enableMore = false;
