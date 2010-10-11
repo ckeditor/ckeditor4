1f2982f0c61fd65344bfb0ffe0cdcd109a959ff6
mani <mani@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-10-11 06:12:28 +0000

    #6377 FF width and height are not shown for smiley in Image properties dialog
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5927 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/smiley/dialogs/smiley.js
+++ _source/plugins/smiley/dialogs/smiley.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -32,7 +32,9 @@ CKEDITOR.dialog.add( 'smiley', function( editor ) {
 					src: src,
 					_cke_saved_src: src,
 					title: title,
-					alt: title
+					alt: title,
+					width: target.$.width,
+					height: target.$.height
 				}
 			});
 
