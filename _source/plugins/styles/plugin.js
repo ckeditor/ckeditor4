242f3e226803a6c34e6e36372f4eb9d17d26253e
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-10-22 09:01:51 +0000

    #6445: regression of [5847], 'getStyleText' must be used in order to to normalize styles text.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@5978 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/styles/plugin.js
+++ _source/plugins/styles/plugin.js
@@ -994,14 +994,8 @@ CKEDITOR.STYLE_OBJECT = 3;
 		}
 
 		// Assign all defined styles.
-		if ( def.styles ) {
-			for ( var i in def.styles ) {
-				if ( !def.styles.hasOwnProperty( i ) )
-					continue;
-
-				el.setStyle( i, def.styles[ i ] );
-			}
-		}
+		if ( styles )
+			el.setAttribute( 'style', styles );
 
 		return el;
 	}
