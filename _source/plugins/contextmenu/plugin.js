87caab58b57d88c4bd9c93b6f7c5f1b0cd1f9704
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-11-01 04:59:51 +0000

    #6534: Workaround to cancel 'contextmenu' event on Opera when trigged by Windows 'menu key'.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6029 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/contextmenu/plugin.js
+++ _source/plugins/contextmenu/plugin.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -183,6 +183,17 @@ CKEDITOR.plugins.contextMenu = CKEDITOR.tools.createClass({
 					this.show( offsetParent, null, offsetX, offsetY );
 				}, 0, this );
 			}, this );
+
+			if ( CKEDITOR.env.opera ) {
+				// 'contextmenu' event triggered by Windows menu key is unpreventable,
+				// cancel the key event itself. (#6534)  
+				element.on( 'keypress', function( evt ) {
+					var domEvent = evt.data;
+
+					if ( domEvent.$.keyCode == 0 )
+						domEvent.preventDefault();
+				});
+			}
 
 			if ( CKEDITOR.env.webkit ) {
 				var holdCtrlKey,
