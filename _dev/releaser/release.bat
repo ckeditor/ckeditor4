cf81d9e824af3adaca09b85d11f0368f06c628ff
wwalc <wwalc@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-11-04 14:11:41 +0000

    Started the 3.4.3 development.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6045 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _dev/releaser/release.bat
+++ _dev/releaser/release.bat
@@ -10,4 +10,4 @@ ECHO.
 
 :: rmdir /S /Q release
 
-java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.4.2 (SVN)" ckeditor_3.4.2_svn --run-before-release=langtool.bat
+java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.4.3 (SVN)" ckeditor_3.4.3_svn --run-before-release=langtool.bat
