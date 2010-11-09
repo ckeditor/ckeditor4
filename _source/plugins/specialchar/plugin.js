6c824ef5c6f8a3ed36e4585f863ff900b933c0ff
tobiasz.cudnik <tobiasz.cudnik@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-11-09 08:09:43 +0000

    #5599 Labels for special characters need to be resourced
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6058 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/specialchar/plugin.js
+++ _source/plugins/specialchar/plugin.js
@@ -8,14 +8,29 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
 CKEDITOR.plugins.add( 'specialchar', {
+	// List of available localizations.
+	availableLangs: { en:1 },
+
 	init: function( editor ) {
-		var pluginName = 'specialchar';
+		var pluginName = 'specialchar',
+			plugin = this;
 
 		// Register the dialog.
 		CKEDITOR.dialog.add( pluginName, this.path + 'dialogs/specialchar.js' );
 
-		// Register the command.
-		editor.addCommand( pluginName, new CKEDITOR.dialogCommand( pluginName ) );
+		editor.addCommand( pluginName, {
+			exec: function() {
+				var langCode = editor.langCode;
+				langCode = plugin.availableLangs[ langCode ] ? langCode : 'en';
+
+				CKEDITOR.scriptLoader.load( CKEDITOR.getUrl( plugin.path + 'lang/' + langCode + '.js' ), function() {
+					CKEDITOR.tools.extend( editor.lang.specialChar, plugin.lang[ langCode ] );
+					editor.openDialog( pluginName );
+				});
+			},
+			modes: { wysiwyg:1 },
+			canUndo: false
+		});
 
 		// Register the toolbar button.
 		editor.ui.addButton( 'SpecialChar', {
