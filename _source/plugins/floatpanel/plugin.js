818091b49973deb80fa1c3621a6c10b1dd35e2db
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2010-11-24 09:46:18 +0000

    #6171: Float panel loading cause unexpected host page scroll in FF.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6114 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/floatpanel/plugin.js
+++ _source/plugins/floatpanel/plugin.js
@@ -269,7 +269,7 @@ CKEDITOR.plugins.add( 'floatpanel', {
 
 					// Set the panel frame focus, so the blur event gets fired.
 					CKEDITOR.tools.setTimeout( function() {
-						iframe.$.contentWindow.focus();
+						( new CKEDITOR.dom.document( iframe.$.contentWindow.document ) ).getBody().focus();
 						// We need this get fired manually because of unfired focus() function.
 						this.allowBlur( true );
 					}, 0, this );
