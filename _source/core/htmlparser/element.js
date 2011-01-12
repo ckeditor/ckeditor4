ed7268ababc1d2aee041c77ae34c7306f587f6d5
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-01-12 06:19:00 +0000

    #6639: cke:li which was used internally to present list item from MS-Word now recognized as block element.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6317 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/core/htmlparser/element.js
+++ _source/core/htmlparser/element.js
@@ -1,4 +1,5 @@
-﻿/*
+﻿﻿
+/*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -34,6 +35,10 @@ CKEDITOR.htmlParser.element = function( name, attributes ) {
 	this.children = [];
 
 	var tagName = attributes[ 'data-cke-real-element-type' ] || name;
+
+	// Reveal the real semantic of our internal custom tag name (#6639).
+	var internalTag = tagName.match( /^cke:(.*)/ );
+	internalTag && ( tagName = internalTag[ 1 ] );
 
 	var dtd = CKEDITOR.dtd,
 		isBlockLike = !!( dtd.$nonBodyContent[ tagName ] || dtd.$block[ tagName ] || dtd.$listItem[ tagName ] || dtd.$tableContent[ tagName ] || dtd.$nonEditable[ tagName ] || tagName == 'br' ),
