5c7008efe532fc640323f426248eb4e3c6931f96
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-01-14 16:33:36 +0000

    #6615: column resizer now display correct cursor over pillars in IE7 and below.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6322 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/tableresize/plugin.js
+++ _source/plugins/tableresize/plugin.js
@@ -143,6 +143,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			resizer.removeListener( 'mousedown', onMouseDown );
 			resizer.removeListener( 'mousemove', onMouseMove );
 
+			needsIEHacks && ( document.getBody().$.contentEditable = true );
 			document.getBody().setStyle( 'cursor', 'auto' );
 
 			// Hide the resizer (remove it on IE7 - #5890).
@@ -288,9 +289,8 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			});
 
 			// In IE6/7, it's not possible to have custom cursors for floating
-			// elements in an editable document. Show the resizer in that case,
-			// to give the user a visual clue.
-			needsIEHacks && resizer.setOpacity( 0.25 );
+			// elements in an editable document.
+			needsIEHacks && ( document.getBody().$.contentEditable = false );
 
 			resizer.on( 'mousedown', onMouseDown, this );
 
