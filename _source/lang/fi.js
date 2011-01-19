7ea78cf00940d176bbc5d83830b701b2da19aa04
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-01-19 09:45:49 +0000

    #6991: French language translation update.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6340 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/lang/fi.js
+++ _source/lang/fi.js
@@ -54,8 +54,8 @@ CKEDITOR.lang[ 'fi' ] = {
 	subscript: 'Alaindeksi',
 	superscript: 'Yläindeksi',
 	horizontalrule: 'Lisää murtoviiva',
-	pagebreak: 'Lisää sivun vaihto',
-	pagebreakAlt: 'Page Break', // MISSING
+	pagebreak: 'Lisää sivunvaihto',
+	pagebreakAlt: 'Sivunvaihto',
 	unlink: 'Poista linkki',
 	undo: 'Kumoa',
 	redo: 'Toista',
@@ -568,11 +568,11 @@ CKEDITOR.lang[ 'fi' ] = {
 	},
 
 	iframe: {
-		title: 'IFrame Properties', // MISSING
-		toolbar: 'IFrame', // MISSING
-		noUrl: 'Please type the iframe URL', // MISSING
-		scrolling: 'Enable scrollbars', // MISSING
-		border: 'Show frame border' // MISSING
+		title: 'IFrame-kehyksen ominaisuudet',
+		toolbar: 'IFrame-kehys',
+		noUrl: 'Anna IFrame-kehykselle lähdeosoite (src)',
+		scrolling: 'Näytä vierityspalkit',
+		border: 'Näytä kehyksen reunat'
 	},
 
 	font: {
@@ -685,8 +685,8 @@ CKEDITOR.lang[ 'fi' ] = {
 	fakeobjects: {
 		anchor: 'Ankkuri',
 		flash: 'Flash animaatio',
-		iframe: 'IFrame', // MISSING
-		hiddenfield: 'Hidden Field', // MISSING
+		iframe: 'IFrame-kehys',
+		hiddenfield: 'Piilokenttä',
 		unknown: 'Tuntematon objekti'
 	},
 
