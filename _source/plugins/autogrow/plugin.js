75a4f7297d6fc92f1fbdb733408ebc7073d9b877
Saare <Saare@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-02-13 13:13:03 +0000

    #6387: AutoGrow may cause an error when instance is destroyed too quickly after height change
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6428 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/autogrow/plugin.js
+++ _source/plugins/autogrow/plugin.js
@@ -7,6 +7,8 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
  * @file AutoGrow plugin
  */ ( function() {
 	var resizeEditor = function( editor ) {
+			if ( !editor.window )
+				return;
 			var doc = editor.document,
 				currentHeight = editor.window.getViewPaneSize().height,
 				newHeight;
