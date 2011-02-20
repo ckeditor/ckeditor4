846e27f00308a945bc107dd05a1e160f667d2f2a
wwalc <wwalc@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-02-20 07:29:45 +0000

    Updated the changelog file and release scripts  with the new version (3.5.3 SVN)
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6463 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _dev/releaser/release.sh
+++ _dev/releaser/release.sh
@@ -12,5 +12,5 @@ fi ;
 LANGTOOL="$(cd $(dirname "$0"); pwd)/langtool.sh"
 
 pushd $DIR
-java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.5.2 (SVN)" ckeditor_3.5.2_svn --run-before-release=$LANGTOOL
+java -jar ckreleaser/ckreleaser.jar ckreleaser.release ../.. release "3.5.3 (SVN)" ckeditor_3.5.3_svn --run-before-release=$LANGTOOL
 popd
