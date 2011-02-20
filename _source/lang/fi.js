571445db1c1d11f70a44f9bdf69e459b34732663
alfonsoml <alfonsoml@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-02-20 18:15:54 +0000

    #7177 Updated Finnish translation
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6471 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/lang/fi.js
+++ _source/lang/fi.js
@@ -177,7 +177,7 @@ CKEDITOR.lang[ 'fi' ] = {
 		cssClasses: 'Tyyliluokat',
 		charset: 'Linkitetty kirjaimisto',
 		styles: 'Tyyli',
-		rel: 'Relationship', // MISSING
+		rel: 'Suhde',
 		selectAnchor: 'Valitse ankkuri',
 		anchorName: 'Ankkurin nimen mukaan',
 		anchorId: 'Ankkurin ID:n mukaan',
