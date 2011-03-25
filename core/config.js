6c6e51ab2e95fbaf70ffe110b6ba73a0cbdecefc
fredck <f.knabben@cksource.com>
2011-03-25 16:57:07 -0300

    Set "kama" as the default skin to load (at least on development).

--- core/config.js
+++ core/config.js
@@ -7,4 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+	config.skin = 'kama';
 };
