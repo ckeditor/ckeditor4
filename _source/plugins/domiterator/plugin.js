c0f336c041dd5218d3d55364597549d969ec7286
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-03-28 03:37:05 +0000

    #7359: block iterator now includes bookmark nodes at the end of block.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6617 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/domiterator/plugin.js
+++ _source/plugins/domiterator/plugin.js
@@ -29,7 +29,11 @@ CKEDITOR.plugins.add( 'domiterator' );
 
 	var beginWhitespaceRegex = /^[\r\n\t ]+$/,
 		// Ignore bookmark nodes.(#3783)
-		bookmarkGuard = CKEDITOR.dom.walker.bookmark( false, true );
+		bookmarkGuard = CKEDITOR.dom.walker.bookmark( false, true ),
+		whitespacesGuard = CKEDITOR.dom.walker.whitespaces( true ),
+		skipGuard = function( node ) {
+			return bookmarkGuard( node ) && whitespacesGuard( node );
+		};
 
 	// Get a reference for the next element, bookmark nodes are skipped.
 	function getNextSourceNode( node, startFromSibling, lastNode ) {
@@ -180,12 +184,15 @@ CKEDITOR.plugins.add( 'domiterator' );
 				// If we are in an element boundary, let's check if it is time
 				// to close the range, otherwise we include the parent within it.
 				if ( range && !closeRange ) {
-					while ( !currentNode.getNext( bookmarkGuard ) && !isLast ) {
+					while ( !currentNode.getNext( skipGuard ) && !isLast ) {
 						var parentNode = currentNode.getParent();
 
 						if ( parentNode.isBlockBoundary( this.forceBrBreak && !parentPre && { br:1 } ) ) {
 							closeRange = 1;
+							includeNode = 0;
 							isLast = isLast || ( parentNode.equals( lastNode ) );
+							// Make sure range includes bookmarks at the end of the block. (#7359)
+							range.setEndAt( parentNode, CKEDITOR.POSITION_BEFORE_END );
 							break;
 						}
 
