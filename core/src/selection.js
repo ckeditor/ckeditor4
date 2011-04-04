a55a3fc5e1e8809a5b27abd6e1c51837a302ec2b
fredck <f.knabben@cksource.com>
2011-04-04 12:44:05 -0300

    Updated the old selection plugin registration. Removed the "select all" command stuff, which will now be a plugin.

--- core/src/selection.js
+++ core/src/selection.js
@@ -82,31 +82,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 		return !CKEDITOR.tools.trim( start.getHtml() ) ? isInlineCt( start ) || singletonBlock( start ) : isInlineCt( start.getChild( offset - 1 ) ) || isInlineCt( start.getChild( offset ) );
 	}
 
-	var selectAllCmd = { modes:{wysiwyg:1,source:1 },
-		readOnly: CKEDITOR.env.ie || CKEDITOR.env.webkit,
-		exec: function( editor ) {
-			switch ( editor.mode ) {
-				case 'wysiwyg':
-					editor.document.$.execCommand( 'SelectAll', false, null );
-					// Force triggering selectionChange (#7008)
-					editor.forceNextSelectionCheck();
-					editor.selectionChange();
-					break;
-				case 'source':
-					// Select the contents of the textarea
-					var textarea = editor.textarea.$;
-					if ( CKEDITOR.env.ie )
-						textarea.createTextRange().execCommand( 'SelectAll' );
-					else {
-						textarea.selectionStart = 0;
-						textarea.selectionEnd = textarea.value.length;
-					}
-					textarea.focus();
-			}
-		},
-		canUndo: false
-	};
-
 	function createFillingChar( doc ) {
 		removeFillingChar( doc );
 
@@ -144,274 +119,268 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 		}
 	}
 
-	CKEDITOR.plugins.add( 'selection', {
-		init: function( editor ) {
-			// On WebKit only, we need a special "filling" char on some situations
-			// (#1272). Here we set the events that should invalidate that char.
-			if ( CKEDITOR.env.webkit ) {
-				editor.on( 'selectionChange', function() {
-					checkFillingChar( editor.document );
-				});
-				editor.on( 'beforeSetMode', function() {
-					removeFillingChar( editor.document );
-				});
-				editor.on( 'key', function( e ) {
-					// Remove the filling char before some keys get
-					// executed, so they'll not get blocked by it.
-					switch ( e.data.keyCode ) {
-						case 13: // ENTER
-						case CKEDITOR.SHIFT + 13: // SHIFT-ENTER
-						case 37: // LEFT-ARROW
-						case 39: // RIGHT-ARROW
-						case 8: // BACKSPACE
-							removeFillingChar( editor.document );
-					}
-				}, null, null, 10 );
-
-				var fillingCharBefore, resetSelection;
-
-				function beforeData() {
-					var doc = editor.document,
-						fillingChar = getFillingChar( doc );
-
-					if ( fillingChar ) {
-						// If cursor is right blinking by side of the filler node, save it for restoring,
-						// as the following text substitution will blind it. (#7437)
-						var sel = doc.$.defaultView.getSelection();
-						if ( sel.type == 'Caret' && sel.anchorNode == fillingChar.$ )
-							resetSelection = 1;
-
-						fillingCharBefore = fillingChar.getText();
-						fillingChar.setText( fillingCharBefore.replace( /\u200B/g, '' ) );
-					}
+	// Setup all editor instances for the necessary selection hooks.
+	CKEDITOR.on( 'instanceCreated', function( ev ) {
+		var editor = ev.editor;
+		// On WebKit only, we need a special "filling" char on some situations
+		// (#1272). Here we set the events that should invalidate that char.
+		if ( CKEDITOR.env.webkit ) {
+			editor.on( 'selectionChange', function() {
+				checkFillingChar( editor.document );
+			});
+			editor.on( 'beforeSetMode', function() {
+				removeFillingChar( editor.document );
+			});
+			editor.on( 'key', function( e ) {
+				// Remove the filling char before some keys get
+				// executed, so they'll not get blocked by it.
+				switch ( e.data.keyCode ) {
+					case 13: // ENTER
+					case CKEDITOR.SHIFT + 13: // SHIFT-ENTER
+					case 37: // LEFT-ARROW
+					case 39: // RIGHT-ARROW
+					case 8: // BACKSPACE
+						removeFillingChar( editor.document );
 				}
+			}, null, null, 10 );
 
-				function afterData() {
-					var doc = editor.document,
-						fillingChar = getFillingChar( doc );
+			var fillingCharBefore, resetSelection;
 
-					if ( fillingChar ) {
-						fillingChar.setText( fillingCharBefore );
+			function beforeData() {
+				var doc = editor.document,
+					fillingChar = getFillingChar( doc );
 
-						if ( resetSelection ) {
-							doc.$.defaultView.getSelection().setPosition( fillingChar.$, fillingChar.getLength() );
-							resetSelection = 0;
-						}
-					}
+				if ( fillingChar ) {
+					// If cursor is right blinking by side of the filler node, save it for restoring,
+					// as the following text substitution will blind it. (#7437)
+					var sel = doc.$.defaultView.getSelection();
+					if ( sel.type == 'Caret' && sel.anchorNode == fillingChar.$ )
+						resetSelection = 1;
+
+					fillingCharBefore = fillingChar.getText();
+					fillingChar.setText( fillingCharBefore.replace( /\u200B/g, '' ) );
 				}
-				editor.on( 'beforeUndoImage', beforeData );
-				editor.on( 'afterUndoImage', afterData );
-				editor.on( 'beforeGetData', beforeData, null, null, 0 );
-				editor.on( 'getData', afterData );
 			}
 
-			editor.on( 'contentDom', function() {
+			function afterData() {
 				var doc = editor.document,
-					body = doc.getBody(),
-					html = doc.getDocumentElement();
+					fillingChar = getFillingChar( doc );
 
-				if ( CKEDITOR.env.ie ) {
-					// Other browsers don't loose the selection if the
-					// editor document loose the focus. In IE, we don't
-					// have support for it, so we reproduce it here, other
-					// than firing the selection change event.
+				if ( fillingChar ) {
+					fillingChar.setText( fillingCharBefore );
 
-					var savedRange, saveEnabled,
-						restoreEnabled = 1;
+					if ( resetSelection ) {
+						doc.$.defaultView.getSelection().setPosition( fillingChar.$, fillingChar.getLength() );
+						resetSelection = 0;
+					}
+				}
+			}
+			editor.on( 'beforeUndoImage', beforeData );
+			editor.on( 'afterUndoImage', afterData );
+			editor.on( 'beforeGetData', beforeData, null, null, 0 );
+			editor.on( 'getData', afterData );
+		}
 
-					// "onfocusin" is fired before "onfocus". It makes it
-					// possible to restore the selection before click
-					// events get executed.
-					body.on( 'focusin', function( evt ) {
-						// If there are elements with layout they fire this event but
-						// it must be ignored to allow edit its contents #4682
-						if ( evt.data.$.srcElement.nodeName != 'BODY' )
-							return;
+		editor.on( 'contentDom', function() {
+			var doc = editor.document,
+				body = doc.getBody(),
+				html = doc.getDocumentElement();
 
-						// If we have saved a range, restore it at this
-						// point.
-						if ( savedRange ) {
-							if ( restoreEnabled ) {
-								// Well not break because of this.
-								try {
-									savedRange.select();
-								} catch ( e ) {}
+			if ( CKEDITOR.env.ie ) {
+				// Other browsers don't loose the selection if the
+				// editor document loose the focus. In IE, we don't
+				// have support for it, so we reproduce it here, other
+				// than firing the selection change event.
 
-								// Update locked selection because of the normalized text nodes. (#6083, #6987)
-								var lockedSelection = doc.getCustomData( 'cke_locked_selection' );
-								if ( lockedSelection ) {
-									lockedSelection.unlock();
-									lockedSelection.lock();
-								}
-							}
+				var savedRange, saveEnabled,
+					restoreEnabled = 1;
 
-							savedRange = null;
-						}
-					});
+				// "onfocusin" is fired before "onfocus". It makes it
+				// possible to restore the selection before click
+				// events get executed.
+				body.on( 'focusin', function( evt ) {
+					// If there are elements with layout they fire this event but
+					// it must be ignored to allow edit its contents #4682
+					if ( evt.data.$.srcElement.nodeName != 'BODY' )
+						return;
 
-					body.on( 'focus', function() {
-						// Enable selections to be saved.
-						saveEnabled = 1;
-
-						saveSelection();
-					});
-
-					body.on( 'beforedeactivate', function( evt ) {
-						// Ignore this event if it's caused by focus switch between
-						// internal editable control type elements, e.g. layouted paragraph. (#4682)
-						if ( evt.data.$.toElement )
-							return;
-
-						// Disable selections from being saved.
-						saveEnabled = 0;
-						restoreEnabled = 1;
-					});
-
-					// IE before version 8 will leave cursor blinking inside the document after
-					// editor blurred unless we clean up the selection. (#4716)
-					if ( CKEDITOR.env.ie && CKEDITOR.env.version < 8 ) {
-						editor.on( 'blur', function( evt ) {
-							// Try/Catch to avoid errors if the editor is hidden. (#6375)
+					// If we have saved a range, restore it at this
+					// point.
+					if ( savedRange ) {
+						if ( restoreEnabled ) {
+							// Well not break because of this.
 							try {
-								editor.document && editor.document.$.selection.empty();
+								savedRange.select();
 							} catch ( e ) {}
-						});
-					}
 
-					// Listening on document element ensures that
-					// scrollbar is included. (#5280)
-					html.on( 'mousedown', function() {
-						// Lock restore selection now, as we have
-						// a followed 'click' event which introduce
-						// new selection. (#5735)
-						restoreEnabled = 0;
-					});
-
-					html.on( 'mouseup', function() {
-						restoreEnabled = 1;
-					});
-
-					// In IE6/7 the blinking cursor appears, but contents are
-					// not editable. (#5634)
-					if ( CKEDITOR.env.ie && ( CKEDITOR.env.ie7Compat || CKEDITOR.env.version < 8 || CKEDITOR.env.quirks ) ) {
-						// The 'click' event is not fired when clicking the
-						// scrollbars, so we can use it to check whether
-						// the empty space following <body> has been clicked.
-						html.on( 'click', function( evt ) {
-							if ( evt.data.getTarget().getName() == 'html' )
-								editor.getSelection().getRanges()[ 0 ].select();
-						});
-					}
-
-					var scroll;
-					// IE fires the "selectionchange" event when clicking
-					// inside a selection. We don't want to capture that.
-					body.on( 'mousedown', function( evt ) {
-						// IE scrolls document to top on right mousedown
-						// when editor has no focus, remember this scroll
-						// position and revert it before context menu opens. (#5778)
-						if ( evt.data.$.button == 2 ) {
-							var sel = editor.document.$.selection;
-							if ( sel.type == 'None' )
-								scroll = editor.window.getScrollPosition();
-						}
-						disableSave();
-					});
-
-					body.on( 'mouseup', function( evt ) {
-						// Restore recorded scroll position when needed on right mouseup.
-						if ( evt.data.$.button == 2 && scroll ) {
-							editor.document.$.documentElement.scrollLeft = scroll.x;
-							editor.document.$.documentElement.scrollTop = scroll.y;
-						}
-						scroll = null;
-
-						saveEnabled = 1;
-						setTimeout( function() {
-							saveSelection( true );
-						}, 0 );
-					});
-
-					body.on( 'keydown', disableSave );
-					body.on( 'keyup', function() {
-						saveEnabled = 1;
-						saveSelection();
-					});
-
-
-					// IE is the only to provide the "selectionchange"
-					// event.
-					doc.on( 'selectionchange', saveSelection );
-
-					function disableSave() {
-						saveEnabled = 0;
-					}
-
-					function saveSelection( testIt ) {
-						if ( saveEnabled ) {
-							var doc = editor.document,
-								sel = editor.getSelection(),
-								nativeSel = sel && sel.getNative();
-
-							// There is a very specific case, when clicking
-							// inside a text selection. In that case, the
-							// selection collapses at the clicking point,
-							// but the selection object remains in an
-							// unknown state, making createRange return a
-							// range at the very start of the document. In
-							// such situation we have to test the range, to
-							// be sure it's valid.
-							if ( testIt && nativeSel && nativeSel.type == 'None' ) {
-								// The "InsertImage" command can be used to
-								// test whether the selection is good or not.
-								// If not, it's enough to give some time to
-								// IE to put things in order for us.
-								if ( !doc.$.queryCommandEnabled( 'InsertImage' ) ) {
-									CKEDITOR.tools.setTimeout( saveSelection, 50, this, true );
-									return;
-								}
+							// Update locked selection because of the normalized text nodes. (#6083, #6987)
+							var lockedSelection = doc.getCustomData( 'cke_locked_selection' );
+							if ( lockedSelection ) {
+								lockedSelection.unlock();
+								lockedSelection.lock();
 							}
+						}
 
-							// Avoid saving selection from within text input. (#5747)
-							var parentTag;
-							if ( nativeSel && nativeSel.type && nativeSel.type != 'Control' && ( parentTag = nativeSel.createRange() ) && ( parentTag = parentTag.parentElement() ) && ( parentTag = parentTag.nodeName ) && parentTag.toLowerCase() in { input:1,textarea:1 } ) {
+						savedRange = null;
+					}
+				});
+
+				body.on( 'focus', function() {
+					// Enable selections to be saved.
+					saveEnabled = 1;
+
+					saveSelection();
+				});
+
+				body.on( 'beforedeactivate', function( evt ) {
+					// Ignore this event if it's caused by focus switch between
+					// internal editable control type elements, e.g. layouted paragraph. (#4682)
+					if ( evt.data.$.toElement )
+						return;
+
+					// Disable selections from being saved.
+					saveEnabled = 0;
+					restoreEnabled = 1;
+				});
+
+				// IE before version 8 will leave cursor blinking inside the document after
+				// editor blurred unless we clean up the selection. (#4716)
+				if ( CKEDITOR.env.ie && CKEDITOR.env.version < 8 ) {
+					editor.on( 'blur', function( evt ) {
+						// Try/Catch to avoid errors if the editor is hidden. (#6375)
+						try {
+							editor.document && editor.document.$.selection.empty();
+						} catch ( e ) {}
+					});
+				}
+
+				// Listening on document element ensures that
+				// scrollbar is included. (#5280)
+				html.on( 'mousedown', function() {
+					// Lock restore selection now, as we have
+					// a followed 'click' event which introduce
+					// new selection. (#5735)
+					restoreEnabled = 0;
+				});
+
+				html.on( 'mouseup', function() {
+					restoreEnabled = 1;
+				});
+
+				// In IE6/7 the blinking cursor appears, but contents are
+				// not editable. (#5634)
+				if ( CKEDITOR.env.ie && ( CKEDITOR.env.ie7Compat || CKEDITOR.env.version < 8 || CKEDITOR.env.quirks ) ) {
+					// The 'click' event is not fired when clicking the
+					// scrollbars, so we can use it to check whether
+					// the empty space following <body> has been clicked.
+					html.on( 'click', function( evt ) {
+						if ( evt.data.getTarget().getName() == 'html' )
+							editor.getSelection().getRanges()[ 0 ].select();
+					});
+				}
+
+				var scroll;
+				// IE fires the "selectionchange" event when clicking
+				// inside a selection. We don't want to capture that.
+				body.on( 'mousedown', function( evt ) {
+					// IE scrolls document to top on right mousedown
+					// when editor has no focus, remember this scroll
+					// position and revert it before context menu opens. (#5778)
+					if ( evt.data.$.button == 2 ) {
+						var sel = editor.document.$.selection;
+						if ( sel.type == 'None' )
+							scroll = editor.window.getScrollPosition();
+					}
+					disableSave();
+				});
+
+				body.on( 'mouseup', function( evt ) {
+					// Restore recorded scroll position when needed on right mouseup.
+					if ( evt.data.$.button == 2 && scroll ) {
+						editor.document.$.documentElement.scrollLeft = scroll.x;
+						editor.document.$.documentElement.scrollTop = scroll.y;
+					}
+					scroll = null;
+
+					saveEnabled = 1;
+					setTimeout( function() {
+						saveSelection( true );
+					}, 0 );
+				});
+
+				body.on( 'keydown', disableSave );
+				body.on( 'keyup', function() {
+					saveEnabled = 1;
+					saveSelection();
+				});
+
+
+				// IE is the only to provide the "selectionchange"
+				// event.
+				doc.on( 'selectionchange', saveSelection );
+
+				function disableSave() {
+					saveEnabled = 0;
+				}
+
+				function saveSelection( testIt ) {
+					if ( saveEnabled ) {
+						var doc = editor.document,
+							sel = editor.getSelection(),
+							nativeSel = sel && sel.getNative();
+
+						// There is a very specific case, when clicking
+						// inside a text selection. In that case, the
+						// selection collapses at the clicking point,
+						// but the selection object remains in an
+						// unknown state, making createRange return a
+						// range at the very start of the document. In
+						// such situation we have to test the range, to
+						// be sure it's valid.
+						if ( testIt && nativeSel && nativeSel.type == 'None' ) {
+							// The "InsertImage" command can be used to
+							// test whether the selection is good or not.
+							// If not, it's enough to give some time to
+							// IE to put things in order for us.
+							if ( !doc.$.queryCommandEnabled( 'InsertImage' ) ) {
+								CKEDITOR.tools.setTimeout( saveSelection, 50, this, true );
 								return;
 							}
-
-							savedRange = nativeSel && sel.getRanges()[ 0 ];
-
-							checkSelectionChangeTimeout.call( editor );
 						}
+
+						// Avoid saving selection from within text input. (#5747)
+						var parentTag;
+						if ( nativeSel && nativeSel.type && nativeSel.type != 'Control' && ( parentTag = nativeSel.createRange() ) && ( parentTag = parentTag.parentElement() ) && ( parentTag = parentTag.nodeName ) && parentTag.toLowerCase() in { input:1,textarea:1 } ) {
+							return;
+						}
+
+						savedRange = nativeSel && sel.getRanges()[ 0 ];
+
+						checkSelectionChangeTimeout.call( editor );
 					}
-				} else {
-					// In other browsers, we make the selection change
-					// check based on other events, like clicks or keys
-					// press.
-
-					doc.on( 'mouseup', checkSelectionChangeTimeout, editor );
-					doc.on( 'keyup', checkSelectionChangeTimeout, editor );
-					doc.on( 'selectionchange', checkSelectionChangeTimeout, editor );
 				}
-			});
+			} else {
+				// In other browsers, we make the selection change
+				// check based on other events, like clicks or keys
+				// press.
 
-			// Clear the cached range path before unload. (#7174)
-			editor.on( 'contentDomUnload', editor.forceNextSelectionCheck, editor );
+				doc.on( 'mouseup', checkSelectionChangeTimeout, editor );
+				doc.on( 'keyup', checkSelectionChangeTimeout, editor );
+				doc.on( 'selectionchange', checkSelectionChangeTimeout, editor );
+			}
+		});
 
-			editor.addCommand( 'selectAll', selectAllCmd );
-			editor.ui.addButton( 'SelectAll', {
-				label: editor.lang.selectAll,
-				command: 'selectAll'
-			});
+		// Clear the cached range path before unload. (#7174)
+		editor.on( 'contentDomUnload', editor.forceNextSelectionCheck, editor );
 
-			editor.selectionChange = checkSelectionChangeTimeout;
-
-			// IE9 might cease to work if there's an object selection inside the iframe (#7639).
-			CKEDITOR.env.ie9Compat && editor.on( 'destroy', function() {
-				var sel = editor.getSelection();
-				sel && sel.getNative().clear();
-			}, null, null, 9 );
-		}
+		// IE9 might cease to work if there's an object selection inside the iframe (#7639).
+		CKEDITOR.env.ie9Compat && editor.on( 'destroy', function() {
+			var sel = editor.getSelection();
+			sel && sel.getNative().clear();
+		}, null, null, 9 );
 	});
+
+	CKEDITOR.editor.prototype.selectionChange = checkSelectionChangeTimeout;
 
 	/**
 	 * Gets the current selection from the editing area when in WYSIWYG mode.
