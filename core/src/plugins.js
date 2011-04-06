4aadbaa24c414e91d58aa1bebfdf5a32bd437980
fredck <f.knabben@cksource.com>
2011-04-06 15:14:01 -0300

    Avoid errors for missing "styles" and "selection" plugins (v3 backwards compat).

--- core/src/plugins.js
+++ core/src/plugins.js
@@ -18,6 +18,10 @@ CKEDITOR.plugins = new CKEDITOR.resourceManager( '../plugins/', 'plugin' );
 
 // PACKAGER_RENAME( CKEDITOR.plugins )
 
+// For v3 backwards compatibility, let's register empty "styles" and "selection" plugins.
+CKEDITOR.plugins.add( 'styles', {} );
+CKEDITOR.plugins.add( 'selection', {} );
+
 CKEDITOR.plugins.load = CKEDITOR.tools.override( CKEDITOR.plugins.load, function( originalLoad ) {
 	return function( name, callback, scope ) {
 		var allPlugins = {};
