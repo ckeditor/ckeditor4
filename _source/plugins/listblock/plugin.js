9764ccaf5982e89248f2961758093b005c395546
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-04-07 10:49:48 +0000

    #7539: Revert [6453] which cause document scroll on fresh open for WebKit.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6651 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/listblock/plugin.js
+++ _source/plugins/listblock/plugin.js
@@ -210,13 +210,6 @@ CKEDITOR.plugins.add( 'listblock', {
 							selected.focus();
 						}, 0 );
 					}
-					// Blur any previously focused list item. (#6671)
-					else {
-						var self = this;
-						setTimeout( function() {
-							self.element.focus();
-						}, 0 );
-					}
 				}
 			}
 		});
