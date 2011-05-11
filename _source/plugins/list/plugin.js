ae2fa371b5529bd27a52aa6af5482e61009abf51
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-05-11 04:48:26 +0000

    #7657: The wrong indentation is applied when converting indented RTL paragraphs into list.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6911 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/list/plugin.js
+++ _source/plugins/list/plugin.js
@@ -314,13 +314,12 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			if ( contentBlock.is( 'pre' ) || headerTagRegex.test( contentBlock.getName() ) )
 				contentBlock.appendTo( listItem );
 			else {
-				// Remove DIR attribute if it was merged into list root.
-				if ( listDir && contentBlock.getDirection() ) {
-					contentBlock.removeStyle( 'direction' );
-					contentBlock.removeAttribute( 'dir' );
-				}
-
 				contentBlock.copyAttributes( listItem );
+				// Remove direction attribute after it was merged into list root. (#7657)
+				if ( listDir && contentBlock.getDirection() ) {
+					listItem.removeStyle( 'direction' );
+					listItem.removeAttribute( 'dir' );
+				}
 				contentBlock.moveChildren( listItem );
 				contentBlock.remove();
 			}
