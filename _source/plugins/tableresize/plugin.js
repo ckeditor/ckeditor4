4eb004f1b8e99d4d5201d912987d15ea55b011a4
Saare <Saare@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-05-17 16:20:09 +0000

    #7130: Table column resize hit testing needs to be finer grained.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6933 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/tableresize/plugin.js
+++ _source/plugins/tableresize/plugin.js
@@ -72,7 +72,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			pillarIndex += td.$.colSpan || 1;
 
 			// Calculate the pillar boundary positions.
-			var pillarLeft, pillarRight, pillarWidth, pillarPadding;
+			var pillarLeft, pillarRight, pillarWidth;
 
 			var x = td.getDocumentPosition().x;
 
@@ -94,9 +94,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 			pillarWidth = Math.max( pillarRight - pillarLeft, 3 );
 
-			// Make the pillar touch area at least 14 pixels wide, for easy to use.
-			pillarPadding = Math.max( Math.round( 7 - ( pillarWidth / 2 ) ), 0 );
-
 			// The pillar should reflects exactly the shape of the hovered
 			// column border line.
 			pillars.push({
@@ -106,7 +103,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 				y: tbodyPosition.y,
 				width: pillarWidth,
 				height: tbody.$.offsetHeight,
-				padding: pillarPadding,
 				rtl: rtl } );
 		}
 
@@ -115,10 +111,9 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 	function getPillarAtPosition( pillars, positionX ) {
 		for ( var i = 0, len = pillars.length; i < len; i++ ) {
-			var pillar = pillars[ i ],
-				pad = pillar.padding;
+			var pillar = pillars[ i ];
 
-			if ( positionX >= pillar.x - pad && positionX <= ( pillar.x + pillar.width + pad ) )
+			if ( positionX >= pillar.x && positionX <= ( pillar.x + pillar.width ) )
 				return pillar;
 		}
 
@@ -305,9 +300,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 				if ( !pillar )
 					return 0;
 
-				var pad = pillar.padding;
-
-				if ( !isResizing && ( posX < pillar.x - pad || posX > ( pillar.x + pillar.width + pad ) ) ) {
+				if ( !isResizing && ( posX < pillar.x || posX > ( pillar.x + pillar.width ) ) ) {
 					detach();
 					return 0;
 				}
