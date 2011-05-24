2fee75977388a0b109799cf364466fd0560fe6d4
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-05-24 06:14:51 +0000

    #6663: Safer table caption field by disabling non-text-only caption element.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@6956 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/table/dialogs/table.js
+++ _source/plugins/table/dialogs/table.js
@@ -101,10 +101,8 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 				heightInput && heightInput.onChange();
 			},
 			onOk: function() {
-				if ( this._.selectedElement ) {
-					var selection = editor.getSelection(),
-						bms = selection.createBookmarks();
-				}
+				var selection = editor.getSelection(),
+					bms = selection.createBookmarks();
 
 				var table = this._.selectedElement || makeElement( 'table' ),
 					me = this,
@@ -214,9 +212,12 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 				// Insert the table element if we're creating one.
 				if ( !this._.selectedElement )
 					editor.insertElement( table );
-				// Properly restore the selection inside table. (#4822)
-				else
+
+				// Properly restore the selection, (#4822) but don't break
+				// because of this, e.g. updated table caption.
+				try {
 					selection.selectBookmarks( bms );
+				} catch ( er ) {}
 
 				return true;
 			},
@@ -526,14 +527,29 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 						id: 'txtCaption',
 						label: editor.lang.table.caption,
 						setup: function( selectedTable ) {
+							this.enable();
+
 							var nodeList = selectedTable.getElementsByTag( 'caption' );
 							if ( nodeList.count() > 0 ) {
 								var caption = nodeList.getItem( 0 );
+
+								var innerHtml = caption.getHtml().replace( /<br>\s*$/i, '' ),
+									innerText = caption.getText();
+
+								if ( innerHtml != innerText ) {
+									this.disable();
+									this.setValue( innerText );
+									return;
+								}
+
 								caption = CKEDITOR.tools.trim( caption.getText() );
 								this.setValue( caption );
 							}
 						},
 						commit: function( data, table ) {
+							if ( !this.isEnabled() )
+								return;
+
 							var caption = this.getValue(),
 								captionElement = table.getElementsByTag( 'caption' );
 							if ( caption ) {
