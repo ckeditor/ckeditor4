4214cb89d26b7b8d32705c104001a3d1b17a2e7c
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-08-30 16:50:44 +0000

    #8050: rewrite of the autogrow logic to better caculate the content height when contents are styled.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7244 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- _source/plugins/autogrow/plugin.js
+++ _source/plugins/autogrow/plugin.js
@@ -6,34 +6,57 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 /**
  * @file AutoGrow plugin
  */ ( function() {
+
+	// Actual content height, figured out by appending check the last element's document position.
+	function contentHeight( scrollable ) {
+		var overflowY = scrollable.getStyle( 'overflow-y' );
+
+		var doc = scrollable.getDocument();
+		// Create a temporary marker element.
+		var marker = CKEDITOR.dom.element.createFromHtml( '<span style="margin:0;padding:0;border:0;clear:both;width:1px;height:1px;display:block;">' + ( CKEDITOR.env.webkit ? '&nbsp;' : '' ) + '</span>', doc );
+		doc[ CKEDITOR.env.ie ? 'getBody' : 'getDocumentElement' ]().append( marker );
+
+		var height = marker.getDocumentPosition( doc ).y + marker.$.offsetHeight;
+		marker.remove();
+		scrollable.setStyle( 'overflow-y', overflowY );
+		return height;
+	}
+
 	var resizeEditor = function( editor ) {
 			if ( !editor.window )
 				return;
+
 			var doc = editor.document,
+				iframe = new CKEDITOR.dom.element( doc.getWindow().$.frameElement ),
+				body = doc.getBody(),
+				htmlElement = doc.getDocumentElement(),
 				currentHeight = editor.window.getViewPaneSize().height,
-				newHeight;
+				// Quirks mode overflows body, standards overflows document element
+				scrollable = doc.$.compatMode == 'BackCompat' ? body : htmlElement,
+				newHeight = contentHeight( scrollable );
 
-			// We can not use documentElement to calculate the height for IE (#6061).
-			// It is not good for IE Quirks, yet using offsetHeight would also not work as expected (#6408).
-			// We do the same for FF because of the html height workaround (#6341).
-			if ( CKEDITOR.env.ie || CKEDITOR.env.gecko )
-				newHeight = doc.getBody().$.scrollHeight + ( CKEDITOR.env.ie && CKEDITOR.env.quirks ? 0 : 24 );
-			else
-				newHeight = doc.getDocumentElement().$.offsetHeight;
+			// Additional space specified by user.
+			newHeight += ( editor.config.autoGrow_bottomSpace || 0 );
 
-			var min = editor.config.autoGrow_minHeight,
-				max = editor.config.autoGrow_maxHeight;
-			( min == undefined ) && ( editor.config.autoGrow_minHeight = min = 200 );
-			if ( min )
-				newHeight = Math.max( newHeight, min );
-			if ( max )
-				newHeight = Math.min( newHeight, max );
+			var min = editor.config.autoGrow_minHeight != undefined ? editor.config.autoGrow_minHeight : 200,
+				max = editor.config.autoGrow_maxHeight || Infinity;
+
+			newHeight = Math.max( newHeight, min );
+			newHeight = Math.min( newHeight, max );
 
 			if ( newHeight != currentHeight ) {
 				newHeight = editor.fire( 'autoGrow', { currentHeight: currentHeight, newHeight: newHeight } ).newHeight;
 				editor.resize( editor.container.getStyle( 'width' ), newHeight, true );
 			}
+
+			if ( scrollable.$.scrollHeight > scrollable.$.clientHeight && newHeight < max )
+				scrollable.setStyle( 'overflow-y', 'hidden' );
+			else
+				scrollable.removeStyle( 'overflow-y' );
+
+
 		};
+
 	CKEDITOR.plugins.add( 'autogrow', {
 		init: function( editor ) {
 			editor.addCommand( 'autogrow', { exec:resizeEditor,modes:{wysiwyg:1 },
@@ -49,6 +72,9 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 						// Disable autogrow when the editor is maximized .(#6339)
 						( !maximize || maximize.state != CKEDITOR.TRISTATE_ON ) ) {
 						setTimeout( function() {
+							resizeEditor( evt.editor );
+							// Second pass to make correction upon
+							// the first resize, e.g. scrollbar.
 							resizeEditor( evt.editor );
 						}, 100 );
 					}
@@ -94,4 +120,13 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
  * @param {Number} data.currentHeight The current height of the editor (before resizing).
  * @param {Number} data.newHeight The new height of the editor (after resizing). It can be changed
  *				to determine a different height value to be used instead.
+ */
+
+
+/**
+ *  Extra height in pixel to leave between the bottom boundary of content with document size when auto resizing.
+ * @name CKEDITOR.config.autoGrow_bottomSpace
+ * @type Number
+ * @default 0
+ * @since 3.6.2
  */
