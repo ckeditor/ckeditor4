243fdcf1080dd251c2be217e5f428778e9148ba2
fredck <f.knabben@cksource.com>
2011-09-16 12:14:07 +0200

    Updated the directory structure to import it on ckeditor-dev.

--- core/ckeditor.js
+++ core/ckeditor.js
@@ -3,64 +3,56 @@ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-// Compressed version of core/ckeditor_base.js. See original for instructions.
+// Compressed version of src/ckeditor_base.js. See original for instructions.
 /*jsl:ignore*/
 if ( !window.CKEDITOR )
-	window.CKEDITOR = (function() {
-	var a = {
-		timestamp: '', version: '%VERSION%', revision: '%REV%', _: {},
-		status: 'unloaded', basePath: (function() {
-			var d = window.CKEDITOR_BASEPATH || '';
-			if ( !d ) {
-				var e = document.getElementsByTagName( 'script' );
-				for ( var f = 0; f < e.length; f++ ) {
-					var g = e[ f ].src.match( /(^|.*[\\\/])ckeditor(?:_basic)?(?:_source)?.js(?:\?.*)?$/i );
-					if ( g ) {
-						d = g[ 1 ];
-						break;
-					}
+	window.CKEDITOR = function() {
+	var b = {
+		timestamp: "", version: "%VERSION%", revision: "%REV%", _: { pending: [] },
+		status: "unloaded", basePath: function() {
+			var a = window.CKEDITOR_BASEPATH || ""; if ( !a )
+				for ( var d = document.getElementsByTagName( "script" ), c = 0; c < d.length; c++ ) {
+				var e = d[ c ].src.match( /(^|.*[\\\/])ckeditor(?:_basic)?(?:_source)?.js(?:\?.*)?$/i );
+				if ( e ) {
+					a = e[ 1 ];
+					break
 				}
 			}
-			if ( d.indexOf( ':/' ) == -1 )
-				if ( d.indexOf( '/' ) === 0 )
-				d = location.href.match( /^.*?:\/\/[^\/]*/ )[ 0 ] + d;
-			else
-				d = location.href.match( /^[^\?]*\/(?:)/ )[ 0 ] + d;
-			return d;
-		})(), getUrl: function( d ) {
-			if ( d.indexOf( ':/' ) == -1 && d.indexOf( '/' ) !== 0 )
-				d = this.basePath + d;
-			if ( this.timestamp && d.charAt( d.length - 1 ) != '/' )
-				d += ( d.indexOf( '?' ) >= 0 ? '&' : '?' ) + ( 't=' ) + this.timestamp;
-			return d;
+			if ( a.indexOf( ":/" ) == -1 )
+				a = a.indexOf( "/" ) === 0 ? location.href.match( /^.*?:\/\/[^\/]*/ )[ 0 ] + a : location.href.match( /^[^\?]*\/(?:)/ )[ 0 ] + a;
+			if ( !a )
+				throw 'The CKEditor installation path could not be automatically detected. Please set the global variable "CKEDITOR_BASEPATH" before creating editor instances.';
+			return a
+		}(), getUrl: function( a ) {
+			if ( a.indexOf( ":/" ) == -1 && a.indexOf( "/" ) !== 0 )
+				a = this.basePath + a;
+			if ( this.timestamp && a.charAt( a.length - 1 ) != "/" && !/[&?]t=/.test( a ) )
+				a += ( a.indexOf( "?" ) >= 0 ? "&" : "?" ) + "t=" + this.timestamp;
+			return a
 		} },
-		b = window.CKEDITOR_GETURL; if ( b ) {
-		var c = a.getUrl;
-		a.getUrl = function( d ) {
-			return b.call( a, d ) || c.call( a, d );
-		};
+		f = window.CKEDITOR_GETURL; if ( f ) {
+		var g = b.url;
+		b.url = function( a ) {
+			return f.call( b, a ) || g.call( b, a )
+		}
 	}
-	return a;
-})();
+	return b
+}();
 /*jsl:end*/
 
-// Uncomment the following line to have a new timestamp generated for each
-// request, having clear cache load of the editor code.
-// CKEDITOR.timestamp = ( new Date() ).valueOf();
-
 if ( CKEDITOR.loader )
-	CKEDITOR.loader.load( 'core/ckeditor' );
+	CKEDITOR.loader.load( 'ckeditor' );
 else {
 	// Set the script name to be loaded by the loader.
-	CKEDITOR._autoLoad = 'core/ckeditor';
+	CKEDITOR._autoLoad = 'ckeditor';
 
 	// Include the loader script.
 	if ( document.body && ( !document.readyState || document.readyState == 'complete' ) ) {
 		var script = document.createElement( 'script' );
 		script.type = 'text/javascript';
-		script.src = CKEDITOR.getUrl( '_source/core/loader.js' );
+		script.src = CKEDITOR.getUrl( 'src/loader.js' );
 		document.body.appendChild( script );
 	} else {
-		document.write( '<script type="text/javascript" src="' + CKEDITOR.getUrl( '_source/core/loader.js' ) + '"></script>' );
+		document.write( '<script type="text/javascript" src="' + CKEDITOR.getUrl( 'src/loader.js' ) + '"></script>' );
 	}
 }
