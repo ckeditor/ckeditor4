1f684e8316b04a2ba52a96228c5077479b7d2510
Garry Yao <yaojun85@gmail.com>
2011-09-21 23:40:36 +0800

    Fixing CKEDITOR.template:
    1.  Undefined variable value;
    2.  Make it possible to cache the compiled result.

--- core/src/template.js
+++ core/src/template.js
@@ -3,22 +3,30 @@ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.template = function( editor, source ) {
-	// Builds an optimized function body for the output() method, focused on performance.
-	// For example, if we have this "source":
-	//	'<div style="{style}">{editorName}</div>'
-	// ... the resulting function body will be (apart from the "buffer" handling):
-	//	return [ '<div style="', data['style'] == undefined ? '$style' : data['style'], '">', data['editorName'] == undefined ? '$editorName' : data['editorName'], '</div>' ].join('');
+(function() {
+	var caches = {};
+	CKEDITOR.template = function( editor, source ) {
+		// Builds an optimized function body for the output() method, focused on performance.
+		// For example, if we have this "source":
+		//	'<div style="{style}">{editorName}</div>'
+		// ... the resulting function body will be (apart from the "buffer" handling):
+		//	return [ '<div style="', data['style'] == undefined ? 'style' : data['style'], '">', data['editorName'] == undefined ? '$editorName' : data['editorName'], '</div>' ].join('');
 
-	var fn = source
-	// Escape all quotation marks (").
-	.replace( /"/g, '\\"' )
-	// Inject the template keys replacement.
-	.replace( /{([^}]+)}/g, function( m, key ) {
-		return "',data['" + key + "']==undefined?'$" + key + "':data['" + key + "'],'";
-	});
+		// Try to read from the cache.
+		if ( caches[ source ] )
+			this.output = caches[ source ];
+		else {
+			var fn = source
+			// Escape all quotation marks (").
+			.replace( /"/g, '\\"' )
+			// Inject the template keys replacement.
+			.replace( /{([^}]+)}/g, function( m, key ) {
+				return "',data['" + key + "']==undefined?'{" + key + "}':data['" + key + "'],'";
+			});
 
-	fn = "return buffer?buffer.push('" + fn + "'):['" + fn + "'].join('');";
+			fn = "return buffer?buffer.push('" + fn + "'):['" + fn + "'].join('');";
+			this.output = caches[ source ] = Function( 'data', 'buffer', fn );
+		}
+	};
 
-	this.output = Function( 'data', 'buffer', fn );
-};
+})();
