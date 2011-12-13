3a3d5d02a0c1ace38e2dfc4d20f79ee1752390eb
cksource_robot <cksource_robot@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-12-13 08:17:28 +0000

    Updated translations from CKEditor Translation Center
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7340 da63caf2-3823-0410-a1e8-d69ccee00dfb

---
+++ _source/plugins/uicolor/lang/et.js
@@ -0,0 +1,13 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'uicolor', 'et', {
+	uicolor: {
+		title: 'Värvivalija kasutajaliides',
+		preview: 'Automaatne eelvaade',
+		config: 'Aseta see sõne oma config.js faili.',
+		predefined: 'Eelmääratud värvikomplektid'
+	}
+});
