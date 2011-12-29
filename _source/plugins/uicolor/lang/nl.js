fe57a5c063b4229c352b0b9f3e91f7b878abdefb
cksource_robot <cksource_robot@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2011-12-29 08:16:14 +0000

    Updated translations from CKEditor Translation Center
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7349 da63caf2-3823-0410-a1e8-d69ccee00dfb

---
+++ _source/plugins/uicolor/lang/nl.js
@@ -0,0 +1,13 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'uicolor', 'nl', {
+	uicolor: {
+		title: 'UI Kleurenkiezer',
+		preview: 'Live voorbeeld',
+		config: 'Plak deze tekst in jouw config.js bestand',
+		predefined: 'Voorgedefinieerde kleurensets'
+	}
+});
