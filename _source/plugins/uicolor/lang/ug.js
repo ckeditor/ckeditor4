db02f8b794aee3e26f9af4a8be5810cbdb24b35f
cksource_robot <cksource_robot@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2012-02-01 08:16:28 +0000

    Updated translations from CKEditor Translation Center
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7357 da63caf2-3823-0410-a1e8-d69ccee00dfb

---
+++ _source/plugins/uicolor/lang/ug.js
@@ -0,0 +1,13 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'uicolor', 'ug', {
+	uicolor: {
+		title: 'ئىشلەتكۈچى ئارايۈزى رەڭ تاللىغۇچ',
+		preview: 'شۇئان ئالدىن كۆزىتىش',
+		config: 'بۇ ھەرپ تىزىقىنى config.js ھۆججەتكە چاپلايدۇ',
+		predefined: 'ئالدىن بەلگىلەنگەن رەڭلەر'
+	}
+});
