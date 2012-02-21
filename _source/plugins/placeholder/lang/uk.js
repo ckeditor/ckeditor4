403f0d46bde57ffaa2600a27e8f9336f676787ca
cksource_robot <cksource_robot@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2012-02-21 08:16:22 +0000

    Updated translations from CKEditor Translation Center
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7365 da63caf2-3823-0410-a1e8-d69ccee00dfb

---
+++ _source/plugins/placeholder/lang/uk.js
@@ -0,0 +1,14 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'placeholder', 'uk', {
+	placeholder: {
+		title: 'Налаштування Заповнювача',
+		toolbar: 'Створити Заповнювач',
+		text: 'Текст Заповнювача',
+		edit: 'Редагувати Заповнювач',
+		textMissing: 'Заповнювач повинен містити текст.'
+	}
+});
