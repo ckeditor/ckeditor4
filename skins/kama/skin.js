e1483e52ed87037cd9b279a8fb5b05356684826f
fredck <f.knabben@cksource.com>
2012-02-29 15:41:19 +0100

    Simplification and cleanup of skin.js.

--- skins/kama/skin.js
+++ skins/kama/skin.js
@@ -10,69 +10,67 @@ CKEDITOR.skin.chameleon = function( editor, part ) {
 		cssId = '.' + editor.id;
 
 	if ( part == 'editor' ) {
-		var cssSelectors = [
-			cssId + " .cke_inner",
-			cssId + "_dialog .cke_dialog_contents",
-			cssId + "_dialog a.cke_dialog_tab",
-			cssId + "_dialog .cke_dialog_footer"
-			].join( ',' );
-		var cssProperties = "background-color: $color !important;";
-
-		css = cssSelectors + '{' + cssProperties + '}';
+		css = cssId + ' .cke_inner,' +
+			cssId + '_dialog .cke_dialog_contents,' +
+			cssId + '_dialog a.cke_dialog_tab,' +
+			cssId + '_dialog .cke_dialog_footer' +
+			'{' +
+				'background-color:$color !important;' +
+			'}';
 	} else if ( part == 'panel' ) {
-		css = ".cke_menuitem .cke_icon_wrapper\
-			   {\
-			   background-color: $color !important;\
-			   border-color: $color !important;\
-			   }\
-			   \
-			   .cke_menuitem a:hover .cke_icon_wrapper,\
-			   .cke_menuitem a:focus .cke_icon_wrapper,\
-			   .cke_menuitem a:active .cke_icon_wrapper\
-			   {\
-			   background-color: $color !important;\
-			   border-color: $color !important;\
-			   }\
-			   \
-			   .cke_menuitem a:hover .cke_label,\
-			   .cke_menuitem a:focus .cke_label,\
-			   .cke_menuitem a:active .cke_label\
-			   {\
-			   background-color: $color !important;\
-			   }\
-			   \
-			   .cke_menuitem a.cke_disabled:hover .cke_label,\
-			   .cke_menuitem a.cke_disabled:focus .cke_label,\
-			   .cke_menuitem a.cke_disabled:active .cke_label\
-			   {\
-			   background-color: transparent !important;\
-			   }\
-			   \
-			   .cke_menuitem a.cke_disabled:hover .cke_icon_wrapper,\
-			   .cke_menuitem a.cke_disabled:focus .cke_icon_wrapper,\
-			   .cke_menuitem a.cke_disabled:active .cke_icon_wrapper\
-			   {\
-			   background-color: $color !important;\
-			   border-color: $color !important;\
-			   }\
-			   \
-			   .cke_menuitem a.cke_disabled .cke_icon_wrapper\
-			   {\
-			   background-color: $color !important;\
-			   border-color: $color !important;\
-			   }\
-			   \
-			   .cke_menuseparator\
-			   {\
-			   background-color: $color !important;\
-			   }\
-			   \
-			   .cke_menuitem a:hover,\
-			   .cke_menuitem a:focus,\
-			   .cke_menuitem a:active\
-			   {\
-			   background-color: $color !important;\
-			   }";
+		css = '.cke_menuitem .cke_icon_wrapper' +
+			'{' +
+				'background-color:$color !important;' +
+				'border-color:$color !important;' +
+			'}' +
+
+			'.cke_menuitem a:hover .cke_icon_wrapper,' +
+			'.cke_menuitem a:focus .cke_icon_wrapper,' +
+			'.cke_menuitem a:active .cke_icon_wrapper' +
+			'{' +
+				'background-color:$color !important;' +
+				'border-color:$color !important;' +
+			'}' +
+
+			'.cke_menuitem a:hover .cke_label,' +
+			'.cke_menuitem a:focus .cke_label,' +
+			'.cke_menuitem a:active .cke_label' +
+			'{' +
+				'background-color:$color !important;' +
+			'}' +
+
+			'.cke_menuitem a.cke_disabled:hover .cke_label,' +
+			'.cke_menuitem a.cke_disabled:focus .cke_label,' +
+			'.cke_menuitem a.cke_disabled:active .cke_label' +
+			'{' +
+				'background-color: transparent !important;' +
+			'}' +
+
+			'.cke_menuitem a.cke_disabled:hover .cke_icon_wrapper,' +
+			'.cke_menuitem a.cke_disabled:focus .cke_icon_wrapper,' +
+			'.cke_menuitem a.cke_disabled:active .cke_icon_wrapper' +
+			'{' +
+				'background-color:$color !important;' +
+				'border-color:$color !important;' +
+			'}' +
+
+			'.cke_menuitem a.cke_disabled .cke_icon_wrapper' +
+			'{' +
+				'background-color:$color !important;' +
+				'border-color:$color !important;' +
+			'}' +
+
+			'.cke_menuseparator' +
+			'{' +
+				'background-color:$color !important;' +
+			'}' +
+
+			'.cke_menuitem a:hover,' +
+			'.cke_menuitem a:focus,' +
+			'.cke_menuitem a:active' +
+			'{' +
+				'background-color:$color !important;' +
+			'}';
 	}
 
 	return css;
