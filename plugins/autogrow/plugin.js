fd85d6ffe566c0dc4a6e57b36c6fb1edba77ed13
Saare <Saare@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2012-03-15 20:04:01 +0000

    #8014: Autogrow does not fire when switching from source
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7391 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- plugins/autogrow/plugin.js
+++ plugins/autogrow/plugin.js
@@ -62,7 +62,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			editor.addCommand( 'autogrow', { exec:resizeEditor,modes:{wysiwyg:1 },
 				readOnly: 1, canUndo: false, editorFocus: false } );
 
-			var eventsList = { contentDom:1,key:1,selectionChange:1,insertElement:1 };
+			var eventsList = { contentDom:1,key:1,selectionChange:1,insertElement:1,mode:1 };
 			editor.config.autoGrow_onStartup && ( eventsList[ 'instanceReady' ] = 1 );
 			for ( var eventName in eventsList ) {
 				editor.on( eventName, function( evt ) {
