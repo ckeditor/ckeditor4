eb04bb625fea6c5db7bcd5f9ab740e8ff1fe899a
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2012-03-23 10:03:10 +0000

    Post-fixing #8698.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7408 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- plugins/dialog/plugin.js
+++ plugins/dialog/plugin.js
@@ -786,18 +786,19 @@ CKEDITOR.DIALOG_RESIZE_BOTH = 3;
 				this._.parentDialog = null;
 				showCover( this._.editor );
 
-				element.on( 'keydown', accessKeyDownHandler );
-				element.on( CKEDITOR.env.opera ? 'keypress' : 'keyup', accessKeyUpHandler );
-
-				// Prevent some keys from bubbling up. (#4269)
-				for ( var event in { keyup:1,keydown:1,keypress:1 } )
-					element.on( event, preventKeyBubbling );
 			} else {
 				this._.parentDialog = CKEDITOR.dialog._.currentTop;
 				var parentElement = this._.parentDialog.getElement().getFirst();
 				parentElement.$.style.zIndex -= Math.floor( this._.editor.config.baseFloatZIndex / 2 );
 				CKEDITOR.dialog._.currentTop = this;
 			}
+
+			element.on( 'keydown', accessKeyDownHandler );
+			element.on( CKEDITOR.env.opera ? 'keypress' : 'keyup', accessKeyUpHandler );
+
+			// Prevent some keys from bubbling up. (#4269)
+			for ( var event in { keyup:1,keydown:1,keypress:1 } )
+				element.on( event, preventKeyBubbling );
 
 			// Register the Esc hotkeys.
 			registerAccessKey( this, this, '\x1b', null, function() {
