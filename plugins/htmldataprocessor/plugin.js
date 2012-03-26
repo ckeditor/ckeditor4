a517d7129c5412eddf09a3ac078b346aa72af714
fredck <fredck@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2012-03-26 08:07:12 +0000

    #8602 : Post-fix, renaming CKEDITOR.rdn to CKEDITOR.rnd.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7417 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- plugins/htmldataprocessor/plugin.js
+++ plugins/htmldataprocessor/plugin.js
@@ -267,7 +267,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 				// Avoid corrupting the inline event attributes (#7243).
 				// We should not rewrite the existed protected attributes, e.g. clipboard content from editor. (#5218)
 				if ( !( /^on/ ).test( attrName ) && attributes.indexOf( 'data-cke-saved-' + attrName ) == -1 )
-					return ' data-cke-saved-' + fullAttr + ' data-cke-' + CKEDITOR.rdn + '-' + fullAttr;
+					return ' data-cke-saved-' + fullAttr + ' data-cke-' + CKEDITOR.rnd + '-' + fullAttr;
 
 				return fullAttr;
 			}) + '>';
@@ -461,7 +461,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 			data = el.getHtml().substr( 1 );
 
 			// Restore shortly protected attribute names.
-			data = data.replace( new RegExp( ' data-cke-' + CKEDITOR.rdn + '-', 'ig' ), ' ' );
+			data = data.replace( new RegExp( ' data-cke-' + CKEDITOR.rnd + '-', 'ig' ), ' ' );
 
 			isPre && ( data = data.replace( /^<pre>|<\/pre>$/gi, '' ) );
 
