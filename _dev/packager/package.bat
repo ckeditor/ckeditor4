6f9c665236dd87fa05eeba48e95ae72fd4bef0d6
Garry Yao <garry.yao@cksource.com>
2012-04-10 13:39:03 +0800

    Initial v4 directory layout.

--- _dev/packager/package.bat
+++
@@ -1,9 +0,0 @@
-::
-:: Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-:: For licensing, see LICENSE.html or http://ckeditor.com/license
-::
-
-@ECHO OFF
-CLS
-
-java -jar ckpackager/ckpackager.jar ../../ckeditor.pack -v
