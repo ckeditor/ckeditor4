36383c5f9c39f317faf201f75caa5d35c9aaf229
alfonsoml <alfonsoml@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2012-05-02 13:52:14 +0000

    Set default language for DevTools plugin to English instead of Bulgarian
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7483 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- plugins/devtools/plugin.js
+++ plugins/devtools/plugin.js
@@ -4,7 +4,7 @@
  */
 
 CKEDITOR.plugins.add( 'devtools', {
-	lang: [ 'bg', 'cs', 'cy', 'da', 'de', 'el', 'en', 'eo', 'et', 'fa', 'fi', 'fr', 'gu', 'he', 'hr', 'it', 'nb', 'nl', 'no', 'pl', 'tr', 'ug', 'uk', 'vi', 'zh-cn' ],
+	lang: [ 'en', 'bg', 'cs', 'cy', 'da', 'de', 'el', 'eo', 'et', 'fa', 'fi', 'fr', 'gu', 'he', 'hr', 'it', 'nb', 'nl', 'no', 'pl', 'tr', 'ug', 'uk', 'vi', 'zh-cn' ],
 
 	init: function( editor ) {
 		editor._.showDialogDefinitionTooltips = 1;
