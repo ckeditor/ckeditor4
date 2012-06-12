b5b5f4d90af9aeae6fa43ab9d6775a5238151402
fredck <f.knabben@cksource.com>
2012-06-12 12:36:43 +0200

    Copied the jslint scripts from v3.

---
+++ dev/jslint/lint.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+::
+:: Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+:: For licensing, see LICENSE.html or http://ckeditor.com/license
+::
+:: Calls the JavaScript Lint (jsl) with the predefined configurations.
+:: If a file name is passed as a parameter it writes there the results,
+:: otherwise it simply outputs it.
+::
+
+IF "%1"=="" GOTO NoParam
+
+ECHO Generating %1...
+bin\jsl -conf lint.conf -nofilelisting -nologo > %1
+
+ECHO.
+ECHO Process completed.
+ECHO.
+
+GOTO End
+
+:NoParam
+
+"../_thirdparty/jsl/jsl.exe" -conf lint.conf -nofilelisting -nologo
+
+ECHO.
+
+:End
