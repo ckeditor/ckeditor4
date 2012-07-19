krymen/ckeditor

#8941: Worked around wysiwyg area disappeared when resizing the window on shared toolbar mode.
git-svn-id: http://svn.ckeditor.com/CKEditor/trunk@7528 da63caf2-3823-0410-a1e8-d69ccee00dfb

_source/plugins/wysiwygarea/plugin.js
@@ -565,8 +565,12 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 						{
 							onResize = function()
 							{
+								// Hide the iframe to get real size of the holder. (#8941)
+								mainElement.setStyle( 'width', '100%' );
 								iframe.hide();
+
 								iframe.setSize( 'width', mainElement.getSize( 'width' ) );
+								mainElement.removeStyle( 'width' );
 								iframe.show();
 							};
 

ckeditor/ckeditor4

git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7528 da63caf2-3823-0410-a1e8-d69ccee00dfb

plugins/wysiwygarea/plugin.js
@@ -101,10 +101,14 @@
 				if ( CKEDITOR.env.webkit ) {
 					// Webkit: iframe size doesn't auto fit well. (#7360)
 					var onResize = function() {
-							iframe.hide();
-							iframe.setSize( 'width', contentSpace.getSize( 'width' ) );
-							iframe.show();
-						};
+						// Hide the iframe to get real size of the holder. (#8941)
+						contentSpace.setStyle( 'width', '100%' );
+
+						iframe.hide();
+						iframe.setSize( 'width', contentSpace.getSize( 'width' ) );
+						contentSpace.removeStyle( 'width' );
+						iframe.show();
+					};
 
 					iframe.setCustomData( 'onResize', onResize );
 