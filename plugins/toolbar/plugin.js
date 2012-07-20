2a7a340e9bea6902cf6838b0706744c6ca4bec14
fredck <f.knabben@cksource.com>
2012-07-20 19:42:42 +0200

    Code formatting fixes.

--- plugins/toolbar/plugin.js
+++ plugins/toolbar/plugin.js
@@ -427,7 +427,7 @@ CKEDITOR.config.toolbarLocation = 'top';
  */
 CKEDITOR.config.toolbar_Basic = [
 	[ 'Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink', '-', 'About' ]
-	];
+];
 
 /**
  * This is the default toolbar definition used by the editor. It contains all
@@ -454,41 +454,19 @@ CKEDITOR.config.toolbar_Basic = [
  * ];
  */
 CKEDITOR.config.toolbar_Full = [
-	{
-	name: 'document', items: [ 'Source', '-', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', '-', 'Templates' ]
-},
-	{
-	name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ]
-},
-	{
-	name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt' ]
-},
-	{
-	name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ]
-},
+	{ name: 'document',	   items: [ 'Source', '-', 'Save', 'NewPage', 'DocProps', 'Preview', 'Print', '-', 'Templates' ] },
+	{ name: 'clipboard',   items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+	{ name: 'editing',     items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt' ] },
+	{ name: 'forms',       items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
 	'/',
-{
-	name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ]
-},
-	{
-	name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ]
-},
-	{
-	name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ]
-},
-	{
-	name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ]
-},
+	{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+	{ name: 'paragraph',   items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
+	{ name: 'links',       items: [ 'Link', 'Unlink', 'Anchor' ] },
+	{ name: 'insert',      items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
 	'/',
-{
-	name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ]
-},
-	{
-	name: 'colors', items: [ 'TextColor', 'BGColor' ]
-},
-	{
-	name: 'tools', items: [ 'Maximize', 'ShowBlocks', '-', 'About' ]
-}
+	{ name: 'styles',      items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+	{ name: 'colors',      items: [ 'TextColor', 'BGColor' ] },
+	{ name: 'tools',       items: [ 'Maximize', 'ShowBlocks', '-', 'About' ] }
 ];
 
 /**
