0fdd34f952b43a8ee88db32333241dbf3dab83ef
Wiktor Walc <w.walc@cksource.com>
2012-08-03 13:52:10 +0200

    Updated builder version number to 1.3

--- dev/builder/build.sh
+++ dev/builder/build.sh
@@ -9,7 +9,7 @@ set -e
 echo "CKBuilder - Builds a release version of ckeditor-dev."
 echo ""
 
-CKBUILDER_VERSION="1.2"
+CKBUILDER_VERSION="1.3"
 CKBUILDER_URL="http://download.cksource.com/CKBuilder/$CKBUILDER_VERSION/ckbuilder.jar"
 
 PROGNAME=$(basename $0)
