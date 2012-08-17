db2cba9bc49660d1c8fc4a632ac34831e24ed10a
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2012-08-17 15:47:54 +0200

    Changed title & footer, added custom CSS with logo. Increased height of the header.

--- dev/docs_build/docs_build.sh
+++ dev/docs_build/docs_build.sh
@@ -10,7 +10,15 @@ echo ""
 # Move to the script directory.
 cd $(dirname $0)
 
-jsduck ../../core ../../plugins ../../ckeditor.js -o api_docs --stats
+jsduck ../../core ../../plugins ../../ckeditor.js -o api_docs \
+	--stats \
+	--title="CKEditor 4 JavaScript API Documentation" \
+	--head-html="<link rel='stylesheet' href='resources/css/ck.css' type='text/css' />" \
+	--footer="Copyright © 2003-2012, <a href='http://cksource.com' style='color: #085585'>CKSource</a> - Frederico Knabben. All rights reserved. |
+	Generated with <a href='https://github.com/senchalabs/jsduck'>JSDuck</a> 4.0.0."
+
+echo "Copying customizations into resources..."
+cp -r resources api_docs
 
 echo ""
 echo "Finished!"
