c0ae4109f4907c8ea7f98e096c7a400bc9ecf895
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2012-08-20 13:36:15 +0200

    Added customs.rb for meta-tags. Hidden license, fileOverwiew and todo tags.

--- dev/docs_build/docs_build.sh
+++ dev/docs_build/docs_build.sh
@@ -15,7 +15,8 @@ jsduck ../../core ../../plugins ../../ckeditor.js -o api_docs \
 	--title="CKEditor 4 JavaScript API Documentation" \
 	--head-html="<link rel='stylesheet' href='resources/css/ck.css' type='text/css' />" \
 	--footer="Copyright © 2003-2012, <a href='http://cksource.com' style='color: #085585'>CKSource</a> - Frederico Knabben. All rights reserved. |
-	Generated with <a href='https://github.com/senchalabs/jsduck'>JSDuck</a> 4.0.0."
+	Generated with <a href='https://github.com/senchalabs/jsduck'>JSDuck</a> 4.0.0." \
+	--meta-tags customs.rb
 
 echo "Copying customizations into resources..."
 cp -r resources api_docs
