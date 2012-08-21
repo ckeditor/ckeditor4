18bb03419b2cfbe315c1db5365f9332fc411cbda
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2012-08-21 14:07:54 +0200

    Docs directories structure has been changed and docs builder readme has been added.

--- dev/docs/build.sh
+++ dev/docs/build.sh
@@ -2,15 +2,29 @@
 # Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 # For licensing, see LICENSE.html or http://ckeditor.com/license
 
+#
+# README
+#
+# To build docs you need to have JSDuck installed in your OS,
+# so global 'jsduck' script is available.
+#
+# The easiest way to do this is by standard rubygems:
+# $ [sudo] gem install jsduck
+#
+# For more information, please refer to https://github.com/senchalabs/jsduck#getting-it
+#
+# Docs will be generated in dev/docs/build/ directory.
+#
+
 set -e
 
-echo "Building the API document into the api_docs directory..."
+echo "Building the API document into the 'build/' directory..."
 echo ""
 
 # Move to the script directory.
 cd $(dirname $0)
 
-jsduck ../../core ../../plugins ../../ckeditor.js -o api_docs \
+jsduck ../../core ../../plugins ../../ckeditor.js -o build \
 	--stats \
 	--title="CKEditor 4 JavaScript API Documentation" \
 	--head-html="<link rel='stylesheet' href='resources/css/ck.css' type='text/css' />" \
@@ -20,7 +34,7 @@ jsduck ../../core ../../plugins ../../ckeditor.js -o api_docs \
 	--warnings=-no_doc
 
 echo "Copying customizations into resources..."
-cp -r resources api_docs
+cp -r resources build
 
 echo ""
 echo "Finished!"
