d0fa365fd129988572331c91ed76d94288f9ef31
garry.yao <garry.yao@da63caf2-3823-0410-a1e8-d69ccee00dfb>
2012-09-27 16:12:22 +0000

    #9281: Fixed update style impl of dialogadvtab plugin.
    git-svn-id: https://svn.ckeditor.com/CKEditor/trunk@7622 da63caf2-3823-0410-a1e8-d69ccee00dfb

--- plugins/dialogadvtab/plugin.js
+++ plugins/dialogadvtab/plugin.js
@@ -121,18 +121,12 @@
 						updateStyle: function( name, value ) {
 							var styles = this.getValue();
 
-							// Remove the current value.
-							if ( styles ) {
-								styles = styles.replace( new RegExp( '\\s*' + name + '\s*:[^;]*(?:$|;\s*)', 'i' ), '' ).replace( /^[;\s]+/, '' ).replace( /\s+$/, '' );
-							}
-
-							if ( value ) {
-								styles && !( /;\s*$/ ).test( styles ) && ( styles += '; ' );
-								styles += name + ': ' + value;
-							}
+							var tmp = editor.document.createElement( 'span' );
+							tmp.setAttribute( 'style', styles );
+							tmp.setStyle( name, value );
+							styles = CKEDITOR.tools.normalizeCssText( tmp.getAttribute( 'style' ) );
 
 							this.setValue( styles, 1 );
-
 						},
 
 						setup: setupAdvParams,
