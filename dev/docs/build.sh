f2068126016f86930a50420c53e79bcdd6f02ee0
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2012-10-02 09:35:49 +0200

    Removed --stats option from docs builder script which is obsolete since JSDuck 4.1.1
    "Remove --stats option. The statistics page wasn't of much use.",
    See: https://github.com/senchalabs/jsduck/wiki/Changelog

--- dev/docs/build.sh
+++ dev/docs/build.sh
@@ -25,7 +25,6 @@ echo ""
 cd $(dirname $0)
 
 jsduck ../../core ../../plugins ../../ckeditor.js -o build \
-	--stats \
 	--title="CKEditor 4 JavaScript API Documentation" \
 	--head-html="<link rel='stylesheet' href='resources/css/ck.css' type='text/css' />" \
 	--footer="Copyright © 2003-2012, <a href='http://cksource.com' style='color: #085585'>CKSource</a> - Frederico Knabben. All rights reserved. |
