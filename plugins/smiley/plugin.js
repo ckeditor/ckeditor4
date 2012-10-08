47aec1a7c0e926a8aa0400d52113d889bb458c01
fredck <f.knabben@cksource.com>
2012-10-08 11:40:42 +0200

    #6731: Corrected the spelling of two image names.

--- plugins/smiley/plugin.js
+++ plugins/smiley/plugin.js
@@ -36,8 +36,8 @@ CKEDITOR.plugins.add( 'smiley', {
  *
  *		// This is actually the default value.
  *		config.smiley_images = [
- *			'regular_smile.gif','sad_smile.gif','wink_smile.gif','teeth_smile.gif','confused_smile.gif','tounge_smile.gif',
- *			'embaressed_smile.gif','omg_smile.gif','whatchutalkingabout_smile.gif','angry_smile.gif','angel_smile.gif','shades_smile.gif',
+ *			'regular_smile.gif','sad_smile.gif','wink_smile.gif','teeth_smile.gif','confused_smile.gif','tongue_smile.gif',
+ *			'embarrassed_smile.gif','omg_smile.gif','whatchutalkingabout_smile.gif','angry_smile.gif','angel_smile.gif','shades_smile.gif',
  *			'devil_smile.gif','cry_smile.gif','lightbulb.gif','thumbs_down.gif','thumbs_up.gif','heart.gif',
  *			'broken_heart.gif','kiss.gif','envelope.gif'
  *		];
@@ -46,8 +46,8 @@ CKEDITOR.plugins.add( 'smiley', {
  * @member CKEDITOR.config
  */
 CKEDITOR.config.smiley_images = [
-	'regular_smile.gif', 'sad_smile.gif', 'wink_smile.gif', 'teeth_smile.gif', 'confused_smile.gif', 'tounge_smile.gif',
-	'embaressed_smile.gif', 'omg_smile.gif', 'whatchutalkingabout_smile.gif', 'angry_smile.gif', 'angel_smile.gif', 'shades_smile.gif',
+	'regular_smile.gif', 'sad_smile.gif', 'wink_smile.gif', 'teeth_smile.gif', 'confused_smile.gif', 'tongue_smile.gif',
+	'embarrassed_smile.gif', 'omg_smile.gif', 'whatchutalkingabout_smile.gif', 'angry_smile.gif', 'angel_smile.gif', 'shades_smile.gif',
 	'devil_smile.gif', 'cry_smile.gif', 'lightbulb.gif', 'thumbs_down.gif', 'thumbs_up.gif', 'heart.gif',
 	'broken_heart.gif', 'kiss.gif', 'envelope.gif' ];
 
