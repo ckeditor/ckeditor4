a5b99a539773c878915f98fe926183a92c8c06ac
Garry Yao <garry.yao@cksource.com>
2012-10-18 22:59:36 +0800

    #9447: Fixed wrong text selection illustration in Opera after applied inline style.

--- core/selection.js
+++ core/selection.js
@@ -1471,6 +1471,15 @@
 				if ( !sel )
 					return;
 
+				// Opera: The above hack work around a *visually wrong* text selection that
+				// happens in certain situation. (#6874, #9447)
+				if ( CKEDITOR.env.opera )
+				{
+					var nativeRng = this.document.$.createRange();
+					nativeRng.selectNodeContents( this.root.$ );
+					sel.addRange( nativeRng );
+				}
+
 				sel.removeAllRanges();
 
 				for ( var i = 0; i < ranges.length; i++ ) {
