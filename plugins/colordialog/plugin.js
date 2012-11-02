cc3c5d20ae84d57de567af47c8d60412ec6faf91
Garry Yao <garry.yao@cksource.com>
2012-11-02 17:43:01 +0800

    Introduced editor.getColorFromDialog.

--- plugins/colordialog/plugin.js
+++ plugins/colordialog/plugin.js
@@ -9,6 +9,54 @@ CKEDITOR.plugins.colordialog = {
 	init: function( editor ) {
 		editor.addCommand( 'colordialog', new CKEDITOR.dialogCommand( 'colordialog' ) );
 		CKEDITOR.dialog.add( 'colordialog', this.path + 'dialogs/colordialog.js' );
+
+		/**
+		 * Open up color dialog and to receive the selected color.
+		 * @param callback The callback when color dialog is closed
+		 * @param callback.color The color value received if selected on the dialog.
+		 * @param [scope] The scope in which the callback will be bound.
+		 */
+		editor.getColorFromDialog = function( callback, scope ) {
+			var onClose = function( evt ) {
+				releaseHandlers( this );
+				var color = evt.name == 'ok' ? this.getValueOf( 'picker', 'selectedColor' ) : null;
+				callback.call( scope, color );
+			};
+			var releaseHandlers = function( dialog ) {
+				dialog.removeListener( 'ok', onClose );
+				dialog.removeListener( 'cancel', onClose );
+			};
+			var bindToDialog = function( dialog ) {
+				dialog.on( 'ok', onClose );
+				dialog.on( 'cancel', onClose );
+			};
+
+			editor.execCommand( 'colordialog' );
+
+			if ( editor._.storedDialogs.colordialog )
+				bindToDialog( editor._.storedDialogs.colordialog );
+			else {
+				CKEDITOR.on( 'dialogDefinition', function( e ) {
+					if ( e.data.name != 'colordialog' )
+						return;
+
+					var definition = e.data.definition;
+
+					e.removeListener();
+					definition.onLoad = CKEDITOR.tools.override( definition.onLoad,
+						function( orginal ) {
+							return function() {
+								bindToDialog( this );
+								definition.onLoad = orginal;
+								if ( typeof orginal == 'function' )
+									orginal.call( this );
+							};
+						} );
+				} );
+			}
+		}
+
+
 	}
 };
 
