5e1a341e2f71ad8b287c400c14cfc4d3559e3b01
fredck <f.knabben@cksource.com>
2012-11-09 10:33:22 +0100

    #9255: Added a introduction comment to the styles.js file.

--- styles.js
+++ styles.js
@@ -3,6 +3,16 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
+// This file contains style definitions that can be used by CKEditor plugins.
+//
+// The most common use for it is the "stylescombo" plugin, which shows a combo
+// in the editor toolbar, containing all styles. Other plugins instead, like
+// the div plugin, use a subset of the styles on their feature.
+//
+// If you don't have plugins that depend on this file, you can simply ignore it.
+// Otherwise it is strongly recommended to customize this file to match your
+// website requirements and design properly.
+
 CKEDITOR.stylesSet.add( 'default', [
 	/* Block Styles */
 
