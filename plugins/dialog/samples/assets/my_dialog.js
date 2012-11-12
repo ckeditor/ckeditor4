91c2a056b37d6958e6f7d3c9d6c74190ab55b51c
Garry Yao <garry.yao@cksource.com>
2012-11-12 19:24:05 +0800

    Moved dialog sample out of core, decoupled with link plugin.

--- plugins/dialog/samples/assets/my_dialog.js
+++ plugins/dialog/samples/assets/my_dialog.js
@@ -17,7 +17,28 @@ CKEDITOR.dialog.add( 'myDialog', function( editor ) {
 					{
 						id: 'input1',
 						type: 'text',
-						label: 'Input 1'
+						label: 'Text Field'
+					},
+					{
+						id: 'select1',
+						type: 'select',
+						label: 'Select Field',
+						items: [
+							[ 'option1', 'value1' ],
+							[ 'option2', 'value2' ]
+						]
+					}
+				]
+			},
+			{
+				id: 'tab2',
+				label: 'Second Tab',
+				title: 'Second Tab',
+				elements: [
+					{
+						id: 'button1',
+						type: 'button',
+						label: 'Button Field'
 					}
 				]
 			}
