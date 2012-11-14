7b24a1634198a2f93b6250dccb6c3b741d36aa1b
fredck <f.knabben@cksource.com>
2012-11-14 17:29:27 +0100

    Defined build configurations for "basic" and "standard" presets.

---
+++ dev/builder/preset-basic-config.js
@@ -0,0 +1,29 @@
+/**
+ * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono',
+	ignore: [
+		'dev',
+		'.gitignore',
+		'.gitattributes',
+		'README.md',
+		'.mailmap'
+	],
+	plugins: {
+		about: 1,
+		basicstyles: 1,
+		clipboard: 1,
+		list: 1,
+		indent: 1,
+		enterkey: 1,
+		entities: 1,
+		link: 1,
+		pastetext: 1,
+		toolbar: 1,
+		undo: 1,
+		wysiwygarea: 1
+	}
+};
