bce1340ece7aa93dd0d651af7cd891e83f47f4dc
fredck <f.knabben@cksource.com>
2012-11-21 13:15:41 +0100

    Added locale-less fallback on the plugin language detection.

--- core/editor.js
+++ core/editor.js
@@ -448,14 +448,21 @@
 						pluginLangs = pluginLangs.split( ',' );
 
 					// Resolve the plugin language. If the current language
-					// is not available, get english or the first one.
+					// is not available, get English or the first one.
 					if ( CKEDITOR.tools.indexOf( pluginLangs, editor.langCode ) >= 0 )
 						lang = editor.langCode;
-					// English is our backup language - try to find it.
-					else if ( CKEDITOR.tools.indexOf( pluginLangs, 'en' ) >= 0 )
-						lang = 'en';
-					else
-						lang = pluginLangs[ 0 ];
+					else {
+						// The language code may have the locale information (zh-cn).
+						// Fall back to locale-less in that case (zh).
+						var langPart = editor.langCode.replace( /-.*/, '' );
+						if ( langPart != editor.langCode && CKEDITOR.tools.indexOf( pluginLangs, langPart ) >= 0 )
+							lang = langPart;
+						// Try the only "generic" option we have: English.
+						else if ( CKEDITOR.tools.indexOf( pluginLangs, 'en' ) >= 0 )
+							lang = 'en';
+						else
+							lang = pluginLangs[ 0 ];
+					}
 
 					if ( !plugin.langEntries || !plugin.langEntries[ lang ] ) {
 						// Put the language file URL into the list of files to
