bf09a6fa05f699831b4f95c832d01f755069ed7f
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2013-01-06 02:17:48 +0100

    Filtering elements - part 1/N.

--- core/datafilter.js
+++ core/datafilter.js
@@ -6,9 +6,14 @@
 (function() {
 	'use strict';
 
+	var DTD = CKEDITOR.dtd,
+		indexOf = CKEDITOR.tools.indexOf,
+		clone = CKEDITOR.tools.clone,
+		parseCssText = CKEDITOR.tools.parseCssText;
+
 	CKEDITOR.dataFilter = function( editor ) {
 		this.customConfig = true;
-		this.allowedRules = [];
+		this.allowedContent = [];
 
 		var allowedContent = editor.config.allowedContent;
 
@@ -18,6 +23,23 @@
 		}
 
 		this.addRules( allowedContent, 1 );
+
+		editor.once( 'pluginsLoaded', function() {
+			var filterFn = this.getFilterFunction();
+
+			// Add element filter at the end of filters queue.
+			editor.dataProcessor.dataFilter.addRules( {
+				elements: {
+					'^': filterFn
+				}
+			}, 100 );
+
+			editor.dataProcessor.htmlFilter.addRules( {
+				elements: {
+					'^': filterFn
+				}
+			}, 100 );
+		}, this );
 	};
 
 	CKEDITOR.dataFilter.prototype = {
@@ -29,11 +51,12 @@
 			if ( typeof newRules == 'string' )
 				newRules = parseRulesString( newRules );
 
-			var rules = this.allowedRules,
-				groupName, rule;
+			var groupName, rule;
 
 			for ( groupName in newRules ) {
-				rule = newRules[ groupName ];
+				// Clone rule, because we'll modify it later.
+				// TODO We don't need deep clone.
+				rule = clone( newRules[ groupName ] );
 
 				if ( typeof rule == 'boolean' )
 					rule = {};
@@ -43,32 +66,138 @@
 				if ( groupName.charAt( 0 ) != '$' )
 					rule.elements = groupName;
 
-				rules.push( rule );
+				this.allowedContent.push( rule );
+			}
+		},
+
+		getFilterFunction: function() {
+			var rules = optimizeRules( this.allowedContent ),
+				elementsRules = rules.elements,
+				genericRules = rules.generic;
+
+			return function( element ) {
+				var name = element.name,
+					status = {
+						valid: false,
+						modified: false,
+						validStyles: {},
+						validClasses: {},
+						validAttributes: {}
+					},
+					rules = elementsRules[ name ],
+					i, l;
+
+				if ( element.name == 'span') console.log( element);
+
+				// Early return - if there are no rules, remove this element.
+				if ( !rules && !genericRules )
+					removeElement( element, name );
+
+				// TODO For performance reasons they may be parsed only on demand.
+				element.styles = parseCssText( element.attributes.style || '', 1 );
+				element.classes = element.attributes[ 'class' ] ? element.attributes[ 'class' ].split( /\s+/ ) : [];
+
+				if ( rules ) {
+					for ( i = 0, l = rules.length; i < l; ++i )
+						applyRule( rules[ i ], element, status, true );
+				}
+
+				if ( genericRules ) {
+					for ( i = 0, l = genericRules.length; i < l; ++i )
+						applyRule( genericRules[ i ], element, status, false );
+				}
+
+				// Finally, if after running all filter rules it is still disallowed - remove it.
+				if ( !status.valid )
+					removeElement( element, name );
 			}
 		}
 	};
 
+	function applyRule( rule, element, status, isSpecific ) {
+		var name = element.name;
+
+		// This generic rule doesn't apply to this element - skip it.
+		if ( !isSpecific && !checkValue( rule.elements, name ) )
+			return;
+
+		// Optimalization - validate only if still invalid.
+		if ( !status.valid ) {
+			// If rule has validator and it accepts this element - make it valid.
+			if ( rule.validate ) {
+				if ( checkValue( rule.validate, element ) )
+					status.valid = true;
+			}
+			// If there's no validator make it valid anyway, because there exists a rule for this element.
+			else
+				status.valid = true;
+		}
+	}
+
+	function checkValue( validator, value1, value2 ) {
+		// Code optimalization. Check if validator exists here.
+		// If not - return true (value is ok).
+		if ( !validator )
+			return true;
+
+		return validator( value1, value2 );
+	}
+
+	function optimizeRule( rule ) {
+		for ( var i in { elements:1,styles:1,attrs:1,classes:1 } ) {
+			if ( rule[ i ] )
+				rule[ i ] = validatorFunction( rule[ i ] );
+		}
+	}
+
+	function optimizeRules( rules ) {
+		var elementsRules = {},
+			genericRules = [],
+			i, l, rule, elements, element;
+
+		for ( i = 0, l = rules.length; i < l; ++i ) {
+			rule = rules[ i ];
+
+			if ( typeof rule.elements == 'string' ) {
+				elements = rule.elements.split( /\s+/ );
+				delete rule.elements;
+
+				// Do not optimize rule.elements.
+				optimizeRule( rule );
+
+				while ( ( element = elements.pop() ) ) {
+					if ( !elementsRules[ element ] )
+						elementsRules[ element ] = [ rule ];
+					else
+						elementsRules[ element ].push( rule );
+				}
+			}
+			else {
+				optimizeRule( rule );
+				genericRules.push( rule );
+			}
+		}
+
+		return {
+			elements: elementsRules,
+			generic: genericRules.length ? genericRules : null
+		};
+	}
+
 	function parseRulesString( input ) {
 			//              <   elements   >< classes ><   styles and attributes   >< separator >
-		var groupPattern = /^([a-z0-9*\s]+)(\.[\w-,]+)?({[\w\-,]+}|\[[\w\-,]+\]){0,2}(?:;\s*|$)/i,
+		var groupPattern = /^([a-z0-9*\s]+)(?:\.([\w-,]+))?({[\w\-,]+}|\[[\w\-,]+\]){0,2}(?:;\s*|$)/i,
 			match,
-			elements, attrsAndStyles, styles, attrs, classes,
+			attrsAndStyles, styles, attrs,
 			rules = {},
 			groupNum = 1;
 
 		input = CKEDITOR.tools.trim( input );
 
 		while ( ( match = input.match( groupPattern ) ) ) {
-			elements = match[ 1 ].toLowerCase();
-			classes = match[ 2 ] || null;
 			styles = attrs = null;
 
-			if ( classes )
-				classes = classes.slice( 1 );
-
 			if ( ( attrsAndStyles = match[ 3 ] ) ) {
-				attrsAndStyles = attrsAndStyles.toLowerCase();
-
 				styles = attrsAndStyles.match( /{([^}]+)}/ );
 				if ( styles )
 					styles = styles[ 1 ];
@@ -79,8 +208,8 @@
 			}
 
 			rules[ '$' + groupNum++ ] = {
-				elements: elements,
-				classes: classes,
+				elements: match[ 1 ],
+				classes: match[ 2 ] || null,
 				styles: styles,
 				attrs: attrs
 			};
@@ -90,6 +219,41 @@
 		}
 
 		return rules;
+	}
+
+	function removeElement( element, name ) {
+		if ( DTD.$block[ name ] ) {
+			// TODO Very brutal way of removing elements.
+			element.name = 'p';
+			element.attributes = {};
+			element.styles = {};
+		}
+		else
+			delete element.name;
+	}
+
+	function validatorFunction( validator ) {
+		var type = typeof validator;
+		if ( type == 'object' )
+			type = validator.test ? 'regexp' : 'array';
+
+		switch ( type ) {
+			case 'function':
+				return validator;
+			case 'string':
+				var arr = validator.split( /,\s+/ );
+				return function( value ) {
+					return indexOf( arr, value ) > -1;
+				};
+			case 'regexp':
+				return function( value ) {
+					return validator.test( value );
+				};
+			case 'array':
+				return function( value ) {
+					return indexOf( validator, value ) > -1;
+				};
+		}
 	}
 
 	// Default editor's rules.
