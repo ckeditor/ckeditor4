8f4645f6b49c1538a60c0e021542fbfc950a400c
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2013-01-31 11:19:11 +0100

    Indent plugin respects config.indentClasses setting when registering allowed content.

--- plugins/indent/plugin.js
+++ plugins/indent/plugin.js
@@ -14,8 +14,8 @@
 
 	function indentCommand( editor, name ) {
 		this.name = name;
-		this.useIndentClasses = editor.config.indentClasses && editor.config.indentClasses.length > 0;
-		if ( this.useIndentClasses ) {
+		var useClasses = this.useIndentClasses = editor.config.indentClasses && editor.config.indentClasses.length > 0;
+		if ( useClasses ) {
 			this.classNameRegex = new RegExp( '(?:^|\\s+)(' + editor.config.indentClasses.join( '|' ) + ')(?=$|\\s)' );
 			this.indentClassMap = {};
 			for ( var i = 0; i < editor.config.indentClasses.length; i++ )
@@ -23,6 +23,17 @@
 		}
 
 		this.startDisabled = name == 'outdent';
+
+		this.allows = {
+			// TODO this may not be a complete list of elements.
+			'div h1 h2 h3 h4 h5 h6 ol p pre ul': {
+				// Do not add elements, but only text-align style if element is validated by other rule.
+				propertiesOnly: true,
+				styles: !useClasses ? 'margin-left,margin-right' : null,
+				classes: useClasses ? editor.config.indentClasses : null
+			}
+		};
+		this.requires = 'p' + ( useClasses ? '(' + editor.config.indentClasses[ 0 ] + ')' : '{margin-left}' );
 	}
 
 	// Returns the CSS property to be used for identing a given element.
@@ -37,15 +48,6 @@
 	indentCommand.prototype = {
 		// It applies to a "block-like" context.
 		context: 'p',
-		allows: {
-			// TODO this may not be a complete list of elements.
-			'div h1 h2 h3 h4 h5 h6 ol p pre ul': {
-				// Do not add elements, but only text-align style if element is validated by other rule.
-				propertiesOnly: true,
-				styles: 'margin-left,margin-right'
-			}
-		},
-		requires: 'p{margin-left}',
 
 		refresh: function( editor, path ) {
 			var list = path && path.contains( listNodeNames ),
