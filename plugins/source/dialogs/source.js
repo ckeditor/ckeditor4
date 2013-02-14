7d10df1ddc5013f7a4ee0c2d806c193c28072dcd
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2013-02-14 14:14:27 +0100

    Fixed selection issues in gecko, webkit and IE on dialog close.

--- plugins/source/dialogs/source.js
+++ plugins/source/dialogs/source.js
@@ -12,17 +12,44 @@ CKEDITOR.dialog.add( 'source', function( editor ) {
 	// Make it use 2/3 of the viewport height.
 	var height = size.height / 1.5;
 
+	// Store old editor data to avoid unnecessary setData.
+	var oldData;
+
 	return {
 		title: editor.lang.source.title,
 		minWidth: 100,
 		minHeight: 100,
 
 		onShow: function() {
-			this.setValueOf( 'main', 'data', editor.getData() );
+			this.setValueOf( 'main', 'data', oldData = editor.getData() );
 		},
 
 		onOk: function() {
-			editor.setData( this.getValueOf( 'main', 'data' ) );
+			var that = this,
+				newData = this.getValueOf( 'main', 'data' );
+
+			// Avoid unnecessary setData. Also preserve selection
+			// when user changed his mind and goes back to wysiwyg editing.
+			if ( newData === oldData )
+				return;
+
+			editor.setData( this.getValueOf( 'main', 'data' ), function() {
+				// Avoid selection error in webkit & gecko.
+				if ( CKEDITOR.env.webkit || CKEDITOR.env.gecko ) {
+					var range = editor.createRange();
+					range.moveToElementEditStart( editor.editable() );
+					range.select();
+				}
+
+				// Fix wrong caret position in IEs.
+				CKEDITOR.env.ie && editor.editable().focus();
+
+				// Close the dialog once getData is done and selection is fixed.
+				that.hide();
+			} );
+
+			// Don't let the dialog close before setData is over.
+			return false;
 		},
 
 		contents: [{
