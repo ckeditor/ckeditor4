f947ea3c34e03d2d1e16e6c0404456821a0c88f4
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2013-03-07 12:31:29 +0100

    Ported fix to bootstrap code.

--- plugins/wysiwygarea/plugin.js
+++ plugins/wysiwygarea/plugin.js
@@ -418,7 +418,7 @@
 					// is fully editable even before the editing iframe is fully loaded (#4455).
 					var bootstrapCode =
 						'<script id="cke_actscrpt" type="text/javascript"' + ( CKEDITOR.env.ie ? ' defer="defer" ' : '' ) + '>' +
-							( isCustomDomain ? ( 'document.domain="' + document.domain + '";' ) : '' ) +
+							( isCustomDomain || editor.config.forceCustomDomain ? ( 'document.domain="' + document.domain + '";' ) : '' ) +
 							'var wasLoaded=0;' +	// It must be always set to 0 as it remains as a window property.
 							'function onload(){' +
 								'if(!wasLoaded)' +	// FF3.6 calls onload twice when editor.setData. Stop that.
