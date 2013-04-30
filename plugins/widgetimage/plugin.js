c2e1c9867a92d595f8ba3d7dc5d5e2ba5bbeaadb
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2013-04-30 16:01:14 +0200

    Use widget's data object in image widget.

--- plugins/widgetimage/plugin.js
+++ plugins/widgetimage/plugin.js
@@ -8,17 +8,19 @@
 		init: function( editor ) {
 			editor.widgets.add( 'image', {
 				init: function() {
+					// Read float style from figure/image and remove it from these elements.
+					// This style will be set on wrapper in #data listener.
 					var floatStyle = this.element.getStyle( 'float' ) || this.parts.image.getStyle( 'float' );
-
-					// Move float style from figure/img to wrapper.
-					this.wrapper.setStyle( 'float', floatStyle );
-					this.element.setStyle( 'float', '' );
-					this.parts.image.setStyle( 'float', '' );
-
-					this.parts.image.$.draggable = false;
+					this.element.removeStyle( 'float' );
+					this.parts.image.removeStyle( 'float' );
+					this.setData( 'floatStyle', floatStyle );
 
 					this.on( 'getOutput', function( evt ) {
 						cleanUpImage( evt.data.getFirst( 'img' ), this );
+					} );
+
+					this.on( 'data', function() {
+						this.wrapper.setStyle( 'float', this.data.floatStyle );
 					} );
 				},
 
@@ -40,7 +42,7 @@
 					captionedImage: function( el, widget ) {
 						var image = el.getFirst( 'img' );
 
-						// If for some reasons image disappeared, remove this widget.
+						// If for some reason image disappeared, remove this widget.
 						if ( !image )
 							return false;
 
@@ -78,9 +80,7 @@
 			delete attrs[ 'data-widget-property' ];
 		}
 
-		delete attrs[ 'draggable' ];
-
-		var floatStyle = widget.wrapper.getStyle( 'float' );
+		var floatStyle = widget.data.floatStyle;
 		if ( floatStyle ) {
 			var styles = CKEDITOR.tools.parseCssText( attrs.style || '' );
 			styles.float = floatStyle;
