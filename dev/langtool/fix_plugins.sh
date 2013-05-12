5dbad1acb33b91192505b5443d0f040139e8f181
Wiktor Walc <w.walc@cksource.com>
2013-05-12 21:33:39 +0200

    Added script to automatically fix lang/availableLang properties in plugins. Fixed some plugins on first run.

---
+++ dev/langtool/fix_plugins.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+# For licensing, see LICENSE.html or http://ckeditor.com/license
+
+# Correct plugin definitions and update lang/availableLangs properties based on available language files
+
+set -e
+
+echo "CKLangTool - corrects CKEditor plugin definitions."
+echo ""
+
+. ./_common.sh || exit
+
+# Run the langtool.
+echo ""
+echo "Starting CKLangTool..."
+echo "Fixing 'lang' properties in plugin files..."
+
+for i in "${plugins[@]}"
+do
+	java -jar cklangtool/$CKLANGTOOL_VERSION/langtool.jar update-plugin ../../plugins/$i
+done
+
+echo "Fixing 'availableLangs' properties in plugin files..."
+
+for i in "${plugins_dialogs[@]}"
+do
+	java -jar cklangtool/$CKLANGTOOL_VERSION/langtool.jar update-plugin --type=dialog ../../plugins/$i
+done
+
+echo ""
