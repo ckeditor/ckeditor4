9edbb4c58805a0c943e46be0604519a277a43810
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2013-06-11 14:27:18 +0200

    Updated year in licenses and missing licenses.

--- plugins/widgetimage/plugin.js
+++ plugins/widgetimage/plugin.js
@@ -1,3 +1,8 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
 'use strict';
 
 (function() {
