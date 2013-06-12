d41ebcb08c9c2cce3730859f3a57c6bd6ecd3ea6
fredck <f.knabben@cksource.com>
2013-06-12 13:26:16 +0200

    Full documentation for the widgetimage plugin code.

--- plugins/widgettime/plugin.js
+++ plugins/widgettime/plugin.js
@@ -6,16 +6,28 @@
 'use strict';
 
 CKEDITOR.plugins.add( 'widgettime', {
+	// Require the "widget" plugin, which provides the Widget System and its API.
 	requires: 'widget,dialog',
+
+	// Let CKEditor know about the plugin icon.
 	icons: 'widgettime',
 
 	init: function( editor ) {
+		// Register the widget with a unique name "time".
 		editor.widgets.add( 'time', {
+			// This is an "inline" widget. It defaults to "block".
 			inline: true,
+
+			// We have a dialog to edit this widget, so here we set its name.
 			dialog: 'widgettime',
+
+			// Let the Widget System create the toolbar button automatically.
 			button: 'Time',
+
+			// The HTML template used for new widgets creation.
 			template: '<time datetime="{dateTime}">{text}</time>',
 
+			// The default data used to fill the above template.
 			defaults: function() {
 				return {
 					dateTime: ( new Date() )[ Date.prototype.toISOString ? 'toISOString' : 'toUTCString' ](),
@@ -23,24 +35,30 @@ CKEDITOR.plugins.add( 'widgettime', {
 				};
 			},
 
+			// Initialization code, called for each widget instance created.
 			init: function() {
+				// Take the widget data out of the DOM.
 				this.setData( {
 					dateTime: this.element.getAttribute( 'datetime' ),
 					text: this.element.getText()
 				} );
 			},
 
+			// Called whenever changes to the widget data happens.
 			data: function() {
+				// Transport the data changes to the DOM.
 				this.element.setAttribute( 'datetime', this.data.dateTime );
 				this.element.setText( this.data.text );
 			},
 
-			// Upcast all time elements.
+			// Check the elements that need to be converted to widgets.
 			upcast: function( el ) {
+				// Convert all <time> elements.
 				return el.name == 'time';
 			}
 		} );
 
+		// Register the editing dialog.
 		CKEDITOR.dialog.add( 'widgettime', this.path + 'dialogs/widgettime.js' );
 	}
 } );
