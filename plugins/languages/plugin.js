34f488ef5eef5b31b1cab48cf66101de3a836c33
m.lewandowski <m.lewandowski@cksource.com>
2013-09-10 17:03:08 +0200

    Removed trailing whitespace.

--- plugins/languages/plugin.js
+++ plugins/languages/plugin.js
@@ -7,7 +7,7 @@
  * @fileOverview Languages plugin.
  */
 (function() {
-	
+
 	var languagesButtonsGroup = 'languages',
 		toolbarOrder = 30,
 		toolbarGroup = 'bidi';
@@ -17,18 +17,18 @@
 		requires: 'menubutton',
 		icons: 'languages',
 		init: function( editor ) {
-			
+
 			var languagesConfigString = editor.config.languages,
 				items = {},
 				parts,
 				curLanguageId, // 2-letter lanugage identifier.
 				i;
-	
+
 			// Parse languagesConfigString, and create items entry for each lang.
 			for ( i = 0 ; i < languagesConfigString.length ; i++ ) {
 				parts = languagesConfigString[ i ].split( ':' );
 				curLanguageId = parts[ 0 ];
-				
+
 				items[ curLanguageId ] = {
 					label: parts[ 1 ],
 					group: languagesButtonsGroup,
@@ -41,7 +41,7 @@
 						editor.applyStyle( this.style );
 					}
 				};
-				
+
 				// Init style property.
 				items[ curLanguageId ].style = new CKEDITOR.style( {
 					element: 'span',
@@ -51,11 +51,11 @@
 					}
 				} );
 			}
-			
+
 			// Initialize group/button.
 			editor.addMenuGroup( languagesButtonsGroup, 1 );
 			editor.addMenuItems( items );
-			
+
 			editor.ui.add( 'Languages', CKEDITOR.UI_MENUBUTTON, {
 				label: 'Language',
 				title: 'Language',
@@ -64,21 +64,21 @@
 				className: 'cke_button_languages',
 				onMenu: function() {
 					var activeItems = {};
-					
+
 					for ( var prop in items ) {
 						if ( items.hasOwnProperty( prop ) )
 							activeItems[ prop ] = CKEDITOR.TRISTATE_ON;
 					}
-					
+
 					return  activeItems;
 				}
 			} );
-			
+
 			// Exposes items variable for testing purposes. // %REMOVE_LINE%
 			this.testOnly_items = items; // %REMOVE_LINE%
 		}
 	});
-	
+
 	// Assign default configuration.
 	CKEDITOR.config.languages = CKEDITOR.config.languages || [ 'fr:French', 'de:Spanish', 'ar:Arabic:rtl' ];
 }());
