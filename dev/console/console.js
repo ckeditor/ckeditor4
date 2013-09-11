d0a332a4b123e24e18eb3638592df0ef3a9d2748
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2013-09-11 10:32:43 +0200

    Added copyright notices.

--- dev/console/console.js
+++ dev/console/console.js
@@ -1,3 +1,8 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
 'use strict';
 
 var CKCONSOLE = (function() {
