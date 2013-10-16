70489951342529d693a55bfeae2b7b121e423499
Piotr Jasiun <p.jasiun@cksource.com>
2013-10-16 15:53:43 +0200

    Check active filter in language plugin.

--- plugins/language/plugin.js
+++ plugins/language/plugin.js
@@ -116,6 +116,11 @@
 						activeItems[ 'language_' + currentLanguagedElement.getAttribute( 'lang' ) ] = CKEDITOR.TRISTATE_ON;
 
 					return activeItems;
+				},
+
+				onStateUpdate: function( status ) {
+					if ( !editor.activeFilter.check( requiredContent ) )
+						this.setState( CKEDITOR.TRISTATE_DISABLED );
 				}
 			} );
 		},
