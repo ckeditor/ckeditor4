c152d89e786eadea5653e56ff5610d1cf041a96a
m.lewandowski <m.lewandowski@cksource.com>
2013-10-17 14:23:38 +0200

    Moved privateContext property initialization go plugin init() method (previously it was inited after uiSpace event).

--- plugins/elementspath/plugin.js
+++ plugins/elementspath/plugin.js
@@ -57,6 +57,12 @@
 	CKEDITOR.plugins.add( 'elementspath', {
 		lang: 'af,ar,bg,bn,bs,ca,cs,cy,da,de,el,en,en-au,en-ca,en-gb,eo,es,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
 		init: function( editor ) {
+			editor._.elementsPath = {
+				idBase: 'cke_elementspath_' + CKEDITOR.tools.getNextNumber() + '_',
+				filters: [],
+				onClick: null
+			};
+
 			editor.on( 'uiSpace', function( event ) {
 				if ( event.data.space == 'bottom' )
 					initElementsPath( editor, event.data );
@@ -72,8 +78,8 @@
 					spaceElement = CKEDITOR.document.getById( spaceId );
 				return spaceElement;
 			},
-			idBase = 'cke_elementspath_' + CKEDITOR.tools.getNextNumber() + '_',
-			privateContext = editor._.elementsPath = { idBase: idBase, filters: [] };
+			privateContext = editor._.elementsPath,
+			idBase = privateContext.idBase;
 
 		bottomSpaceData.html += '<span id="' + spaceId + '_label" class="cke_voice_label">' + editor.lang.elementspath.eleLabel + '</span>' +
 			'<span id="' + spaceId + '" class="cke_path" role="group" aria-labelledby="' + spaceId + '_label">' + emptyHtml + '</span>';
