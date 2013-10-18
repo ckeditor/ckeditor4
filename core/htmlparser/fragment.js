9fe8a5fb7ee48340c115763248da8469a64534fc
Piotr Jasiun <p.jasiun@cksource.com>
2013-10-18 14:40:49 +0200

    Check only if data-cke-survive attribute exists, not the value.

--- core/htmlparser/fragment.js
+++ core/htmlparser/fragment.js
@@ -46,7 +46,7 @@ CKEDITOR.htmlParser.fragment = function() {
 
 	function isRemoveEmpty( node ) {
 		// Keep marked element event if it is empty.
-		if ( node.attributes[ 'data-cke-survive' ] === 'true' )
+		if ( node.attributes[ 'data-cke-survive' ] )
 			return false;
 
 		// Empty link is to be removed when empty but not anchor. (#7894)
