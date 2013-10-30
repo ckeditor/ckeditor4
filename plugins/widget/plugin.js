92e3f34a1b1c25224504985bb28e117f3a4acc5c
Piotr Jasiun <p.jasiun@cksource.com>
2013-10-30 11:42:36 +0100

    Code style.

--- plugins/widget/plugin.js
+++ plugins/widget/plugin.js
@@ -280,7 +280,7 @@
 			for ( i = 0, count = wrappers.count(); i < count; i++ ) {
 				wrapper = wrappers.getItem( i );
 
-				if( !this.getByElement( wrapper ) ) {
+				if ( !this.getByElement( wrapper ) ) {
 					wrapper.addClass( 'cke_widget_new' );
 					this.initOn( wrapper.findOne( '.cke_widget_element' ) );
 				}
