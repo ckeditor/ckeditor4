abccedfa0e356ba483379ce91c15a8da64655ea7
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2013-11-04 12:33:48 +0100

    Removed comments from en lang file.

--- plugins/image2/lang/en.js
+++ plugins/image2/lang/en.js
@@ -3,15 +3,15 @@ Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'image2', 'en', {
-	alt: 'Alternative Text',						// Inherit from image plugin.
-	btnUpload: 'Send it to the Server',				// Inherit from image plugin.
-	captioned: 'Captioned image', 					// NEW property.
-	infoTab: 'Image Info',							// Inherit from image plugin.
-	lockRatio: 'Lock Ratio',						// Inherit from image plugin.
-	menu: 'Image Properties',						// Inherit from image plugin.
-	resetSize: 'Reset Size', 						// Inherit from image plugin.
-	resizer: 'Click and drag to resize',			// NEW property.
-	title: 'Image Properties',						// Inherit from image plugin.
-	uploadTab: 'Upload',							// NEW property (but same as lang.image.upload).
-	urlMissing: 'Image source URL is missing.' 		// Inherit from image plugin.
+	alt: 'Alternative Text',
+	btnUpload: 'Send it to the Server',
+	captioned: 'Captioned image',
+	infoTab: 'Image Info',
+	lockRatio: 'Lock Ratio',
+	menu: 'Image Properties',
+	resetSize: 'Reset Size',
+	resizer: 'Click and drag to resize',
+	title: 'Image Properties',
+	uploadTab: 'Upload',
+	urlMissing: 'Image source URL is missing.'
 } );
