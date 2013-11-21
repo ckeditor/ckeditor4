b4734c20cff99b0cff5e752a3d5c96e400b5c266
Synchro <marcus@synchromedia.co.uk>
2013-11-21 10:01:03 +0100

    Small language tweak

--- lang/en-ca.js
+++ lang/en-ca.js
@@ -64,7 +64,7 @@ CKEDITOR.lang[ 'en-ca' ] = {
 		advancedTab: 'Advanced',
 		validateNumberFailed: 'This value is not a number.',
 		confirmNewPage: 'Any unsaved changes to this content will be lost. Are you sure you want to load new page?',
-		confirmCancel: 'Some of the options have been changed. Are you sure to close the dialog?',
+		confirmCancel: 'Some of the options have been changed. Are you sure you want to close the dialog?',
 		options: 'Options', // MISSING
 		target: 'Target',
 		targetNew: 'New Window (_blank)', // MISSING
