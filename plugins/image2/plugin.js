3850ada1658376cb3fadd52c6f6f5e1d0b232878
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2013-12-02 12:35:27 +0100

    Renamed: Plugin icons.

--- plugins/image2/plugin.js
+++ plugins/image2/plugin.js
@@ -17,7 +17,7 @@
 	CKEDITOR.plugins.add( 'image2', {
 		lang: 'af,ar,bg,bn,bs,ca,cs,cy,da,de,el,en,en-au,en-ca,en-gb,eo,es,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
 		requires: 'widget,dialog',
-		icons: 'image2',
+		icons: 'image',
 		hidpi: true,
 
 		onLoad: function( editor ) {
