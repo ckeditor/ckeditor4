986323082e3e5c763d5f578beea230dbe9679ef5
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2013-12-03 13:11:37 +0100

    Minor code-style fix.

--- core/style.js
+++ core/style.js
@@ -1204,9 +1204,11 @@ CKEDITOR.STYLE_OBJECT = 3;
 			// The 'class' element value must match (#1318).
 			if ( ( attName == 'class' || this._.definition.fullMatch ) && element.getAttribute( attName ) != normalizeProperty( attName, attributes[ attName ] ) )
 				continue;
+
 			// Do not touch data-* attributes (#11011).
 			if ( attName.slice( 0, 5 ) == 'data-' )
 				continue;
+
 			removeEmpty = element.hasAttribute( attName );
 			element.removeAttribute( attName );
 		}
