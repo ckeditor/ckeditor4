7b5ca69c0b489b97912c7bb8092c15e47cbc0ec8
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2013-12-05 11:00:58 +0100

    Extracted to make clear.

--- plugins/richcombo/plugin.js
+++ plugins/richcombo/plugin.js
@@ -184,14 +184,20 @@ CKEDITOR.plugins.add( 'richcombo', {
 					ev = new CKEDITOR.dom.event( ev );
 
 					var keystroke = ev.getKeystroke();
+
+					// ARROW-DOWN
+					// This call is duplicated in plugins/toolbar/plugin.js in itemKeystroke().
+					// Move focus to the first element after drop down was opened by the arrow down key.
+					if ( keystroke == 40 ) {
+						editor.once( 'panelShow', function( evt ) {
+							evt.data._.panel._.currentBlock.onKeyDown( 40 );
+						} );
+					}
+
 					switch ( keystroke ) {
-						case 40: // ARROW-DOWN
-							// This call is duplicated in plugins/toolbar/plugin.js in itemKeystroke().
-							editor.once( 'panelShow', function( evt ) {
-								evt.data._.panel._.currentBlock.onKeyDown( 40 );
-							} );
 						case 13: // ENTER
 						case 32: // SPACE
+						case 40: // ARROW-DOWN
 							// Show panel
 							CKEDITOR.tools.callFunction( clickFn, element );
 							break;
