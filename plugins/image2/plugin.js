2d3ff5cca63226792d5d22d7119480812802ec6f
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2013-12-13 12:34:09 +0100

    config.captionedClass considered in ACF rules of widget definition.

--- plugins/image2/plugin.js
+++ plugins/image2/plugin.js
@@ -109,6 +109,8 @@
 	// @param {CKEDITOR.editor}
 	// @returns {Object}
 	function widgetDef( editor ) {
+		var captionedClass = editor.config.image2_captionedClass;
+
 		return {
 			// Widget-specific rules for Allowed Content Filter.
 			allowedContent: {
@@ -119,7 +121,7 @@
 				},
 				figcaption: true,
 				figure: {
-					classes: '!caption',
+					classes: '!' + captionedClass,
 					styles: 'float,display'
 				},
 				img: {
