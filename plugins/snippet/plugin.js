dbdaf0ed57b222078fc2a2b92dc03b88e54351ca
m.lewandowski <m.lewandowski@cksource.com>
2014-02-04 15:58:16 +0100

    Codestyle and copyright update.

--- plugins/snippet/plugin.js
+++ plugins/snippet/plugin.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
@@ -9,7 +9,7 @@
 
 'use strict';
 
-(function() {
+( function() {
 
 	CKEDITOR.plugins.add( 'snippet', {
 		requires: 'widget,ajax,dialog',
@@ -215,4 +215,4 @@
 			editor._.snippet = {};
 	}
 
-})();
+} )();
