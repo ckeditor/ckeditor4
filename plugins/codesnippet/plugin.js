5ea623881ab7dcf704dfc637a3f2ae4518d2aefd
m.lewandowski <m.lewandowski@cksource.com>
2014-03-12 11:30:13 +0100

    Update license comments.

--- plugins/codesnippet/plugin.js
+++ plugins/codesnippet/plugin.js
@@ -1,6 +1,6 @@
 /**
  * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
  /**
