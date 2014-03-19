222b154332305adfe16c8e5ad61106ace8b292d1
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2014-03-19 15:00:19 +0100

    Consider link as a first child of figure while upcasting.

--- plugins/image2/plugin.js
+++ plugins/image2/plugin.js
@@ -758,7 +758,7 @@
 
 			// No center wrapper has been found.
 			else if ( name == 'figure' && el.hasClass( captionedClass ) )
-				image = el.getFirst( 'img' );
+				image = el.getFirst( 'img' ) || el.getFirst( 'a' ).getFirst( 'img' );
 
 			// Upcast linked image like <a><img/></a>.
 			else if ( isLinkedOrStandaloneImage( el ) )
