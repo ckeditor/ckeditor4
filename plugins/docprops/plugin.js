dc8f488824ddb46a3791d54ee30db2925b2e4b0d
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-03-28 09:36:24 +0100

    Added missing requirement in docprops plugin.

--- plugins/docprops/plugin.js
+++ plugins/docprops/plugin.js
@@ -4,7 +4,7 @@
  */
 
 CKEDITOR.plugins.add( 'docprops', {
-	requires: 'wysiwygarea,dialog',
+	requires: 'wysiwygarea,dialog,colordialog',
 	lang: 'af,ar,bg,bn,bs,ca,cs,cy,da,de,el,en,en-au,en-ca,en-gb,eo,es,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
 	icons: 'docprops,docprops-rtl', // %REMOVE_LINE_CORE%
 	hidpi: true, // %REMOVE_LINE_CORE%
