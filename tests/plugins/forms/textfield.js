7714234a2e6edace79c43290fde476b23c9205cf
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-06-02 11:54:26 +0200

    Got rid of async:setUp which is not yet supported by Bender.

--- tests/plugins/forms/textfield.js
+++ tests/plugins/forms/textfield.js
@@ -1,23 +1,15 @@
 /* bender-tags: editor,unit */
 /* bender-ckeditor-plugins: dialog,button,forms,toolbar */
 
-bender.editor = { config : { autoParagraph : false } };
+bender.editor = { config: { autoParagraph: false } };
 
-bender.test(
-{
-	// Setup reset editor to empty.
-	'async:setUp' : function() {
-		var tc = this;
-		var editor = tc.editor;
-		editor.setData( '', function() {
-			tc.callback();
-		} );
-	},
+bender.test( {
 
-	'test fill fields (text) ' : function() {
+	'test fill fields (text) ': function() {
 		var bot = this.editorBot;
 
-		bot.dialog( 'textfield', function( dialog ) {
+		bot.setData( '', function() {
+			bot.dialog( 'textfield', function( dialog ) {
 				dialog.setValueOf( 'info', '_cke_saved_name', 'name' );
 				dialog.setValueOf( 'info', 'value', 'value' );
 
@@ -25,12 +17,14 @@ bender.test(
 
 				assert.areSame( '<input name="name" type="text" value="value" />', bot.getData( true ) );
 			} );
+		} );
 	},
 
-	'test fill fields (password)' : function() {
+	'test fill fields (password)': function() {
 		var bot = this.editorBot;
 
-		bot.dialog( 'textfield', function( dialog ) {
+		bot.setData( '', function() {
+			bot.dialog( 'textfield', function( dialog ) {
 				dialog.setValueOf( 'info', 'type', 'password' );
 				dialog.setValueOf( 'info', '_cke_saved_name', 'name' );
 				dialog.setValueOf( 'info', 'value', 'value' );
@@ -39,21 +33,22 @@ bender.test(
 
 				assert.areSame( '<input name="name" type="password" value="value" />', bot.getData( true ) );
 			} );
+		} );
 	},
 
-	'test empty fields' : function() {
+	'test empty fields': function() {
 		var bot = this.editorBot;
 
 		bot.setHtmlWithSelection( '[<input name="name" type="text" value="value" />]' );
 
 		bot.dialog( 'textfield', function( dialog ) {
-				dialog.setValueOf( 'info', '_cke_saved_name', '' );
-				dialog.setValueOf( 'info', 'value', '' );
+			dialog.setValueOf( 'info', '_cke_saved_name', '' );
+			dialog.setValueOf( 'info', 'value', '' );
 
-				dialog.getButton( 'ok' ).click();
+			dialog.getButton( 'ok' ).click();
 
-				assert.areSame( '<input type="text" />', bot.getData( false, true ) );
-			} );
+			assert.areSame( '<input type="text" />', bot.getData( false, true ) );
+		} );
 	},
 
 	'test read attributes (email)': function() {
@@ -62,15 +57,15 @@ bender.test(
 		bot.setHtmlWithSelection( '[<input name="name" type="email" value="test@host.com" />]' );
 
 		bot.dialog( 'textfield', function( dialog ) {
-				assert.areSame( 'name', dialog.getValueOf( 'info', '_cke_saved_name' ) );
-				assert.areSame( 'email', dialog.getValueOf( 'info', 'type' ) );
-				assert.areSame( 'test@host.com', dialog.getValueOf( 'info', 'value' ) );
+			assert.areSame( 'name', dialog.getValueOf( 'info', '_cke_saved_name' ) );
+			assert.areSame( 'email', dialog.getValueOf( 'info', 'type' ) );
+			assert.areSame( 'test@host.com', dialog.getValueOf( 'info', 'value' ) );
 
-				dialog.setValueOf( 'info', 'type', 'text' );
+			dialog.setValueOf( 'info', 'type', 'text' );
 
-				dialog.getButton( 'ok' ).click();
+			dialog.getButton( 'ok' ).click();
 
-				assert.areSame( '<input name="name" type="text" value="test@host.com" />', bot.getData( true ) );
-			} );
+			assert.areSame( '<input name="name" type="text" value="test@host.com" />', bot.getData( true ) );
+		} );
 	}
 } );
