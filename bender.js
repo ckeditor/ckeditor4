0470689f3c10596db1c70681d215f29b8c8088e3
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-06-02 15:07:50 +0200

    Fixed ported regressions - missing ones, unnecessary ones.

--- bender.js
+++ bender.js
@@ -38,12 +38,20 @@ var config = {
 				'!/_'
 			],
 			regressions: {
-				'tests/core/dom/range/movetoelement#test_moveToElementEditStart4' : 'env.ie6Compat',
-				'tests/core/dom/range#test enlarge element (HTML comments)' : 'env.ie6Compat',
-				'tests/core/editable/wysiwyg#testFocus' : 'env.ie6Compat',
+				// IE8 (#11242)
 				'tests/core/style/editor#test apply inline style on non-editable inline element - at non-editable inline boundary': 'env.ie && env.version == 8',
 				'tests/core/style/editor#test remove inline style from non-editable inline element - at non-editable inline boundary': 'env.ie && env.version == 8',
-				'tests/core/dom/range/enlarge#test_enlarge_element12': 'env.safari'
+
+				// Safari (#11111)
+				'tests/core/dom/range/enlarge#test_enlarge_element12': 'env.safari',
+
+				// Safari (#11980)
+				'tests/core/editable/keystrokes/delbackspacequirks/collapsed#test backspace #2': 'env.safari',
+				'tests/core/editable/keystrokes/delbackspacequirks/collapsed#test backspace #3': 'env.safari',
+				'tests/core/editable/keystrokes/delbackspacequirks/collapsed#test backspace #9': 'env.safari',
+				'tests/core/editable/keystrokes/delbackspacequirks/collapsed#test backspace, merge #2': 'env.safari',
+				'tests/core/editable/keystrokes/delbackspacequirks/collapsed#test backspace, merge #3': 'env.safari',
+				'tests/core/editable/keystrokes/delbackspacequirks/collapsed#test backspace, merge #8': 'env.safari',
 			}
 		},
 
@@ -55,15 +63,17 @@ var config = {
 				'!/_'
 			],
 			regressions: {
-				'tests/plugins/image/image#test update image (remove)' : 'env.ie6Compat || env.ie7Compat || env.ie8Compat',
-				'tests/plugins/magicline/magicline#middle-edge triggers' : 'env.ie6Compat',
-				'tests/plugins/magicline/magicline#single-edge triggers' : 'env.ie6Compat',
-				'tests/plugins/magicline/magicline#get size' : 'env.ie6Compat',
-				'tests/plugins/magicline/magicline#is line' : 'env.ie6Compat',
+				// IE10
 				'tests/plugins/ajax/ajax#test_loadXml_sync' : 'env.ie && env.version > 9',
 				'tests/plugins/ajax/ajax#test_loadXml_async' : 'env.ie && env.version > 9',
+
+				// IE8 (#11242)
 				'tests/plugins/indent/indent#test indent next to inline non-editable': 'env.ie && env.version == 8',
+
+				// IE8 (#11055)
 				'tests/plugins/widget/nestededitables#test pasting widget which was copied (d&d) when its nested editable was focused': 'env.ie && env.version == 8',
+
+				// Firefox (#11399)
 				'tests/plugins/widget/nestededitables#test selection in nested editable is preserved after opening and closing dialog - inline editor': 'env.gecko'
 			}
 		},
