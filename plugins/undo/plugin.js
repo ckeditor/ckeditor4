ace61713c983256e62a3a89c8ad7d22ceed50838
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-06-13 12:20:37 +0200

    Cached often used object.

--- plugins/undo/plugin.js
+++ plugins/undo/plugin.js
@@ -11,7 +11,8 @@
 'use strict';
 
 ( function() {
-	var keystrokes = [ CKEDITOR.CTRL + 90 /*Z*/, CKEDITOR.CTRL + 89 /*Y*/, CKEDITOR.CTRL + CKEDITOR.SHIFT + 90 /*Z*/ ];
+	var keystrokes = [ CKEDITOR.CTRL + 90 /*Z*/, CKEDITOR.CTRL + 89 /*Y*/, CKEDITOR.CTRL + CKEDITOR.SHIFT + 90 /*Z*/ ],
+		backspaceOrDelete = { 8: 1, 46: 1 };
 
 	CKEDITOR.plugins.add( 'undo', {
 		lang: 'af,ar,bg,bn,bs,ca,cs,cy,da,de,el,en,en-au,en-ca,en-gb,eo,es,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,tt,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
@@ -113,7 +114,7 @@
 				// Keyup executes main snapshot logic.
 				editable.attachListener( editable, 'keyup', function( evt ) {
 					var keyCode = evt.data.getKey(),
-						ieFunctionKeysWorkaround = CKEDITOR.env.ie && keyCode in { 8: 1, 46: 1 }; /* backspace / delete */
+						ieFunctionKeysWorkaround = CKEDITOR.env.ie && keyCode in backspaceOrDelete;
 
 					if ( inputFired || ieFunctionKeysWorkaround ) {
 						// Reset flag indicating input event.
