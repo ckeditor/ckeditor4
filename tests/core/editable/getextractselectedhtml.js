4845b2ec64ba13fae5868435119ffd12b3892197
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2014-06-23 17:13:42 +0200

    Tests: Extended coverage for editable#getSelectedHtmlFromRange.

--- tests/core/editable/getextractselectedhtml.js
+++ tests/core/editable/getextractselectedhtml.js
@@ -18,31 +18,10 @@
 
 	var setWithHtml = bender.tools.range.setWithHtml,
 		getWithHtml = bender.tools.range.getWithHtml,
-		container = new CKEDITOR.dom.element( 'div' );
+		container = new CKEDITOR.dom.element( 'div' ),
+		img_src = '%BASE_PATH%_assets/img.gif';
 
-	function g( html, expected ) {
-		return function() {
-			var editable = this.editables.inline,
-				range = setWithHtml( editable, html ),
-				docFragment = editable.getSelectedHtmlFromRange( range );
-
-			assert.areSame( expected, docFragment.getHtml(), 'Selected HTML' );
-			assert.areSame( html, getWithHtml( editable, range ), 'HTML of editable, untouched once get' );
-		};
-	}
-
-	function e( html, expected ) {
-		return function() {
-			var editable = this.editables.inline,
-				range = setWithHtml( editable, html );
-
-			editable.extractSelectedHtmlFromRange( range );
-
-			assert.areSame( expected, getWithHtml( editable, range ), 'HTML of editable, once extracted' );
-		};
-	}
-
-	bender.test( {
+	var tests = {
 		'async:init': function() {
 			var that = this;
 
@@ -56,29 +35,145 @@
 
 				that.callback();
 			} );
-		},
+		}
+	};
 
-		'test get: #1 (text)': 					g( '{x}', 																'x' ),
-		'test get: #2 (text)': 					g( '{}x', 																'' ),
-		'test get: #3 (text)': 					g( '{}x', 																'' ),
-		'test get: #4 (text)': 					g( '<p>x<b>{y}</b>x</p>', 												'<b>y</b>' ),
-		'test get: #5 (text)': 					g( '<p>x<b>y{y}y</b>x</p>', 											'<b>y</b>' ),
-		'test get: #6 (text)': 					g( '<p><b>{y}</b></p>', 												'<b>y</b>' ),
-		'test get: #7 (text)': 					g( '<p>x<a href="#">{y}</a>x</p>', 										'<a href="#">y</a>' ),
-		'test get: #8 (text)': 					g( '<p>x<a href="#">x{y}x</a>x</p>', 									'<a href="#">y</a>' ),
-		'test get: #9 (text)': 					g( '<p><span style="color:red">{y}</span></p>', 						'<span style="color:red">y</span>' ),
-		'test get: #10 (text)': 				g( '<p>x<span style="color:red">{y}</span>x</p>', 						'<span style="color:red">y</span>' ),
-		'test get: #11 (text)': 				g( '<p>{x<b>y}</b>x</p>', 												'<p>x<b>y</b></p>' ),
-		'test get: #12 (text)': 				g( '<p>x<b>{y</b>x}</p>', 												'<p><b>y</b>x</p>' ),
-		'test get: #13 (text)': 				g( '<p>{x<b>y</b>x}</p>', 												'<p>x<b>y</b>x</p>' ),
-		'test get: #14 (text)': 				g( '<p>x{<b>y</b>}x</p>', 												'<p><b>y</b></p>' ),
-		'test get: #15 (text)': 				g( '<p>x{</p><p>}x</p>', 												'' ),
-		'test get: #16 (text)': 				g( '<p>{x</p><p>x}</p>', 												'<p>x</p><p>x</p>' ),
-
-		//'test get: #9 (text)': 							g( '<table><tbody><tr>[<td>foo</td><td>x]y</td></tr></tbody></table>', 								'<span style="color:red">{y}</span>' ),
-
-		'test extract: #1 (text)': 				e( '<p>x<a href="#">{y}</a>x</p>', 										'<p>x{}x</p>' ),
-		'test extract: #2 (text)': 				e( '<p>x<span>{y}</span>x</p>', 										'<p>x{}x</p>' ),
-		'test extract: #3 (text)': 				e( '<p>{x}</p>', 														'{}' )
+	// <DEV>
+	var playground = CKEDITOR.document.createElement( 'dl' );
+	playground.on( 'paste', function( e ) {
+		console.log( e.data.$.clipboardData.getData( 'text/html' ) );
 	} );
+	// </DEV>
+
+	function decodeBoguses( html ) {
+		return html.replace( /@/g, '<br />' );
+	}
+
+	function addTests( cases, method, prefix ) {
+		var group, i, tc, name;
+
+		for ( group in cases ) {
+			for ( i = 0; i < cases[ group ].length; i++ ) {
+				tc = cases[ group ][ i ];
+				name = 'test ' + prefix + ', ' + group + ' #' + ( i + 1 );
+
+				// <DEV>
+				CKEDITOR.dom.element.createFromHtml( '<dt>' + name + ':</dt>' ).appendTo( playground );
+				CKEDITOR.dom.element.createFromHtml( '<dd contenteditable="true" style="outline: 1px dashed orange; font-family: monospace">' + tc[ 0 ] + '</dd>' ).appendTo( playground );
+				// </DEV>
+
+				tests[ name ] = method( tc[ 0 ], tc[ 1 ] );
+			}
+		}
+	}
+
+	function assertGetSelectedHtmlFromRange( html, expected ) {
+		return function() {
+			html = decodeBoguses( html );
+			expected = decodeBoguses( expected );
+
+			var editable = this.editables.inline,
+				range = setWithHtml( editable, html ),
+				docFragment = editable.getSelectedHtmlFromRange( range );
+
+			assert.areSame( expected, docFragment.getHtml(), 'Selected HTML' );
+			assert.areSame( html, getWithHtml( editable, range ), 'HTML of editable, untouched once get' );
+		};
+	}
+
+	function assertExtractSelectedHtmlFromRange( html, expected ) {
+		return function() {
+			html = decodeBoguses( html );
+			expected = decodeBoguses( expected );
+
+			var editable = this.editables.inline,
+				range = setWithHtml( editable, html );
+
+			editable.extractSelectedHtmlFromRange( range );
+
+			assert.areSame( expected, getWithHtml( editable, range ), 'HTML of editable, once extracted' );
+		};
+	}
+
+	addTests( {
+		'no block': [
+			[ '{x}', 																'x' ],
+			[ '{}x', 																'']
+		],
+		'block': [
+			[ '<p>{x}</p>', 														'<p>x</p>' ],
+			[ '<p>x{x}</p>', 														'x' ],
+			[ '<p>{x}x</p>', 														'x' ],
+			[ '<p>x{x}x</p>', 														'x' ],
+		],
+		'cross-block': [
+			[ '<p>x{</p><p>}x</p>', 												'' ],
+			[ '<p>{x</p><p>x}</p>', 												'<p>x</p><p>x</p>' ],
+			[ '<p>y{x</p><p>x}y</p>', 												'<p>x</p><p>x</p>' ],
+			[ '<blockquote>y{x</blockquote><p>x}y</p>', 							'<blockquote>x</blockquote><p>x</p>' ],
+			[ '<blockquote>y{x</blockquote><div>x</div><p>x}y</p>', 				'<blockquote>x</blockquote><div>x</div><p>x</p>' ],
+			[ '<div>y<div>{x</div></div><div>x</div><p>x}y</p>', 					'<div><div>x</div></div><div>x</div><p>x</p>' ],
+		],
+		'inline': [
+			[ '<p>x<b>{y}</b>x</p>', 												'<b>y</b>' ],
+			[ '<p>x<b>y{y}y</b>x</p>', 												'<b>y</b>' ],
+			[ '<p><b>{y}</b></p>', 													'<b>y</b>' ],
+			[ '<p>x<a href="#">{y}</a>x</p>', 										'<a href="#">y</a>' ],
+			[ '<p>x<a href="#">x{y}x</a>x</p>', 									'<a href="#">y</a>' ],
+			[ '<p>x<b id="foo">{y}</b>x</p>', 										'<b id="foo">{y}</b>' ],
+			[ '<p><b style="color:red">{y}</b></p>', 								'<b style="color:red">y</b>' ],
+			[ '<p>x<b style="color:red">{y}</b>x</p>', 								'<b style="color:red">y</b>' ],
+
+		],
+		'cross-inline': [
+			[ '<p>{x<b>y}</b>x</p>', 												'x<b>y</b>' ],
+			[ '<p>x<b>{y</b>x}</p>', 												'<b>y</b>x' ],
+			[ '<p>{x<b>y</b>x}</p>', 												'<p>x<b>y</b>x</p>' ],
+			[ '<p>x{<b>y</b>}x</p>', 												'<b>y</b>' ],
+			[ '<p><b>{x</b><b>y}</b></p>', 											'<p><b>x</b><b>y</b></p>' ],
+			[ '<p>x<b class="a">{y</b><b class="b">y}</b>x</p>', 					'<b class="a">y</b><b class="b">y</b>' ],
+			[ '<p>x<b class="a">{y</b>x<b class="b">y}</b>x</p>', 					'<b class="a">y</b>x<b class="b">y</b>' ],
+		],
+		'bogus': [
+			[ '<p>{x}@</p>', 														'<p>x</p>' ],
+			[ '<p><b>{x}</b>@</p>', 												'<b>x</b>' ],
+			[ '<p><b>{x}@</b></p>', 												'<b>x</b>' ],
+		],
+		'tables': [
+			[ '<table><tbody><tr><td>{x}</td></tr></tbody></table>', 				'x' ],
+			[ '<table><tbody><tr><td>x{x}x</td></tr></tbody></table>', 				'x' ],
+			[ '<table><tbody><tr><td>y{x</td><td>x}y</td></tr></tbody></table>', 	'<table><tbody><tr><td>x</td><td>x</td></tr></tbody></table>' ],
+			[ '<table><tbody><tr>[<td>x</td><td>x}y</td></tr></tbody></table>', 	'<table><tbody><tr><td>x</td><td>x</td></tr></tbody></table>' ],
+			[ '<table><tbody><tr><td>{x</td></tr></tbody></table><table><tbody><tr><td>x</td></tr><tr><td>x}</td><td>y</td></tr></tbody></table>',
+																					'<table><tbody><tr><td>x</td></tr></tbody></table><table><tbody><tr><td>x</td></tr><tr><td>x</td></tr></tbody></table>' ],
+		],
+		'lists': [
+			[ '<ol><li>{x}</li></ol>', 												'x' ],
+			[ '<ol><li>x{x}x</li></ol>', 											'x' ],
+			[ '<ol><li>y{x</li><li>x}y</li></ol>', 									'<ol><li>x</li><li>x</li></ol>' ],
+			[ '<ol><li>y{x</li></ol><ol><li>x}y</li></ol>', 						'<ol><li>x</li></ol><ol><li>x</li></ol>' ],
+			[ '<ol><li><ul><li>y{x</li></ul></li><li>x}y</li></ol>', 				'<ol><li><ul><li>x</li></ul></li><li>x</li></ol>' ],
+		],
+		'various anchored in element': [
+			[ '<p>[]x</p>', 														'' ],
+			[ '<p>[x]</p>', 														'<p>x</p>' ],
+			[ '<p>x[x]</p>', 														'x' ],
+			[ '<p>x<b>[y]</b>x</p>', 												'<b>y</b>' ],
+			[ '<p>x<a href="#">[y]</a>x</p>', 										'<a href="#">y</a>' ],
+			[ '<p>x[<b>y</b>]x</p>', 												'<b>y</b>' ],
+			[ '<table><tbody><tr>[<td>x</td>]</tr></tbody></table>', 				'<table><tbody><tr><td>x</td></tr></tbody></table>' ],
+			[ '<table><tbody><tr><td>x[</td><td>y</td>]</tr></tbody></table>', 		'<table><tbody><tr><td></td><td>y</td></tr></tbody></table>' ],
+			[ '<ol><li>x[</li></ol><ol><li>x]y</li></ol>', 							'<ol><li></li></ol><ol><li>x</li></ol>' ],
+			[ '<p>[x@]</p>', 														'<p>x</p>' ],
+			[ '<p>[x]@</p>', 														'<p>x</p>' ],
+			[ '[<hr>]', 															'<hr>' ],
+			[ '[<img src="' + img_src + '">]', 										'<img src="' + img_src + '">' ],
+		]
+	}, assertGetSelectedHtmlFromRange, 'getSelectedHtmlFromRange' );
+
+	bender.test( tests );
+
+	// <DEV>
+	playground.appendTo( CKEDITOR.document.getBody() );
+	// </DEV>
 } )();
