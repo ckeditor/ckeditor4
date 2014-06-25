772e14ecf063ccaf762cee52c81e1cf1c0c02c4e
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2014-06-25 16:12:13 +0200

    Tests: Updated and extended tests for editable#getSelectedHtmlFromRange.

--- tests/core/editable/getextractselectedhtml.js
+++ tests/core/editable/getextractselectedhtml.js
@@ -46,7 +46,7 @@
 	// </DEV>
 
 	function decodeBoguses( html ) {
-		return html.replace( /@/g, '<br />' );
+		return html.replace( /@/g, '<br>' );
 	}
 
 	function addTests( cases, method, prefix ) {
@@ -59,7 +59,7 @@
 
 				// <DEV>
 				CKEDITOR.dom.element.createFromHtml( '<dt>' + name + ':</dt>' ).appendTo( playground );
-				CKEDITOR.dom.element.createFromHtml( '<dd contenteditable="true" style="outline: 1px dashed orange; font-family: monospace">' + tc[ 0 ] + '</dd>' ).appendTo( playground );
+				CKEDITOR.dom.element.createFromHtml( '<dd contenteditable="true" style="outline: 1px dashed orange; font-family: monospace">' + decodeBoguses( tc[ 0 ] ) + '</dd>' ).appendTo( playground );
 				// </DEV>
 
 				tests[ name ] = method( tc[ 0 ], tc[ 1 ] );
@@ -117,12 +117,14 @@
 		'inline': [
 			[ '<p>x<b>{y}</b>x</p>', 												'<b>y</b>' ],
 			[ '<p>x<b>y{y}y</b>x</p>', 												'<b>y</b>' ],
-			[ '<p><b>{y}</b></p>', 													'<b>y</b>' ],
+			[ '<p><b>{y}</b></p>', 													'<p><b>y</b></p>' ],
 			[ '<p>x<a href="#">{y}</a>x</p>', 										'<a href="#">y</a>' ],
 			[ '<p>x<a href="#">x{y}x</a>x</p>', 									'<a href="#">y</a>' ],
-			[ '<p>x<b id="foo">{y}</b>x</p>', 										'<b id="foo">{y}</b>' ],
-			[ '<p><b style="color:red">{y}</b></p>', 								'<b style="color:red">y</b>' ],
+			[ '<p>x<b id="foo">{y}</b>x</p>', 										'<b id="foo">y</b>' ],
+			[ '<p><b style="color:red">{y}</b></p>', 								'<p><b style="color:red">y</b></p>' ],
 			[ '<p>x<b style="color:red">{y}</b>x</p>', 								'<b style="color:red">y</b>' ],
+			[ '<p>x<i><b>{y}</b></i></p>', 											'<i><b>y</b></i>' ],
+			[ '<p><i><b>{y}</b></i></p>', 											'<p><i><b>y</b></i></p>' ],
 
 		],
 		'cross-inline': [
@@ -131,12 +133,14 @@
 			[ '<p>{x<b>y</b>x}</p>', 												'<p>x<b>y</b>x</p>' ],
 			[ '<p>x{<b>y</b>}x</p>', 												'<b>y</b>' ],
 			[ '<p><b>{x</b><b>y}</b></p>', 											'<p><b>x</b><b>y</b></p>' ],
+			[ '<p><b>x{x</b><b>y}y</b></p>', 										'<b>x</b><b>y</b>' ],
 			[ '<p>x<b class="a">{y</b><b class="b">y}</b>x</p>', 					'<b class="a">y</b><b class="b">y</b>' ],
 			[ '<p>x<b class="a">{y</b>x<b class="b">y}</b>x</p>', 					'<b class="a">y</b>x<b class="b">y</b>' ],
 		],
 		'bogus': [
 			[ '<p>{x}@</p>', 														'<p>x</p>' ],
-			[ '<p><b>{x}</b>@</p>', 												'<b>x</b>' ],
+			[ '<p>{x@]</p>', 														'<p>x</p>' ],
+			[ '<p><b>{x}</b>@</p>', 												'<p><b>x</b></p>' ],
 			[ '<p><b>{x}@</b></p>', 												'<b>x</b>' ],
 			[ '<p>{x}<br>@</p>', 													'x' ],
 		],
@@ -144,13 +148,17 @@
 			[ '<table><tbody><tr><td>{x}</td></tr></tbody></table>', 				'x' ],
 			[ '<table><tbody><tr><td>x{x}x</td></tr></tbody></table>', 				'x' ],
 			[ '<table><tbody><tr><td>y{x</td><td>x}y</td></tr></tbody></table>', 	'<table><tbody><tr><td>x</td><td>x</td></tr></tbody></table>' ],
+			[ '<div><table><tbody><tr><td>y{x</td><td>x}y</td></tr></tbody></table></div>',
+																					'<table><tbody><tr><td>x</td><td>x</td></tr></tbody></table>' ],
+			[ '<div><table><tbody><tr><td>{x}</td></tr></tbody></table></div>',		'x' ],
 			[ '<table><tbody><tr>[<td>x</td><td>x}y</td></tr></tbody></table>', 	'<table><tbody><tr><td>x</td><td>x</td></tr></tbody></table>' ],
 			[ '<table><tbody><tr><td>{x</td></tr></tbody></table><table><tbody><tr><td>x</td></tr><tr><td>x}</td><td>y</td></tr></tbody></table>',
 																					'<table><tbody><tr><td>x</td></tr></tbody></table><table><tbody><tr><td>x</td></tr><tr><td>x</td></tr></tbody></table>' ],
 		],
 		'lists': [
-			[ '<ol><li>{x}</li></ol>', 												'x' ],
 			[ '<ol><li>x{x}x</li></ol>', 											'x' ],
+			[ '<ol><li>{x}</li></ol>', 												'<ol><li>x</li></ol>' ],
+			[ '<div><ol><li>{x}</li></ol></div>', 									'<ol><li>x</li></ol>' ],
 			[ '<ol><li>y{x</li><li>x}y</li></ol>', 									'<ol><li>x</li><li>x</li></ol>' ],
 			[ '<ol><li>y{x</li></ol><ol><li>x}y</li></ol>', 						'<ol><li>x</li></ol><ol><li>x</li></ol>' ],
 			[ '<ol><li><ul><li>y{x</li></ul></li><li>x}y</li></ol>', 				'<ol><li><ul><li>x</li></ul></li><li>x</li></ol>' ],
