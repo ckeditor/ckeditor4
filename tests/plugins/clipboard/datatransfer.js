13a069f5fcbaae10b1bcb1e50ea32262bc4b62d5
Piotr Jasiun <p.jasiun@cksource.com>
2014-07-03 11:29:57 +0200

    Tests: Improved datatransfer tests descriptions.

--- tests/plugins/clipboard/datatransfer.js
+++ tests/plugins/clipboard/datatransfer.js
@@ -57,21 +57,21 @@ bender.test( {
 		CKEDITOR.plugins.clipboard.resetDataTransfer();
 	},
 
-	'test dataTransfer id': function() {
+	'test id': function() {
 		var evt1 = createDragDropEventMock(),
 			evt2 = createDragDropEventMock(),
 			dataTransfer1a = new CKEDITOR.plugins.clipboard.dataTransfer( evt1 ),
 			dataTransfer1b = new CKEDITOR.plugins.clipboard.dataTransfer( evt1 ),
 			dataTransfer2 = new CKEDITOR.plugins.clipboard.dataTransfer( evt2 );
 
-		assert.areSame( dataTransfer1a.id, dataTransfer1b.id );
+		assert.areSame( dataTransfer1a.id, dataTransfer1b.id, 'Ids for object based on the same event should be the same.' );
 
 		// In IE10+ we can not use any data type besides text, so id is fixed.
 		if ( !CKEDITOR.env.ie || CKEDITOR.env.version < 10 )
-			assert.areNotSame( dataTransfer1a.id, dataTransfer2.id );
+			assert.areNotSame( dataTransfer1a.id, dataTransfer2.id, 'Ids for object based on different events should be different.' );
 	},
 
-	'test dataTransfer internal': function() {
+	'test internal drag drop': function() {
 		var bot = this.bots.editor1,
 			editor = this.editors.editor1,
 			evt, dataTransfer;
@@ -84,7 +84,7 @@ bender.test( {
 		dataTransfer = new CKEDITOR.plugins.clipboard.dataTransfer( evt, editor );
 		dataTransfer.setTargetEditor( editor );
 
-		assert.areSame( CKEDITOR.DATA_TRANSFER_INTERNAL, dataTransfer.getTransferType() );
+		assert.areSame( CKEDITOR.DATA_TRANSFER_INTERNAL, dataTransfer.getTransferType(), 'transferType' );
 		assert.areSame( 'x<b>foo</b>x', bender.tools.fixHtml( dataTransfer.dataValue ), 'dataValue' );
 		assert.areSame( 'html', dataTransfer.dataType, 'dataType' );
 		assert.areSame( editor, dataTransfer.sourceEditor, 'sourceEditor' );
@@ -93,7 +93,7 @@ bender.test( {
 
 	},
 
-	'test dataTransfer external text': function() {
+	'test drop text from external source': function() {
 		var editor = this.editors.editor1,
 			evt, dataTransfer;
 
@@ -103,7 +103,7 @@ bender.test( {
 		dataTransfer = new CKEDITOR.plugins.clipboard.dataTransfer( evt );
 		dataTransfer.setTargetEditor( editor );
 
-		assert.areSame( CKEDITOR.DATA_TRANSFER_EXTERNAL, dataTransfer.getTransferType() );
+		assert.areSame( CKEDITOR.DATA_TRANSFER_EXTERNAL, dataTransfer.getTransferType(), 'transferType' );
 		assert.areSame( 'x&lt;b&gt;foo&lt;/b&gt;x', dataTransfer.dataValue, 'dataValue' );
 		assert.areSame( 'text', dataTransfer.dataType, 'dataType' );
 		assert.isUndefined( dataTransfer.sourceEditor, 'sourceEditor' );
@@ -111,7 +111,7 @@ bender.test( {
 		assert.areSame( 'x<b>foo</b>x', dataTransfer.getData( 'Text' ), 'getData( \'Text\' )' );
 	},
 
-	'test dataTransfer external html': function() {
+	'test drop html from external source': function() {
 		var editor = this.editors.editor1,
 			evt, dataTransfer;
 
@@ -124,7 +124,7 @@ bender.test( {
 		dataTransfer = new CKEDITOR.plugins.clipboard.dataTransfer( evt );
 		dataTransfer.setTargetEditor( editor );
 
-		assert.areSame( CKEDITOR.DATA_TRANSFER_EXTERNAL, dataTransfer.getTransferType() );
+		assert.areSame( CKEDITOR.DATA_TRANSFER_EXTERNAL, dataTransfer.getTransferType(), 'transferType' );
 		assert.isUndefined( dataTransfer.sourceEditor, 'sourceEditor' );
 		assert.areSame( editor, dataTransfer.targetEditor, 'targetEditor' );
 
@@ -140,7 +140,7 @@ bender.test( {
 		}
 	},
 
-	'test dataTransfer cross': function() {
+	'test drag drop between editors': function() {
 		var bot1 = this.bots.editor1,
 			editor1 = this.editors.editor1,
 			editor2 = this.editors.editor2,
@@ -154,7 +154,7 @@ bender.test( {
 		dataTransfer = new CKEDITOR.plugins.clipboard.dataTransfer( evt, editor1 );
 		dataTransfer.setTargetEditor( editor2 );
 
-		assert.areSame( CKEDITOR.DATA_TRANSFER_CROSS_EDITORS, dataTransfer.getTransferType() );
+		assert.areSame( CKEDITOR.DATA_TRANSFER_CROSS_EDITORS, dataTransfer.getTransferType(), 'transferType' );
 		assert.areSame( 'x<b>foo</b>x', bender.tools.fixHtml( dataTransfer.dataValue ), 'dataValue' );
 		assert.areSame( 'html', dataTransfer.dataType, 'dataType' );
 		assert.areSame( editor1, dataTransfer.sourceEditor, 'sourceEditor' );
@@ -168,7 +168,7 @@ bender.test( {
 
 		dataTransfer.setData( 'Text', 'foo' );
 
-		assert.areSame( 'foo', dataTransfer.getData( 'Text' ) );
+		assert.areSame( 'foo', dataTransfer.getData( 'Text' ), 'data should match set data' );
 
 	},
 
@@ -178,13 +178,13 @@ bender.test( {
 			dataTransferA = CKEDITOR.plugins.clipboard.initDataTransfer( evt1 ),
 			dataTransferB = CKEDITOR.plugins.clipboard.initDataTransfer( evt1 );
 
-		assert.areSame( dataTransferA, dataTransferB );
+		assert.areSame( dataTransferA, dataTransferB, 'If we init dataTransfer object twice on the same event this should be the same object.' );
 
 		CKEDITOR.plugins.clipboard.resetDataTransfer();
 
 		dataTransferB = CKEDITOR.plugins.clipboard.initDataTransfer( evt2 );
 
-		assert.areNotSame( dataTransferA, dataTransferB );
+		assert.areNotSame( dataTransferA, dataTransferB, 'If we init dataTransfer object twice on different events these should be different objects.' );
 	},
 
 	'test initDataTransfer constructor': function() {
@@ -197,7 +197,7 @@ bender.test( {
 			dataTransfer = CKEDITOR.plugins.clipboard.initDataTransfer( evt, editor );
 		dataTransfer.setTargetEditor( editor );
 
-		assert.areSame( CKEDITOR.DATA_TRANSFER_INTERNAL, dataTransfer.getTransferType() );
+		assert.areSame( CKEDITOR.DATA_TRANSFER_INTERNAL, dataTransfer.getTransferType(), 'transferType' );
 		assert.areSame( 'x<b>foo</b>x', bender.tools.fixHtml( dataTransfer.dataValue ), 'dataValue' );
 		assert.areSame( 'html', dataTransfer.dataType, 'dataType' );
 		assert.areSame( editor, dataTransfer.sourceEditor, 'sourceEditor' );
