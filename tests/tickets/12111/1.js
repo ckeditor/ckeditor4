467f22fe2aa3f97375509ef088fb72d78f0c8607
Marek Lewandowski <m.lewandowski@cksource.com>
2014-07-17 15:51:17 +0200

    Selection needs to be applied only for image/anchor images. It should not be changed on other dialogs.

---
+++ tests/tickets/12111/1.js
@@ -0,0 +1,47 @@
+/* bender-tags: editor,unit */
+/* bender-ckeditor-plugins: link,image */
+
+(function() {
+	'use strict';
+
+	bender.editor = {};
+
+	bender.test( {
+		'test image properties with link plugin enabled': function() {
+			var bot = this.editorBot,
+				editor = this.editor,
+				inputHtml = '<p><a id="imgLink" href="http://ckeditor.com">li[<img id="img1" alt="" src="assets/sample.jpg" />]nk</a></p>';
+
+			bot.setHtmlWithSelection( inputHtml );
+
+			var evt = {
+				element: bot.editor.editable().findOne( 'img#img1' )
+			};
+
+			editor.once( 'dialogShow', function( event ) {
+				var dialog = CKEDITOR.dialog.getCurrent(),
+					urlField;
+
+				resume( function() {
+					try {
+						assert.isTrue( !!dialog, 'dialog is created' );
+						urlField = dialog.getContentElement( 'info', 'txtUrl' ).getInputElement();
+						assert.areEqual( urlField.getValue(), 'assets/sample.jpg' );
+					} catch ( e ) {
+						// Propagate the exception.
+						throw e;
+					} finally {
+						// In any case hide the dialog.
+						if ( dialog ) {
+							dialog.hide();
+						}
+					}
+				} );
+			} );
+
+			editor.fire( 'doubleclick', evt );
+
+			wait();
+		}
+	} );
+})();
