fb360ce9092c3404c5ec53c107df516d17b55ebb
Shaohua Zhou <shao@versal.com>
2014-07-28 14:20:19 -0700

    Use createBookmarks2, mostly working

--- plugins/removeallformat/plugin.js
+++ plugins/removeallformat/plugin.js
@@ -1,19 +1,27 @@
 CKEDITOR.plugins.add( 'removeallformat', {
 	icons: 'removeallformat',
+	//A list of plugins that are required by this plugin.
+	//Note that this property doesn't guarantee the loading order of the plugins.
+	requires : [ 'removeformat' ],
 	init: function( editor ) {
-		//plugin logic
-		console.log('removeallformat init');
-
-		//The exec method defines a function that will be fired
-		//when the removeallformat command is executed.
 		editor.addCommand( 'removeallformat', {
+			//The exec method defines a function that will be fired
+			//when the removeallformat command is executed.
 			exec: function( editor ) {
+				//call removeFormat first
+				editor.execCommand('removeFormat');
+
+				//remove additional block level formats
 				var selection = editor.getSelection();
-				var ranges = selection.getRanges();
+
+				//http://stackoverflow.com/questions/16835365/set-cursor-to-specific-position-in-ckeditor
+				//http://stackoverflow.com/questions/15620115/ckeditor-change-selection-without-losing-it
+				//bookmarks are either normal (based on node references), serializable (based on spans)
+				//or 3rd type (based on node addresses)
+				var bookmarks = selection.createBookmarks2();
 				editor.insertText( selection.getSelectedText() );
-				// selection.selectRanges( [ ranges ] );
-				// var element = selection.getStartElement();
-				// selection.selectElement( element );
+
+				editor.getSelection().selectBookmarks( bookmarks );
 			}
 		});
 
