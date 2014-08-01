ffe23d2397f360650161e0954c903b8d7b0ed64a
Shaohua Zhou <shao@versal.com>
2014-08-01 13:58:08 -0700

    Reduce to two fonts

--- plugins/font/plugin.js
+++ plugins/font/plugin.js
@@ -214,15 +214,8 @@
  * @cfg {String} [font_names=see source]
  * @member CKEDITOR.config
  */
-CKEDITOR.config.font_names = 'Arial/Arial, Helvetica, sans-serif;' +
-	'Comic Sans MS/Comic Sans MS, cursive;' +
-	'Courier New/Courier New, Courier, monospace;' +
-	'Georgia/Georgia, serif;' +
-	'Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
-	'Tahoma/Tahoma, Geneva, sans-serif;' +
-	'Times New Roman/Times New Roman, Times, serif;' +
-	'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;' +
-	'Verdana/Verdana, Geneva, sans-serif';
+CKEDITOR.config.font_names = 'Sans serif/AvenirNext, Avenir, sans-serif;' +
+	'Serif/Georgia, serif;';
 
 /**
  * The text to be displayed in the Font combo is none of the available values
