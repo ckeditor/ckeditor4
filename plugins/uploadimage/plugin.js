ca6f79d8d9fb05b6530a472f38755cfb67070db5
Piotr Jasiun <p.jasiun@cksource.com>
2014-08-14 15:48:33 +0200

    Introduce uploadimage plugin.

---
+++ plugins/uploadimage/plugin.js
@@ -0,0 +1,12 @@
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.plugins.add( 'uploadimage', {
+	requires: 'clipboard',
+	lang: 'en', // %REMOVE_LINE_CORE%
+	init: function( editor ) {
+
+	}
+} );
