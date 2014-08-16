420cce4844a17d7dcf9d3c413265fec20688a98d
Muthiah Annamalai <ezhillang@gmail.com>
2014-08-16 00:20:39 -0400

    Tamil - building sample

---
+++ plugins/language/lang/ta.js
@@ -0,0 +1,10 @@
+/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.plugins.setLang( 'language', 'ta', {
+	button: 'மொழி தேர்வு',
+	remove: 'மொழி நீக்கவும்'
+} );
+
