7dca2db5e38740cba5beb62cc0a50c41f64845fd
Piotr Jasiun <p.jasiun@cksource.com>
2014-08-20 14:54:50 +0200

    Added scope. Use strict.

--- plugins/uploadimage/plugin.js
+++ plugins/uploadimage/plugin.js
@@ -2,96 +2,99 @@
  * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
+'use strict';
 
-CKEDITOR.plugins.add( 'uploadimage', {
-	requires: 'widget,clipboard',
-	lang: 'en', // %REMOVE_LINE_CORE%
-	init: function( editor ) {
-		editor.widgets.add( 'uploadimage', {
-			upcast: function( el, data ) {
-				if ( el.name != 'img' ||
-					!el.attributes[ 'data-cke-special-image' ] ||
-					el.attributes[ 'src' ].substring( 0, 5 ) != 'data:' )
+( function() {
+	CKEDITOR.plugins.add( 'uploadimage', {
+		requires: 'widget,clipboard',
+		lang: 'en', // %REMOVE_LINE_CORE%
+		init: function( editor ) {
+			editor.widgets.add( 'uploadimage', {
+				upcast: function( el, data ) {
+					if ( el.name != 'img' ||
+						!el.attributes[ 'data-cke-special-image' ] ||
+						el.attributes[ 'src' ].substring( 0, 5 ) != 'data:' )
+						return;
+
+					return el;
+				},
+
+				downcast: function() {
+					return new CKEDITOR.htmlParser.text( '' );
+				},
+
+				init: function() {
+				},
+
+				data: function( data ) {
+				}
+			} );
+
+			editor.on( 'paste', function( evt ) {
+				var data = evt.data,
+					dataTransfer = data.dataTransfer,
+					filesCount = dataTransfer.getFilesCount();
+
+				if ( data.dataValue || !filesCount ) {
 					return;
+				}
 
-				return el;
-			},
+				var loadedFilesCount = 0,
+					file, reader, i;
 
-			downcast: function() {
-				return new CKEDITOR.htmlParser.text( '' );
-			},
+				evt.cancel();
 
-			init: function() {
-			},
+				dataTransfer.cacheData();
 
-			data: function( data ) {
-			}
-		} );
+				for ( i = 0; i < filesCount; i++ ) {
+					file = dataTransfer.getFile( i ),
+					reader = new FileReader();
 
-		editor.on( 'paste', function( evt ) {
-			var data = evt.data,
-				dataTransfer = data.dataTransfer,
-				filesCount = dataTransfer.getFilesCount();
+					reader.onload = function( evt ) {
+						console.log( evt );
+						var img = new CKEDITOR.dom.element( 'img' );
+						img.setAttributes( {
+							'src': evt.target.result,
+							'data-cke-special-image': 1,
+							'data-cke-file-name': file.name
+						} );
+						data.dataValue += img.getOuterHtml();
 
-			if ( data.dataValue || !filesCount ) {
-				return;
-			}
+						loadedFilesCount++;
 
-			var loadedFilesCount = 0,
-				file, reader, i;
+						if ( loadedFilesCount == filesCount ) {
+							editor.fire( 'paste', data );
+						}
+					};
 
-			evt.cancel();
+					reader.readAsDataURL( file );
+				}
+			} );
 
-			dataTransfer.cacheData();
+			editor.on( 'paste', function( evt ) {
+				var data = evt.data,
+					dataTransfer = data.dataTransfer;
 
-			for ( i = 0; i < filesCount; i++ ) {
-				file = dataTransfer.getFile( i ),
-				reader = new FileReader();
+				var file = dataTransfer.getFile( 0 );
 
-				reader.onload = function( evt ) {
-					console.log( evt );
-					var img = new CKEDITOR.dom.element( 'img' );
-					img.setAttributes( {
-						'src': evt.target.result,
-						'data-cke-special-image': 1,
-						'data-cke-file-name': file.name
-					} );
-					data.dataValue += img.getOuterHtml();
+				var xhr = new XMLHttpRequest();
 
-					loadedFilesCount++;
+				var formData = new FormData();
+				formData.append( 'upload', file );
 
-					if ( loadedFilesCount == filesCount ) {
-						editor.fire( 'paste', data );
-					}
-				};
+				xhr.open( "POST", editor.config.filebrowserImageUploadUrl, true ); // method, url, async
 
-				reader.readAsDataURL( file );
-			}
-		} );
-
-		editor.on( 'paste', function( evt ) {
-			var data = evt.data,
-				dataTransfer = data.dataTransfer;
-
-			var file = dataTransfer.getFile( 0 );
-
-			var xhr = new XMLHttpRequest();
-
-			var formData = new FormData();
-			formData.append( 'upload', file );
-
-			xhr.open( "POST", editor.config.filebrowserImageUploadUrl, true ); // method, url, async
-
-			xhr.onreadystatechange = function() {
-				if ( xhr.readyState == 4 ) { // completed
-					if ( xhr.status == 200 ) { // OK
-						console.log( 'onreadystatechange:' );
-						console.log( xhr.responseText );
+				xhr.onreadystatechange = function() {
+					if ( xhr.readyState == 4 ) { // completed
+						if ( xhr.status == 200 ) { // OK
+							console.log( 'onreadystatechange:' );
+							console.log( xhr.responseText );
+						}
 					}
 				}
-			}
 
-			xhr.send( formData );
-		} );
-	}
-} );
+				xhr.send( formData );
+			} );
+		}
+	} );
+} )();
