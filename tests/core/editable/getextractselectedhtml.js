4c0aaa8721ccd72f6170d0a6e50309962ff13f4f
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2014-09-03 16:08:24 +0200

    Refactoring of tests. Used assert.isInnerHtmlMatching.

--- tests/core/editable/getextractselectedhtml.js
+++ tests/core/editable/getextractselectedhtml.js
@@ -26,6 +26,14 @@
 		img_src = '%BASE_PATH%_assets/img.gif';
 
 	var tests = {
+		_should: {
+			ignore: {
+				// 'test get: bogus #2': !CKEDITOR.env.needsBrFiller,
+				// 'test get: bogus #8': !CKEDITOR.env.needsBrFiller,
+				// 'test get: various anchored in element #10': !CKEDITOR.env.needsBrFiller,
+			}
+		},
+
 		'async:init': function() {
 			var that = this;
 
@@ -50,7 +58,7 @@
 	// </DEV>
 
 	function decodeBoguses( html ) {
-		return html.replace( /@/g, '<br>' );
+		return html.replace( /@/g, '<br />' );
 	}
 
 	function addTests( cases, editor ) {
@@ -77,61 +85,65 @@
 		CKEDITOR.tools.extend( tests, testsGet, testsExtract );
 	}
 
+	var compareInnerHtmlOptions = {
+		noInterWS: true,
+		fixStyles: true,
+		compareSelection: true,
+		normalizeSelection: false,
+	};
+
 	function assertGetSelectedHtmlFromRange( editor, html, expected ) {
 		return function() {
 			html = decodeBoguses( html );
-			expected = decodeBoguses( expected );
 
 			var editable = this.editables[ editor ],
 				range = setWithHtml( editable, html ),
 				docFragment = editable.getSelectedHtmlFromRange( range );
 
-			assert.areSame( expected, docFragment.getHtml(), 'Selected HTML' );
-			assert.areSame( html, getWithHtml( editable, range ), 'HTML of editable, untouched once get' );
+			assert.isInnerHtmlMatching( expected, docFragment.getHtml(), compareInnerHtmlOptions, 'Selected HTML' );
+			assert.isInnerHtmlMatching( html, getWithHtml( editable, range ), compareInnerHtmlOptions, 'HTML of editable, untouched once get' );
 		};
 	}
 
 	function assertExtractSelectedHtmlFromRange( editor, html, htmlGet, htmlWithSelection ) {
 		return function() {
 			html = decodeBoguses( html );
-			htmlGet = decodeBoguses( htmlGet );
-			htmlWithSelection = decodeBoguses( htmlWithSelection );
 
 			var editable = this.editables[ editor ],
 				range = setWithHtml( editable, html ),
 				docFragment = editable.extractSelectedHtmlFromRange( range );
 
-			assert.areSame( htmlGet, docFragment.getHtml(), 'HTML which has been extracted' );
-			assert.areSame( htmlWithSelection, getWithHtml( editable, range ), 'HTML of editable, once extracted' );
+			assert.isInnerHtmlMatching( htmlGet, docFragment.getHtml(), compareInnerHtmlOptions, 'HTML which has been extracted' );
+			assert.isInnerHtmlMatching( htmlWithSelection, getWithHtml( editable, range ), compareInnerHtmlOptions, 'HTML of editable, once extracted' );
 		};
 	}
 
 	addTests( {
 		'no block': [
 /* 1 */		[ '{a}', 																'a',															'[]@' ],
-/* 2 */		[ '{}a', 																'',																'[]a' ],
+/* 2 */		[ '{}a', 																'',																'[]a' ]
 		],
 /* 1 */	'block': [
 /* 2 */		[ '<p>{a}</p>', 														'a',															'<p>[]@</p>' ],
 /* 3 */		[ '<p>a{b}</p>', 														'b',															'<p>a[]</p>' ],
 /* 4 */		[ '<p>{a}b</p>', 														'a',															'<p>[]b</p>' ],
-/* 5 */		[ '<p>a{b}c</p>', 														'b',															'<p>a[]c</p>' ],
+/* 5 */		[ '<p>a{b}c</p>', 														'b',															'<p>a[]c</p>' ]
 		],
 		'cross-block': [
-/* 1 */		[ '<p>a{</p><p>}b</p>', 												'<br data-cke-eol="1">',										'<p>a[]b</p>' ],
-/* 2 */		[ '<p>a{@</p><p>}b</p>', 												'<br data-cke-eol="1">',										'<p>a[]b</p>' ],
+/* 1 */		[ '<p>a{</p><p>}b</p>', 												'<br data-cke-eol="1" />',										'<p>a[]b</p>' ],
+/* 2 */		[ '<p>a{@</p><p>}b</p>', 												'<br data-cke-eol="1" />',										'<p>a[]b</p>' ],
 /* 3 */		[ '<p>{a</p><p>b}</p>', 												'<p>a</p><p>b</p>',												'<p>[]@</p>' ],
 /* 4 */		[ '<h1>{a</h1><p>b}</p>', 												'<h1>a</h1><p>b</p>',											'<h1>[]@</h1>' ],
 /* 5 */		[ '<p>a{b</p><p>c}d</p>',											 	'<p>b</p><p>c</p>',												'<p>a[]d</p>' ],
 /* 6 */		[ '<blockquote>a{b</blockquote><p>c}d</p>', 							'<blockquote>b</blockquote><p>c</p>',							'<blockquote>a[]d</blockquote>' ],
 /* 7 */		[ '<blockquote>a{b</blockquote><div>c</div><p>d}e</p>', 				'<blockquote>b</blockquote><div>c</div><p>d</p>',				'<blockquote>a[]e</blockquote>' ],
 /* 8 */		[ '<div>a<div>{b</div></div><div>c</div><p>d}e</p>', 					'<div><div>b</div></div><div>c</div><p>d</p>',					'<div>a<div>[]e</div></div>' ], /*!*/
-/* 9 */		[ '<p>a{b</p><p>}c</p>', 												'<p>b</p><br data-cke-eol="1">',								'<p>a[]c</p>' ],
-/* 10 */	[ '<p>a{</p><p>b}c</p>', 												'<br data-cke-eol="1"><p>b</p>',								'<p>a[]c</p>' ],
-/* 11 */	[ '<p>a{b</p><p>c</p><p>}d</p>', 										'<p>b</p><p>c</p><br data-cke-eol="1">',						'<p>a[]d</p>' ],
-/* 12 */	[ '<p>ab{</p><p>c</p><p>}de</p>', 										'<br data-cke-eol="1"><p>c</p><br data-cke-eol="1">',			'<p>ab[]de</p>' ],
+/* 9 */		[ '<p>a{b</p><p>}c</p>', 												'<p>b</p><br data-cke-eol="1" />',								'<p>a[]c</p>' ],
+/* 10 */	[ '<p>a{</p><p>b}c</p>', 												'<br data-cke-eol="1" /><p>b</p>',								'<p>a[]c</p>' ],
+/* 11 */	[ '<p>a{b</p><p>c</p><p>}d</p>', 										'<p>b</p><p>c</p><br data-cke-eol="1" />',						'<p>a[]d</p>' ],
+/* 12 */	[ '<p>ab{</p><p>c</p><p>}de</p>', 										'<br data-cke-eol="1" /><p>c</p><br data-cke-eol="1" />',		'<p>ab[]de</p>' ],
 /* 13 */	[ '<h1><b>{a</b></h1><p>b}</p>', 										'<h1><b>a</b></h1><p>b</p>',									'<h1>[]@</h1>' ],
-/* 14 */	[ '<h1>{a</h1><p><b>b}</b></p>', 										'<h1>a</h1><p><b>b</b></p>',									'<h1>[]@</h1>' ],
+/* 14 */	[ '<h1>{a</h1><p><b>b}</b></p>', 										'<h1>a</h1><p><b>b</b></p>',									'<h1>[]@</h1>' ]
 		],
 		'inline': [
 /* 1 */		[ '<p>a<b>{b}</b>c</p>', 												'<b>b</b>',														'<p>a[]c</p>' ],
@@ -145,12 +157,12 @@
 /* 9 */		[ '<p>a<i><b>{b}</b></i></p>', 											'<i><b>b</b></i>',												'<p>a[]</p>' ],
 /* 10 */	[ '<p>a<i>b<b>{c}</b></i></p>', 										'<i><b>c</b></i>',												'<p>a<i>b[]</i></p>' ],
 /* 11 */	[ '<p><i><b>{a}</b></i></p>', 											'<i><b>a</b></i>',												'<p>[]@</p>' ],
-/* 12 */	[ '[<br>]',																'<br>',															'[]@' ],
-/* 13 */	[ 'a{<br><br>}b',														'<br><br>',														'a[]b' ],
+/* 12 */	[ '[<br />]',															'<br />',														'[]@' ],
+/* 13 */	[ 'a{<br /><br />}b',													'<br /><br />',													'a[]b' ],
 /* 14 */	[ '<p>[<b>a</b>]</p>',													'<b>a</b>',														'<p>[]@</p>' ],
 /* 15 */	[ '<p>a{<b>b}</b>c</p>',												'<b>b</b>',														'<p>a[]c</p>' ],
-/* 16 */	[ '<p>a{<img src="' + img_src + '">}b</p>',								'<img src="' + img_src + '">',									'<p>a[]b</p>' ],
-/* 17 */	[ '<p>a{<a href="foo"><img src="' + img_src + '"></a>}b</p>',			'<a href="foo"><img src="' + img_src + '"></a>',				'<p>a[]b</p>' ],
+/* 16 */	[ '<p>a{<img src="' + img_src + '" />}b</p>',							'<img src="' + img_src + '" />',								'<p>a[]b</p>' ],
+/* 17 */	[ '<p>a{<a href="foo"><img src="' + img_src + '" /></a>}b</p>',			'<a href="foo"><img src="' + img_src + '" /></a>',				'<p>a[]b</p>' ]
 		],
 		'cross-inline': [
 /* 1 */		[ '<p>{a<b>b}</b>c</p>', 												'a<b>b</b>',													'<p>[]c</p>' ],
@@ -160,17 +172,16 @@
 /* 5 */		[ '<p><b>{a</b><b>b}</b></p>', 											'<b>a</b><b>b</b>',												'<p>[]@</p>' ],
 /* 6 */		[ '<p><b>a{b</b><b>c}d</b></p>', 										'<b>b</b><b>c</b>',												'<p><b>a[]d</b></p>' ],
 /* 7 */		[ '<p>a<b class="a">{b</b><b class="b">c}</b>d</p>', 					'<b class="a">b</b><b class="b">c</b>',							'<p>a[]d</p>' ],
-/* 8 */		[ '<p>a<b class="a">{b</b>c<b class="b">d}</b>e</p>', 					'<b class="a">b</b>c<b class="b">d</b>',						'<p>a[]e</p>' ],
+/* 8 */		[ '<p>a<b class="a">{b</b>c<b class="b">d}</b>e</p>', 					'<b class="a">b</b>c<b class="b">d</b>',						'<p>a[]e</p>' ]
 		],
 		'bogus': [
 /* 1 */		[ '<p>{a}@</p>', 														'a',															'<p>[]@</p>' ],
 /* 2 */		[ '<p>{a@]</p>', 														'a',															'<p>[]@</p>' ],
 /* 3 */		[ '<p><b>{a}</b>@</p>', 												'<b>a</b>',														'<p>[]@</p>' ],
-/* 4 */		[ '<p><b>{a}@</b></p>', 												'<b>a</b>',														'<p>[]@</p>' ],
-/* 5 */		[ '<p>{a}<br>@</p>', 													'a',															'<p>[]<br>@</p>' ],
-/* 6 */		[ '{a<br>]@',															'a<br>',														'[]@' ],
-/* 7 */		[ '<p>{a<br>]@</p>',													'a<br>',														'<p>[]@</p>' ],
-/* 8 */		[ '<div>b<p>{a@]</p>b</div>', 											'a',															'<div>b<p>[]@</p>b</div>' ],
+/* 4 */		[ '<p>{a}<br />@</p>', 													'a',															'<p>[]<br />@</p>' ],
+/* 5 */		[ '{a<br />]@',															'a<br />',														'[]@' ],
+/* 6 */		[ '<p>{a<br />]@</p>',													'a<br />',														'<p>[]@</p>' ],
+/* 7 */		[ '<div>b<p>{a@]</p>b</div>', 											'a',															'<div>b<p>[]@</p>b</div>' ]
 		],
 		'tables': [
 /* 1 */		[ '<table><tbody><tr><td>{a}</td></tr></tbody></table>', 				'a',															'<table><tbody><tr><td>[]@</td></tr></tbody></table>' ],
@@ -203,7 +214,7 @@
 																																					'<table><tbody><tr><td>[]e</td><td>f</td></tr></tbody></table>' ],
 /* 15 */	[ '<p>[a</p><table><tbody><tr><td>b</td><td>cd</td></tr></tbody></table><p>e}f</p>',
 																					'<p>a</p><table><tbody><tr><td>b</td><td>cd</td></tr></tbody></table><p>e</p>',
-																																					'<p>[]@</p><table><tbody><tr><td>@</td><td>@</td></tr></tbody></table><p>f</p>' ],
+																																					'<p>[]@</p><table><tbody><tr><td>@</td><td>@</td></tr></tbody></table><p>f</p>' ]
 		],
 		'lists': [
 /* 1 */		[ '<ol><li>a{b}c</li></ol>', 											'b',															'<ol><li>a[]c</li></ol>' ],
@@ -216,7 +227,7 @@
 /* 8 */		[ '<ol><li>a{b<ul><li>c}d</li><li>e</li></ul></li><li>f</li></ol>', 	'<ol><li>b<ul><li>c</li></ul></li></ol>',						'<ol><li>a[]d<ul><li>e</li></ul></li><li>f</li></ol>' ],
 /* 9 */		[ '<ol><li>a</li><li>b{c</li><li>d}e</li></ol>', 						'<ol><li>c</li><li>d</li></ol>',								'<ol><li>a</li><li>b[]e</li></ol>' ],
 /* 10 */	[ '<ol><li>a{b</li><li><b>c}d</b></li></ol>', 							'<ol><li>b</li><li><b>c</b></li></ol>',							'<ol><li>a[]<b>d</b></li></ol>' ],
-/* 11 */	[ '<ol><li><b>a{b</b></li><li><b>c}d</b></li></ol>', 					'<ol><li><b>b</b></li><li><b>c</b></li></ol>',					'<ol><li><b>a[]d</b></li></ol>' ],
+/* 11 */	[ '<ol><li><b>a{b</b></li><li><b>c}d</b></li></ol>', 					'<ol><li><b>b</b></li><li><b>c</b></li></ol>',					'<ol><li><b>a[]d</b></li></ol>' ]
 		],
 		'various anchored in element': [
 /* 1 */		[ '<p>[]a</p>', 														'',																'<p>[]a</p>' ],
@@ -230,14 +241,14 @@
 /* 9 */		[ '<table><tbody><tr>[<td>a</td><td>b</td>]</tr></tbody></table>', 		'<table><tbody><tr><td>a</td><td>b</td></tr></tbody></table>',	'<p>[]@</p>' ],
 /* 10 */	[ '<p>[a@]</p>', 														'a',															'<p>[]@</p>' ],
 /* 11 */	[ '<p>[a]@</p>', 														'a',															'<p>[]@</p>' ],
-/* 12 */	[ '<p>[a]<br>@</p>', 													'a',															'<p>[]<br>@</p>' ],
-/* 13 */	[ '<p>[a<br>]@</p>', 													'a<br>',														'<p>[]@</p>' ],
-/* 14 */	[ '[<hr>]', 															'<hr>',															'[]@' ],
-/* 15 */	[ '[<img src="' + img_src + '">]', 										'<img src="' + img_src + '">',									'[]@' ],
-/* 16 */	[ '<p>[<img src="' + img_src + '">]</p>', 								'<img src="' + img_src + '">',									'<p>[]@</p>' ],
-/* 17 */	[ '<br>[<br>]<br>',														'<br>',															'<br>[]<br>' ],
+/* 12 */	[ '<p>[a]<br />@</p>', 													'a',															'<p>[]<br />@</p>' ],
+/* 13 */	[ '<p>[a<br />]@</p>', 													'a<br />',														'<p>[]@</p>' ],
+/* 14 */	[ '[<hr />]', 															'<hr />',														'[]@' ],
+/* 15 */	[ '[<img src="' + img_src + '" />]', 									'<img src="' + img_src + '" />',								'[]@' ],
+/* 16 */	[ '<p>[<img src="' + img_src + '" />]</p>', 							'<img src="' + img_src + '" />',								'<p>[]@</p>' ],
+/* 17 */	[ '<br />[<br />]<br />',												'<br />',														'<br />[]<br />' ],
 /* 18 */	[ '<table><thead><tr>[<th>a</th>]</tr></thead><tbody><tr><td>b</td></tr></tbody></table>',
-																					'a',															'<table><tbody><tr><td>b</td></tr></tbody></table>' ],
+																					'a',															'<table><tbody><tr><td>b</td></tr></tbody></table>' ]
 		]
 	}, 'inline' );
 
@@ -246,7 +257,7 @@
 /* 1 */		[ '{a}',																'a',															'[]@' ],
 /* 2 */		[ 'a<b>{b}</b>c',														'<b>b</b>',														'a[]c' ],
 /* 3 */		[ '{a<b>b</b>c}',														'a<b>b</b>c',													'[]@' ],
-/* 4 */		[ '[a<b>b</b>c]',														'a<b>b</b>c',													'[]@' ],
+/* 4 */		[ '[a<b>b</b>c]',														'a<b>b</b>c',													'[]@' ]
 		]
 	}, 'header' );
 
