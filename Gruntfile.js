f77ba706fd59277f4b83399c5d9a636fc8b5d740
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-09-21 11:50:59 +0200

    Covered more images with the grunt task and made JSHint accept the grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,37 @@
+/* jshint node: true, browser: false, es3: false */
+
 'use strict';
 
 module.exports = function( grunt ) {
 	grunt.initConfig( {
 		imagemin: {
-			pluginsskins: {
+			plugins: {
 				files: [ {
 					expand: true,
 					cwd: '.',
 					src: [
-						'skins/*/images/*.png',
-						'plugins/*/images/*.png',
-						'plugins/*/images/hidpi/*.png'
+						'plugins/*/images/**/*.{png,jpg,gif}'
+					]
+				} ]
+			},
+
+			skins: {
+				files: [ {
+					expand: true,
+					cwd: '.',
+					src: [
+						'skins/*/images/**/*.{png,jpg,gif}'
+					]
+				} ]
+			},
+
+			samples: {
+				files: [ {
+					expand: true,
+					cwd: '.',
+					src: [
+						'samples/**/*.{png,jpg,gif}',
+						'plugins/*/samples/**/*.{png,jpg,gif}'
 					]
 				} ]
 			}
