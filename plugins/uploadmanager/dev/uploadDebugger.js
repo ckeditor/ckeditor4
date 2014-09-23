d5bd5618b278b8a3405c4dd612d4f2db0fe08582
Piotr Jasiun <p.jasiun@cksource.com>
2014-09-23 15:58:14 +0200

    Added upload debugger.

---
+++ plugins/uploadmanager/dev/uploadDebugger.js
@@ -0,0 +1,42 @@
+/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+'use strict';
+
+( function() {
+	var xhr = XMLHttpRequest;
+
+	XMLHttpRequest.prototype.baseSend = XMLHttpRequest.prototype.send;
+
+	XMLHttpRequest.prototype.send = function( data ) {
+		var that = this,
+			baseOnProgress = this.onprogress,
+			baseOnLoad = this.onload;
+
+		this.onprogress = function( evt ) {};
+
+		this.onload = function( evt ) {
+			var total = 1000 * 10,
+				loaded = 0;
+
+			function progress() {
+				setTimeout( function() {
+					loaded += 1000;
+
+					if ( loaded < total ) {
+						evt.loaded = loaded;
+						baseOnProgress( evt );
+						progress();
+					} else {
+						baseOnLoad( evt );
+					}
+				}, 300 );
+			}
+
+			progress();
+		};
+
+		this.baseSend( data );
+	};
+} )();
