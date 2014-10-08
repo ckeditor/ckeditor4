2793cfe472417e016c22813e678bc22cdf99e18f
Piotr Jasiun <p.jasiun@cksource.com>
2014-10-08 10:08:28 +0200

    Added gif support.

--- plugins/uploadimage/plugin.js
+++ plugins/uploadimage/plugin.js
@@ -12,7 +12,7 @@
 				uploadUrl = filetools.getUploadUrl( editor.config, 'image' );
 
 			filetools.addUploadWidget( editor, 'uploadimage', {
-				supportedExtentions: 'jpg,jpeg,png',
+				supportedExtentions: 'jpg,jpeg,png,gif',
 
 				uploadUrl: uploadUrl,
 
