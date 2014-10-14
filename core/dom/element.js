9397dd414df982e2e7922ecf21714598af5504d7
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-10-14 13:13:50 +0200

    API docs: Minor improvement.

--- core/dom/element.js
+++ core/dom/element.js
@@ -138,6 +138,8 @@ CKEDITOR.dom.element.clearMarkers = function( database, element, removeFromDatab
 		 *		element.addClass( 'classB' ); // <div class="classA classB">
 		 *		element.addClass( 'classA' ); // <div class="classA classB">
 		 *
+		 * **Note:** Since CKEditor 4.5 this method cannot be used with multiple classes (`'classA classB'`).
+		 *
 		 * @chainable
 		 * @method addClass
 		 * @param {String} className The name of the class to be added.
