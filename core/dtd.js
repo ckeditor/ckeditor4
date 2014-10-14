d096343ebe42be3be66464a1f70c535ed517ec98
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-10-14 15:46:11 +0200

    Added dtd.main.

--- core/dtd.js
+++ core/dtd.js
@@ -81,7 +81,7 @@ CKEDITOR.dtd = ( function() {
 		FO = {
 			address: 1, article: 1, aside: 1, blockquote: 1, details: 1, div: 1, dl: 1, fieldset: 1,
 			figure: 1, footer: 1, form: 1, h1: 1, h2: 1, h3: 1, h4: 1, h5: 1, h6: 1, header: 1, hgroup: 1,
-			hr: 1, menu: 1, nav: 1, ol: 1, p: 1, pre: 1, section: 1, table: 1, ul: 1
+			hr: 1, main: 1, menu: 1, nav: 1, ol: 1, p: 1, pre: 1, section: 1, table: 1, ul: 1
 		},
 		// Metadata elements.
 		M = { command: 1, link: 1, meta: 1, noscript: 1, script: 1, style: 1 },
@@ -160,6 +160,7 @@ CKEDITOR.dtd = ( function() {
 		legend: P,
 		li: F,
 		link: E,
+		main: F,
 		map: F,
 		mark: P, // Treat as normal inline element (not a transparent one).
 		menu: X( { li: 1 }, F ),
@@ -245,7 +246,7 @@ CKEDITOR.dtd = ( function() {
 		 */
 		$blockLimit: {
 			article: 1, aside: 1, audio: 1, body: 1, caption: 1, details: 1, dir: 1, div: 1, dl: 1,
-			fieldset: 1, figcaption: 1, figure: 1, footer: 1, form: 1, header: 1, hgroup: 1, menu: 1, nav: 1,
+			fieldset: 1, figcaption: 1, figure: 1, footer: 1, form: 1, header: 1, hgroup: 1, main: 1, menu: 1, nav: 1,
 			ol: 1, section: 1, table: 1, td: 1, th: 1, tr: 1, ul: 1, video: 1
 		},
 
@@ -260,7 +261,7 @@ CKEDITOR.dtd = ( function() {
 		$editable: {
 			address: 1, article: 1, aside: 1, blockquote: 1, body: 1, details: 1, div: 1, fieldset: 1,
 			figcaption: 1, footer: 1, form: 1, h1: 1, h2: 1, h3: 1, h4: 1, h5: 1, h6: 1, header: 1, hgroup: 1,
-			nav: 1, p: 1, pre: 1, section: 1
+			main: 1, nav: 1, p: 1, pre: 1, section: 1
 		},
 
 		/**
