63e0282fc725f25b8fd5626659fff2dcdc280db3
Piotrek Koszuliński <pkoszulinski@gmail.com>
2014-11-03 15:37:10 +0100

    Tests: Added tests for applying one font style over another.

--- tests/plugins/font/font.js
+++ tests/plugins/font/font.js
@@ -6,11 +6,13 @@
 	bender.editor = true;
 
 	var htmlMatchingOpts = {
-		fixStyles: true
-	};
+			fixStyles: true
+		},
+		ffArial = 'font-family:arial,helvetica,sans-serif',
+		ffCS = 'font-family:comic sans ms,cursive';
 
 	bender.test( {
-		'test apply font size (collapsed cursor)': function() {
+		'test apply font size (collapsed selection)': function() {
 			var bot = this.editorBot,
 				editor = this.editor;
 
@@ -34,7 +36,7 @@
 			} );
 		},
 
-		'test apply font size (text range)': function() {
+		'test apply font size (text selection)': function() {
 			var bot = this.editorBot;
 
 			bender.tools.selection.setWithHtml( bot.editor, '<p>{foo}</p>' );
@@ -42,6 +44,107 @@
 				combo.onClick( 48 );
 				assert.isInnerHtmlMatching( '<p><span style="font-size:48px">foo</span>@</p>',
 					bot.editor.editable().getHtml(), htmlMatchingOpts );
+			} );
+		},
+
+		'test apply font size over another font size (collapsed selection)': function() {
+			var bot = this.editorBot,
+				editor = bot.editor;
+
+			bender.tools.selection.setWithHtml( bot.editor, '<p>x<span style="font-size:48px">f{}oo</span>x</p>' );
+			bot.combo( 'FontSize', function( combo ) {
+				combo.onClick( 24 );
+
+				this.wait( function() {
+					// We lose (dunno where) the empty span on IE8, so let's insert something.
+					editor.insertText( 'bar' );
+					assert.isInnerHtmlMatching(
+						'<p>x<span style="font-size:48px">f</span><span style="font-size:24px">bar</span><span style="font-size:48px">oo</span>x@</p>',
+						editor.editable().getHtml(), htmlMatchingOpts );
+				}, 0 );
+			} );
+		},
+
+		'test apply font size over another font size (collapsed selection at the existing span boundary)': function() {
+			var bot = this.editorBot,
+				editor = bot.editor;
+
+			bender.tools.selection.setWithHtml( bot.editor, '<p>x<span style="font-size:48px">{}foo</span>x</p>' );
+			bot.combo( 'FontSize', function( combo ) {
+				combo.onClick( 24 );
+
+				this.wait( function() {
+					// We lose (dunno where) the empty span on IE8, so let's insert something.
+					editor.insertText( 'bar' );
+					assert.isInnerHtmlMatching(
+						'<p>x<span style="font-size:24px">bar</span><span style="font-size:48px">foo</span>x@</p>',
+						editor.editable().getHtml(), htmlMatchingOpts );
+				}, 0 );
+			} );
+		},
+
+		'test apply font size over another font size (text selection)': function() {
+			var bot = this.editorBot,
+				editor = bot.editor;
+
+			bender.tools.selection.setWithHtml( bot.editor, '<p>x<span style="font-size:48px">f{o}o</span>x</p>' );
+			bot.combo( 'FontSize', function( combo ) {
+				combo.onClick( 24 );
+				assert.isInnerHtmlMatching(
+					'<p>x<span style="font-size:48px">f</span><span style="font-size:24px">o</span><span style="font-size:48px">o</span>x@</p>',
+					editor.editable().getHtml(), htmlMatchingOpts );
+			} );
+		},
+
+		'test apply font size over another font size (the existing span selection)': function() {
+			var bot = this.editorBot,
+				editor = bot.editor;
+
+			bender.tools.selection.setWithHtml( bot.editor, '<p>x<span style="font-size:48px">{foo}</span>x</p>' );
+			bot.combo( 'FontSize', function( combo ) {
+				combo.onClick( 24 );
+				assert.isInnerHtmlMatching(
+					'<p>x<span style="font-size:24px">foo</span>x@</p>',
+					editor.editable().getHtml(), htmlMatchingOpts );
+			} );
+		},
+
+		'test apply font size over another font size (selection containing other span)': function() {
+			var bot = this.editorBot,
+				editor = bot.editor;
+
+			bender.tools.selection.setWithHtml( bot.editor, '<p>x{f<span style="font-size:48px">o</span>o}x</p>' );
+			bot.combo( 'FontSize', function( combo ) {
+				combo.onClick( 24 );
+				assert.isInnerHtmlMatching(
+					'<p>x<span style="font-size:24px">foo</span>x@</p>',
+					editor.editable().getHtml(), htmlMatchingOpts );
+			} );
+		},
+
+		'test apply font size over font family (check possible false positive match)': function() {
+			var bot = this.editorBot,
+				editor = bot.editor;
+
+			bender.tools.selection.setWithHtml( bot.editor, '<p>x<span style="' + ffArial + '">f{o}o</span>x</p>' );
+			bot.combo( 'FontSize', function( combo ) {
+				combo.onClick( 24 );
+				assert.isInnerHtmlMatching(
+					'<p>x<span style="' + ffArial + '">f<span style="font-size:24px">o</span>o</span>x@</p>',
+					editor.editable().getHtml(), htmlMatchingOpts );
+			} );
+		},
+
+		'test apply font family over another font family (text selection)': function() {
+			var bot = this.editorBot,
+				editor = bot.editor;
+
+			bender.tools.selection.setWithHtml( bot.editor, '<p>x<span style="' + ffArial + '">f{o}o</span>x</p>' );
+			bot.combo( 'Font', function( combo ) {
+				combo.onClick( 'Comic Sans MS' );
+				assert.isInnerHtmlMatching(
+					'<p>x<span style="' + ffArial + '">f</span><span style="' + ffCS + '">o</span><span style="' + ffArial + '">o</span>x@</p>',
+					editor.editable().getHtml(), htmlMatchingOpts );
 			} );
 		}
 	} );
