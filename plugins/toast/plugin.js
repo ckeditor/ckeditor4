3a83f1bb52b819f0af1b8912407c79fc9f7f6bb0
Piotr Jasiun <p.jasiun@cksource.com>
2014-11-05 16:47:19 +0100

    Toast.hide().

--- plugins/toast/plugin.js
+++ plugins/toast/plugin.js
@@ -37,13 +37,15 @@ function toast( editor, options ) {
 	this.type = options.type;
 	this.duration = options.duration;
 	this.progress = options.progress;
+	this.id = CKEDITOR.tools.getUniqueId();
 }
 
 toast.prototype = {
 	show: function() {
-		var container = this.editor.container,
+		var toast = this,
+			container = this.editor.container,
 			toastArea = container.findOne( '.cke_toasts_area' ),
-			progress, message, type;
+			progress, message, type, toastElement;
 
 		if ( this.progress ) {
 			progress = Math.round( this.progress * 100 );
@@ -61,14 +63,28 @@ toast.prototype = {
 			container.findOne( '.cke_contents' ).append( toastArea );
 		}
 
-		toastArea.appendHtml(
-			'<div class="cke_toast cke_toast_' + type + '">' +
+		toastElement = CKEDITOR.dom.element.createFromHtml(
+			'<div class="cke_toast cke_toast_' + type + '" id="' + this.id + '">' +
 				( progress ? '<span style="width: ' + progress + '%" class="cke_toast_progress"></span>' : '' ) +
 				'<p class="cke_toast_message">' + message + '</p>' +
 				'<a class="cke_toast_close" href="javascript:void(0)" title="Close" role="button">' +
 					'<span class="cke_label">X</span>' +
 				'</a>' +
 			'</div>' );
+
+		toastElement.findOne( '.cke_toast_close' ).on( 'click', function() {
+			toast.hide();
+		} );
+
+		toastArea.append( toastElement );
+	},
+
+	hide: function() {
+		var toastElement = this.editor.container.getDocument().getById( this.id );
+
+		if ( toastElement ) {
+			toastElement.remove();
+		}
 	}
 };
 
