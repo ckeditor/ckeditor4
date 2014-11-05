ae5ac3bb61ef89d5457901555c7cf6c129cfcbce
Piotr Jasiun <p.jasiun@cksource.com>
2014-11-05 16:08:56 +0100

    Toast plugin. Show method.

--- plugins/toast/plugin.js
+++ plugins/toast/plugin.js
@@ -5,39 +5,71 @@
 
 CKEDITOR.plugins.add( 'toast', {
 	init: function( editor ) {
+		editor.makeToast = function( message, type, durationOrProgress ) {
+			var duration, progress;
 
+			if ( type == 'progress' ) {
+				progress = durationOrProgress;
+			} else {
+				duration = durationOrProgress;
+			}
+
+			var toast = new CKEDITOR.plugins.toast( editor, {
+				message: message,
+				type: type,
+				duration: duration,
+				progress: progress
+			} );
+
+			toast.show();
+
+			return toast;
+		};
 	}
 } );
 
-CKEDITOR.plugins.toast = {
-	make: function() {
+CKEDITOR.DURATION_LONG = 5000;
+CKEDITOR.DURATION_SHORT = 2000;
 
-	}
+function toast( editor, options ) {
+	this.editor = editor;
+	this.message = options.message;
+	this.type = options.type;
+	this.duration = options.duration;
+	this.progress = options.progress;
 }
 
-CKEDITOR.DURATION_LONG = 3000;
-CKEDITOR.DURATION_SHORT = 700;
+toast.prototype = {
+	show: function() {
+		var container = this.editor.container,
+			toastArea = container.findOne( '.cke_toasts_area' ),
+			progress, message, type;
 
-var toast = CKEDITOR.plugins.toast.make( {
-	title: 'Text',
-	message: 'Text',
-	progress: '0.23',
-	type: CKEDITOR.DURATION_LONG,
-	icon: 'http://someicon.',
-	duration: 100
-} );
+		if ( this.progress ) {
+			progress = Math.round( this.progress * 100 );
+			message = this.message +  ' ' + progress + '%... ';
+			type = 'info';
+		} else {
+			progress = false,
+			message = this.message,
+			type = this.type;
+		}
 
-var warning = CKEDITOR.plugins.toast.makeWarning( {
-	title: 'Text',
-	message: 'Text',
-	progress: '0.23',
-	type: INFO,
-	icon: FILE,
-	duration: 100 } );
+		if ( !toastArea ) {
+			toastArea = new CKEDITOR.dom.element( 'div' );
+			toastArea.addClass( 'cke_toasts_area' );
+			container.findOne( '.cke_contents' ).append( toastArea );
+		}
 
-toast.update( {
-	message: 'Text 2',
-	important: true
-} );
+		toastArea.appendHtml(
+			'<div class="cke_toast cke_toast_' + type + '">' +
+				( progress ? '<span style="width: ' + progress + '%" class="cke_toast_progress"></span>' : '' ) +
+				'<p class="cke_toast_message">' + message + '</p>' +
+				'<a class="cke_toast_close" href="javascript:void(0)" title="Close" role="button">' +
+					'<span class="cke_label">X</span>' +
+				'</a>' +
+			'</div>' );
+	}
+};
 
-toast.hide();
+CKEDITOR.plugins.toast = toast;
