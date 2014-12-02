72e765755549b29ed5e5b6a55e9b66ec9f1aeebf
Rebrov Boris <zipper@medde.ru>
2014-12-02 15:51:03 +0300

    custom data support for widget command

--- plugins/widget/plugin.js
+++ plugins/widget/plugin.js
@@ -1762,7 +1762,7 @@
 	// @param {CKEDITOR.plugins.widget.definition} widgetDef
 	function addWidgetCommand( editor, widgetDef ) {
 		editor.addCommand( widgetDef.name, {
-			exec: function() {
+			exec: function( editor, commandData ) {
 				var focused = editor.widgets.focused;
 				// If a widget of the same type is focused, start editing.
 				if ( focused && focused.name == widgetDef.name )
@@ -1782,7 +1782,7 @@
 					// Append wrapper to a temporary document. This will unify the environment
 					// in which #data listeners work when creating and editing widget.
 					temp.append( wrapper );
-					instance = editor.widgets.initOn( element, widgetDef );
+					instance = editor.widgets.initOn( element, widgetDef, commandData && commandData.startupData );
 
 					// Instance could be destroyed during initialization.
 					// In this case finalize creation if some new widget
