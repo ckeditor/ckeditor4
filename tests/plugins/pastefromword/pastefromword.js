f53b845e84f4acbdac55190d11327a4b4c28ffac
Piotr Jasiun <p.jasiun@cksource.com>
2014-12-17 16:17:49 +0100

    Tests: improved tests to better cover scenarios.

--- tests/plugins/pastefromword/pastefromword.js
+++ tests/plugins/pastefromword/pastefromword.js
@@ -6,16 +6,7 @@
 
 	bender.editor = true;
 
-	var stub;
-
 	bender.test( {
-		tearDown: function() {
-			if ( stub ) {
-				stub.restore();
-				stub = null;
-			}
-		},
-
 		'test whether default filter is loaded': function() {
 			var editor = this.editor;
 
@@ -32,44 +23,6 @@
 			} );
 
 			wait();
-		},
-
-		// #10032
-		'test reset forceFromWord': function() {
-			var editor = this.editor;
-
-			stub = sinon.stub( editor, 'getClipboardData', function( options, callback ) {
-				if ( callback ) {
-					callback( { dataValue: '<p><span style="font-family:arial">foo</span></p>' } );
-				}
-			} );
-
-			editor.once( 'paste', firstPaste, null, null, 999 );
-
-			editor.execCommand( 'pastefromword' );
-
-			wait();
-
-			function firstPaste( evt ) {
-				resume( function() {
-					assert.areSame( '<p>foo</p>', evt.data.dataValue, 'First paste should be from word.' );
-
-					editor.once( 'paste', secondPaste, null, null, 999 );
-
-					editor.fire( 'paste', {
-						dataValue: '<p><span style="font-family:arial">foo</span></p>'
-					} );
-
-					wait();
-				} );
-			}
-
-			function secondPaste( evt ) {
-				resume( function() {
-					assert.areSame( '<p><span style="font-family:arial">foo</span></p>',
-						evt.data.dataValue, 'Second paste should NOT be from word.' );
-				} );
-			}
 		}
 	} );
 
---
+++ tests/plugins/pastefromword/reset-forcefromword-start-from-regular-paste.js
@@ -0,0 +1,8 @@
+/* bender-tags: editor,unit,clipboard */
+/* bender-ckeditor-plugins: pastefromword */
+/* bender-include: _helpers/resetForceFromWord.js */
+/* global testScenario */
+
+'use strict';
+
+testScenario( [ 0, 1, 0, 1, 1, 0 ], '%TEST_DIR%_assets/customFilter.js' );
