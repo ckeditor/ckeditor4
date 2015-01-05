2cf5258150875d58a26791509abc23c53037c17d
Piotr Jasiun <p.jasiun@cksource.com>
2015-01-05 16:46:17 +0100

    Tests: Check bender.editorsBots in tests.

--- tests/utils/initeditor/editorsasyncinit.js
+++ tests/utils/initeditor/editorsasyncinit.js
@@ -22,6 +22,7 @@ bender.test( {
 		assert.areSame( 2, CKEDITOR.tools.objectKeys( this.editors ).length, 'this.editors' );
 		assert.areSame( 2, CKEDITOR.tools.objectKeys( this.editorsBots ).length, 'this.editorsBots' );
 		assert.areSame( 2, CKEDITOR.tools.objectKeys( bender.editors ).length, 'bender.editors' );
+		assert.areSame( 2, CKEDITOR.tools.objectKeys( bender.editorsBots ).length, 'bender.editorsBots' );
 		assert.areSame( 'framed', bender.editors.framed.name );
 		assert.areSame( 'inline', bender.editors.inline.name );
 	}
