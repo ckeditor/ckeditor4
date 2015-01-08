373a371ebbff1a48167825b78363e3f4270ea9e3
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-01-08 13:31:24 +0100

    Slightly improved API docs for the new methods.

--- core/editable.js
+++ core/editable.js
@@ -621,6 +621,8 @@
 			},
 
 			/**
+			 * A base of the {@link #getSelectedHtml} method.
+			 *
 			 * @since 4.5
 			 * @method getSelectedHtmlFromRange
 			 * @param {CKEDITOR.dom.range} range
@@ -821,7 +823,11 @@
 			} )(),
 
 			/**
+			 * A base of the {@link #extractSelectedHtml} method.
+			 *
 			 * **Note:** The range is modified so it matches desired selection after extraction.
+			 * But the selection is not made.
+			 *
 			 * @since 4.5
 			 * @param {CKEDITOR.dom.range} range
 			 * @returns {CKEDITOR.dom.documentFragment}
@@ -1392,8 +1398,26 @@
 			} )(),
 
 			/**
+			 * Gets the selected HTML (it is returned as a {@link CKEDITOR.dom.documentFragment document fragment}
+			 * or a string). This method is designed to work as a user would expect the copy functionality to work.
+			 * For instance, if the following selection has been made:
+			 *
+			 *		<p>a<b>b{c}d</b>e</p>
+			 *
+			 * The following HTML will be returned:
+			 *
+			 *		<b>c</b>
+			 *
+			 * As you can see, the information about the bold has been preserved, even though the selection was
+			 * anchored inside the `<b>` element.
+			 *
+			 * See also:
+			 *
+			 * * the {@link #extractSelectedHtml} method,
+			 * * the {@link #getSelectedHtmlFromRange} method.
+			 *
 			 * @since 4.5
-			 * @param {Boolean} toString
+			 * @param {Boolean} [toString] If `true`, then a stringified HTML will be returned.
 			 * @returns {CKEDITOR.dom.documentFragment/String}
 			 */
 			getSelectedHtml: function( toString ) {
@@ -1403,8 +1427,17 @@
 			},
 
 			/**
+			 * Gets the selected HTML (it is returned as a {@link CKEDITOR.dom.documentFragment document fragment}
+			 * or a string) and removes the selected part of the DOM. This method is designed to work as user would
+			 * expect the cut and delete functionalities to work.
+			 *
+			 * See also:
+			 *
+			 * * the {@link #getSelectedHtml} method,
+			 * * the {@link #extractSelectedHtmlFromRange} method.
+			 *
 			 * @since 4.5
-			 * @param {Boolean} toString
+			 * @param {Boolean} [toString] If `true`, then a stringified HTML will be returned.
 			 * @returns {CKEDITOR.dom.documentFragment/String}
 			 */
 			extractSelectedHtml: function( toString ) {
