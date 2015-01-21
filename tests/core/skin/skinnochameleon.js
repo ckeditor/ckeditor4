5346133120f50e83ff3d66a73dd7d3bcba178932
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-01-21 11:24:47 +0100

    Post-merge adjustment.

--- tests/core/skin/skinnochameleon.js
+++ tests/core/skin/skinnochameleon.js
@@ -10,29 +10,22 @@
 		caughtError = e;
 	};
 
+	bender.editors = {
+		editor: {
+			startupData: '<p>foo</p>',
+			config: {
+				skin: 'skinnochameleon,%TEST_DIR%/_assets/skins/skinnochameleon/',
+				uiColor: '#333888'
+			}
+		}
+	};
+
 	bender.test( {
-		'async:init': function() {
-			var tc = this;
-
-			bender.tools.setUpEditors( {
-				editor: {
-					startupData: '<p>foo</p>',
-					config: {
-						skin: 'skinnochameleon,%TEST_DIR%/_assets/skins/skinnochameleon/',
-						uiColor: '#333888'
-					}
-				}
-			}, function( editors ) {
-				tc.editor = editors.editor;
-				setTimeout( tc.callback, 0 );
-			} );
-		},
-
 		'test skin with no chameleon functionality and custom uiColor': function() {
 			bender.error = originalErrorFunc;
 
 			assert.isNull( caughtError, 'An error is not thrown during editor initialisation' );
-			assert.areSame( 'ready', this.editor.status, 'Editor is really ready' );
+			assert.areSame( 'ready', this.editors.editor.status, 'Editor is really ready' );
 			assert.areSame( 'skinnochameleon', CKEDITOR.skin );
 		}
 	} );
