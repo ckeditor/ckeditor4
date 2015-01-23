5e729599e88c703304271f3a7ce2a27bb4a15d85
Piotr Jasiun <p.jasiun@cksource.com>
2015-01-23 17:36:53 +0100

    Changed sameData: ZWS does not appear in data.

--- tests/_benderjs/ckeditor/static/extensions.js
+++ tests/_benderjs/ckeditor/static/extensions.js
@@ -108,15 +108,14 @@
 	};
 
 	/**
-	 * Asserts that HTML data are the same. Use {@link bender.tools.compatHtml} to sort attributes, fix styles,
-	 * zero-width spaces and encode Nbsp.
+	 * Asserts that HTML data are the same. Use {@link bender.tools.compatHtml} to sort attributes, fix styles and encode Nbsp.
 	 *
 	 * @param {String} expected
 	 * @param {String} actual
 	 * @param {String} [message]
 	 */
 	bender.assert.sameData = function( expected, actual, message ) {
-		assert.areSame( expected, bender.tools.compatHtml( actual, false, true, true, true, true ), message );
+		assert.areSame( expected, bender.tools.compatHtml( actual, false, true, false, true, true ), message );
 	};
 
 	// add support test ignore
