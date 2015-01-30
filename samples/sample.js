0978ac627603859b420a6907bafcf6671323f5b6
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-01-30 12:01:14 +0100

    Basics of editor initialisation in unstable environment.

--- samples/sample.js
+++ samples/sample.js
@@ -3,5 +3,41 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
+/* exported initSample */
+
 if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 )
 	CKEDITOR.tools.enableHtml5Elements( document );
+
+var initSample = ( function() {
+	var wysiwygareaAvailable = isWysiwygareaAvailable(),
+		isBBCodeBuiltIn = !!CKEDITOR.plugins.get( 'bbcode' );
+
+	return function() {
+		var editorElement = CKEDITOR.document.getById( 'editor' );
+
+		// :(((
+		if ( isBBCodeBuiltIn ) {
+			editorElement.setHtml( 'This is [b]your[/b] [url=http://ckeditor.com]editor[/url].' );
+		}
+
+		// Depending on the wysiwygare plugin availability initialize classic or inline editor.
+		if ( wysiwygareaAvailable ) {
+			CKEDITOR.replace( 'editor' );
+		} else {
+			editorElement.setAttribute( 'contenteditable', 'true' );
+			CKEDITOR.inline( 'editor' );
+
+			// TODO we can consider displaying some info box that
+			// without wysiwygarea the classic editor may not work.
+		}
+	};
+
+	function isWysiwygareaAvailable() {
+		// If in development mode, then the wysiwygarea must be available.
+		if ( CKEDITOR.revision == '%REV%' ) {
+			return true;
+		}
+
+		return !!CKEDITOR.plugins.get( 'wysiwygarea' );
+	}
+} )();
