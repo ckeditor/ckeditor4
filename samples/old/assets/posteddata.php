8198a7a7e850bbfced34e55b41dfff708caa137e
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-01-30 14:25:14 +0100

    Moved all old samples to samples/old/ directory.

--- samples/old/assets/posteddata.php
+++ samples/old/assets/posteddata.php
@@ -9,7 +9,7 @@ For licensing, see LICENSE.md or http://ckeditor.com/license
 <head>
 	<meta charset="utf-8">
 	<title>Sample &mdash; CKEditor</title>
-	<link rel="stylesheet" href="sample_old.css">
+	<link rel="stylesheet" href="sample.css">
 </head>
 <body>
 	<h1 class="samples">
