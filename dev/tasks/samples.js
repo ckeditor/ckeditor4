454b105fafd20f5adf7db6b997f7a132310552f1
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2015-02-04 18:05:49 +0100

    Created separate grunt less tasks for basicsample and toolbarconfigurator.

--- dev/tasks/samples.js
+++ dev/tasks/samples.js
@@ -3,22 +3,59 @@
 'use strict';
 
 module.exports = function( grunt ) {
+	var cssBanner = [
+		'/*!',
+		' Copyright (c) 2003-' + new Date().getFullYear() + ', CKSource - Frederico Knabben. All rights reserved.',
+		' For licensing, see LICENSE.html or http://cksource.com/ckeditor/license',
+		' */'
+	].join( '\n' );
+
 	grunt.config.merge( {
 		less: {
 			basicsample: {
-				files: {
-					'samples/css/sample.css': 'samples/less/sample.less',
-				},
+				files: [
+					{
+						src: 'samples/less/sample.less',
+						dest: 'samples/css/sample.css'
+					}
+				],
 
 				options: {
-					banner: [
-						'/*!',
-						' Copyright (c) 2003-' + new Date().getFullYear() + ', CKSource - Frederico Knabben. All rights reserved.',
-						' For licensing, see LICENSE.html or http://cksource.com/ckeditor/license',
-						' */'
-					].join( '\n' ),
 					paths: [ 'samples/' ],
-					compress: true
+
+					sourceMap: true,
+					sourceMapFilename: 'samples/css/sample.css.map',
+					sourceMapURL: 'sample.css.map',
+					sourceMapRootpath: '/'
+				}
+			},
+
+			toolbareditor: {
+				files: [
+					{
+						src: 'samples/toolbareditor/less/toolbarmodifier.less',
+						dest: 'samples/toolbareditor/css/toolbarmodifier.css'
+					}
+				],
+
+				options: {
+					paths: [ 'samples/toolbareditor' ],
+
+					sourceMap: true,
+					sourceMapFilename: 'samples/toolbareditor/css/toolbarmodifier.css.map',
+					sourceMapURL: 'toolbarmodifier.css.map',
+					sourceMapRootpath: '/'
+				}
+			},
+
+			'samples-production': {
+				files: [
+					'<%= less.basicsample.files %>',
+					'<%= less.toolbareditor.files %>'
+				],
+				options: {
+					banner: cssBanner,
+					compress: true,
 				}
 			}
 		},
@@ -27,6 +64,14 @@ module.exports = function( grunt ) {
 			basicsample: {
 				files: '<%= less.basicsample.options.paths[ 0 ] + "/**/*.less" %>',
 				tasks: [ 'less:basicsample' ],
+				options: {
+					nospawn: true
+				}
+			},
+
+			toolbareditor: {
+				files: '<%= less.toolbareditor.options.paths[ 0 ] + "/**/*.less" %>',
+				tasks: [ 'less:toolbareditor' ],
 				options: {
 					nospawn: true
 				}
