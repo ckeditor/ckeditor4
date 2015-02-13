ca3672c5d021b8c9eef7d936e347b22cece3db69
Piotr Jasiun <p.jasiun@cksource.com>
2015-02-13 12:24:53 +0100

    Added bender.config.mathJaxLibPath to tests so it is possible to use local MJ.

--- tests/plugins/mathjax/frameWrapper.js
+++ tests/plugins/mathjax/frameWrapper.js
@@ -17,7 +17,7 @@
 				doc = iFrame.getFrameDocument(),
 				fakeEditor = {
 					config: {
-						mathJaxLib: 'http://cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML'
+						mathJaxLib: bender.config.mathJaxLibPath || 'http://cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML'
 					},
 					fire: function() {
 						//mock
--- tests/plugins/mathjax/mathjax.js
+++ tests/plugins/mathjax/mathjax.js
@@ -6,7 +6,7 @@
 
 	CKEDITOR.disableAutoInline = true;
 
-	var mathJaxLib = 'http://cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML';
+	var mathJaxLib = bender.config.mathJaxLibPath || 'http://cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML';
 
 	var editor;
 
