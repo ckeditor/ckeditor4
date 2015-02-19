ea384e4c85da7957a0fafa953d6d8ec715098282
Artur Delura <adelura@gmail.com>
2015-02-19 15:01:19 +0100

    Proper comments style in licence header.

--- dev/tasks/samples.js
+++ dev/tasks/samples.js
@@ -4,7 +4,7 @@
 
 module.exports = function( grunt ) {
 	var cssBanner = [
-		'/*!',
+		'/**',
 		' Copyright (c) 2003-' + new Date().getFullYear() + ', CKSource - Frederico Knabben. All rights reserved.',
 		' For licensing, see LICENSE.html or http://cksource.com/ckeditor/license',
 		' */'
