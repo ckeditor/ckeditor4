586f26b0a2402403777cda318fb469ec0445532b
alex.wheeler <awheeler@totalwebservices.net>
2015-03-31 12:10:37 -0700

    Updated Forms plugin to allow support of HTML5 Form Validation plugin

---
+++ plugins/html5validation/lang/en.js
@@ -0,0 +1,9 @@
+/*
+Copyright (c) 2015, Total Web Services. All rights reserved.
+*/
+CKEDITOR.plugins.setLang('html5validation', 'en', {
+	patternLabel: 'Validation Pattern',
+	requiredLabel: 'Required?',
+	validationTabTitle: 'Form Validation',
+	novalidateLabel: 'Novalidate?'
+});
