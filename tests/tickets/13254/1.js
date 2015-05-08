0c0db1c6d0e5e12e7c3acf1b89df8b9a846e822f
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-05-08 14:47:46 +0200

    Tests: Simplified tests.

--- tests/tickets/13254/1.js
+++ tests/tickets/13254/1.js
@@ -1,33 +1,30 @@
-/* bender-tags: editor,unit */
+/* bender-tags: indent,indentblock */
 /* bender-ckeditor-plugins: button,indent,indentblock,divarea */
 
-( function() {
-	'use strict';
+'use strict';
 
-	bender.test( {
-		'test indenting and outdenting block in divarea plugin container inside li': function() {
-			bender.editorBot.create( {
-				creator: 'replace',
-				name: 'editor'
-			}, function( bot ) {
-				var editor = bot.editor;
+bender.editor = {
+	creator: 'replace',
+	name: 'editor'
+};
 
-				editor.focus();
-				bender.tools.selection.setWithHtml( editor, '<p>hello{}</p>' );
+bender.test( {
+	'test indenting and outdenting block in divarea plugin container inside li': function() {
+		var editor = this.editor;
+		editor.focus();
+		bender.tools.selection.setWithHtml( editor, '<p>hello{}</p>' );
 
-				assert.areSame( 2, editor.getCommand( 'indent' ).state, 'initial indent state' );
-				assert.areSame( 0, editor.getCommand( 'outdent' ).state, 'initial outdent state' );
+		assert.areSame( 2, editor.getCommand( 'indent' ).state, 'initial indent state' );
+		assert.areSame( 0, editor.getCommand( 'outdent' ).state, 'initial outdent state' );
 
-				editor.execCommand( 'indent' );
+		editor.execCommand( 'indent' );
 
-				assert.areSame( 2, editor.getCommand( 'indent' ).state, 'indent state after indenting' );
-				assert.areSame( 2, editor.getCommand( 'outdent' ).state, 'outdent state after indenting' );
+		assert.areSame( 2, editor.getCommand( 'indent' ).state, 'indent state after indenting' );
+		assert.areSame( 2, editor.getCommand( 'outdent' ).state, 'outdent state after indenting' );
 
-				editor.execCommand( 'outdent' );
+		editor.execCommand( 'outdent' );
 
-				assert.areSame( 2, editor.getCommand( 'indent' ).state, 'indent state after outdenting' );
-				assert.areSame( 0, editor.getCommand( 'outdent' ).state, 'outdent state after outdenting' );
-			} );
-		}
-	} );
-} )();
+		assert.areSame( 2, editor.getCommand( 'indent' ).state, 'indent state after outdenting' );
+		assert.areSame( 0, editor.getCommand( 'outdent' ).state, 'outdent state after outdenting' );
+	}
+} );
