7c9a189834caaaac2c99249f533d41b5e43ee894
ssipasseuth <sam@taotesting.com>
2015-05-12 12:17:31 +0200

    hacked scriploader to stop loading js resources

--- core/scriptloader.js
+++ core/scriptloader.js
@@ -20,7 +20,9 @@ CKEDITOR.scriptLoader = ( function() {
 
 	return {
 		/**
-		 * Loads one or more external script checking if not already loaded
+		 * This method has been hacked to prevent dynamic script loading
+         * 
+         * Loads one or more external script checking if not already loaded
 		 * previously by this function.
 		 *
 		 *		CKEDITOR.scriptLoader.load( '/myscript.js' );
@@ -46,10 +48,8 @@ CKEDITOR.scriptLoader = ( function() {
 		 * URLs that have been properly loaded and the second the failed ones.
 		 * @param {Object} [scope] The scope (`this` reference) to be used for
 		 * the callback call. Defaults to {@link CKEDITOR}.
-		 * @param {Boolean} [showBusy] Changes the cursor of the document while
-		 * the script is loaded.
 		 */
-		load: function( scriptUrl, callback, scope, showBusy ) {
+		load: function( scriptUrl, callback, scope ) {
 			var isString = ( typeof scriptUrl == 'string' );
 
 			if ( isString )
@@ -58,8 +58,7 @@ CKEDITOR.scriptLoader = ( function() {
 			if ( !scope )
 				scope = CKEDITOR;
 
-			var scriptCount = scriptUrl.length,
-				completed = [],
+			var completed = [],
 				failed = [];
 
 			var doCallback = function( success ) {
@@ -71,88 +70,7 @@ CKEDITOR.scriptLoader = ( function() {
 					}
 				};
 
-			if ( scriptCount === 0 ) {
-				doCallback( true );
-				return;
-			}
-
-			var checkLoaded = function( url, success ) {
-					( success ? completed : failed ).push( url );
-
-					if ( --scriptCount <= 0 ) {
-						showBusy && CKEDITOR.document.getDocumentElement().removeStyle( 'cursor' );
-						doCallback( success );
-					}
-				};
-
-			var onLoad = function( url, success ) {
-					// Mark this script as loaded.
-					uniqueScripts[ url ] = 1;
-
-					// Get the list of callback checks waiting for this file.
-					var waitingInfo = waitingList[ url ];
-					delete waitingList[ url ];
-
-					// Check all callbacks waiting for this file.
-					for ( var i = 0; i < waitingInfo.length; i++ )
-						waitingInfo[ i ]( url, success );
-				};
-
-			var loadScript = function( url ) {
-					if ( uniqueScripts[ url ] ) {
-						checkLoaded( url, true );
-						return;
-					}
-
-					var waitingInfo = waitingList[ url ] || ( waitingList[ url ] = [] );
-					waitingInfo.push( checkLoaded );
-
-					// Load it only for the first request.
-					if ( waitingInfo.length > 1 )
-						return;
-
-					// Create the <script> element.
-					var script = new CKEDITOR.dom.element( 'script' );
-					script.setAttributes( {
-						type: 'text/javascript',
-						src: url
-					} );
-
-					if ( callback ) {
-						if ( CKEDITOR.env.ie && CKEDITOR.env.version < 11 ) {
-							// FIXME: For IE, we are not able to return false on error (like 404).
-							script.$.onreadystatechange = function() {
-								if ( script.$.readyState == 'loaded' || script.$.readyState == 'complete' ) {
-									script.$.onreadystatechange = null;
-									onLoad( url, true );
-								}
-							};
-						} else {
-							script.$.onload = function() {
-								// Some browsers, such as Safari, may call the onLoad function
-								// immediately. Which will break the loading sequence. (#3661)
-								setTimeout( function() {
-									onLoad( url, true );
-								}, 0 );
-							};
-
-							// FIXME: Opera and Safari will not fire onerror.
-							script.$.onerror = function() {
-								onLoad( url, false );
-							};
-						}
-					}
-
-					// Append it to <head>.
-					script.appendTo( CKEDITOR.document.getHead() );
-
-					CKEDITOR.fire( 'download', url ); // %REMOVE_LINE%
-				};
-
-			showBusy && CKEDITOR.document.getDocumentElement().setStyle( 'cursor', 'wait' );
-			for ( var i = 0; i < scriptCount; i++ ) {
-				loadScript( scriptUrl[ i ] );
-			}
+            doCallback( true );
 		},
 
 		/**
