2c5a256700bc73409aa324f21cc33efa7b067922
ssipasseuth <sam@taotesting.com>
2015-05-13 12:15:52 +0200

    phpdoc

--- dev/builder/taobuilder/OneFileCompiler.php
+++ dev/builder/taobuilder/OneFileCompiler.php
@@ -1,24 +1,66 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
  */
 
 /**
- * Description of one
+ * The compiler is complementary to the builder. 
+ * 
+ * It optimizes the ck package for TAO.
+ * 1. reduce the plugin files to a minimum
+ * 2. merge all required js files into a single one
+ * 3. keep the TAO skin only
  *
- * @author sam
+ * @author Sam <sam@taotesting.com>
  */
 class OneFileCompiler
 {
-
+    /**
+     * The name of the main ckeditor js file
+     */
     const CKFile = 'ckeditor.js';
-
+    
+    /**
+     * The language the xkeditor will be compiled in
+     * @var string 
+     */
     protected $lang = 'en';
+    
+    /**
+     * The directory were the initial ck minified source is located
+     * @var string 
+     */
     protected $basePath = '';
+    
+    /**
+     * The directory were the compiled and optimized ck file will be
+     * @var string 
+     */
     protected $outputPath = '';
-
+    
+    /**
+     * The constructor
+     * 
+     * @param string $basePath
+     * @param string $outputPath
+     * @param string $lang
+     * @throws Exception when the basePath does not contain the main ckeditor.js file
+     */
     public function __construct($basePath, $outputPath, $lang = 'en'){
         $this->lang = $lang;
         if(file_exists($basePath.self::CKFile)){
@@ -28,23 +70,40 @@ class OneFileCompiler
             throw new Exception('no ckeditor found in base path '.$basePath.self::CKFile);
         }
     }
-
+    
+    /**
+     * Append the content of the given file to the output ckeditor.js file
+     * 
+     * @param string $file
+     */
     protected function append($file){
         file_put_contents($this->outputPath.self::CKFile, PHP_EOL.file_get_contents($file), FILE_APPEND);
         $this->log($file);
     }
-
+    
+    /**
+     * Some logging mechanism
+     * 
+     * @param string $message
+     */
     protected function log($message){
 //        var_dump($message);
     }
-
+    
+    /**
+     * Compile the core js files into the output ckeditor.js file
+     */
     protected function compileCore(){
         $this->append($this->basePath.'lang/en.js');
         $this->append($this->basePath.'config.js');
         $this->append($this->basePath.'styles.js');
         $this->append($this->basePath.'adapters/jquery.js');
     }
-
+    
+    /**
+     * Compile the plugin js files into the output ckeditor.js file
+     * @param array $plugins the plugins to be compiled
+     */
     protected function compilePlugins($plugins = array()){
 
         foreach($plugins as $plugin){
@@ -69,7 +128,15 @@ class OneFileCompiler
             }
         }
     }
-
+    
+    /**
+     * Get the resources in the given directory recursively.
+     * The resources are classified according the three types : js, css and img
+     * 
+     * @param string $dirname
+     * @param array $exclusions the folder or files to be excluded during directory scanning
+     * @return array
+     */
     protected function getDirectoryResources($dirname, $exclusions = array()){
         $files = array(
             'js' => array(),
@@ -99,8 +166,15 @@ class OneFileCompiler
 
         return $files;
     }
-
-    protected function getPluginsResources($basePath, $plugins){
+    
+    /**
+     * Get the resources for the plugins
+     * 
+     * @param string $basePath
+     * @param array $plugins
+     * @return array
+     */
+    protected function getPluginsResources($basePath, $plugins = array()){
         $files = array();
         foreach($plugins as $plugin){
             $files = array_merge_recursive($files, $this->getDirectoryResources($basePath.'plugins/'.$plugin.'/'));
@@ -109,20 +183,33 @@ class OneFileCompiler
         $files['img'][] = $basePath.'plugins/icons_hidpi.png';
         return $files;
     }
-
+    
+    /**
+     * Get the resources for the ck core
+     * 
+     * @param string $basePath
+     * @return array
+     */
     protected function getCoreResources($basePath){
-        $files = array();
-        $files = array_merge_recursive($files, $this->getDirectoryResources($basePath.'skins/tao/', array('scss', 'css'))); //skip scss and css folders
+        $files = $this->getDirectoryResources($basePath.'skins/tao/', array('scss', 'css')); //skip scss and css folders
         $files = array_merge_recursive($files, $this->getDirectoryResources($basePath.'adapters/'));
         return $files;
     }
-
+    
+    /**
+     * Prepare the compilation
+     */
     protected function compileInit(){
         //init the compilation by copying the main ckeditor.js file into the destination path
         $this->copy($this->basePath.self::CKFile, $this->outputPath.self::CKFile);
     }
-
-    protected function compileFinish($plugins){
+    
+    /**
+     * Finish the compilation
+     * 
+     * @param array $plugins
+     */
+    protected function compileFinish($plugins = array()){
 
         //copy resource file to destination
         $resources = $this->getOriginalResources($plugins);
@@ -141,29 +228,51 @@ class OneFileCompiler
         $this->copy($this->outputPath.'skins/tao/images/icons.png', $this->outputPath.'plugins/icons.png');
         $this->copy($this->outputPath.'skins/tao/images/icons.png', $this->outputPath.'plugins/icons_hidpi.png');
     }
-
-    public function compile($plugins){
+    
+    /**
+     * Launch the compilation
+     * 
+     * @param array $plugins
+     */
+    public function compile($plugins = array()){
 
         $this->compileInit();
         $this->compileCore();
         $this->compilePlugins($plugins);
         $this->compileFinish($plugins);
     }
-
+    
+    /**
+     * Finish the compilation
+     * 
+     * @param array $plugins
+     */
     public function getOriginalResources($plugins){
         $resources = array_merge_recursive($this->getCoreResources($this->basePath), $this->getPluginsResources($this->basePath, $plugins));
         return array_map(function($file){
             return str_replace($this->basePath, '', $file);
         }, $resources);
     }
-
+    
+    /**
+     * Finish the compilation
+     * 
+     * @param array $plugins
+     */
     public function getOutputResources($plugins){
         $resources = array_merge_recursive($this->getCoreResources($this->outputPath), $this->getPluginsResources($this->outputPath, $plugins));
         return array_map(function($file){
             return str_replace($this->outputPath, '', $file);
         }, $resources);
     }
-
+    
+    /**
+     * Copy a file
+     * 
+     * @param string $source
+     * @param string $destination
+     * @return boolean
+     */
     protected function copy($source, $destination){
 
         if(!is_readable($source)){
