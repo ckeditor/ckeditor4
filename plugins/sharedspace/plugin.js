6e67ab354ae6819aace56b0b9d6cd06a5e419f76
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-05-20 13:01:34 +0200

    API docs.

--- plugins/sharedspace/plugin.js
+++ plugins/sharedspace/plugin.js
@@ -127,6 +127,7 @@
  *			top: 'someElementId'
  *		};
  *
+ *		// (Since 4.5.0)
  *		// Place the toolbar inside a DOM element passed by a reference. The
  *		// elements path will remain attached to the editor UI.
  *		var htmlElement = document.getElementById( 'someElementId' );
