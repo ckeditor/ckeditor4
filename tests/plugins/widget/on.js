3ec4b26b57f90451855fb07cefa0f91f66a28673
Artur Delura <adelura@gmail.com>
2015-06-01 12:38:17 +0200

    Tests: More tests written.

--- tests/plugins/widget/on.js
+++ tests/plugins/widget/on.js
@@ -70,6 +70,69 @@
 			widget.fire( 'action' );
 
 			assert.isTrue( cbSpy.calledOnce );
+		},
+
+		'test event fired with data': function() {
+			Repository = CKEDITOR.plugins.widget.repository;
+			Widget = CKEDITOR.plugins.widget;
+
+			var repo = new Repository( editorMock ),
+				element = mockElement(),
+				widget = new Widget( repo, 1, element, { name: 'image' }, {} ),
+				cbSpy = sinon.spy();
+
+			repo.on( 'image', 'action', cbSpy );
+			widget.fire( 'action' );
+
+			assert.areSame( widget, cbSpy.args[ 0 ][ 0 ].data.target );
+		},
+
+		'test event fired for element added to repo after callback': function() {
+			Repository = CKEDITOR.plugins.widget.repository;
+			Widget = CKEDITOR.plugins.widget;
+
+			var repo = new Repository( editorMock ),
+				element = mockElement(),
+				cbSpy = sinon.spy();
+
+			repo.on( 'image', 'action', cbSpy );
+
+			var widget = new Widget( repo, 1, element, { name: 'image' }, {} );
+			widget.fire( 'action' );
+
+			assert.isTrue( cbSpy.calledOnce );
+		},
+
+		'test event not fired for different widget type': function() {
+			Repository = CKEDITOR.plugins.widget.repository;
+			Widget = CKEDITOR.plugins.widget;
+
+			var repo = new Repository( editorMock ),
+				element = mockElement(),
+				cbSpy = sinon.spy();
+
+			repo.on( 'image', 'action', cbSpy );
+
+			var widget = new Widget( repo, 1, element, { name: 'notimage' }, {} );
+			widget.fire( 'action' );
+
+			assert.isTrue( cbSpy.notCalled );
+		},
+
+		'test event not fired for different widget type 2': function() {
+			Repository = CKEDITOR.plugins.widget.repository;
+			Widget = CKEDITOR.plugins.widget;
+
+			var repo = new Repository( editorMock ),
+				element = mockElement(),
+				cbSpy = sinon.spy();
+
+			repo.on( 'notimage', 'action', cbSpy );
+
+			var widget = new Widget( repo, 1, element, { name: 'image' }, {} );
+			widget.fire( 'action' );
+
+			assert.isTrue( cbSpy.notCalled );
 		}
 	} );
 }() );
