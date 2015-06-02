00ed21907192c00ed84d782ac0ba190fb9ac4db8
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2015-06-02 12:11:25 +0200

    Removed -moz and -o prefixes from transform.

--- samples/js/sf.js
+++ samples/js/sf.js
@@ -464,9 +464,7 @@ var SF = ( function() {
                 top: "50px",
                 width: width,
                 '-ms-transform': 'translateX(-50%)',
-                '-moz-transform': 'translateX(-50%)',
                 '-webkit-transform': 'translateX(-50%)',
-                '-o-transform': 'translateX(-50%)',
                 'transform': 'translateX(-50%)'
             })
             .stylize(getOption('modalStyles', {
