da76389621fb83acad843a34ae839181dc8f90ed
Szymon Cofalik <s.cofalik@cksource.com>
2015-06-11 15:06:34 +0200

    Extended CKEDITOR.dom.node.getParents by optional lastParent argument.

--- tests/core/dom/node.js
+++ tests/core/dom/node.js
@@ -673,6 +673,19 @@
 
 			assert.areSame( 3, node.getParents().length );
 			assert.areSame( node.getParents()[ 0 ], node.getParents( true )[ 2 ] );
+
+		},
+
+		test_getParents_guard: function() {
+			var guard = newNode( document.getElementById( 'guardParent' ) ),
+				node = newNode( document.getElementById( 'getParentsWithGuard' ) ),
+				wrongParent = newNode( document.getElementById( 'remove' ) );
+
+			assert.areSame( 3, node.getParents( false, guard ).length );
+			// guard element is not a parent of node - all nodes up to root are returned (+ body, html)
+			assert.areSame( 5, node.getParents( false, wrongParent ).length );
+			assert.areSame( node.getParents( false, guard )[ 0 ], guard );
+			assert.areSame( node.getParents( false, guard )[ 0 ], node.getParents( true, guard )[ 2 ] );
 		},
 
 		test_getCommonAncestor: function() {
