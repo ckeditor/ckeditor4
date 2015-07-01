89a3e1665f0dd9e7211a12e9d02b9564d4037a9d
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-07-01 17:34:07 +0200

    Started 4.5.1 development.

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,8 @@
 CKEditor 4 Changelog
 ====================
 
+## CKEditor 4.5.1
+
 ## CKEditor 4.5
 
 New Features:
--- dev/builder/build.sh
+++ dev/builder/build.sh
@@ -57,7 +57,7 @@ echo "Starting CKBuilder..."
 
 JAVA_ARGS=${ARGS// -t / } # Remove -t from arrgs
 
-VERSION="4.5.0 DEV"
+VERSION="4.5.1 DEV"
 REVISION=$(git rev-parse --verify --short HEAD)
 SEMVER_REGEX="^([0-9]+)\.([0-9]+)\.([0-9]+)(\-[0-9A-Za-z-]+)?(\+[0-9A-Za-z-]+)?$"
 
