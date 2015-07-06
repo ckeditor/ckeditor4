175e0538edc83df479bf32716a2a5a7998b09712
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-07-06 16:40:41 +0200

    Comentary.

--- plugins/clipboard/plugin.js
+++ plugins/clipboard/plugin.js
@@ -2210,6 +2210,8 @@
 				return;
 			}
 
+			// If we use the text type to bind the ID, then if someone tries to set the text, we must also
+			// update ID accordingly. #13468.
 			if ( clipboardIdDataType == 'Text' && type == 'Text' ) {
 				this.id = value;
 			}
