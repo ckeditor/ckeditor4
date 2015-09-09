76e68157465c0276c42a444c38679c33191359d6
Iliya Kostadinov <iliyakostadinov@users.noreply.github.com>
2015-09-09 23:02:59 +0300

    Fix for radio button onChange event in IE9+ (http://dev.ckeditor.com/ticket/12733)

--- plugins/dialogui/plugin.js
+++ plugins/dialogui/plugin.js
@@ -1290,7 +1290,7 @@ CKEDITOR.plugins.add( 'dialogui', {
 			 */
 			eventProcessors: {
 				onChange: function( dialog, func ) {
-					if ( !CKEDITOR.env.ie )
+					if ( !CKEDITOR.env.ie || ( CKEDITOR.env.version > 8 ) )
 						return commonEventProcessors.onChange.apply( this, arguments );
 					else {
 						dialog.on( 'load', function() {
