e062f430893507957886484d24bb1f6a096d5400
Tadeusz <tadeusz.piskozub@gmail.com>
2015-10-05 10:58:32 +0200

    Improve filtering, parse list structure.

--- plugins/pastefromword/filter/default.js
+++ plugins/pastefromword/filter/default.js
@@ -1,3 +1,5 @@
+
+
 /* globals CKEDITOR, CKEDITOR_MOCK */
 
 ( function( CKEDITOR ) {
@@ -15,25 +17,38 @@
 		var fragment = CKEDITOR.htmlParser.fragment.fromHtml( mswordHtml );
 
 		var filter = new CKEDITOR.htmlParser.filter( {
-			// Each element is processed through the '^' function, then
-			// any matching pattern and finally through the '$' function.
-			elements: {
-				'^': function( element ) {
-
-					normalizeStyles( element );
-
-					/*jshint -W024 */
-					element.attributes.class = ( element.attributes.class || '' ).replace( /msonormal/i, '' );
-					/*jshint +W024 */
-
-					deleteEmptyAttributes( element );
-				}
+			root: function( element ) {
+				element.filterChildren( filter );
+				createLists( element );
 			},
 			elementNames: [
 				[ new RegExp( invalidTags.join( '|' ) ), '' ] // Remove invalid tags.
 			],
+			// Each element is processed through the '^' function, then
+			// any matching pattern and finally through the '$' function.
+			elements: {
+				'^': function( element ) {
+					/*jshint -W024 */
+					if ( tools.checkIfAnyArrayItemMatches( ( element.attributes.class || '' ).split( ' ' ), /MsoListParagraph/ ) ) {
+						convertToCkeli( element );
+					}
+					/*jshint +W024 */
+				}
+			},
+			attributes: {
+				'style': function( styles, element ) {
+					// Returning false deletes the attribute.
+					return falseIfEmpty( normalizedStyles( element ) );
+				},
+				'class': function( classes ) {
+					return falseIfEmpty( classes.replace( /msonormal|msolistparagraph\w*/ig, '' ) );
+				}
+			},
 			comment: function() {
-
+				return false;
+			},
+			text: function( content ) {
+				return content.replace( /&nbsp;/g, ' ' );
 			}
 		} );
 
@@ -45,33 +60,93 @@
 		return writer.getHtml();
 	};
 
-	function normalizeStyles( element ) {
+	function falseIfEmpty( value ) {
+		if ( value === '' ) {
+			return false;
+		}
+		return value;
+	}
+
+	function normalizedStyles( element ) {
 		var styles = tools.parseCssText( element.attributes.style );
 
 		var keys = tools.objectKeys( styles );
 
 		for ( var i = keys.length - 1; i >= 0; i-- ) {
-			if ( keys[ i ].match( /^mso\-/ ) ) {
+			if ( keys[ i ].match( /^(mso\-|margin\-left|text\-indent)/ ) ) {
 				delete styles[ keys[ i ] ];
 			}
 		}
 
-		element.attributes.style = CKEDITOR.tools.writeCssText( styles );
+		return CKEDITOR.tools.writeCssText( styles );
 	}
 
-	function deleteEmptyAttributes( element ) {
-		var deleteWhenEmpty = [
-			'style',
-			'class'
-		];
+	function convertToCkeli( element ) {
+		element.name = 'cke:li';
+		element.attributes[ 'cke-list-level' ] = +element.attributes.style.match( /level(\d+)/ )[1];
+	}
 
-		for ( var i = 0; i < deleteWhenEmpty.length; i++ ) {
-			var attribute = deleteWhenEmpty[ i ];
+	function createLists( root ) {
+		var listElements = [];
 
-			if ( !element.attributes[ attribute ] ) {
-				delete element.attributes[ attribute ];
+		// Select list elements.
+		for ( var i = 0; i < root.children.length; i++ ) {
+			var element = root.children[ i ];
+
+			if ( element.name == 'cke:li' ) {
+				element.name = 'li';
+				listElements.push( element );
+			}
+		}
+
+		// Chop data into continuous lists.
+		var lists = [ [ listElements[0] ] ];
+		var lastList = lists[ 0 ];
+
+		for ( i = 1; i < listElements.length; i++ ) {
+			element = listElements[ i ];
+			var previous = listElements[ i - 1 ];
+			var level = element.attributes[ 'cke-list-level' ];
+
+			if ( element.previous !== previous ) {
+				lists.push( lastList = [] );
+			}
+
+			lastList.push( element );
+		}
+
+		for ( i = 0; i < lists.length; i++ ) {
+			var list = lists[ i ];
+			var containerStack = [ new CKEDITOR.htmlParser.element( 'ul', {} ) ];
+			var innermostContainer = containerStack[ 0 ];
+
+			innermostContainer.insertBefore( list[0] );
+
+			for ( var j = 0; j < list.length; j++ ) {
+				element = list[ j ];
+
+				level = element.attributes[ 'cke-list-level' ];
+
+				while ( level > containerStack.length ) {
+					// Create a list nested in a list element
+					var container = new CKEDITOR.htmlParser.element( 'li', {} );
+					var content = new CKEDITOR.htmlParser.element( 'ul', {} );
+					container.add( content );
+
+					innermostContainer.add( container );
+
+					containerStack.push( content );
+					innermostContainer = content;
+				}
+
+				while ( level < containerStack.length ) {
+					containerStack.pop();
+					innermostContainer = containerStack[ containerStack.length - 1 ];
+				}
+
+				element.remove();
+				innermostContainer.add( element );
 			}
 		}
 	}
-
 } )( typeof CKEDITOR_MOCK !== 'undefined' ? CKEDITOR_MOCK : CKEDITOR ); // Testability, yeah!
