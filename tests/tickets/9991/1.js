a6c034c66550d2c20b389a7079bf674dae3cf97f
Tadeusz <tadeusz.piskozub@gmail.com>
2015-10-08 18:06:49 +0200

    Tests: Revamp the tests (prototype).

--- tests/tickets/9991/1.js
+++ tests/tickets/9991/1.js
@@ -16,11 +16,17 @@
 		}
 	};
 
-	var engines = [
-		'chrome',
-		'internet_explorer',
-		'firefox'
-	];
+	function loadFixture( url, fn ) {
+		assert.isObject( CKEDITOR.ajax, 'Ajax plugin is required' );
+
+		CKEDITOR.ajax.load( url, function( data ) {
+			resume( function() {
+				fn( data );
+			} );
+		} );
+
+		wait();
+	}
 
 	var compat = bender.tools.compatHtml;
 
@@ -29,43 +35,73 @@
 			assertPasteEvent( editor, { dataValue: input },
 				function( data, msg ) {
 					assert.areSame( compat( output ).toLowerCase(), compat( data.dataValue ).toLowerCase(), msg );
-				}, 'tc1', true );
+				}, 'test case', true );
 		};
 	}
 
-	var testData = {};
+	var browsers = [
+			'chrome',
+			'firefox',
+			//'ie8',
+			//'ie9',
+			//'ie10',
+			'ie11'
+		],
+		wordVersions = [
+			'word2013'
+		],
+		tests = {
+			'Bold': true,
+			'Colors': true,
+			'Fonts': true,
+			'Italic': true,
+			'Only_paragraphs': true,
+			'Ordered_list': true,
+			'Ordered_list_multiple': true,
+			'Ordered_list_multiple_edgy': true,
+			'Paragraphs_with_headers': true,
+			'Simple_table': true,
+			'Spacing': true,
+			'Text_alignment': true,
+			'Underline': true,
+			'Unordered_list': true,
+			'Unordered_list_multiple': true
+		},
+		keys = CKEDITOR.tools.objectKeys( tests ),
+		testData = {};
 
-	var fixtureNames = [
-		'Bold.docx',
-		'Colors.docx',
-		'Fonts.docx',
-		'Italic.docx',
-		'Only_paragraphs.docx',
-		'Ordered_list.docx',
-		'Ordered_list_multiple.docx',
-		'Ordered_list_multiple_edgy.docx',
-		'Paragraphs_with_headers.docx',
-		'Simple_table.docx',
-		'Spacing.docx',
-		'Text_alignment.docx',
-		'Underline.docx',
-		'Unordered_list.docx',
-		'Unordered_list_multiple.docx'
-	];
+	for ( var i = 0; i < keys.length; i++ ) {
 
-	var engineName;
-	for ( var i = 0; i < fixtureNames.length; i++ ) {
-		for ( var j = 0; j < engines.length; j++ ) {
-			engineName = engines[ j ];
+		for ( var j = 0; j < wordVersions.length; j++ ) {
+			for ( var k = 0; k < browsers.length; k++ ) {
 
-			testData[ 'test ' + fixtureNames[ i ] + ' ' + engineName ] = ( function( fixtureName ) {
-				return function() {
-					if ( !engineName )
-						assert.ignore();
+				// Callback hell |m|,
+				testData[ 'test ' + keys[ i ] + ' ' + wordVersions[ j ] + ' ' + browsers[ k ] ] = ( function( fixtureName, wordVersion, browser ) {
+					return function() {
+						var that = this;
 
-					bender.tools.testExternalInputOutput( '_assets/' + fixtureName + '_' + engineName + '.html', testWordFilter( this.editor ) );
-				};
-			} )( fixtureNames[ i ] );
+						loadFixture( [ '_fixtures', fixtureName, wordVersion, browser ].join( '/' ) + '.html', function( input ) {
+							if ( input === null ) {
+								assert.ignore();
+							}
+
+							loadFixture( [ '_fixtures', fixtureName, 'special_cases', wordVersion + '_' + browser ].join( '/' ) + '.html' , function( output ) {
+								if ( output === null ) {
+									loadFixture( '_fixtures/' + fixtureName + '/expected.html' , function( output ) {
+										if ( output === null ) {
+											assert.ignore();
+										}
+
+										testWordFilter( that.editor )( input, output );
+									} );
+								} else {
+									testWordFilter( that.editor )( input, output );
+								}
+							} );
+						} );
+					};
+				} )( keys[ i ], wordVersions[ j ], browsers[ k ] );
+			}
 		}
 	}
 
