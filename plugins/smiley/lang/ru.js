ba9366474ccf01173aa0163dd49da59447b7b481
Andrej <anhr@mail.ru>
2015-10-14 17:31:43 +0700

    Localization of the smiley's titles
    Currently localized English and Russian. Any other languages welcome

--- plugins/smiley/lang/ru.js
+++ plugins/smiley/lang/ru.js
@@ -5,5 +5,26 @@ For licensing, see LICENSE.md or http://ckeditor.com/license
 CKEDITOR.plugins.setLang( 'smiley', 'ru', {
 	options: 'Выбор смайла',
 	title: 'Вставить смайл',
-	toolbar: 'Смайлы'
-} );
+	toolbar: 'Смайлы',
+	smiley: 'улыбка',
+	sad: 'грустно',
+	wink: 'подмигивание',
+	laugh: 'смех',
+	frown: 'хмуриться',
+	cheeky: 'дерзкий',
+	blush: 'румянец',
+	surprise: 'сюрприз',
+	indecision: 'нерешительность',
+	angry: 'гнев',
+	angel: 'ангел',
+	cool: 'круто',
+	devil: 'дьявол',
+	crying: 'не плакать',
+	enlightened: 'просвещенный',
+	no: 'нет',
+	yes: 'да',
+	heart: 'сердце',
+	broken_heart: 'разбитое сердце',
+	kiss: 'поцелуй',
+	mail: 'почта'
+});
