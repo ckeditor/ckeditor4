37b98fd31ace0e0446ee1b999b6316f9c413aaca
Andrej <anhr@mail.ru>
2015-10-14 17:12:49 +0700

    Localization of the smiley's titles.

--- plugins/smiley/lang/en.js
+++ plugins/smiley/lang/en.js
@@ -5,5 +5,26 @@ For licensing, see LICENSE.md or http://ckeditor.com/license
 CKEDITOR.plugins.setLang( 'smiley', 'en', {
 	options: 'Smiley Options',
 	title: 'Insert a Smiley',
-	toolbar: 'Smiley'
+	toolbar: 'Smiley',
+	smiley: 'smiley',
+	sad: 'sad',
+	wink: 'wink',
+	laugh: 'laugh',
+	frown: 'frown',
+	cheeky: 'cheeky',
+	blush: 'blush',
+	surprise: 'surprise',
+	indecision: 'indecision',
+	angry: 'angry',
+	angel: 'angel',
+	cool: 'cool',
+	devil: 'devil',
+	crying: 'crying',
+	enlightened: 'enlightened',
+	no: 'no',
+	yes: 'yes',
+	heart: 'heart',
+	broken_heart: 'broken heart',
+	kiss: 'kiss',
+	mail: 'mail'
 } );
