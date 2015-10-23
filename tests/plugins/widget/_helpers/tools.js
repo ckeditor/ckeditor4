ed22237b0c9ab5178ab82a85709ae46b5b0d9c89
Szymon Kupś <szymonkups@gmail.com>
2015-10-23 13:02:11 +0200

    Changed regexp in widget test tools to also match tests in widgetsintegration.js

--- tests/plugins/widget/_helpers/tools.js
+++ tests/plugins/widget/_helpers/tools.js
@@ -382,7 +382,7 @@ var widgetTestsTools = ( function() {
 			'data-cke-widget-wrapper="1" ' +
 			'tabindex="-1"',
 		widgetWrapperAttributes:
-			'class="cke_widget_wrapper cke_widget_new cke_widget_(?:inline|block) cke_widget_test(?:inline|block)?" ' +
+			'class="cke_widget_wrapper cke_widget_new cke_widget_(?:inline|block) cke_widget_test(?:1|2|inline|block)?" ' +
 			'contenteditable="false" ' +
 			'(?:data-cke-display-name="[a-z0-9]+" )?' +
 			'(?:data-cke-expando="[0-9]+" )?' +
