178e6de7ea1f5f93f660d19eab559761eb3cfe26
Marek Lewandowski <m.lewandowski@cksource.com>
2015-10-28 13:20:09 +0100

    Reverted ru.js changes made in e9fa4328 for smiley plugin.

--- plugins/smiley/lang/ru.js
+++ plugins/smiley/lang/ru.js
@@ -5,26 +5,5 @@ For licensing, see LICENSE.md or http://ckeditor.com/license
 CKEDITOR.plugins.setLang( 'smiley', 'ru', {
 	options: 'Выбор смайла',
 	title: 'Вставить смайл',
-	toolbar: 'Смайлы',
-	smiley: 'улыбка',
-	sad: 'грустно',
-	wink: 'подмигивание',
-	laugh: 'смех',
-	frown: 'хмуриться',
-	cheeky: 'дерзкий',
-	blush: 'румянец',
-	surprise: 'сюрприз',
-	indecision: 'нерешительность',
-	angry: 'гнев',
-	angel: 'ангел',
-	cool: 'круто',
-	devil: 'дьявол',
-	crying: 'не плакать',
-	enlightened: 'просвещенный',
-	no: 'нет',
-	yes: 'да',
-	heart: 'сердце',
-	broken_heart: 'разбитое сердце',
-	kiss: 'поцелуй',
-	mail: 'почта'
-});
+	toolbar: 'Смайлы'
+} );
