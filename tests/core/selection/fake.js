d7eeda520ba712c0321036f450d04927a84193cd
Szymon Kupś <szymonkups@gmail.com>
2015-11-16 15:15:02 +0100

    CKEDITOR.warn used in sinon.stub instad of console.log.

--- tests/core/selection/fake.js
+++ tests/core/selection/fake.js
@@ -273,21 +273,14 @@ bender.test( {
 			bookmarks = sel.createBookmarks2(),
 			selectRangesSpy = sinon.spy( sel, 'selectRanges' );
 
-		if ( window.console ) {
-			// Override to avoid logging the CKE's warning about selection not being fake any more,
-			// so the console stays clean when the test passes.
-			var consoleLogSpy = sinon.stub( window.console, 'log' );
-		}
+		var ckeditorWarnStub = sinon.stub( CKEDITOR, 'warn' );
 
 		bookmarks.isFake = 1;
 		sel.selectBookmarks( bookmarks );
+		ckeditorWarnStub.restore();
 
 		assert.isTrue( selectRangesSpy.calledOnce );
 		assert.isFalse( !!sel.isFake, 'isFake is reset' );
-
-		if ( consoleLogSpy ) {
-			consoleLogSpy.restore();
-		}
 	},
 
 	'Fake-selection bookmark (serializable)': function() {
