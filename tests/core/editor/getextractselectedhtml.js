886b556c20e41643b2ea5db59e31d8089185fa24
Tomasz Jakut <vepomoc@gmail.com>
2016-01-26 10:03:11 +0100

    Added unit test for partial table copying.

--- tests/core/editor/getextractselectedhtml.js
+++ tests/core/editor/getextractselectedhtml.js
@@ -128,6 +128,10 @@ bender.test( {
 
 	// #13884.
 	'test getSelectedHtml with multiple ranges': function() {
+		if ( CKEDITOR.env.ie ) {
+			assert.ignore();
+		}
+
 		var editor = this.editors.editor,
 			input = '<p>' +
 					'<table>' +
@@ -162,6 +166,48 @@ bender.test( {
 		sel.selectRanges( ranges );
 
 		assert.isInnerHtmlMatching( '<table><tbody><tr><td>11@</td><td>22</td></tr></tbody></table>', editor.getSelectedHtml( true ) );
+	},
+
+	// #13884.
+	'test getSelectedHtml with partial table selection': function() {
+		if ( !CKEDITOR.env.gecko ) {
+			assert.ignore();
+		}
+
+		var editor = this.editors.editor,
+			input = '<p>' +
+					'<table>' +
+						'<tr>' +
+							'<td>11</td>' +
+							'<td>22</td>' +
+						'</tr>' +
+						'<tr>' +
+							'<td>44</td>' +
+							'<td>55</td>' +
+						'</tr>' +
+					'</table>' +
+				'</p>',
+				sel = editor.getSelection(),
+				ranges = [],
+				tableCells,
+				curRange,
+				i;
+
+		bender.tools.selection.setWithHtml( editor, input );
+
+		// Find first cells in rows.
+		tableCells = editor.editable().find( 'tr td:first-child' );
+
+		for ( i = 0; i < tableCells.count(); i++ ) {
+			curRange = editor.createRange();
+			curRange.setStartBefore( tableCells.getItem( i ) );
+			curRange.setEndAfter( tableCells.getItem( i ) );
+			ranges.push( curRange );
+		}
+
+		sel.selectRanges( ranges );
+
+		assert.isInnerHtmlMatching( '<table><tbody><tr><td>11@</td></tr><tr><td>44</td></tr></tbody></table>', editor.getSelectedHtml( true ) );
 	},
 
 	'test extractSelectedHtml with removeEmptyBlock': function() {
