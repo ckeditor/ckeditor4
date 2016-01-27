d1aef16a41f6d3049875633773ceb4378b443d92
Tomasz Jakut <vepomoc@gmail.com>
2016-01-27 15:35:41 +0100

    Added convertElementToStyle function and initial handling for applying styles with mouse.

--- plugins/formatpainter/plugin.js
+++ plugins/formatpainter/plugin.js
@@ -6,13 +6,35 @@
 ( function() {
 	'use strict';
 
+	function convertElementToStyle( element ) {
+		return new CKEDITOR.style( { element: element.getName() } );
+	}
+
 	var commandDefinition = {
 		startDisabled: true,
-		exec: function() {
+		contextSensitive: true,
+		exec: function( editor ) {
+			var style = convertElementToStyle( editor.elementPath().lastElement );
 
+			this.setState( CKEDITOR.TRISTATE_ON );
+
+			editor.editable().once( 'click', function( evt ) {
+				var range = editor.getSelection().getRanges()[ 0 ];
+
+				if ( !range ) {
+					return;
+				}
+
+				style.apply( editor );
+			} );
 		},
-		refresh: function() {
 
+		refresh: function( editor, path ) {
+			var element = path.lastElement;
+			if (  element && element.getName() !== 'p' && element.getName() !== 'body' )
+				this.setState( CKEDITOR.TRISTATE_OFF );
+			else
+				this.setState( CKEDITOR.TRISTATE_DISABLED );
 		}
 	};
 
