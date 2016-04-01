6a684eeb1c2d0eaefb386ec65f2a8fd3f06dc96e
Marek Lewandowski <m.lewandowski@cksource.com>
2016-04-01 11:28:15 +0200

    Removed Asturian lang files, as there are not enough translations yet.

--- plugins/button/lang/ast.js
+++
@@ -1,8 +0,0 @@
-/**
- * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
- */
-
-CKEDITOR.plugins.setLang( 'button', 'ast', {
-	selectedLabel: '%1 (Seleicionáu)'
-} );
--- plugins/language/lang/ast.js
+++
@@ -1,9 +0,0 @@
-/**
- * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
- */
-
-CKEDITOR.plugins.setLang( 'language', 'ast', {
-	button: 'Afitar llingua',
-	remove: 'Quitar llingua'
-} );
