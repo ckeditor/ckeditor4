a0aa5b352ae210eae785f4ce263f257adbcedcea
kunalgarg86 <kugarg@microsoft.com>
2016-05-03 17:15:11 -0700

    Making change to remove the submit handler from form to avoid leak

--- core/editor.js
+++ core/editor.js
@@ -762,7 +762,8 @@
 					}
 
 					// Remove 'submit' events registered on form element before destroying.(#3988)
-					editor.on( 'destroy', function() {
+					editor.on('destroy', function () {
+					  form.$.submit = null;
 						form.removeListener( 'submit', onSubmit );
 					} );
 				}
