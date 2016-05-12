295511cd46321de6868e9bbcfa53e1ed7aa72e86
dpidcock <dpidcock@google.com>
2016-05-12 09:37:18 -0700

    Format test according to jscs

--- tests/plugins/htmlwriter/htmlwriter.js
+++ tests/plugins/htmlwriter/htmlwriter.js
@@ -1,7 +1,7 @@
 /* bender-tags: editor,unit */
 /* bender-ckeditor-plugins: wysiwygarea, htmlwriter */
 bender.test( {
-	'test extra line break': function () {
+	'test extra line break': function() {
 		var data = '<div>Text <strong>inline</strong> Text <p>paragraph</p></div>';
 
 		bender.editorBot.create( {
@@ -12,31 +12,31 @@ bender.test( {
 				allowedContent: true,
 
 				on: {
-					instanceReady: function ( evt ) {
-						evt.editor.dataProcessor.writer.setRules('p', {
+					instanceReady: function( evt ) {
+						evt.editor.dataProcessor.writer.setRules( 'p', {
 							indent: true,
 							breakBeforeOpen: true,
 							breakAfterOpen: true,
 							breakBeforeClose: true,
 							breakAfterClose: true
-						});
+						} );
 
-						evt.editor.dataProcessor.writer.setRules('div', {
+						evt.editor.dataProcessor.writer.setRules( 'div', {
 							indent: true,
 							breakBeforeOpen: false,
 							breakAfterOpen: true,
 							breakBeforeClose: false,
 							breakAfterClose: true
-						});
+						} );
 					}
 				}
 			}
-		}, function ( bot ) {
-			bot.setData( data, function () {
-				var afterFormat = bot.getData(false, false);
+		}, function( bot ) {
+			bot.setData( data, function() {
+				var afterFormat = bot.getData( false, false );
 
 				// Trigger getData a second time to reveal bug.
-				assert.areSame( afterFormat, bot.getData(false, false) );
+				assert.areSame( afterFormat, bot.getData( false, false ) );
 			} );
 		} );
 	}
