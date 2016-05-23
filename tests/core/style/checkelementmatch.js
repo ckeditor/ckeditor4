c819b16ac4036c28ffe0f422a07e436a80a6c6ec
k.krzton <k.krzton@cksource.com>
2016-05-23 15:52:01 +0200

    Tests: additionla test case added.

--- tests/core/style/checkelementmatch.js
+++ tests/core/style/checkelementmatch.js
@@ -13,12 +13,14 @@
 		// #14252
 		'test styles color attribute normalization': function() {
 			var styleItalic = new CKEDITOR.style( { element: 'h2', attributes: { 'style': 'font-style:italic;' } } ),
-				styleSubtitle = new CKEDITOR.style( { element: 'h3', attributes: { 'style': 'color:#aaa;font-style:italic;' } } ),
-				styleSpecialContainer = new CKEDITOR.style( { element: 'div', attributes: { 'style': 'background:#eee;border:1px solid #ccc;padding:5px 10px;' } } );
+				styleSubtitle = new CKEDITOR.style( { element: 'h3', attributes: { 'style': 'color:#a1a2a3;font-style:italic;' } } ),
+				styleSpecialContainer = new CKEDITOR.style( { element: 'div', attributes: { 'style': 'background:#eee;border:1px solid #ccc;padding:5px 10px;' } } ),
+				stylesYellow = new CKEDITOR.style( { element: 'span', styles: { color: '#ff0' } } );
 
 			assert.isTrue( styleItalic.checkElementMatch( newElement( document.getElementById( 'italictitle' ) ), true ), 'Italic Title styles match.' );
 			assert.isTrue( styleSubtitle.checkElementMatch( newElement( document.getElementById( 'subtitle' ) ), true ), 'Subtitle styles match.' );
 			assert.isTrue( styleSpecialContainer.checkElementMatch( newElement( document.getElementById( 'specialcontainer' ) ), true ), 'Special Container styles match.' );
+			assert.isTrue( stylesYellow.checkElementMatch( newElement( document.getElementById( 'yellow' ) ), true ), 'Yellow styles match.' );
 		}
 	} );
 } )();
