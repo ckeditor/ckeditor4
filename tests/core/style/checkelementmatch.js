31d3cca255610d4af5dbf6b05ae11d7500c1fd92
Tadeusz Piskozub <tadeusz.piskozub@gmail.com>
2016-05-23 10:58:27 +0200

    Tests: Reworded the tests so they match the convention.

--- tests/core/style/checkelementmatch.js
+++ tests/core/style/checkelementmatch.js
@@ -16,9 +16,9 @@
 				styleSubtitle = new CKEDITOR.style( { element: 'h3', attributes: { 'style': 'color:#aaa;font-style:italic;' } } ),
 				styleSpecialContainer = new CKEDITOR.style( { element: 'div', attributes: { 'style': 'background:#eee;border:1px solid #ccc;padding:5px 10px;' } } );
 
-			assert.isTrue( styleItalic.checkElementMatch( newElement( document.getElementById( 'italictitle' ) ), true ), 'Italic Title styles does not match.' );
-			assert.isTrue( styleSubtitle.checkElementMatch( newElement( document.getElementById( 'subtitle' ) ), true ), 'Subtitle styles does not match.' );
-			assert.isTrue( styleSpecialContainer.checkElementMatch( newElement( document.getElementById( 'specialcontainer' ) ), true ), 'Special Container styles does not match.' );
+			assert.isTrue( styleItalic.checkElementMatch( newElement( document.getElementById( 'italictitle' ) ), true ), 'Italic Title styles match.' );
+			assert.isTrue( styleSubtitle.checkElementMatch( newElement( document.getElementById( 'subtitle' ) ), true ), 'Subtitle styles match.' );
+			assert.isTrue( styleSpecialContainer.checkElementMatch( newElement( document.getElementById( 'specialcontainer' ) ), true ), 'Special Container styles match.' );
 		}
 	} );
 } )();
