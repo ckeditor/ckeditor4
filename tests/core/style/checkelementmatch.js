f2f98227fb06c5af4be8b13a165330d24a79d441
Tomasz Jakut <vepomoc@gmail.com>
2016-05-24 11:04:58 +0200

    Added test cases for uppercase hex colors.

--- tests/core/style/checkelementmatch.js
+++ tests/core/style/checkelementmatch.js
@@ -15,12 +15,16 @@
 			var styleItalic = new CKEDITOR.style( { element: 'h2', attributes: { 'style': 'font-style:italic;' } } ),
 				styleSubtitle = new CKEDITOR.style( { element: 'h3', attributes: { 'style': 'color:#a1a2a3;font-style:italic;' } } ),
 				styleSpecialContainer = new CKEDITOR.style( { element: 'div', attributes: { 'style': 'background:#eee;border:1px solid #ccc;padding:5px 10px;' } } ),
-				stylesYellow = new CKEDITOR.style( { element: 'span', styles: { color: '#ff0' } } );
+				stylesYellow = new CKEDITOR.style( { element: 'span', styles: { color: '#ff0' } } ),
+				stylesYellowUpper = new CKEDITOR.style( { element: 'span', styles: { color: '#FFFF00' } } ),
+				stylesYellowUpperShort = new CKEDITOR.style( { element: 'span', styles: { color: '#FF0' } } );
 
 			assert.isTrue( styleItalic.checkElementMatch( newElement( document.getElementById( 'italictitle' ) ), true ), 'Italic Title styles match.' );
 			assert.isTrue( styleSubtitle.checkElementMatch( newElement( document.getElementById( 'subtitle' ) ), true ), 'Subtitle styles match.' );
 			assert.isTrue( styleSpecialContainer.checkElementMatch( newElement( document.getElementById( 'specialcontainer' ) ), true ), 'Special Container styles match.' );
-			assert.isTrue( stylesYellow.checkElementMatch( newElement( document.getElementById( 'yellow' ) ), true ), 'Yellow styles match.' );
+			assert.isTrue( stylesYellow.checkElementMatch( newElement( document.getElementById( 'yellow' ) ), true ), 'Yellow styles match (short).' );
+			assert.isTrue( stylesYellowUpper.checkElementMatch( newElement( document.getElementById( 'yellow' ) ), true ), 'Yellow styles match (long, upper-case).' );
+			assert.isTrue( stylesYellowUpperShort.checkElementMatch( newElement( document.getElementById( 'yellow' ) ), true ), 'Yellow styles match (short, upper-case).' );
 		}
 	} );
 } )();
