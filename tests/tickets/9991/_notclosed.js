ce7f76ecb2220d6199d3c986619d452512007b4d
Marek Lewandowski <m.lewandowski@cksource.com>
2016-10-31 21:39:45 +0100

    Updated whatever parts of expectation make sense. Other parts of this failing test are iffy and needs further investigation.

--- tests/tickets/9991/_notclosed.js
+++ tests/tickets/9991/_notclosed.js
@@ -3,7 +3,7 @@
 /* bender-ckeditor-plugins: pastefromword,ajax,basicstyles,bidi,font,link,toolbar,colorbutton,image */
 /* bender-ckeditor-plugins: list,liststyle,sourcearea,format,justify,table,tableresize,tabletools,indent,indentblock,div,dialog */
 /* jshint ignore:end */
-/* bender-include: ../../plugins/clipboard/_helpers/pasting.js,_lib/q.js,_helpers/assertWordFilter.js,_helpers/createTestCase.js */
+/* bender-include: _lib/q.js,_helpers/promisePasteEvent.js,_lib/q.js,_helpers/assertWordFilter.js,_helpers/createTestCase.js */
 /* global createTestCase */
 
 /**
@@ -14,9 +14,7 @@
 	'use strict';
 
 	bender.editor = {
-		config: {
-			allowedContent: 'b; i; p[style]{margin};'
-		}
+		config: {}
 	};
 
 	var browsers = [
@@ -78,7 +76,7 @@
 	for ( i = 0; i < ticketKeys.length; i++ ) {
 		for ( k = 0; k < browsers.length; k++ ) {
 			if ( ticketTests[ ticketKeys[ i ] ] === true || CKEDITOR.tools.indexOf( ticketTests[ ticketKeys[ i ] ], wordVersion ) !== -1 ) {
-				testData[ [ 'test', ticketKeys[ i ], wordVersion, browsers[ k ] ].join( ' ' ) ] = createTestCase( ticketKeys[ i ], wordVersion, browsers[ k ], bender.tools.testExternalInput, true );
+				testData[ [ 'test', ticketKeys[ i ], wordVersion, browsers[ k ] ].join( ' ' ) ] = createTestCase( ticketKeys[ i ], wordVersion, browsers[ k ], bender.tools.testExternalInput, true, true );
 			}
 		}
 	}
