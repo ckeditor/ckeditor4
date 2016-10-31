340fbbd768b403b4f711fa8c0c36eb7de0233340
Marek Lewandowski <m.lewandowski@cksource.com>
2016-10-31 15:58:40 +0100

    Fixed expected value, left margin is no longer preserved for list items.

--- tests/tickets/9991/functions.js
+++ tests/tickets/9991/functions.js
@@ -192,7 +192,7 @@
 				'<p style="margin-left:.25in"><span lang="EN-GB" style="font-size:8.0pt"></span></p>' +
 				'<p style="margin-left:.25in"><span lang="EN-GB" style="font-size:8.0pt"></span></p>' +
 				'<ul style="list-style-type:circle">' +
-				'<li style="margin-left:.5in"><span style="tab-stops:list .5in"><span lang="EN-GB" style="font-size:8.0pt"></span>' +
+				'<li><span style="tab-stops:list .5in"><span lang="EN-GB" style="font-size:8.0pt"></span>' +
 				'<span lang="EN-GB" style="font-size:8.0pt">This line is size 8, TNR</span></span></li>' +
 				'<li ><span style="tab-stops:list .5in"><span lang="EN-GB" style="font-size:10.0pt">' +
 				'<span style="font-family:&quot;Georgia&quot;,serif">This one is size 10, <st1:country-region w:st="on"><st1:place w:st="on">Georgia</st1:place></st1:country-region>' +
