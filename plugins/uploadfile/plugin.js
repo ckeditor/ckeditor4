7631029b1ada7b286458c32d727c2d08c45bc20d
Anna Tomanek <a.tomanek@cksource.com>
2016-11-10 14:39:56 +0100

    Copyright headers added or updated.

--- plugins/uploadfile/plugin.js
+++ plugins/uploadfile/plugin.js
@@ -1,7 +1,8 @@
 /**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
+ 
 'use strict';
 
 ( function() {
