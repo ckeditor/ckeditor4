f133a76a00c20d1718fd2dd1b0fc278ca2613fcc
Marek Lewandowski <m.lewandowski@cksource.com>
2016-12-03 08:13:29 +0100

    Fixed a case where editable#insertElementIntoRange would hang the browser with certain list selection.

--- tests/tickets/14755/1.js
+++ tests/tickets/14755/1.js
@@ -10,20 +10,50 @@
 		}
 	};
 
+	var htmlMatchingOpts = {
+		compareSelection: true,
+		normalizeSelection: true
+	};
+
 	bender.test( {
 		'test regular case': function() {
+			var editor = this.editor;
+
 			this.editorBot.setHtmlWithSelection( '<ol><li>[aaaaaaaaaaaaaaaa</li><li>&nbsp;]</li></ol>' );
 
 			this.editorBot.dialog( 'table', function( dialog ) {
 				assert.isTrue( true );
 				dialog.getButton( 'ok' ).click();
+
+				assert.isMatching(
+					// jscs:disable maximumLineLength
+					/<ol><li><table border="1" cellpadding="1" cellspacing="1" style="width: 500px;"><tbody><tr><td><br \/><\/td><td><br \/><\/td><\/tr><tr><td><br \/><\/td><td><br \/><\/td><\/tr><tr><td><br \/><\/td><td><br \/><\/td><\/tr><\/tbody><\/table><p>\^<br \/><\/p><\/li><\/ol>/,
+					// jscs:enable maximumLineLength
+					bender.tools.html.prepareInnerHtmlForComparison( bender.tools.selection.getWithHtml( editor ), htmlMatchingOpts )
+				);
 			} );
+		},
+
+		'test simplified case': function() {
+			// Simply insert a strong.
+			var editor = this.editor,
+				element = CKEDITOR.dom.element.createFromHtml( '<strong>foo</strong>' );
+
+			this.editorBot.setHtmlWithSelection( '<ol><li>[aaa</li><li class="bbb">&nbsp;]</li></ol>' );
+
+			this.editor.editable().insertElement( element );
+
+			assert.isMatching(
+				/<ol><li><strong>foo<\/strong>\^(?:<br \/>)?<\/li><li class="bbb"><\/li><\/ol>/,
+				bender.tools.html.prepareInnerHtmlForComparison( bender.tools.selection.getWithHtml( editor ), htmlMatchingOpts )
+			);
 		},
 
 		'test bug case': function() {
 			this.editorBot.setHtmlWithSelection( '<ol><li>[aaaaaaaaaaaaaaaa</li><li class="bbbbbbbbbbbbb">&nbsp;]</li></ol>' );
 
 			this.editorBot.dialog( 'table', function( dialog ) {
+				// It simply shouldn't hang.
 				assert.isTrue( true );
 				dialog.getButton( 'ok' ).click();
 			} );
