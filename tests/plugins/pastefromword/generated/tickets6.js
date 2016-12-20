ef7705feed6fc21772f00819a035c78d6606031a
Tadeusz Piskozub <tadeusz.piskozub@gmail.com>
2016-12-20 16:36:41 +0100

    Tests: Update Edge tests.

--- tests/plugins/pastefromword/generated/tickets6.js
+++ tests/plugins/pastefromword/generated/tickets6.js
@@ -19,11 +19,21 @@
 		],
 		wordVersion = 'word2016',
 		ticketTests = {
-			'16745MixedListsAndParagraphs': [ 'word2016' ]
+			'Ordered_list': true,
+			'Ordered_list_multiple': true,
+			'Unordered_list': true,
+			'Unordered_list_multiple': true,
+			'Tickets/16745MixedListsAndParagraphs': true
 		},
 		testData = {
 			_should: {
-				ignore: {}
+				ignore: {
+					'test Ordered_list word2016 edge': !CKEDITOR.env.edge,
+					'test Ordered_list_multiple word2016 edge': !CKEDITOR.env.edge,
+					'test Unordered_list word2016 edge': !CKEDITOR.env.edge,
+					'test Unordered_list_multiple word2016 edge': !CKEDITOR.env.edge,
+					'test Tickets/16745MixedListsAndParagraphs word2016 edge': !CKEDITOR.env.edge
+				}
 			}
 		},
 		ticketKeys = CKEDITOR.tools.objectKeys( ticketTests ),
@@ -31,14 +41,14 @@
 
 	for ( i = 0; i < ticketKeys.length; i++ ) {
 		for ( k = 0; k < browsers.length; k++ ) {
-			if ( ticketTests[ ticketKeys[ i ] ] === true || CKEDITOR.tools.indexOf( ticketTests[ ticketKeys[ i ] ], wordVersion ) !== -1 ) {
+			if ( ticketTests[ ticketKeys[ i ] ] === true ) {
 				var testName = [ 'test', ticketKeys[ i ], wordVersion, browsers[ k ] ].join( ' ' );
 
 				if ( CKEDITOR.env.ie && CKEDITOR.env.version <= 11 ) {
 					testData._should.ignore[ testName ] = true;
 				}
 
-				testData[ testName ] = createTestCase( ticketKeys[ i ], wordVersion, browsers[ k ], true, true );
+				testData[ testName ] = createTestCase( ticketKeys[ i ], wordVersion, browsers[ k ], false, true );
 			}
 		}
 	}
