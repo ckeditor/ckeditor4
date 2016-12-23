058d04887931f61ef23c0eb2df5f383d372f3cd7
Marek Lewandowski <m.lewandowski@cksource.com>
2016-12-23 12:27:17 +0100

    Linked the test in test suite.

--- tests/plugins/pastefromword/generated/tickets6.js
+++ tests/plugins/pastefromword/generated/tickets6.js
@@ -23,6 +23,7 @@
 			'Ordered_list_multiple': true,
 			'Unordered_list': true,
 			'Unordered_list_multiple': true,
+			'Unordered_list_adjusted_margin': true,
 			'Tickets/16745MixedListsAndParagraphs': true
 		},
 		testData = {
@@ -32,6 +33,7 @@
 					'test Ordered_list_multiple word2016 edge': !CKEDITOR.env.edge,
 					'test Unordered_list word2016 edge': !CKEDITOR.env.edge,
 					'test Unordered_list_multiple word2016 edge': !CKEDITOR.env.edge,
+					'test Unordered_list_adjusted_margin word2016 edge': !CKEDITOR.env.edge,
 					'test Tickets/16745MixedListsAndParagraphs word2016 edge': !CKEDITOR.env.edge
 				}
 			}
