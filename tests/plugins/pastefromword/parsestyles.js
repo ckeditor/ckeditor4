6a3859f2a5213f14b8e729d5ecb1f7d8ed88f8e0
Tomasz Jakut <vepomoc@gmail.com>
2017-02-10 11:46:33 +0100

    Added unit tests.

---
+++ tests/plugins/pastefromword/parsestyles.js
@@ -0,0 +1,50 @@
+/* bender-tags: editor,unit,clipboard,pastefromword */
+/* bender-ckeditor-plugins: pastefromword */
+/* bender-include: ../copyformatting/_helpers/tools.js */
+
+( function() {
+	'use strict';
+
+
+	bender.editor = {};
+
+	function testStyles( name ) {
+		bender.tools.testInputOut( name, function( styles, parsed ) {
+			var stylesObj = JSON.parse( parsed ),
+				tested = CKEDITOR.parseStyles( styles );
+
+			objectAssert.areDeepEqual( stylesObj, tested, name );
+		} );
+	}
+
+	var tests = {
+		setUp: function() {
+			if ( !CKEDITOR.plugins.clipboard.isCustomDataTypesSupported ) {
+				assert.ignore();
+			}
+		},
+
+		'test if CKEDITOR.parseStyles parses styles correctly': function() {
+			testStyles( 'styles1' );
+			testStyles( 'styles2' );
+			testStyles( 'multiple' );
+			testStyles( 'empty' );
+			testStyles( 'font-face' );
+		},
+
+		'test parsing styles from real style element': function() {
+			var expected = {
+					'.MsoChpDefault': {
+						'font-family': 'Calibri'
+					}
+				},
+				actual = CKEDITOR.parseStyles( CKEDITOR.document.getById( 'real-style' ) );
+
+			objectAssert.areDeepEqual( expected, actual );
+		}
+	};
+
+	CKEDITOR.scriptLoader.load( CKEDITOR.getUrl( '/plugins/pastefromword/filter/default.js' ), function() {
+		bender.test( tests );
+	}, null, true );
+} )();
