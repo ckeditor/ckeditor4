b35c98edab8837bfdb5edcd0060b93ff15d469af
Marek Lewandowski <m.lewandowski@cksource.com>
2017-02-20 16:48:37 +0100

    Added tests for a list-looking set of paragraphs.

--- tests/plugins/pastefromword/generated/tickets5.js
+++ tests/plugins/pastefromword/generated/tickets5.js
@@ -25,7 +25,8 @@
 			'14867examples': [ 'word2013' ],
 			'16593regular_paste': [ 'word2013' ],
 			'16833Numbered_lists': [ 'word2013' ],
-			'16817SampleDocForDataLossBug': [ 'word2013', 'word2016' ]
+			'16817SampleDocForDataLossBug': [ 'word2013', 'word2016' ],
+			'16860Faked_list': true
 		},
 		testData = {
 			_should: {
