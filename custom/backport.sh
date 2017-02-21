fb2eaa057bd703ab1defb26e9651cef4f99011e6
Nicholas Hoyt <nhoyt@illinois.edu>
2017-02-21 15:15:27 -0600

    [custom] Add branch cleanup commands

--- custom/backport.sh
+++ custom/backport.sh
@@ -3,11 +3,6 @@
 # Once the remote name has been created and fetched, the following commands
 # allow you to backport cherry-picked subtree commits to the original repo.
 
-echo
-echo "# The following commands are used for for backporting subtree commits"
-echo "# to their original repositories. Please create and fetch the remote"
-echo "# by running remotes.sh prior to running these commands."
-
 if [ $# -eq 0 ]; then
     echo "Syntax:"
     echo "    $0 <plugin-name>"
@@ -35,6 +30,11 @@ if [ $VALID == false ]; then
     exit 1
 fi
 
+echo
+echo "# The following commands are used for for backporting subtree commits"
+echo "# to their original repositories. Please create and fetch the remote"
+echo "# by running remotes.sh prior to running these commands."
+
 ENTRIES=6
 
 echo
@@ -50,12 +50,18 @@ echo "# Then cherry-pick the commits we want to backport:"
 echo "git cherry-pick -x --strategy=subtree <commit-hash>"
 
 echo
-echo "# Finally, view the commits in the backport branch with:"
+echo "# View the commits in the backport branch with:"
 echo "git log --oneline --decorate --stat -${ENTRIES}"
 
 echo
-echo "# And push to the upstream remote-name and branch created when"
-echo "# the subtree was added:"
+echo "# Push cherry-picked commits to the upstream remote and branch"
+echo "# that were created when the subtree was added:"
 echo "git push $REMOTE HEAD:master"
+
+echo
+echo "# Finally, switch back to the development branch and delete"
+echo "# the no-longer-needed backport branch:"
+echo "git checkout a11yfirst"
+echo "git branch -d backport-${REMOTE}"
 
 echo
