a6dc3630a7588115226f22becda7d18ac894d887
Nicholas Hoyt <nhoyt@illinois.edu>
2017-02-21 14:55:26 -0600

    [custom] Initial commit

---
+++ custom/remotes.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo
+echo "# The following commands are needed for both setting up subtrees and"
+echo "# for backporting subtree commits to their original repositories."
+echo "# See backport.sh and subtrees.sh for additional commands."
+
+SUBTREES="custom block-format heading inline-style"
+
+for NAME in $SUBTREES
+do
+  echo
+  echo \# $NAME
+
+  # Create named remote for subtree's central repo
+  cmd="git remote add $NAME https://github.com/a11yfirst/${NAME}.git"
+  echo $cmd
+
+  # Fetch the remote ref into the index
+  cmd="git fetch $NAME"
+  echo $cmd
+done
+
+echo
