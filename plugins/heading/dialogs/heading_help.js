b840f766bf5aa86bb3e7347d4876572acd777925
Jon Gunderson <jongund@illinois.edu>
2017-02-25 16:36:19 -0600

    [heading] added initial implementation of a help dialog box for headings

--- plugins/heading/dialogs/heading_help.js
+++ plugins/heading/dialogs/heading_help.js
@@ -4,25 +4,25 @@
  */
 
 CKEDITOR.dialog.add( 'help', function( editor ) {
-	var lang = editor.lang.about;
+    var lang = editor.lang.about;
 
-	return {
-		title: "Heading Help",
-		minWidth: 500,
-		minHeight: 300,
-		contents: [ {
-			id: 'tab1',
-			label: '',
-			title: '',
-			expand: true,
-			padding: 0,
-			elements: [
-				{
-					type: 'html',
-					html: '<p>information about using headings</p>'
-				}
-			]
-		} ],
-		buttons: [ CKEDITOR.dialog.cancelButton ]
-	};
+    return {
+        title: "Heading Help",
+        minWidth: 500,
+        minHeight: 300,
+        contents: [ {
+            id: 'tab1',
+            label: '',
+            title: '',
+            expand: true,
+            padding: 0,
+            elements: [
+                {
+                    type: 'html',
+                    html: '<p>information about using headings</p>'
+                }
+            ]
+        } ],
+        buttons: [ CKEDITOR.dialog.cancelButton ]
+    };
 } );
