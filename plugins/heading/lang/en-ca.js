450f308f9b44e6af949ffd795c5ff0e7f2b0b660
Nicholas Hoyt <nhoyt@illinois.edu>
2017-02-25 17:12:51 -0600

    [heading] Integrate label changes after merge

--- plugins/heading/lang/en-ca.js
+++ plugins/heading/lang/en-ca.js
@@ -3,14 +3,14 @@ Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'heading', 'en-ca', {
-    label: 'Heading',
-    panelTitle: 'Heading',
-    helpLabel: 'Help',
-    level_h1: 'Level 1',
-    level_h2: 'Level 2',
-    level_h3: 'Level 3',
-    level_h4: 'Level 4',
-    level_h5: 'Level 5',
-    level_h6: 'Level 6',
-    level_p: 'Remove Format',
+  label:      'Heading',
+  panelTitle: 'Select heading level',
+  helpLabel:  'Help',
+  level_h1:   'Level 1',
+  level_h2:   'Level 2',
+  level_h3:   'Level 3',
+  level_h4:   'Level 4',
+  level_h5:   'Level 5',
+  level_h6:   'Level 6',
+  level_p:    'Remove format',
 } );
