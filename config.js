3744f7b71807df4e5dfeddbe5b85a335ea4bff93
Timur Useinudzha <timur4iku@gmail.com>
2017-03-06 20:02:39 +0300

    PAR-5342 Bold unbold doesn't work correct

--- config.js
+++ config.js
@@ -74,7 +74,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 	config.colorButton_colors = '000000,434343,666666,999999,B7B7B7,CCCCCC,D9D9D9,EFEFEF,F3F3F3,F2F3F4,980000,FF0000,FF9901,FFFF02,00FF00,00FFFF,4A86E8,0000FF,9900FF,FF00FF,E6B8AF,F4CCCB,FC35CD,FFF2CC,D9E9D3,CF30E3,C8DAF8,CEE2F3,D9D2E9,EAD1DC,DD7E6B,EA9999,F9CA9C,FFE599,B6D7A8,A2C4C9,A4C2F4,9FC4E7,B4A7D5,D5A6BD,cc4125,e06666,f6b269,ffd966,93c47d,75a5af,6d9deb,6fa8dc,8e7cc3,c27b9f,a61c00,cb1b00,e69037,f1c232,6aa84e,44818e,3b78d8,3d85c6,674fa7,a54d79,84200c,991201,b45f05,bf9000,38761d,144f5c,1255cc,0a5293,341c75,741b47,5b0f00,660800,784005,7f6000,264f13,0d333d,1c4487,073763,20124d,4c1330';
-	config.coreStyles_bold = { element: 'span', styles: { 'font-weight': 'bold' }, overrides: 'strong' };
+	config.coreStyles_bold = { element: 'span', styles: { 'font-weight': 'bold' }, overrides: 'b' };
 	config.coreStyles_italic = { element: 'span', styles: { 'font-style': 'italic' }, overrides: 'em' };
 	config.coreStyles_underline = { element: 'span', styles: { 'text-decoration': 'underline' }, overrides: 'u' };
 
