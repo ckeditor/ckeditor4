e1e5c79df02b4d8a24208b77c939c0fb7dd06fd2
Nicholas Hoyt <nhoyt@illinois.edu>
2017-03-15 11:45:06 -0500

    [blockformat] Initial commit

---
+++ plugins/blockformat/lang/en.js
@@ -0,0 +1,10 @@
+/*
+Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'blockformat', 'en', {
+  label:      'Block Format',
+  panelTitle: 'Select block format',
+  blockquoteLabel: 'Blockquote',
+  helpLabel:  'Help',
+} );
