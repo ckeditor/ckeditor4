a91f4b3c6a04af1c58e520a0adbeaeed1c282751
Nicholas Hoyt <nhoyt@illinois.edu>
2017-03-30 14:21:54 -0500

    Add conditional code to fix menubutton text label

--- plugins/a11yfirst/plugin.js
+++ plugins/a11yfirst/plugin.js
@@ -5,6 +5,7 @@
 
 CKEDITOR.plugins.add( 'a11yfirst', {
   init: function ( editor ) {
+    // Pull request: Add template and method in plugins/listblock/plugin.js
     var listSeparator = CKEDITOR.addTemplate( 'panel-list-separator',
       '<div id="{id}" role="separator" style="border-bottom: 1px solid #d1d1d1"></div>' );
 
@@ -14,8 +15,51 @@ CKEDITOR.plugins.add( 'a11yfirst', {
       this._.pendingHtml.push( listSeparator.output( { id: id } ) );
     };
 
+    // Pull request: Add method in plugins/richcombo/plugin.js
     CKEDITOR.ui.richCombo.prototype.addSeparator = function () {
       this._.list.addSeparator();
     };
+
+    // Pull request: Remove else clause from setState in plugins/button/plugin.js
+    if (editor.config.menubuttonTextLabel) {
+      // console.log( 'menubuttonTextLabel: ' + editor.config.menubuttonTextLabel );
+
+      CKEDITOR.ui.button.prototype.setState = function ( state ) {
+        if ( this._.state == state )
+          return false;
+
+        this._.state = state;
+
+        var element = CKEDITOR.document.getById( this._.id );
+
+        if ( element ) {
+          element.setState( state, 'cke_button' );
+
+          state == CKEDITOR.TRISTATE_DISABLED ?
+            element.setAttribute( 'aria-disabled', true ) :
+            element.removeAttribute( 'aria-disabled' );
+
+          if ( !this.hasArrow ) {
+            // Note: aria-pressed attribute should not be added to menuButton instances. (#11331)
+            state == CKEDITOR.TRISTATE_ON ?
+              element.setAttribute( 'aria-pressed', true ) :
+              element.removeAttribute( 'aria-pressed' );
+          }
+          // Do not update button label by appending (Selected)
+          /*
+          else {
+            var newLabel = state == CKEDITOR.TRISTATE_ON ?
+              this._.editor.lang.button.selectedLabel.replace( /%1/g, this.label ) : this.label;
+            CKEDITOR.document.getById( this._.id + '_label' ).setText( newLabel );
+          }
+          */
+
+          return true;
+        }
+        else {
+          return false;
+        }
+      };
+    }
   }
 });
