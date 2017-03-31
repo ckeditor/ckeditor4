c8d5e713977aecc9ba67eb7dad9a8c4b69c6a479
Marek Lewandowski <m.lewandowski@cksource.com>
2017-03-31 11:37:14 +0200

    Docs: more docs corrections separator. [skip ci]

--- core/editor.js
+++ core/editor.js
@@ -1325,11 +1325,12 @@
 
 		/**
 		 * Returns the keystroke that is assigned to a specified {@link CKEDITOR.command}. If no keystroke is assigned,
-		 * it returns null.
-		 * Since version 4.7.0 this function also accepts a command name instead of a command instance.
+		 * it returns `null`.
+		 *
+		 * Since version 4.7.0 this function also accepts `command` parameter as a string.
 		 *
 		 * @since 4.6.0
-		 * @param {CKEDITOR.command|String} command The {@link CKEDITOR.command} instance or a string with command name.
+		 * @param {CKEDITOR.command/String} command The {@link CKEDITOR.command} instance or a string with command name.
 		 * @returns {Number/null} The keystroke assigned to the provided command or `null` if there is no keystroke.
 		 */
 		getCommandKeystroke: function( command ) {
