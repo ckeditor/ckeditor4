317063147c14a50548914fbe26613fd69b3a57d4
Tomasz Jakut <vepomoc@gmail.com>
2017-04-06 13:05:48 +0200

    Added unit tests.

---
+++ tests/plugins/pastefromword/command.js
@@ -0,0 +1,118 @@
+/* bender-tags: editor,unit,clipboard */
+/* bender-ckeditor-plugins: link, pastefromword */
+/* bender-include: ../clipboard/_helpers/pasting.js */
+/* global assertPasteNotification */
+
+( function() {
+	'use strict';
+
+	bender.editors = {
+		classic: {
+			config: {
+				language: 'en'
+			}
+		},
+
+		inline: {
+			creator: 'inline',
+			config: {
+				language: 'en'
+			}
+		}
+	};
+
+	var cmdData = {
+			name: 'pastefromword'
+		},
+		tests = {
+			'test prevented direct access to clipboard': function( editor, bot ) {
+				bot.setData( '', function() {
+					var keystroke = CKEDITOR.tools.keystrokeToString( editor.lang.common.keyboard,
+						CKEDITOR.CTRL + 86 ),
+						expected = {
+							content: '',
+							count: 1,
+							msg: 'Your browser doesn\'t allow you to paste this way. Press ' +
+								'<kbd aria-label="' + keystroke.aria + '">' + keystroke.display + '</kbd> to paste.'
+						},
+						pasteData = {
+							dataValue: '<a href="http://ckeditor.com">Foobar</a>',
+							prevent: true
+						};
+
+					pasteData.prevent = true;
+
+					assertPasteNotification( editor, expected, cmdData, pasteData );
+				} );
+			},
+
+			'test forcing notification on paste': function( editor, bot ) {
+				bot.setData( '', function() {
+					var keystroke = CKEDITOR.tools.keystrokeToString( editor.lang.common.keyboard,
+						CKEDITOR.CTRL + 86 ),
+						expected = {
+							content: '',
+							count: 1,
+							msg: 'Your browser doesn\'t allow you to paste this way. Press ' +
+								'<kbd aria-label="' + keystroke.aria + '">' + keystroke.display + '</kbd> to paste.'
+						},
+						pasteData = {
+							dataValue: '<a href="http://ckeditor.com">Foobar</a>',
+							prevent: true
+						},
+						cmdForceData = {
+							name: 'pastefromword',
+							notification: true
+						};
+
+					pasteData.prevent = true;
+
+					assertPasteNotification( editor, expected, cmdForceData, pasteData );
+				} );
+			},
+
+			'test skipping notification on paste': function( editor, bot ) {
+				bot.setData( '', function() {
+					var pasteData = {
+							dataValue: 'foo',
+							prevent: true
+						},
+						expected = {
+							content: '',
+							count: 0
+						},
+						cmdPreventData = {
+							name: 'pastefromword',
+							notification: false
+						};
+
+					assertPasteNotification( editor, expected, cmdPreventData, pasteData );
+				} );
+			},
+
+			'test customising notification on paste': function( editor, bot ) {
+				bot.setData( '', function() {
+					var msg = 'CKEditor is the best!',
+						pasteData = {
+							dataValue: 'foo',
+							prevent: true
+						},
+						expected = {
+							content: '',
+							msg: msg,
+							count: 1
+						},
+						cmdPreventData = {
+							name: 'pastefromword',
+							notification: msg
+						};
+
+					assertPasteNotification( editor, expected, cmdPreventData, pasteData );
+				} );
+			}
+		};
+
+	tests = bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests );
+
+	bender.test( tests );
+} )();
