6bdc3dcee614c9f4f3ad63ca912272b033fd5cb6
Krzysztof Krztoń <k.krzton@cksource.com>
2017-04-11 07:43:17 +0200

    Tests: refactoring.

--- tests/plugins/pastefromword/generated/excel.js
+++ tests/plugins/pastefromword/generated/excel.js
@@ -20,11 +20,12 @@
 	bender.test( createTestSuite( {
 		browsers: [
 			'datatransfer', // chrome, safari
-			'firefox_datatransfer', // ff
+			'datatransfer_firefox', // ff
 			'ie8'
 		],
 		wordVersions: [
-			'excel2013'
+			'excel2013',
+			'excel2016'
 		],
 		tests: {
 			'Table_text_attributes/Cell_text': true,
@@ -34,13 +35,17 @@
 			_should: {
 				ignore: {
 					'test Table_text_attributes/Mixed excel2013 datatransfer': CKEDITOR.env.gecko || CKEDITOR.env.ie,
-					'test Table_text_attributes/Mixed excel2013 firefox_datatransfer': !CKEDITOR.env.gecko,
+					'test Table_text_attributes/Mixed excel2013 datatransfer_firefox': !CKEDITOR.env.gecko,
+					'test Table_text_attributes/Mixed excel2016 datatransfer': CKEDITOR.env.gecko || CKEDITOR.env.ie,
+					'test Table_text_attributes/Mixed excel2016 datatransfer_firefox': !CKEDITOR.env.gecko,
+
 					'test Table_text_attributes/Mixed excel2013 ie8': !( CKEDITOR.env.ie && CKEDITOR.env.version == 8 ),
 					'test Table_text_attributes/Cell_text excel2013 datatransfer': CKEDITOR.env.ie && CKEDITOR.env.version == 8,
 					'test Table_text_attributes/Cell_text excel2013 ie8': !( CKEDITOR.env.ie && CKEDITOR.env.version == 8 )
 				}
 			}
 		},
+		ignoreAll: CKEDITOR.env.edge,
 		customFilters: [ pfwTools.filters.style ]
 	} ) );
 } )();
