4a207ffb49e20ed4186ff565e05a1193bdc7e49b
Krzysztof Krztoń <k.krzton@cksource.com>
2017-04-13 12:11:59 +0200

    Do not check image styles if it is a root element.

--- plugins/pastefromword/filter/default.js
+++ plugins/pastefromword/filter/default.js
@@ -95,7 +95,8 @@
 						}
 					};
 
-					if ( element.parent ) {
+					// If the parent is DocumentFragment it does not have any attributes. #16912
+					if ( element.parent && element.parent.type != CKEDITOR.NODE_DOCUMENT_FRAGMENT ) {
 						var attrs = element.parent.attributes,
 							style = attrs.style || attrs.STYLE;
 						if ( style && style.match( /mso\-list:\s?Ignore/ ) ) {
