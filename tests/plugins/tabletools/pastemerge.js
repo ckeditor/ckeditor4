a4506bcc13f944fb2d6fb25a485f8925edc77d8c
Marek Lewandowski <m.lewandowski@cksource.com>
2017-04-18 16:27:52 +0200

    Tests: aligned code style.

--- tests/plugins/tabletools/pastemerge.js
+++ tests/plugins/tabletools/pastemerge.js
@@ -11,31 +11,31 @@
 	};
 
 	var tests = {
-			'test doesnt break regular paste': function( editor ) {
-				bender.tools.setHtmlWithSelection( editor, '<p>foo^bar</p>' );
-				bender.tools.emulatePaste( editor, '<p>bam</p>' );
+		'test doesnt break regular paste': function( editor ) {
+			bender.tools.setHtmlWithSelection( editor, '<p>foo^bar</p>' );
+			bender.tools.emulatePaste( editor, '<p>bam</p>' );
 
-				editor.once( 'afterPaste', function() {
-					resume( function() {
-						assert.areSame( '<p>foobambar</p>', editor.getData() );
-					} );
+			editor.once( 'afterPaste', function() {
+				resume( function() {
+					assert.areSame( '<p>foobambar</p>', editor.getData() );
 				} );
+			} );
 
-				wait();
-			},
+			wait();
+		},
 
-			'test merge row after': createPasteTestCase( 'merge-row-after', '2cells1row' ),
+		'test merge row after': createPasteTestCase( 'merge-row-after', '2cells1row' ),
 
-			'test merge row before': createPasteTestCase( 'merge-row-before', '2cells1row' ),
+		'test merge row before': createPasteTestCase( 'merge-row-before', '2cells1row' ),
 
-			'test merge multi rows after': createPasteTestCase( 'merge-rows-after', '2cells2rows' ),
+		'test merge multi rows after': createPasteTestCase( 'merge-rows-after', '2cells2rows' ),
 
-			'test merge multi rows before': createPasteTestCase( 'merge-rows-before', '2cells2rows' ),
+		'test merge multi rows before': createPasteTestCase( 'merge-rows-before', '2cells2rows' ),
 
-			'test merge multi rows after empty cell': createPasteTestCase( 'merge-rows-after-empty', '2cells2rows' ),
+		'test merge multi rows after empty cell': createPasteTestCase( 'merge-rows-after-empty', '2cells2rows' ),
 
-			'test merge multi rows before empty cell': createPasteTestCase( 'merge-rows-before-empty', '2cells2rows' )
-		};
+		'test merge multi rows before empty cell': createPasteTestCase( 'merge-rows-before-empty', '2cells2rows' )
+	};
 
 	bender.test( bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests ) );
 } )();
