b3cbe0f024d9d9c75afc7aa220ead514f2cf67ac
Marek Lewandowski <m.lewandowski@cksource.com>
2017-04-18 12:47:30 +0200

    Corrected indentation, used sinon stubs to count call count.

--- tests/plugins/tabletools/pasteflow.js
+++ tests/plugins/tabletools/pasteflow.js
@@ -12,37 +12,33 @@
 	};
 
 	var tests = {
-			'test paste flow': function( editor, bot ) {
-				bender.tools.testInputOut( 'simple-paste', function( source, expected ) {
-					var beforePasteCount = 0,
-						pasteCount = 0,
-						afterPasteCount = 0;
-					bot.setHtmlWithSelection( source );
+		'test paste flow': function( editor, bot ) {
+			bender.tools.testInputOut( 'simple-paste', function( source, expected ) {
+				var beforePasteStub = sinon.stub(),
+					pasteStub = sinon.stub(),
+					afterPasteStub = sinon.stub();
+				bot.setHtmlWithSelection( source );
 
-					editor.once( 'beforePaste', function() {
-						beforePasteCount++;
+				editor.on( 'beforePaste', beforePasteStub );
+				editor.on( 'paste', pasteStub, null, null, 0 );
+				editor.once( 'afterPaste', function() {
+					resume( function() {
+						afterPasteStub();
+
+						bender.assert.beautified.html( expected, bot.editor.getData() );
+
+						assert.areSame( 1, beforePasteStub.callCount, 'beforePaste even count' );
+						assert.areSame( 1, pasteStub.callCount, 'paste event count' );
+						assert.areSame( 1, afterPasteStub.callCount, 'afterPaste event count' );
 					} );
-					editor.once( 'paste', function() {
-						pasteCount++;
-					}, null, null, 0 );
-					editor.once( 'afterPaste', function() {
-						resume( function() {
-							afterPasteCount++;
-
-							bender.assert.beautified.html( expected, bot.editor.getData() );
-
-							assert.areSame( 1, beforePasteCount, 'beforePaste was fired' );
-							assert.areSame( 1, pasteCount, 'paste was fired' );
-							assert.areSame( 1, afterPasteCount, 'afterPaste was fired' );
-						} );
-					} );
-
-					bender.tools.emulatePaste( editor, CKEDITOR.document.getById( '2cells1row' ).getOuterHtml() );
-
-					wait();
 				} );
-			}
-		};
+
+				bender.tools.emulatePaste( editor, CKEDITOR.document.getById( '2cells1row' ).getOuterHtml() );
+
+				wait();
+			} );
+		}
+	};
 
 	bender.test( bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests ) );
 } )();
