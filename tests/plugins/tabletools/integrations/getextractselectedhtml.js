50c4dd9561bd8f2d7c27f116c1cce98b3bc73f86
Marek Lewandowski <m.lewandowski@cksource.com>
2017-05-10 17:34:08 +0200

    Tests: removed duplicated test case.

--- tests/plugins/tabletools/integrations/getextractselectedhtml.js
+++ tests/plugins/tabletools/integrations/getextractselectedhtml.js
@@ -45,33 +45,6 @@
 			} );
 		},
 
-		'test getSelectedHtml with multiple ranges (Safari/IE8 case)': function( editor ) {
-			var input = '<p>' +
-						'<table>' +
-							'<tr>' +
-								'<td>11</td>' +
-								'<td>22</td>' +
-							'</tr>' +
-							'<tr>' +
-								'<td>44</td>' +
-								'<td>55</td>' +
-							'</tr>' +
-						'</table>' +
-					'</p>',
-					sel = editor.getSelection(),
-					ranges = [];
-
-			bender.tools.selection.setWithHtml( editor, input );
-
-			// Get ranges for cells in the first row.
-			ranges = tableToolsHelpers.getRangesForCells( editor, [ 0, 1 ] );
-
-			sel.selectRanges( ranges );
-
-			assert.isInnerHtmlMatching( '<table class="cke_table-faked-selection-table"><tbody><tr><td class="cke_table-faked-selection">11@</td>' +
-				'<td class="cke_table-faked-selection">22</td></tr></tbody></table>', editor.getSelectedHtml( true ) );
-		},
-
 		// #13884.
 		'test getSelectedHtml with partial table selection': function( editor ) {
 			bender.tools.testInputOut( 'partialTableSeleciton', function( input, expected ) {
