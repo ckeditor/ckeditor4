e692ae1d6e3f8809274e03b8585cf4fa50fa7702
Mateusz Samsel <m.samsel@cksource.com>
2017-05-26 15:45:41 +0200

    Adding simple test.

---
+++ tests/core/editable/insertionpreserveid.html
@@ -0,0 +1,1 @@
+<div style="height:500px"></div>
---
+++ tests/core/editable/insertionpreserveid.js
@@ -0,0 +1,78 @@
+/* bender-tags: editor,unit,insertion */
+
+(function() {
+	'use strict';
+
+	bender.editors = {
+		paragraph: {
+			name: "classic",
+			config: {
+				allowedContent: "span p[id](*){*}",
+				enterMode: CKEDITOR.ENTER_P
+			}
+		},
+		divarea: {
+			name: "divarea",
+			config: {
+				allowedContent: "span div[id](*){*}",
+				enterMode: CKEDITOR.ENTER_DIV
+			}
+		}
+	};
+
+	bender.test({
+		'test insert inline text into inline element with id': function() {
+			var editor = this.editors.paragraph,
+				bot = this.editorBots.paragraph;
+
+			bot.setHtmlWithSelection( '<p><span class="marker" id="abc">te^st</span></p>' );
+			editor.insertText( '789' );
+			assert.areSame( '<p><span class="marker" id="abc">te789st</span></p>', bot.getData() );
+
+		},
+
+		'test insert nothing into inline element with id': function() {
+			var editor = this.editors.paragraph,
+				bot = this.editorBots.paragraph;
+
+			bot.setHtmlWithSelection( '<p style="text-align:right"><span class="marker" id="abc">te^st</span></p>' );
+			editor.insertText( '' );
+			assert.areSame( '<p style="text-align:right"><span class="marker" id="abc">test</span></p>', bot.getData() );
+
+
+		},
+
+		'test insert text into multiple inline element with ids': function() {
+			var editor = this.editors.paragraph,
+				bot = this.editorBots.paragraph;
+
+			bot.setHtmlWithSelection( '<p style="text-align:right"><span class="marker" id="abc"><span id="def"><span id="ghj">te^st</span></span></span></p>' );
+			editor.insertText( 'stte' );
+			assert.areSame( '<p style="text-align:right"><span class="marker" id="abc"><span id="def"><span id="ghj">testtest</span></span></span></p>', bot.getData() );
+
+
+		},
+
+		'test insert text into inline element with id div mode': function() {
+			var editor = this.editors.divarea,
+				bot = this.editorBots.divarea;
+
+			bot.setHtmlWithSelection( '<div><span class="marker" id="abc">te^st</span></div>' );
+			editor.insertText( '789' );
+			assert.areSame( '<div><span class="marker" id="abc">te789st</span></div>', bot.getData() );
+		},
+
+		'test insert nothing into inline element with id div mode': function() {
+			var editor = this.editors.divarea,
+				bot = this.editorBots.divarea;
+
+			bot.setHtmlWithSelection( '<div style="text-align:right"><span class="marker" id="abc">te^st</span></div>' );
+			editor.insertText( '' );
+			assert.areSame( '<div style="text-align:right"><span class="marker" id="abc">test</span></div>', bot.getData() );
+		}
+
+
+	});
+
+
+}());
