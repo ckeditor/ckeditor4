b9b39b483dcef5ebcee1728992cdecf36f1d57db
Krzysztof Krztoń <k.krzton@cksource.com>
2017-06-01 15:51:49 +0200

    Refactoring.

--- tests/core/htmldataprocessor.js
+++ tests/core/htmldataprocessor.js
@@ -1330,7 +1330,7 @@
 	// IE8 treats it as an URL and opens it which reloads the whole page.
 	// While on Edge the tests passes, the browser prompts with "open as" dialog.
 	// On Chrome Linux it prompts `Open xdg-open?` dialog, which gains focus so other tests requiring focus fails.
-	if ( !( CKEDITOR.env.ie && CKEDITOR.env.version == 8 || CKEDITOR.env.edge || CKEDITOR.env.chrome && bender.tools.env.linux ) ) {
+	if ( !( CKEDITOR.env.ie && CKEDITOR.env.version == 8 || CKEDITOR.env.edge ) && !( CKEDITOR.env.chrome && bender.tools.env.linux ) ) {
 		addXssTC( tcs, 'iframe with src=javascript 4',
 			'<p><iframe src="&#10;&#106;javascript:window.parent.%xss%;"></iframe></p>',
 			// In IE9 the new line entity (&#10;) is removed.
