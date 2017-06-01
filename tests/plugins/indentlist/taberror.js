dab07d5edc4a3d88c43a0fc712a3f8446e251b05
Mateusz Samsel <m.samsel@cksource.com>
2017-06-01 15:36:49 +0200

    Creating manual and unit tests.

---
+++ tests/plugins/indentlist/taberror.js
@@ -0,0 +1,30 @@
+/* bender-tags: editor,unit */
+/* bender-ckeditor-plugins: toolbar,indentlist */
+/* bender-ui: collapsed */
+
+(function () {
+	'use strict';
+
+	bender.editor = {
+		creator: 'inline'
+	};
+
+	bender.test({
+		'test tab in inline editor': function() {
+			var editor = this.editor;
+
+			editor.focus();
+			var sel = editor.getSelection();
+			sel.removeAllRanges();
+			editor.fire( 'key', {
+				domEvent: {
+					getKey: function() {
+						return 9;
+					}
+				},
+				keyCode: 9
+			} );
+			assert.pass();
+		}
+	});
+}());
