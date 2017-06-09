7b56d091bd4ee2bf7f5690bb562c029090ce381f
Mateusz Samsel <m.samsel@cksource.com>
2017-06-09 11:48:35 +0200

    Fixing linting and provide urls to tickets.

--- tests/core/focusmanager/lostfocus.js
+++ tests/core/focusmanager/lostfocus.js
@@ -1,29 +1,26 @@
 /* bender-tags: editor,unit */
 /* bender-ckeditor-plugins: divarea */
 
-bender.editor = {
-	extraPlugins: 'divarea',
-	allowedContent: true
-};
+bender.editor = {};
 
-bender.test({
+bender.test( {
 	'test lost focus in editor after click': function() {
-		// #17020 - test for checking if focusmanager reset ranges in divarea editor (bug introduced in #13446).
+		// Ticket: https://dev.ckeditor.com/ticket/17020
+		// Test for checking if focusmanager reset ranges in divarea editor (bug introduced in https://dev.ckeditor.com/ticket/13446).
 		// It's necessary to keep focus in browser to get proper result of test. Focus in console or other window will cause test to pass.
 		var editor = this.editor;
 		editor.focus();
 		bender.tools.setHtmlWithSelection( editor, '<p>[foo] <strong>bar</strong></p>' );
-		CKEDITOR.document.getById('focusable').focus();
-		var element = editor.editable().findOne('strong');
+		CKEDITOR.document.getById( 'focusable' ).focus();
 		var selection = editor.getSelection();
-		selection.selectElement(element);
+		selection.selectElement( editor.editable().findOne( 'strong' ) );
 		wait( function() {
 			// refresh selection
 			var sel = editor.getSelection();
-			sel.unlock();
 			sel.reset();
-			assert.isInnerHtmlMatching( '<p>foo [<strong>bar</strong>]@</p>', bender.tools.range.getWithHtml( editor.editable(), sel.getRanges()[0] ) );
-		}, 210);
+			assert.isInnerHtmlMatching( '<p>foo [<strong>bar</strong>]@</p>',
+					bender.tools.range.getWithHtml( editor.editable(), sel.getRanges()[ 0 ] ) );
+		}, 210 );
 
 	}
-});
+} );
