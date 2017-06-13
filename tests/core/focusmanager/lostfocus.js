b9adda56b7c2cf5d57b2f32770b6f31368aa75f2
Marek Lewandowski <m.lewandowski@cksource.com>
2017-06-13 13:13:01 +0200

    Tests: adjusted indentation and some api docs. [skip ci]

--- tests/core/focusmanager/lostfocus.js
+++ tests/core/focusmanager/lostfocus.js
@@ -5,9 +5,9 @@ bender.editor = {};
 
 bender.test( {
 	'test lost focus in editor after click': function() {
-		// Ticket: https://dev.ckeditor.com/ticket/17020
 		// Test for checking if focusmanager reset ranges in divarea editor (bug introduced in https://dev.ckeditor.com/ticket/13446).
-		// It's necessary to keep focus in browser to get proper result of test. Focus in console or other window will cause test to pass.
+		// It's necessary to keep focus in browser to get proper result of test. Focus in console or other window will cause test to
+		// pass (https://dev.ckeditor.com/ticket/17020).
 		var editor = this.editor;
 		editor.focus();
 		bender.tools.setHtmlWithSelection( editor, '<p>[foo] <strong>bar</strong></p>' );
@@ -15,11 +15,11 @@ bender.test( {
 		var selection = editor.getSelection();
 		selection.selectElement( editor.editable().findOne( 'strong' ) );
 		wait( function() {
-			// refresh selection
+			// Refresh selection.
 			var sel = editor.getSelection();
 			sel.reset();
 			assert.isInnerHtmlMatching( '<p>foo [<strong>bar</strong>]@</p>',
-					bender.tools.range.getWithHtml( editor.editable(), sel.getRanges()[ 0 ] ) );
+				bender.tools.range.getWithHtml( editor.editable(), sel.getRanges()[ 0 ] ) );
 		}, 210 );
 
 	}
