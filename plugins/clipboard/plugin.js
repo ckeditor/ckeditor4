80f958f9d639bea58ef0e16469617e12bddd775f
Beata <b.delura@cksource.com>
2017-06-30 13:55:36 +0200

    Changing Data URL implementation to Object URL for Chrome, Firefox and Edge.

--- plugins/clipboard/plugin.js
+++ plugins/clipboard/plugin.js
@@ -142,49 +142,30 @@
 			initPasteClipboard( editor );
 			initDragDrop( editor );
 
-			// Convert image file (if present) to base64 string for Firefox and Chrome on Mac and for Firefox on Windows.
+			// Convert image file (if present) to blob url for Firefox and Chrome.
 			// Do it as the first step as the conversion is asynchronous and should hold all further paste processing.
-			if ( CKEDITOR.env.gecko || ( !CKEDITOR.env.windows && CKEDITOR.env.chrome ) ) {
-				var supportedImageTypes = [ 'image/png', 'image/jpeg', 'image/gif' ],
-					latestId;
+			var supportedImageTypes = [ 'image/png', 'image/jpeg', 'image/gif' ],
+				latestId;
 
-				editor.on( 'paste', function( evt ) {
-					var dataObj = evt.data,
-						data = dataObj.dataValue,
-						dataTransfer = dataObj.dataTransfer;
+			editor.on( 'paste', function( evt ) {
+				var dataObj = evt.data,
+					data = dataObj.dataValue,
+					dataTransfer = dataObj.dataTransfer;
 
-					// If data empty check for image content inside data transfer. http://dev.ckeditor.com/ticket/16705
-					if ( !data && dataObj.method == 'paste' && dataTransfer && dataTransfer.getFilesCount() == 1 && latestId != dataTransfer.id ) {
-						var file = dataTransfer.getFile( 0 );
+				// If data empty check for image content inside data transfer. http://dev.ckeditor.com/ticket/16705
+				if ( !data && dataObj.method == 'paste' && dataTransfer && dataTransfer.getFilesCount() == 1 && latestId != dataTransfer.id ) {
+					var file = dataTransfer.getFile( 0 );
 
-						if ( CKEDITOR.tools.indexOf( supportedImageTypes, file.type ) != -1 ) {
-							var fileReader = new FileReader();
+					if ( CKEDITOR.tools.indexOf( supportedImageTypes, file.type ) != -1 ) {
+						var url = URL.createObjectURL( file );
+						latestId = dataObj.dataTransfer.id;
 
-							// Convert image file to img tag with base64 image.
-							fileReader.addEventListener( 'load', function() {
-								evt.data.dataValue = '<img src="' + fileReader.result + '" />';
-								editor.fire( 'paste', evt.data );
-							}, false );
-
-							// Proceed with normal flow if reading file was aborted.
-							fileReader.addEventListener( 'abort', function() {
-								editor.fire( 'paste', evt.data );
-							}, false );
-
-							// Proceed with normal flow if reading file failed.
-							fileReader.addEventListener( 'error', function() {
-								editor.fire( 'paste', evt.data );
-							}, false );
-
-							fileReader.readAsDataURL( file );
-
-							latestId = dataObj.dataTransfer.id;
-
-							evt.stop();
-						}
+						evt.stop();
+						evt.data.dataValue = '<img src="' + url + '" data-cke-to-replace="' + latestId + '">';
+						editor.fire( 'paste', evt.data );
 					}
-				}, null, null, 1 );
-			}
+				}
+			}, null, null, 1 );
 
 			editor.on( 'paste', function( evt ) {
 				// Init `dataTransfer` if `paste` event was fired without it, so it will be always available.
