28e5575cf3758eb626472a79c13c983ec0cd71d4
Mateusz Samsel <m.samsel@cksource.com>
2017-07-06 15:20:10 +0200

    Add unit and manual tests to format menu.

---
+++ tests/plugins/format/manual/preserveformatafterclick.md
@@ -0,0 +1,10 @@
+@bender-tags: 4.8.0, feature, 584
+@bender-ui: collapsed
+@bender-ckeditor-plugins: wysiwygarea, toolbar, format, elementspath, sourcearea
+
+1. Select text
+1. Try to apply multiple times this same format to one selection (different than normal).
+
+**Expected:** Choosen format should remain this same all the time.
+
+**Unexpected:** Text format toggle between styled and unstyled (Normal) one.
--- tests/plugins/format/plugin.js
+++ tests/plugins/format/plugin.js
@@ -25,5 +25,29 @@ bender.test( {
 		bot.setHtmlWithSelection( '<fieldset><legend>^foo</legend><form>bar</form></fieldset>' );
 		var name = 'Format', combo = ed.ui.get( name );
 		assert.areSame( CKEDITOR.TRISTATE_DISABLED, combo._.state, 'check state disabled when not in context' );
+	},
+
+	// Drop down format menu should not have possibility to toggle option (#584).
+	'test apply format style twice': function() {
+		var bot = this.editorBot,
+			editor = this.editor,
+			name = 'Format',
+			combo = editor.ui.get( name );
+
+		bot.setHtmlWithSelection( '<p>^foo</p>' );
+		// apply format 1st time
+		assert.areSame( CKEDITOR.TRISTATE_OFF, combo._.state, 'check state OFF' );
+		bot.combo( name, function( combo ) {
+			assert.areSame( CKEDITOR.TRISTATE_ON, combo._.state, 'check state ON when opened' );
+			combo.onClick( 'h1' );
+			assert.areSame( '<h1>^foo</h1>', bot.htmlWithSelection(), 'applied h1 block style' );
+			// apply format 2nd time
+			bot.combo( name, function( combo ) {
+				assert.areSame( CKEDITOR.TRISTATE_ON, combo._.state, 'check state ON when opened' );
+				combo.onClick( 'h1' );
+				assert.areSame( '<h1>^foo</h1>', bot.htmlWithSelection(), 'applied h1 block style' );
+			} );
+		} );
 	}
+
 } );
