bc685d60f31614738d00534c68558f468abd8d92
Mateusz Samsel <m.samsel@cksource.com>
2017-07-25 13:35:32 +0200

    Review fixes

--- tests/core/focusmanager/lostfocus.js
+++ tests/core/focusmanager/lostfocus.js
@@ -5,13 +5,9 @@ bender.editor = true;
 
 bender.test( {
 	'test lost focus in editor after click': function() {
-		// Test for checking if focusmanager reset ranges in divarea editor (bug introduced in https://dev.ckeditor.com/ticket/13446).
-		// It's necessary to keep focus in a browser to get proper result of test. Having focus in a console or other window will cause test to
-		// pass (https://dev.ckeditor.com/ticket/17020).
-
 		// Assert for IE8 returns different selection comparing to other browsers.
-		// Normal browser <p>foo [<strong>bar</strong>]</p>
 		// IE8: <p>foo [<strong>bar]</strong></p>
+		// Others: <p>foo [<strong>bar</strong>]</p>
 		if ( CKEDITOR.env.ie && CKEDITOR.env.version <= 8 ) {
 			assert.ignore();
 		}
