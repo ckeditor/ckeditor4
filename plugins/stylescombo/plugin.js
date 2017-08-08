8ceaa5c51a68531b71ea57292c02c640765004d7
Tomasz Jakut <vepomoc@gmail.com>
2017-08-08 14:36:19 +0200

    Rewrite onRender method of stylescombo plugin.

--- plugins/stylescombo/plugin.js
+++ plugins/stylescombo/plugin.js
@@ -114,28 +114,74 @@
 				},
 
 				onRender: function() {
-					editor.on( 'selectionChange', function( ev ) {
-						var currentValue = this.getValue(),
-							elementPath = ev.data.path,
-							elements = elementPath.elements;
+					editor.on( 'selectionChange', function( evt ) {
+						var combo = this,
+							ranges = evt.data.selection.getRanges(),
+							elementPath = evt.data.path,
+							forEach = CKEDITOR.tools.array.forEach,
+							markers = {},
+							previous = {};
 
-						// For each element into the elements path.
-						for ( var i = 0, count = elements.length, element; i < count; i++ ) {
-							element = elements[ i ];
+						function processNode( node ) {
+							var path = new CKEDITOR.dom.elementPath( node );
+
+							function getNewValue( value, node ) {
+								if ( previous.node && node.contains( previous.node ) ) {
+									return previous.value;
+								}
+
+								if ( !previous.value || previous.value === value ) {
+									previous = {
+										value: value,
+										node: node
+									};
+
+									return value;
+								}
+
+								return '';
+							}
+
+							function checkStyles( node ) {
+								if ( node.getCustomData( 'processed_font' ) ) {
+									return;
+								}
+
+								CKEDITOR.dom.element.setMarker( markers, node, 'processed_font', true );
+								for ( var value in styles ) {
+									if ( styles[ value ].checkElementRemovable( node, true, editor ) ) {
+										combo.setValue( getNewValue( value, node ) );
+									}
+								}
+							}
 
 							// Check if the element is removable by any of
 							// the styles.
-							for ( var value in styles ) {
-								if ( styles[ value ].checkElementRemovable( element, true, editor ) ) {
-									if ( value != currentValue )
-										this.setValue( value );
-									return;
-								}
-							}
+							forEach( path.elements, checkStyles );
 						}
 
-						// If no styles match, just empty it.
-						this.setValue( '' );
+						forEach( ranges, function( range ) {
+							var element,
+								walker;
+
+							if ( range.collapsed ) {
+								processNode( elementPath.lastElement );
+							} else {
+								walker = new CKEDITOR.dom.walker( range );
+
+								walker.evaluator = function( node ) {
+									return node.type === CKEDITOR.NODE_TEXT;
+								};
+
+								walker.reset();
+
+								while ( element = walker.next() ) {
+									processNode( element );
+								}
+							}
+						} );
+
+						CKEDITOR.dom.element.clearAllMarkers( markers );
 					}, this );
 				},
 
