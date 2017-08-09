8691a540ccac2fa648359d57443cbdf591e190dd
Beata <b.delura@cksource.com>
2017-08-09 15:55:08 +0200

    Creating unit tests.

---
+++ tests/plugins/image2/resizerwithreadonly.js
@@ -0,0 +1,49 @@
+/* bender-tags: editor,widget */
+/* bender-ckeditor-plugins: image2,toolbar */
+
+( function() {
+	'use strict';
+
+	bender.test( {
+		'test readOnly set to false': function() {
+			bender.editorBot.create( {
+				name: 'editor1'
+			}, function( bot ) {
+				var editor = bot.editor;
+
+				bot.dialog( 'image', function( dialog ) {
+					dialog.setValueOf( 'info', 'src', '_assets/foo.png' );
+					dialog.getButton( 'ok' ).click();
+
+					assert.areSame(
+						'<span class="cke_image_resizer" title="Click and drag to resize">​</span>',
+						editor.editable().getElementsByTag( 'span' ).$[ 2 ].outerHTML
+					);
+				} );
+			} );
+		},
+
+		'test readOnly set to true': function() {
+			bender.editorBot.create( {
+				name: 'editor2',
+				config: {
+					readOnly: true
+				}
+			}, function( bot ) {
+				var editor = bot.editor,
+					listener;
+
+				listener = editor.on( 'dataReady', function() {
+					listener.removeListener();
+
+					resume( function() {
+						assert.areSame( 2, editor.editable().getElementsByTag( 'span' ).$.length );
+					} );
+				} );
+
+				editor.setData( '<img src="_assets/foo.png" alt="" />' );
+				wait();
+			} );
+		}
+	} );
+}() );
