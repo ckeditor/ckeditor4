bbb9e027cba3ef5c6eabb7d23f086d334edde702
Jon Gunderson <jongund@illinois.edu>
2017-09-01 09:28:25 -0500

    updated language files and copyright for inline style

---
+++ plugins/a11yfirst/lang/en-gb.js
@@ -0,0 +1,23 @@
+/*
+Copyright (c) 2017, University of Illinois - Nicholas Hoyt and Jon Gunderson. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'a11yfirst', 'en', {
+  toolbar: 'Getting Started with Accessibility',
+  importanceLabel: 'Importance',
+  importanceTitle: 'Importance of accessible authoring',
+  importanceItems: [
+    'Importance item 1.',
+    'Importance item 2.',
+    'Importance item 3.',
+    'Importance item 4.'
+  ],
+  featuresLabel: 'A11y Features',
+  featuresTitle: 'A11y Features to support accessible authoring',
+  featuresItems: [
+    'Headings Menu',
+    'Format Menu.',
+    'Inline Stlye Menu.'
+  ]
+
+} );
