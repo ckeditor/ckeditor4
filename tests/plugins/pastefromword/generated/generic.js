ccea86f984a4bcaa75b21db3851556ce17affc8a
Mateusz Samsel <m.samsel@cksource.com>
2017-09-12 14:17:34 +0200

    Unit tests.

--- tests/plugins/pastefromword/generated/generic.js
+++ tests/plugins/pastefromword/generated/generic.js
@@ -46,6 +46,7 @@
 			'Text_alignment': true,
 			'Underline': true,
 			'Unordered_list': true,
+			'Unordered_list_special_char_bullet': true,
 			'Table_alignment': true,
 			'Table_vertical_alignment': true
 		},
