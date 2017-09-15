c5ffdd02c39e5f329d4b0d1982b0fbb3bc1686ee
Wojciech Wozniak <wwozniak92@gmail.com>
2017-09-15 12:25:22 +0200

    Simplify version extraction.

--- dev/builder/build.sh
+++ dev/builder/build.sh
@@ -57,7 +57,7 @@ echo "Starting CKBuilder..."
 
 JAVA_ARGS=${ARGS// -t / } # Remove -t from args.
 
-VERSION=$(cat ./../../package.json | grep \"version\"\: | awk -F: '{ print $2}' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+VERSION=$(grep '"version":' ./../../package.json | sed $'s/[\t\",: ]//g; s/version//g' | tr -d '[[:space:]]')
 REVISION=$(git rev-parse --verify --short HEAD)
 
 # If the current revision is not tagged with any CKE version, it means it's a "dirty" build. We
