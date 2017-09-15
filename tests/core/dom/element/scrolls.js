aaaae7d0a2748443b1a468953dc3b3accb2a0e81
Mateusz Samsel <m.samsel@cksource.com>
2017-09-15 15:21:44 +0200

    Small refactoring in tests

--- tests/core/dom/element/scrolls.js
+++ tests/core/dom/element/scrolls.js
@@ -4,10 +4,16 @@
 	'use strict';
 	bender.editor = true;
 	bender.test( {
+		_assertBothScrolls: function( expected, actual ) {
+			assert.areSame( expected.x, actual.scrollLeft );
+			assert.areSame( expected.y, actual.scrollTop );
+		},
+
 		'test getDocumentScroll': function() {
-			var element = document.getElementById( 'small' );
-			var ckEl = new CKEDITOR.dom.element( element );
-			var doc, docEl, result, resultOnlyTop;
+			var element = document.getElementById( 'small' ),
+				ckEl = new CKEDITOR.dom.element( element ),
+				doc,
+				docEl;
 
 			// Reset position with native methods
 			doc = ckEl.getDocument();
@@ -18,44 +24,33 @@
 			ckEl.$.scrollLeft = 0;
 
 			// document no-scroll, element no-scroll
-			result = ckEl.getDocumentScroll();
-			resultOnlyTop = ckEl.getDocumentScroll( true );
-			assert.areSame( 0, resultOnlyTop );
-			assert.areSame( 0, result.scrollTop );
-			assert.areSame( 0, result.scrollLeft );
+			assert.areSame( 0, ckEl.getDocumentScroll( true ) );
+			this._assertBothScrolls( { x: 0, y: 0 }, ckEl.getDocumentScroll() );
 
 			// document no-scroll, element scroll
 			ckEl.$.scrollTop = 123;
 			ckEl.$.scrollLeft = 123;
-			result = ckEl.getDocumentScroll();
-			resultOnlyTop = ckEl.getDocumentScroll( true );
-			assert.areSame( 0, resultOnlyTop );
-			assert.areSame( 0, result.scrollTop );
-			assert.areSame( 0, result.scrollLeft );
+			assert.areSame( 0, ckEl.getDocumentScroll( true ) );
+			this._assertBothScrolls( { x: 0, y: 0 }, ckEl.getDocumentScroll() );
 
 			// document scroll, element scroll
 			docEl.scrollTop = 20;
 			docEl.scrollLeft = 20;
-			result = ckEl.getDocumentScroll();
-			resultOnlyTop = ckEl.getDocumentScroll( true );
-			assert.areSame( 20, resultOnlyTop );
-			assert.areSame( 20, result.scrollTop );
-			assert.areSame( 20, result.scrollLeft );
+			assert.areSame( 20, ckEl.getDocumentScroll( true ) );
+			this._assertBothScrolls( { x: 20, y: 20 }, ckEl.getDocumentScroll() );
 
 			// document scroll, element no-scroll
 			ckEl.$.scrollTop = 0;
 			ckEl.$.scrollLeft = 0;
-			result = ckEl.getDocumentScroll();
-			resultOnlyTop = ckEl.getDocumentScroll( true );
-			assert.areSame( 20, resultOnlyTop );
-			assert.areSame( 20, result.scrollTop );
-			assert.areSame( 20, result.scrollLeft );
+			assert.areSame( 20, ckEl.getDocumentScroll( true ) );
+			this._assertBothScrolls( { x: 20, y: 20 }, ckEl.getDocumentScroll() );
 		},
 
 		'test getScroll': function() {
-			var element = document.getElementById( 'small' );
-			var ckEl = new CKEDITOR.dom.element( element );
-			var doc, docEl, result, resultOnlyTop;
+			var element = document.getElementById( 'small' ),
+				ckEl = new CKEDITOR.dom.element( element ),
+				doc,
+				docEl;
 
 			// Reset position with native methods
 			doc = ckEl.getDocument();
@@ -66,90 +61,66 @@
 			ckEl.$.scrollLeft = 0;
 
 			// document no-scroll, element no-scroll
-			result = ckEl.getScroll();
-			resultOnlyTop = ckEl.getScroll( true );
-			assert.areSame( 0, resultOnlyTop );
-			assert.areSame( 0, result.scrollTop );
-			assert.areSame( 0, result.scrollLeft );
+			assert.areSame( 0, ckEl.getScroll( true ) );
+			this._assertBothScrolls( { x: 0, y: 0 }, ckEl.getScroll() );
 
 			// document no-scroll, element scroll
 			ckEl.$.scrollTop = 321;
 			ckEl.$.scrollLeft = 321;
-			result = ckEl.getScroll();
-			resultOnlyTop = ckEl.getScroll( true );
-			assert.areSame( 321, resultOnlyTop );
-			assert.areSame( 321, result.scrollTop );
-			assert.areSame( 321, result.scrollLeft );
+			assert.areSame( 321, ckEl.getScroll( true ) );
+			this._assertBothScrolls( { x: 321, y: 321 }, ckEl.getScroll() );
 
 			// document scroll, element scroll
-			doc = ckEl.getDocument();
-			docEl = doc.$.documentElement || doc.$.body;
 			docEl.scrollTop = 20;
 			docEl.scrollLeft = 20;
-			result = ckEl.getScroll();
-			resultOnlyTop = ckEl.getScroll( true );
-			assert.areSame( 321, resultOnlyTop );
-			assert.areSame( 321, result.scrollTop );
-			assert.areSame( 321, result.scrollLeft );
+			assert.areSame( 321, ckEl.getScroll( true ) );
+			this._assertBothScrolls( { x: 321, y: 321 }, ckEl.getScroll() );
 
 			// document scroll, element no-scroll
 			ckEl.$.scrollTop = 0;
 			ckEl.$.scrollLeft = 0;
-			result = ckEl.getScroll();
-			resultOnlyTop = ckEl.getScroll( true );
-			assert.areSame( 0, resultOnlyTop );
-			assert.areSame( 0, result.scrollTop );
-			assert.areSame( 0, result.scrollLeft );
+			assert.areSame( 0, ckEl.getScroll( true ) );
+			this._assertBothScrolls( { x: 0, y: 0 }, ckEl.getScroll() );
 		},
 
 		'test setDocumentScroll': function() {
-			var element = document.getElementById( 'small' );
-			var ckEl = new CKEDITOR.dom.element( element );
+			var element = document.getElementById( 'small' ),
+				ckEl = new CKEDITOR.dom.element( element );
 
 			ckEl.setDocumentScroll( 0, 0 );
-			assert.areSame( 0, ckEl.getDocumentScroll().scrollTop );
-			assert.areSame( 0, ckEl.getDocumentScroll().scrollLeft );
+			this._assertBothScrolls( { x: 0, y: 0 }, ckEl.getDocumentScroll() );
 
 			ckEl.setDocumentScroll( 100 );
-			assert.areSame( 100, ckEl.getDocumentScroll().scrollTop );
-			assert.areSame( 0, ckEl.getDocumentScroll().scrollLeft );
+			this._assertBothScrolls( { x: 0, y: 100 }, ckEl.getDocumentScroll() );
 
 			ckEl.setDocumentScroll( 123, 23 );
-			assert.areSame( 123, ckEl.getDocumentScroll().scrollTop );
-			assert.areSame( 23, ckEl.getDocumentScroll().scrollLeft );
+			this._assertBothScrolls( { x: 23, y: 123 }, ckEl.getDocumentScroll() );
 
 			ckEl.setDocumentScroll( 0 );
-			assert.areSame( 0, ckEl.getDocumentScroll().scrollTop );
-			assert.areSame( 23, ckEl.getDocumentScroll().scrollLeft );
+			this._assertBothScrolls( { x: 23, y: 0 }, ckEl.getDocumentScroll() );
 
 			ckEl.setDocumentScroll( 0, 0 );
-			assert.areSame( 0, ckEl.getDocumentScroll().scrollTop );
-			assert.areSame( 0, ckEl.getDocumentScroll().scrollLeft );
+			this._assertBothScrolls( { x: 0, y: 0 }, ckEl.getDocumentScroll() );
 		},
 
 		'test setScroll': function() {
-			var element = document.getElementById( 'small' );
-			var ckEl = new CKEDITOR.dom.element( element );
+			var element = document.getElementById( 'small' ),
+				ckEl = new CKEDITOR.dom.element( element );
 
 			ckEl.setScroll( 0, 0 );
-			assert.areSame( 0, ckEl.getScroll().scrollTop );
-			assert.areSame( 0, ckEl.getScroll().scrollLeft );
+			this._assertBothScrolls( { x: 0, y: 0 }, ckEl.getScroll() );
 
 			ckEl.setScroll( 100 );
-			assert.areSame( 100, ckEl.getScroll().scrollTop );
-			assert.areSame( 0, ckEl.getScroll().scrollLeft );
+			this._assertBothScrolls( { x: 0, y: 100 }, ckEl.getScroll() );
 
 			ckEl.setScroll( 123, 23 );
-			assert.areSame( 123, ckEl.getScroll().scrollTop );
-			assert.areSame( 23, ckEl.getScroll().scrollLeft );
+			this._assertBothScrolls( { x: 23, y: 123 }, ckEl.getScroll() );
 
 			ckEl.setScroll( 0 );
-			assert.areSame( 0, ckEl.getScroll().scrollTop );
-			assert.areSame( 23, ckEl.getScroll().scrollLeft );
+			this._assertBothScrolls( { x: 23, y: 0 }, ckEl.getScroll() );
 
 			ckEl.setScroll( 0, 0 );
-			assert.areSame( 0, ckEl.getScroll().scrollTop );
-			assert.areSame( 0, ckEl.getScroll().scrollLeft );
+			this._assertBothScrolls( { x: 0, y: 0 }, ckEl.getScroll() );
 		}
 
 	} );
--- tests/core/editable/scrolls.js
+++ tests/core/editable/scrolls.js
@@ -23,13 +23,26 @@
 
 	var tests = {
 		'test getEditableScroll': function( editor ) {
-			var sel = bender.tools.selection.setWithHtml( editor, '<div style="width:4000px;height:4000px;border:1px solid blue;"></div><p style="margin-left:3800px">[selection]</p>' );
-			var editable = editor.editable();
+			var sel = bender.tools.selection.setWithHtml( editor, '<div style="width:4000px;height:4000px;border:1px solid blue;"></div><p style="margin-left:3800px">[selection]</p>' ),
+				editable = editor.editable(),
+				doc,
+				docEl;
+
+			// IE automatically scrolls to selection, we need to reset that.
+			if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 ) {
+				doc = editable.getDocument();
+				docEl = doc.$.documentElement || doc.$.body;
+				docEl.scrollTop = 0;
+				docEl.scrollLeft = 0;
+				editable.$.scrollTop = 0;
+				editable.$.scrollLeft = 0;
+			}
 
 			assert.areSame( 0, editable.getEditableScroll( true ) );
 			assert.areSame( 0, editable.getEditableScroll().scrollTop );
 			assert.areSame( 0, editable.getEditableScroll().scrollLeft );
 
+			// Usage of some good working old method.
 			sel.scrollIntoView();
 
 			assert.areNotSame( 0, editable.getEditableScroll( true ) );
