72bfeaa3775e79935a74770538e035bdb7cdb7de
Tomasz Jakut <vepomoc@gmail.com>
2017-09-20 13:29:09 +0200

    Create easyimage plugin.

---
+++ plugins/easyimage/plugin.js
@@ -0,0 +1,11 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+( function() {
+	'use strict';
+
+	CKEDITOR.plugins.add( 'easyimage', {
+	} );
+}() );
