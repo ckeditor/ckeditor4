64b8649ec3fd34110d0b496754e9ec004aa77d23
Tomasz Jakut <vepomoc@gmail.com>
2017-09-20 15:48:55 +0200

    Add templates for commands and overwrite default dialog.

--- plugins/easyimage/plugin.js
+++ plugins/easyimage/plugin.js
@@ -7,5 +7,68 @@
 	'use strict';
 
 	CKEDITOR.plugins.add( 'easyimage', {
+		requires: 'image2,contextmenu',
+		lang: 'en',
+
+		init: function( editor ) {
+			editor.addCommand( 'eiFullImage', {
+				exec: function() {}
+			} );
+			editor.addCommand( 'eiSideImage', {
+				exec: function() {}
+			} );
+			editor.addCommand( 'eiAltText', {
+				exec: function() {}
+			} );
+
+			editor.addMenuGroup( 'easyimage' );
+			editor.addMenuItems( {
+				eiFullImage: {
+					label: editor.lang.easyimage.commands.fullImage,
+					command: 'eiFullImage',
+					group: 'easyimage',
+					order: 1
+				},
+
+				eiSideImage: {
+					label: editor.lang.easyimage.commands.sideImage,
+					command: 'eiSideImage',
+					group: 'easyimage',
+					order: 2
+				},
+
+				eiAltText: {
+					label: editor.lang.easyimage.commands.altText,
+					command: 'eiAltText',
+					group: 'easyimage',
+					order: 3
+				}
+			} );
+
+			// Overwrite some image2 defaults
+			editor.on( 'widgetDefinition', function( evt ) {
+				var oldInit = evt.data.init;
+
+				if ( evt.data.name !== 'image' ) {
+					return;
+				}
+
+				// Block default image dialog.
+				evt.data.dialog = null;
+
+				evt.data.init = function() {
+					oldInit.call( this );
+
+					this.on( 'contextMenu', function( evt ) {
+						// Remove "Image Properties" option.
+						delete evt.data.image;
+
+						evt.data.eiFullImage = CKEDITOR.TRISTATE_OFF;
+						evt.data.eiSideImage = CKEDITOR.TRISTATE_OFF;
+						evt.data.eiAltText = CKEDITOR.TRISTATE_OFF;
+					} );
+				};
+			} );
+		}
 	} );
 }() );
