23b1733e719228a82ce9b3130c2ca8ab60d112a2
Tomasz Jakut <vepomoc@gmail.com>
2017-09-20 15:49:23 +0200

    Add default language file.

---
+++ plugins/easyimage/lang/en.js
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'easyimage', 'en', {
+	commands: {
+		fullImage: 'Full Size Image',
+		sideImage: 'Side Image',
+		altText: 'Change image alternative text'
+	}
+} );
