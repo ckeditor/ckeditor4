898f7e4a9f42244084693f5353a813e56244c770
Beata <b.delura@cksource.com>
2017-09-21 11:35:09 +0200

    Fixing unit tests.

--- tests/plugins/fakeobjects/pastefakeelement.js
+++ tests/plugins/fakeobjects/pastefakeelement.js
@@ -1,5 +1,5 @@
 /* bender-tags: editor */
-/* bender-ckeditor-plugins: clipboard, iframe */
+/* bender-ckeditor-plugins: clipboard, iframe, link */
 
 ( function() {
 	'use-strict';
@@ -10,8 +10,11 @@
 			dataTransfer = new CKEDITOR.plugins.clipboard.dataTransfer( nativeData );
 
 		nativeData.files.push( {
-			type: 'text/html'
+			type: 'text/html',
+			data: dataValue
 		} );
+
+		nativeData.setData( 'text/html', dataValue );
 
 		dataTransfer.cacheData();
 
@@ -22,8 +25,6 @@
 			type: 'auto'
 		} );
 	}
-
-	bender.editor = true;
 
 	if ( CKEDITOR.env.ie && CKEDITOR.env.version < 10 ) {
 		bender.ignore();
@@ -41,18 +42,24 @@
 				editor.once( 'paste', function() {
 					resume( function() {
 						assert.areSame(
-							'<p><iframe frameborder="&quot;0&quot;" src="&quot;https://example.com&quot;" width="&quot;100&quot;" height="&quot;100&quot;&gt;&lt;/p"></iframe></p>',
+							'<p><iframe></iframe></p>',
 							editor.getData()
+						);
+						assert.beautified.html(
+							'<p><img data-cke-realelement="%3Ciframe%3E%3C%2Fiframe%3E" data-cke-real-node-type="1" alt="IFrame" title="IFrame" ' +
+							'data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+							'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+							'data-cke-real-element-type="iframe" data-cke-resizable="true" class="cke_iframe"><br/></p>',
+							editor.editable().getHtml()
 						);
 					} );
 				} );
 
 				mockPasteFile(
 					editor,
-					'<img data-cke-realelement="%3Ciframe%20frameborder%3D%22%26quot%3B0%26quot%3B%22%20src%3D%22%26quot%3Bhttps%3A%2F%2Fexample.com' +
-					'%26quot%3B%22%20width%3D%22%26quot%3B100%26quot%3B%22%20height%3D%22%26quot%3B100%26quot%3B%26gt%3B%26lt%3B%2Fp%22%3E%3C%2Fiframe%3E" ' +
-					'data-cke-real-node-type="1" alt="IFrame" title="IFrame" align="" src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
-					'data-cke-real-element-type="iframe" data-cke-resizable="true" />'
+					'<img class="cke_iframe" data-cke-realelement="%3Ciframe%3E%3C%2Fiframe%3E" data-cke-real-node-type="1" ' +
+					'alt="IFrame" title="IFrame" align="" src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+					'data-cke-real-element-type="iframe" data-cke-resizable="true">'
 				);
 
 				wait();
@@ -66,20 +73,26 @@
 			}, function( bot ) {
 				var editor = bot.editor;
 
-				editor.once( 'paste', function() {
+				editor.on( 'paste', function() {
 					resume( function() {
 						assert.areSame(
 							'<p><a id="anchor" name="anchor"></a></p>',
 							editor.getData()
+						);
+						assert.beautified.html(
+							'<p><img data-cke-realelement="%3Ca%20id%3D%22anchor%22%20data-cke-saved-name%3D%22anchor%22%20name%3D%22anchor%22%3E%3C%2Fa%3E" ' +
+							'data-cke-real-node-type="1" alt="Anchor" title="Anchor" data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+							'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" data-cke-real-element-type="anchor" class="cke_anchor"><br /></p>',
+							editor.editable().getHtml()
 						);
 					} );
 				} );
 
 				mockPasteFile(
 					editor,
-					'<img data-cke-realelement="%3Ca%20id%3D%22anchor%22%20name%3D%22anchor%22%20data-cke-saved-name%3D%22anchor%22%3E%3C%2Fa%3E" ' +
-					'data-cke-real-node-type="1" alt="Anchor" title="Anchor" align="" src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
-					'data-cke-real-element-type="anchor" />'
+					'<img data-cke-realelement="%3Ca%20id%3D%22anchor%22%20data-cke-saved-name%3D%22anchor%22%20name%3D%22anchor%22%3E%3C%2Fa%3E" ' +
+					'data-cke-real-node-type="1" alt="Anchor" title="Anchor" data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+					'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" data-cke-real-element-type="anchor" class="cke_anchor">'
 				);
 
 				wait();
