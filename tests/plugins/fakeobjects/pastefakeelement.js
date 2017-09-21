e8d9e15a3a3da399d567c90ffca3d349c5b3d914
Beata <b.delura@cksource.com>
2017-09-21 14:57:52 +0200

    Unit test code refactor. Adding new unit tests.

--- tests/plugins/fakeobjects/pastefakeelement.js
+++ tests/plugins/fakeobjects/pastefakeelement.js
@@ -36,6 +36,7 @@
 				name: 'editor1'
 			}, function( bot ) {
 				var editor = bot.editor;
+
 				editor.once( 'paste', function() {
 					resume( function() {
 						assert.areSame(
@@ -43,12 +44,17 @@
 							editor.getData()
 						);
 
-						assert.beautified.html(
-							'<img data-cke-realelement="%3Ciframe%3E%3C%2Fiframe%3E" data-cke-real-node-type="1" alt="IFrame" title="IFrame" ' +
-							'data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
-							'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
-							'data-cke-real-element-type="iframe" data-cke-resizable="true" class="cke_iframe">',
-							editor.editable().getHtml().match( /<img.+?>/g )[ 0 ]
+						assert.isInnerHtmlMatching(
+							bender.tools.html.prepareInnerHtmlForComparison(
+								'<p><img data-cke-realelement="%3Ciframe%3E%3C%2Fiframe%3E" data-cke-real-node-type="1" alt="IFrame" title="IFrame" ' +
+								'data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+								'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+								'data-cke-real-element-type="iframe" data-cke-resizable="true" class="cke_iframe">@</p>',
+								{ fixStyles: true }
+							),
+
+							editor.editable().getHtml(),
+							{ fixStyles: true }
 						);
 					} );
 				} );
@@ -64,7 +70,6 @@
 			} );
 		},
 
-		// #638
 		'test paste fake anchor': function() {
 			bender.editorBot.create( {
 				name: 'editor2'
@@ -77,11 +82,17 @@
 							'<p><a id="anchor" name="anchor"></a></p>',
 							editor.getData()
 						);
-						assert.beautified.html(
-							'<img data-cke-realelement="%3Ca%20id%3D%22anchor%22%20data-cke-saved-name%3D%22anchor%22%20name%3D%22anchor%22%3E%3C%2Fa%3E" ' +
-							'data-cke-real-node-type="1" alt="Anchor" title="Anchor" data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
-							'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" data-cke-real-element-type="anchor" class="cke_anchor">',
-							editor.editable().getHtml().match( /<img.+?>/g )[ 0 ]
+
+						assert.isInnerHtmlMatching(
+							bender.tools.html.prepareInnerHtmlForComparison(
+								'<p><img data-cke-realelement="%3Ca%20id%3D%22anchor%22%20data-cke-saved-name%3D%22anchor%22%20name%3D%22anchor%22%3E%3C%2Fa%3E" ' +
+								'data-cke-real-node-type="1" alt="Anchor" title="Anchor" data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+								'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" data-cke-real-element-type="anchor" class="cke_anchor">@</p>',
+								{ fixStyles: true }
+							),
+
+							editor.editable().getHtml(),
+							{ fixStyles: true }
 						);
 					} );
 				} );
@@ -91,6 +102,76 @@
 					'<img data-cke-realelement="%3Ca%20id%3D%22anchor%22%20data-cke-saved-name%3D%22anchor%22%20name%3D%22anchor%22%3E%3C%2Fa%3E" ' +
 					'data-cke-real-node-type="1" alt="Anchor" title="Anchor" data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
 					'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" data-cke-real-element-type="anchor" class="cke_anchor">'
+				);
+
+				wait();
+			} );
+		},
+
+		'test paste fake object with additional content': function() {
+			bender.editorBot.create( {
+				name: 'editor3'
+			}, function( bot ) {
+				var editor = bot.editor;
+
+				editor.on( 'paste', function() {
+					resume( function() {
+						assert.beautified.html(
+							'<p>Lorem ipsum</p>' +
+							'<p><iframe frameborder="0" height="100" src="https://example.com" width="100"></iframe></p>',
+							editor.getData()
+						);
+
+						assert.isInnerHtmlMatching(
+							bender.tools.html.prepareInnerHtmlForComparison(
+								'<p>Lorem ipsum</p>' +
+								'<p><img data-cke-realelement="%3Ciframe%20frameborder%3D%220%22%20height%3D%22100%22%20src%3D%22https%3A%2F%2Fexample.com%22%20width%3D%22100%22%3E%3C%2Fiframe%3E" ' +
+								'data-cke-real-node-type="1" alt="IFrame" title="IFrame" data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+								'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" data-cke-real-element-type="iframe" data-cke-resizable="true" ' +
+								'style="height:100px; width:100px" class="cke_iframe">@</p>',
+								{ fixStyles: true }
+							),
+
+							editor.editable().getHtml(),
+							{ fixStyles: true }
+						);
+					} );
+				} );
+
+				mockPasteFile(
+					editor,
+					'<p>Lorem ipsum</p>' +
+					'<p><img data-cke-realelement="%3Ciframe%20frameborder%3D%220%22%20height%3D%22100%22%20src%3D%22https%3A%2F%2Fexample.com%22%20width%3D%22100%22%3E%3C%2Fiframe%3E" ' +
+					'data-cke-real-node-type="1" alt="IFrame" title="IFrame" data-cke-saved-src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" ' +
+					'src="data:image/gif;base64,R0lGODlhAQABAPABAP///wAAACH5BAEKAAAALAAAAAABAAEAAAICRAEAOw==" data-cke-real-element-type="iframe" data-cke-resizable="true" ' +
+					'style="height:100px; width:100px" class="cke_iframe"></p>'
+				);
+
+				wait();
+			} );
+		},
+
+		'test paste iframe with image2': function() {
+			bender.editorBot.create( {
+				name: 'editor4',
+				config: {
+					extraPlugins: 'image2'
+				}
+			}, function( bot ) {
+				var editor = bot.editor;
+
+				editor.on( 'paste', function() {
+					resume( function() {
+						assert.beautified.html(
+							'<p><img alt="" src="../../_assets/lena.jpg" /></p><p><iframe frameborder="0" src="https://example.com" width="100" height="100"></iframe></p>',
+							editor.getData()
+						);
+					} );
+				} );
+
+				mockPasteFile(
+					editor,
+					'<p><img alt="" src="../../_assets/lena.jpg" /></p><p><iframe frameborder="0" src="https://example.com" width="100" height="100"></iframe></p>'
 				);
 
 				wait();
