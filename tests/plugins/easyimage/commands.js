35c0d1528076441b90cfe73680247160b0f61d44
Tomasz Jakut <vepomoc@gmail.com>
2017-09-21 16:19:50 +0200

    Add unit tests for eiFullImage and eiSideImage.

--- tests/plugins/easyimage/commands.js
+++ tests/plugins/easyimage/commands.js
@@ -18,39 +18,78 @@
 		}
 	};
 
-	var tests = {
-		tearDown: function() {
-			var currentDialog = CKEDITOR.dialog.getCurrent();
-
-			if ( currentDialog ) {
-				currentDialog.hide();
+	function assertMenuItemsState( items, asserts ) {
+		CKEDITOR.tools.array.forEach( items, function( item ) {
+			if ( asserts[ item.command ] ) {
+				assert.areSame( asserts[ item.command ], item.state,
+					'Menu item ' + item.command + ' has appropriate state' );
 			}
-		},
+		} );
+	}
 
-		'test eiAltText command': function( editor, bot ) {
+	var widgetHtml = '<figure class="image easyimage"><img src="../image2/_assets/foo.png" alt="foo"><figcaption>Test image</figcaption></figure>',
+		tests = {
+			tearDown: function() {
+				var currentDialog = CKEDITOR.dialog.getCurrent();
 
-			bot.setData( '<figure class="image"><img src="../image2/_assets/foo.png" alt="foo"><figcaption>Test image</figcaption></figure>', function() {
-				editor.once( 'dialogShow', function( evt ) {
-					resume( function() {
-						var dialog = evt.data;
+				if ( currentDialog ) {
+					currentDialog.hide();
+				}
+			},
 
-						assert.areSame( 'foo', dialog.getValueOf( 'info', 'txtAlt' ),
-							'Initial value is fetched from image' );
+			'test eiAltText command': function( editor, bot ) {
+				bot.setData( widgetHtml, function() {
+					editor.once( 'dialogShow', function( evt ) {
+						resume( function() {
+							var dialog = evt.data;
 
-						dialog.setValueOf( 'info', 'txtAlt', 'bar' );
-						dialog.getButton( 'ok' ).click();
+							assert.areSame( 'foo', dialog.getValueOf( 'info', 'txtAlt' ),
+								'Initial value is fetched from image' );
 
-						assert.areSame( 'bar', editor.editable().findOne( 'img' ).getAttribute( 'alt' ),
-							'Alt text of image is changed' );
+							dialog.setValueOf( 'info', 'txtAlt', 'bar' );
+							dialog.getButton( 'ok' ).click();
+
+							assert.areSame( 'bar', editor.editable().findOne( 'img' ).getAttribute( 'alt' ),
+								'Alt text of image is changed' );
+						} );
+					} );
+
+					editor.widgets.instances[ 0 ].focus();
+					editor.execCommand( 'eiAltText' );
+					wait();
+				} );
+			},
+
+			'test eiFullImage and eiSideImage commands': function( editor, bot ) {
+				bot.setData( widgetHtml, function() {
+					var widget = editor.widgets.getByElement( editor.editable().findOne( 'figure' ) );
+
+					widget.focus();
+
+					assert.isFalse( widget.element.hasClass( 'easyimage--side' ), 'Image does not have side class' );
+
+					bot.contextmenu( function( menu ) {
+						assertMenuItemsState( menu.items, {
+							eiFullImage: CKEDITOR.TRISTATE_ON,
+							eiSideImage: CKEDITOR.TRISTATE_OFF
+						} );
+
+						editor.execCommand( 'eiSideImage' );
+
+						assert.isTrue( widget.element.hasClass( 'easyimage--side' ), 'Image has side class' );
+
+						bot.contextmenu( function( menu ) {
+							assertMenuItemsState( menu.items, {
+								eiFullImage: CKEDITOR.TRISTATE_OFF,
+								eiSideImage: CKEDITOR.TRISTATE_ON
+							} );
+
+							menu.hide();
+						} );
 					} );
 				} );
-
-				editor.widgets.instances[ 0 ].focus();
-				editor.execCommand( 'eiAltText' );
-				wait();
-			} );
-		}
-	};
+			}
+		};
 
 	tests = bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests );
 	bender.test( tests );
