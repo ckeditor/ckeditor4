5b019ce805d19f5a4563e8a35c395a7804821721
Beata <b.delura@cksource.com>
2017-09-25 16:11:03 +0200

    Removing unneeded test.

--- tests/plugins/easyimage/imageresizer.js
+++ tests/plugins/easyimage/imageresizer.js
@@ -19,44 +19,10 @@
 	};
 
 	var widgetHtml = '<figure class="image easyimage"><img src="../image2/_assets/foo.png" alt="foo"><figcaption>Test image</figcaption></figure>',
-		regexResizerHtml = /class="cke_image_resizer"/g,
 		tests = {
-
 		'test disabled image resizer': function( editor, bot ) {
 			bot.setData( widgetHtml, function() {
-				assert.isNull( editor.editable().getHtml().match( regexResizerHtml ) );
-			} );
-		},
-
-		'test image2_disableResizer disabled': function( editor, bot ) {
-			editor.config.image2_disableResizer = true;
-
-			bot.setData( widgetHtml, function() {
-				assert.isNull( editor.editable().getHtml().match( regexResizerHtml ) );
-			} );
-		},
-
-		'test image2_disableResizer enabled': function( editor, bot ) {
-			editor.config.image2_disableResizer = false;
-
-			bot.setData( widgetHtml, function() {
-				assert.isNull( editor.editable().getHtml().match( regexResizerHtml ) );
-			} );
-		},
-
-		'test readOnly disabled': function( editor, bot ) {
-			editor.config.readOnly = false;
-
-			bot.setData( widgetHtml, function() {
-				assert.isNull( editor.editable().getHtml().match( regexResizerHtml ) );
-			} );
-		},
-
-		'test readOnly enabled': function( editor, bot ) {
-			editor.config.readOnly = true;
-
-			bot.setData( widgetHtml, function() {
-				assert.isNull( editor.editable().getHtml().match( regexResizerHtml ) );
+				assert.isNull( editor.editable().findOne( '[class=cke_image_resizer]' ) );
 			} );
 		}
 	};
