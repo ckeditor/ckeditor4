2ea8acd33f4706f15e40120e0d93a341b60b1cfd
Mateusz Samsel <m.samsel@cksource.com>
2017-10-02 16:48:48 +0200

    RegExp change to match more edge cases and review fixes: - update test for using more fixtures - update docs - rename variables - small code improvements

--- tests/plugins/pastefromwordimage/helpers.js
+++ tests/plugins/pastefromwordimage/helpers.js
@@ -5,38 +5,99 @@
 	'use strict';
 
 	bender.test( {
+		cacheBuster: function() {
+			return ( new Date() ).getTime().toString() + Math.floor( Math.random() * 1000000 ).toString();
+		},
+
 		'test img tags extraction from html string': function() {
-			bender.tools.testInputOut( 'source1', function( input, output ) {
-				var expectedResult = JSON.parse( output.replace( /<!--/, '' ).replace( /-->/, '' ) ),
-					actualResult = CKEDITOR.plugins.pastefromwordimage.extractImgTagsFromHtmlString( input ),
-					i;
+			var sourceLength = 5,
+				i;
 
-				assert.areSame( expectedResult.length, actualResult.length );
+			for ( i = 0; i < sourceLength; i++ ) {
+				bender.tools.testInputOut( 'source' + i, function( input, output ) {
+					var expectedResult = JSON.parse( output.replace( /<!--/, '' ).replace( /-->/, '' ) ),
+						actualResult = CKEDITOR.plugins.pastefromwordimage.extractImgTagsFromHtml( input ),
+						j;
 
-				for ( i = 0; i < actualResult.length; i++ ) {
-					assert.areSame( expectedResult[ i ][ 0 ], actualResult[ i ][ 0 ] );
-					assert.areSame( expectedResult[ i ][ 1 ], actualResult[ i ][ 1 ] );
-				}
-			} );
+					assert.areSame( expectedResult.length, actualResult.length );
+					for ( j = 0; j < actualResult.length; j++ ) {
+						assert.areSame( expectedResult[ j ], actualResult[ j ] );
+					}
+				} );
+			}
 		},
 
 		'test extract images hexstrings from rtf': function() {
-			var actualResult,
+			var oldEnv = CKEDITOR.env.mac,
+				actualResult,
 				testCase,
+				// Test description:
+				// 1. Single Image
+				// 2. Mixed: image online, image offline, word shape
+				// 3. No images in RTF
+				// 4. Image with hyperlink
+				// 5. Image with changed wrapping option
 				testCases = [
 				{
-					rtf: CKEDITOR.ajax.load( './_fixtures/helpers1.rtf' ),
-					result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers1.json' ) )
+					rtf: CKEDITOR.ajax.load( './_fixtures/helpers1.rtf' + '?' + this.cacheBuster() ),
+					result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers1.json' + '?' + this.cacheBuster() ) )
 				},
 				{
-					rtf: CKEDITOR.ajax.load( './_fixtures/helpers2.rtf' ),
-					result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers2.json' ) )
+					rtf: CKEDITOR.ajax.load( './_fixtures/helpers2.rtf' + '?' + this.cacheBuster()  ),
+					result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers2.json' + '?' + this.cacheBuster() ) )
 				},
 				{
-					rtf: CKEDITOR.ajax.load( './_fixtures/helpers3.rtf' ),
-					result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers3.json' ) )
+					rtf: CKEDITOR.ajax.load( './_fixtures/helpers3.rtf' + '?' + this.cacheBuster() ),
+					result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers3.json' + '?' + this.cacheBuster() ) )
+				},
+				{
+					rtf: CKEDITOR.ajax.load( './_fixtures/helpers4.rtf' + '?' + this.cacheBuster() ),
+					result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers4.json' + '?' + this.cacheBuster() ) )
+				},
+				{
+					rtf: CKEDITOR.ajax.load( './_fixtures/helpers5.rtf' + '?' + this.cacheBuster() ),
+					result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers5.json' + '?' + this.cacheBuster() ) )
 				}
 			];
+			CKEDITOR.env.mac = false;
+			while ( testCase = testCases.shift() ) {
+				actualResult = CKEDITOR.plugins.pastefromwordimage.extractImagesFromRtf( testCase.rtf );
+				assert.areSame( testCase.result.length, actualResult.length );
+				for ( var i = 0; i < actualResult.length; i++ ) {
+					objectAssert.areEqual( testCase.result[ i ], actualResult[ i ] );
+				}
+			}
+			CKEDITOR.env.mac = oldEnv;
+		},
+
+		'test extract images hexstrings from rtf for MacOS': function() {
+			var oldEnv = CKEDITOR.env.mac,
+				actualResult,
+				testCase,
+				// Test description:
+				// 1. Single Image
+				// 2. Mixed: image online, image offline, word shape
+				// 3. Image with hyperlink
+				// 4. Image with changed wrapping option
+				testCases = [
+					{
+						rtf: CKEDITOR.ajax.load( './_fixtures/helpers6.rtf' + '?' + this.cacheBuster() ),
+						result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers6.json' + '?' + this.cacheBuster() ) )
+					},
+					{
+						rtf: CKEDITOR.ajax.load( './_fixtures/helpers7.rtf' + '?' + this.cacheBuster() ),
+						result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers7.json' + '?' + this.cacheBuster() ) )
+					},
+					{
+						rtf: CKEDITOR.ajax.load( './_fixtures/helpers8.rtf' + '?' + this.cacheBuster() ),
+						result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers8.json' + '?' + this.cacheBuster() ) )
+					},
+					{
+						rtf: CKEDITOR.ajax.load( './_fixtures/helpers9.rtf' + '?' + this.cacheBuster() ),
+						result: JSON.parse( CKEDITOR.ajax.load( './_fixtures/helpers9.json' + '?' + this.cacheBuster() ) )
+					}
+				];
+			CKEDITOR.env.mac = true;
 
 			while ( testCase = testCases.shift() ) {
 				actualResult = CKEDITOR.plugins.pastefromwordimage.extractImagesFromRtf( testCase.rtf );
@@ -45,6 +106,7 @@
 					objectAssert.areEqual( testCase.result[ i ], actualResult[ i ] );
 				}
 			}
+			CKEDITOR.env.mac = oldEnv;
 		}
 	} );
 } )();
