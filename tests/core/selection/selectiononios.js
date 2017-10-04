8f07b04c90910d3f9b439036076aff4925529088
Beata <b.delura@cksource.com>
2017-10-04 12:54:07 +0200

    Creating test for selection with setting link and fake element.

--- tests/core/selection/selectiononios.js
+++ tests/core/selection/selectiononios.js
@@ -1,19 +1,19 @@
 /* bender-tags: editor */
-/* bender-ckeditor-plugins: wysiwygarea, basicstyles, toolbar, floatingspace, colorbutton, link, colordialog */
+/* bender-ckeditor-plugins: wysiwygarea, basicstyles, toolbar, floatingspace, colorbutton, image2, link, colordialog */
 
 ( function() {
 	'use strict';
 
-	if ( !CKEDITOR.env.iOS ) {
-		bender.ignore();
-		return;
-	}
+	// if ( !CKEDITOR.env.iOS ) {
+	// 	bender.ignore();
+	// 	return;
+	// }
 
-	function onDialogShow( editor ) {
+	function onDialogShow( editor, pageId, elementId, value ) {
 		editor.on( 'dialogShow', function( evt ) {
 			setTimeout( function() {
 				var dialog = evt.data;
-				dialog.setValueOf( 'picker', 'selectedColor', '#ff3333' );
+				dialog.setValueOf( pageId, elementId, value );
 				dialog.getButton( 'ok' ).click();
 				resume();
 			}, 0 );
@@ -30,30 +30,9 @@
 	}
 
 	bender.test( {
-		'test changing text color': function() {
+		'test loosing selection': function() {
 			bender.editorBot.create( {
 				name: 'editor1',
-				creator: 'inline'
-			}, function( bot ) {
-				var editor = bot.editor,
-					txtColorBtn = editor.ui.get( 'TextColor' ),
-					frame;
-
-				editor.on( 'dialogHide', function() {
-					assert.areEqual( '<p>Lore<span style="color:#ff3333">m ips</span>um</p>', editor.getData() );
-				} );
-
-				onDialogShow( editor );
-				bender.tools.selection.setWithHtml( editor, '<p>Lore{m ips}um</p>' );
-
-				txtColorBtn.click( editor );
-				openColorDialog( frame );
-			} );
-		},
-
-		'test loosing selection on iOS': function() {
-			bender.editorBot.create( {
-				name: 'editor2',
 				creator: 'inline'
 			}, function( bot ) {
 				var editor = bot.editor,
@@ -69,8 +48,80 @@
 				assert.areEqual( 4, editor.getSelection().getRanges()[ 0 ].startOffset );
 				assert.areEqual( 9, editor.getSelection().getRanges()[ 0 ].endOffset );
 			} );
-		}
+		},
 
-		// TODO: test with link, test with fake selection
+		'test changing text color': function() {
+			bender.editorBot.create( {
+				name: 'editor2',
+				creator: 'inline'
+			}, function( bot ) {
+				var editor = bot.editor,
+					txtColorBtn = editor.ui.get( 'TextColor' ),
+					frame;
+
+				editor.on( 'dialogHide', function() {
+					assert.areEqual( '<p>Lore<span style="color:#ff3333">m ips</span>um</p>', editor.getData() );
+				} );
+
+				onDialogShow( editor, 'picker', 'selectedColor', '#ff3333' );
+				bender.tools.selection.setWithHtml( editor, '<p>Lore{m ips}um</p>' );
+
+				txtColorBtn.click( editor );
+				openColorDialog( frame );
+			} );
+		},
+
+		'test setting link': function() {
+			bender.editorBot.create( {
+				name: 'editor3',
+				creator: 'inline'
+			}, function( bot ) {
+				var editor = bot.editor,
+				linkButton = editor.ui.get( 'Link' ),
+				unlinkButton = editor.ui.get( 'Unlink' );
+
+				editor.on( 'dialogHide', function() {
+					assert.areEqual( '<p><a href="http://foo.com">Lorem</a> ipsum</p>', editor.getData() );
+
+					unlinkButton.click( editor );
+					assert.areEqual( '<p>Lorem ipsum</p>', editor.getData() );
+				} );
+
+				onDialogShow( editor, 'info', 'url', 'foo.com' );
+				bender.tools.selection.setWithHtml( editor, '<p>{Lorem} ipsum</p>' );
+
+				linkButton.click( editor );
+				wait();
+			} );
+		},
+
+		'test select fake element': function() {
+			bender.editorBot.create( {
+				name: 'editor4',
+				creator: 'inline',
+				extraPlugins: 'image2',
+				startupData: '<figure id="image" class="image"><img alt="" src="../../_assets/lena.jpg" /><figcaption>Caption</figcaption></figure>',
+				config: {
+					extraAllowedContent: 'figure a[id]'
+				}
+			}, function( bot ) {
+				var editor = bot.editor,
+					figure = editor.document.getById( 'image' ),
+					linkButton = editor.ui.get( 'Link' );
+
+				editor.on( 'dialogHide', function() {
+					assert.areEqual(
+						'<figure id="image" class="image"><a href="http://foo.com"><img alt="" src="../../_assets/lena.jpg" /></a><figcaption>Caption</figcaption></figure>',
+						editor.getData()
+					);
+				} );
+
+				linkButton.click( editor );
+				onDialogShow( editor, 'info', 'url', 'foo.com' );
+
+				editor.getSelection().selectElement( figure );
+				wait();
+			} );
+		}
 	} );
 } )();
