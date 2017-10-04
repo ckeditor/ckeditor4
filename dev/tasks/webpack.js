79dd12f9aa25266c3e21efa86dd2aac634463030
Marek Lewandowski <m.lewandowski@cksource.com>
2017-10-04 13:52:47 +0200

    Consider last two releases of target browsers when bundling Cloud Services lib.

--- dev/tasks/webpack.js
+++ dev/tasks/webpack.js
@@ -35,10 +35,10 @@ module.exports = function( grunt ) {
 											modules: false,
 											targets: {
 												browsers: [
-													'last 1 Chrome versions',
-													'last 1 Firefox versions',
-													'last 1 Safari versions',
-													'last 1 Edge versions'
+													'last 2 Chrome versions',
+													'last 2 Firefox versions',
+													'last 2 Safari versions',
+													'last 2 Edge versions'
 												]
 											}
 										} ]
