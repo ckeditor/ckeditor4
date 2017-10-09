883a4333a5b1ec6e989807224268325dda4c3e2b
Beata <b.delura@cksource.com>
2017-10-09 14:52:01 +0200

    Fixing unit tests on IE and Edge.

--- tests/plugins/colorbutton/highlightedcolor.js
+++ tests/plugins/colorbutton/highlightedcolor.js
@@ -8,26 +8,8 @@
 
 	bender.test( {
 		// 1008
-		'test highlighted color with text color button': function() {
-			var editor = this.editor,
-				bot = this.editorBot,
-				txtColorBtn = editor.ui.get( 'TextColor' );
-
-			resume( function() {
-				assert.areSame( 'true', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 23 ].getAttribute( 'aria-selected' ) );
-				assert.areSame( 'Black', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 23 ].title );
-				assert.areSame( '000000', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 23 ].dataset.value );
-			} );
-
-			bot.setHtmlWithSelection( '[<h1 style="color: #000000;">Moo</h1>]' );
-			txtColorBtn.click( editor );
-
-			wait();
-		},
-
 		'test highlighted color with background color button': function() {
 			var editor = this.editor,
-				bot = this.editorBot,
 				bgColorBtn = editor.ui.get( 'BGColor' );
 
 			resume( function() {
@@ -36,15 +18,30 @@
 				assert.areSame( '999999', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 22 ].dataset.value );
 			} );
 
-			bot.setHtmlWithSelection( '[<h1 style="background: #999999">Moo</h1>]' );
+			bender.tools.selection.setWithHtml( editor, '<h1 style="background: #999999">{Moo}</h1>' );
 			bgColorBtn.click( editor );
+
+			wait();
+		},
+
+		'test highlighted color with text color button': function() {
+			var editor = this.editor,
+				txtColorBtn = editor.ui.get( 'TextColor' );
+
+			resume( function() {
+				assert.areSame( 'true', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 23 ].getAttribute( 'aria-selected' ) );
+				assert.areSame( 'Black', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 23 ].title );
+				assert.areSame( '000000', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 23 ].dataset.value );
+			} );
+
+			bender.tools.selection.setWithHtml( editor, '<h1 style="color: #000000;">{Moo}</h1>' );
+			txtColorBtn.click( editor );
 
 			wait();
 		},
 
 		'test highlighted color with background and text color button': function() {
 			var editor = this.editor,
-				bot = this.editorBot,
 				txtColorBtn = editor.ui.get( 'TextColor' ),
 				bgColorBtn = editor.ui.get( 'BGColor' );
 
@@ -54,12 +51,13 @@
 				assert.areSame( 'DDDDDD', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 16 ].dataset.value );
 
 				txtColorBtn.click( editor );
+
 				assert.areSame( 'true', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 17 ].getAttribute( 'aria-selected' ) );
 				assert.areSame( 'White', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 17 ].title );
 				assert.areSame( 'FFFFFF', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 17 ].dataset.value );
 			} );
 
-			bot.setHtmlWithSelection( '[<h1 style="color: #ffffff; background: #dddddd">Moo</h1>]' );
+			bender.tools.selection.setWithHtml( editor, '<h1 style="color: #ffffff; background: #dddddd">{Moo}</h1>' );
 			bgColorBtn.click( editor );
 
 			wait();
