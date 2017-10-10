e819785b106bc7a2eaf9b76d7f07485539c445c5
Beata <b.delura@cksource.com>
2017-10-10 10:13:08 +0200

    Fixing unit tests.

--- tests/plugins/colorbutton/highlightedcolor.js
+++ tests/plugins/colorbutton/highlightedcolor.js
@@ -13,9 +13,9 @@
 				bgColorBtn = editor.ui.get( 'BGColor' );
 
 			resume( function() {
-				assert.areSame( 'true', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 22 ].getAttribute( 'aria-selected' ) );
-				assert.areSame( 'Dark Gray', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 22 ].title );
-				assert.areSame( '999999', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 22 ].dataset.value );
+				assert.areSame( 'true', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 22 ).getAttribute( 'aria-selected' ) );
+				assert.areSame( 'Dark Gray', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 22 ).$.title );
+				assert.areSame( '999999', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 22 ).getAttribute( 'data-value' ) );
 			} );
 
 			bender.tools.selection.setWithHtml( editor, '<h1 style="background: #999999">{Moo}</h1>' );
@@ -29,9 +29,9 @@
 				txtColorBtn = editor.ui.get( 'TextColor' );
 
 			resume( function() {
-				assert.areSame( 'true', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 23 ].getAttribute( 'aria-selected' ) );
-				assert.areSame( 'Black', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 23 ].title );
-				assert.areSame( '000000', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 23 ].dataset.value );
+				assert.areSame( 'true', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 23 ).getAttribute( 'aria-selected' ) );
+				assert.areSame( 'Black', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 23 ).$.title );
+				assert.areSame( '000000', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 23 ).getAttribute( 'data-value' ) );
 			} );
 
 			bender.tools.selection.setWithHtml( editor, '<h1 style="color: #000000;">{Moo}</h1>' );
@@ -46,15 +46,15 @@
 				bgColorBtn = editor.ui.get( 'BGColor' );
 
 			resume( function() {
-				assert.areSame( 'true', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 16 ].getAttribute( 'aria-selected' ) );
-				assert.areSame( 'Light Gray', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 16 ].title );
-				assert.areSame( 'DDDDDD', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 16 ].dataset.value );
+				assert.areSame( 'true', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 16 ).getAttribute( 'aria-selected' ) );
+				assert.areSame( 'Light Gray', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 16 ).$.title );
+				assert.areSame( 'DDDDDD', bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 16 ).getAttribute( 'data-value' ) );
 
 				txtColorBtn.click( editor );
 
-				assert.areSame( 'true', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 17 ].getAttribute( 'aria-selected' ) );
-				assert.areSame( 'White', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 17 ].title );
-				assert.areSame( 'FFFFFF', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).$[ 17 ].dataset.value );
+				assert.areSame( 'true', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 17 ).getAttribute( 'aria-selected' ) );
+				assert.areSame( 'White', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 17 ).$.title );
+				assert.areSame( 'FFFFFF', txtColorBtn._.panel.getBlock( txtColorBtn._.id ).element.find( 'a.cke_colorbox' ).getItem( 17 ).getAttribute( 'data-value' ) );
 			} );
 
 			bender.tools.selection.setWithHtml( editor, '<h1 style="color: #ffffff; background: #dddddd">{Moo}</h1>' );
