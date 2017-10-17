fd91cb305e88a177c2a0ce992bbe87237f79ef6c
Marek Lewandowski <m.lewandowski@cksource.com>
2017-10-17 14:30:30 +0200

    Added a copyright note.

--- plugins/cloudservices/plugin.js
+++ plugins/cloudservices/plugin.js
@@ -1,3 +1,8 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
 ( function() {
 	'use strict';
 
