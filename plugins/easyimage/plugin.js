a695fb4beef7377071c49e214b646d79e5ecfc50
Marek Lewandowski <m.lewandowski@cksource.com>
2017-10-18 17:03:09 +0200

    Corrected Cloud Services loader type name after recent refactoring.

--- plugins/easyimage/plugin.js
+++ plugins/easyimage/plugin.js
@@ -138,7 +138,7 @@
 
 			loadMethod: 'loadAndUpload',
 
-			loaderType: CKEDITOR.plugins.cloudservices.fileLoader,
+			loaderType: CKEDITOR.plugins.cloudservices.cloudServicesLoader,
 
 			additionalRequestParameters: {
 				isEasyImage: true
