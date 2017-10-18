6f08bec3de53b123c34142fd76afdda8c49e1a1b
Wojciech Wozniak <wwozniak92@gmail.com>
2017-10-18 02:03:27 +0200

    Unit test refactor.

--- tests/plugins/inlinetoolbar/positioning.js
+++ tests/plugins/inlinetoolbar/positioning.js
@@ -1,88 +1,82 @@
 /* bender-tags: inlinetoolbar */
 /* bender-ckeditor-plugins: toolbar,link,inlinetoolbar */
-/* bender-include: ../balloonpanel/_helpers/tools.js */
-/* global balloonTestsTools */
 
+/*
+divarea test are failing, its upstream issue from balloonpanel. issue #1064
+*/
 ( function() {
 	'use strict';
+	var createContent = function() {
+		var rawData = '';
+		for ( var i = 0; i < 80; i++ ) {
+			rawData += '<p>This is the editor data.</p>';
+			if ( i === 40 ) rawData +=  '<p>foo bar baz<span id="marker">bom</span></p>';
+		}
+		return rawData;
+	};
+	document.getElementById( 'editor1' ).innerHTML = createContent();
+	document.getElementById( 'divarea' ).innerHTML = createContent();
 
-	bender.editor = {
-		name: 'editor1',
-		config: {
-			extraAllowedContent: 'span[id]',
-			height: 300,
-			width: 300
+	bender.editors = {
+		editor1: {
+			name: 'editor1',
+			creator: 'replace',
+			config: {
+				extraAllowedContent: 'span[id]',
+				height: 200,
+				width: 600
+			}
+		},
+		divarea: {
+			name: 'divarea',
+			creator: 'replace',
+			config: {
+				extraPlugins: 'divarea',
+				extraAllowedContent: 'span[id]',
+				height: 200,
+				width: 600
+			}
 		}
 	};
 
-	var spy = sinon.spy,
-		tests = {
-			// There's no need to call editor.window.scrollTo, because balloon logic initially considers only
-			// scrolling of **outer window**. It does not care about editable window, as its scrolling is already
-			// "contained" in result returned by the element.getClientRect() of marker.
-			_getFrameMethodReplaced: false,
+	var tests = {
 
-			setUp: function() {
-				this.getViewPaneSize = sinon.stub( this.editor.window, 'getViewPaneSize', function() {
-					return {
-						width: 1183,
-						height: 1818
-					};
-				} );
-				this.inlineToolbar = new CKEDITOR.ui.inlineToolbarView( this.editor, {
-					width: 100,
-					height: 200
-				} );
+		'test divaera - out of view - bottom center': function( editor ) {
+			var inlineToolbar = new CKEDITOR.ui.inlineToolbarView( editor, {
+				width: 100,
+				height: 200
+			} );
+			var markerElement = editor.editable().findOne( '#marker' );
+			var frame = editor.editable().isInline() ? editor.editable().getClientRect() : editor.window.getFrame().getClientRect();
+			var elementFrame = markerElement.getClientRect();
+			inlineToolbar.create( markerElement );
+			var inlineToobarRect = inlineToolbar.parts.panel.getClientRect();
+			assert.areEqual( frame.left + elementFrame.left + elementFrame.width / 2 - 50, inlineToobarRect.left, 'left align' );
+			//We have to add 1px because of border
+			assert.areEqual( inlineToobarRect.top + inlineToolbar.height + inlineToolbar.triangleHeight + 1,
+				frame.top + frame.height, 'top align' );
+			inlineToolbar.destroy();
+			inlineToolbar = null;
+		},
 
-				this.markerElement = this.editor.editable().findOne( '#marker' );
+		'test divaera - out of view - hcenter top': function( editor ) {
+			var inlineToolbar = new CKEDITOR.ui.inlineToolbarView( editor, {
+				width: 100,
+				height: 200
+			} );
+			var markerElement = editor.editable().findOne( '#marker' );
+			var frame = editor.editable().isInline() ? editor.editable().getClientRect() : editor.window.getFrame().getClientRect();
+			var elementFrame = markerElement.getClientRect();
+			markerElement.getParent().getNext().scrollIntoView( true );
+			inlineToolbar.create( markerElement );
+			var inlineToobarRect = inlineToolbar.parts.panel.getClientRect();
+			assert.areEqual( frame.left + elementFrame.left + elementFrame.width / 2 - 50, inlineToobarRect.left, 'left align' );
+			assert.areEqual( frame.top, inlineToobarRect.top - inlineToolbar.triangleHeight, 'top align' );
+			inlineToolbar.destroy();
+			inlineToolbar = null;
+		}
+	};
+	tests = bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests );
 
-				// Make sure that global window is scrolled down to 0,0.
-				window.scrollTo( 0, 0 );
-
-				if ( !this._getFrameMethodReplaced ) {
-					// The problem is also window.getFrame().getClientRect() as it returns different results from dashboard and directly.
-					this._getFrameMethodReplaced = true;
-					var orig = this.editor.window.getFrame;
-
-					this.editor.window.getFrame = function() {
-						var ret = orig.call( this );
-
-						if ( ret ) {
-							ret.getClientRect = function() {
-								return { height: 300, width: 300, left: 1, bottom: 643, right: 301, top: 343 };
-							};
-						}
-
-						return ret;
-					};
-				}
-				this.moveSpy = spy( this.inlineToolbar, 'move' );
-			},
-
-			tearDown: function() {
-				this.moveSpy.restore();
-				this.getViewPaneSize.restore();
-				this.inlineToolbar.destroy();
-				this.inlineToolbar = null;
-			},
-
-
-			'test classic - out of view - bottom center': function() {
-				// Position acts as if the editor viewport was at position x: 260, 0.
-				this.markerElement.getClientRect = sinon.stub().returns( { height: 15, width: 25, left: 129, bottom: 416.34375, right: 154, top: 401.34375 } );
-				balloonTestsTools.attachBalloon( this.inlineToolbar, this.markerElement );
-
-				balloonTestsTools.assertMoveTo( this.moveSpy, 92.5, 422 );
-
-			},
-
-			'test classic - out of view - hcenter top': function() {
-				// Position acts as if the editor viewport was at position x: 260, 500.
-				this.markerElement.getClientRect = sinon.stub().returns( { height: 15, width: 25, left: 129, bottom: -75.65625, right: 154, top: -90.65625 } );
-				balloonTestsTools.attachBalloon( this.inlineToolbar, this.markerElement );
-
-				balloonTestsTools.assertMoveTo( this.moveSpy, 92.5, 363 );
-			}
-		};
 	bender.test( tests );
 } )();
