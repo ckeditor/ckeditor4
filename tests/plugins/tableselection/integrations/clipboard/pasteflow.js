f41f9d67ca077d3cd390a5268d20f72d75651124
Mateusz Samsel <m.samsel@cksource.com>
2017-10-31 13:47:44 +0100

    Split test into separate editors to remove possibility of failings on Edge 16+.

--- tests/plugins/tableselection/integrations/clipboard/pasteflow.js
+++ tests/plugins/tableselection/integrations/clipboard/pasteflow.js
@@ -6,62 +6,68 @@
 ( function() {
 	'use strict';
 
-	bender.editors = {
-		classic: {},
-		inline: {
-			creator: 'inline'
-		}
-	};
+	var getNumber = ( function() {
+		var i = 0;
+		return function() {
+			return i++;
+		};
+	} )();
 
-	function testPasteFlow( bot, caseName, fixture ) {
-		var editor = bot.editor;
-
-		bender.tools.testInputOut( caseName, function( source, expected ) {
-			var beforePasteStub = sinon.stub(),
+	function testPasteFlow( caseName, fixture, creator ) {
+		bender.editorBot.create( {
+			creator: creator,
+			name: creator + '-' + caseName + '-' + getNumber()
+		}, function( bot ) {
+			var editor = bot.editor;
+			bender.tools.testInputOut( caseName, function( source, expected ) {
+				var beforePasteStub = sinon.stub(),
 				pasteStub = sinon.stub(),
 				afterPasteStub = sinon.stub(),
 				removeBeforeStub,
 				removePasteStub,
 				removeAfterStub;
 
-			bot.setHtmlWithSelection( source );
+				bot.setHtmlWithSelection( source );
 
-			removeBeforeStub = editor.on( 'beforePaste', beforePasteStub );
-			removePasteStub = editor.on( 'paste', pasteStub, null, null, 0 );
-			removeAfterStub = editor.on( 'afterPaste', afterPasteStub );
+				removeBeforeStub = editor.on( 'beforePaste', beforePasteStub );
+				removePasteStub = editor.on( 'paste', pasteStub, null, null, 0 );
+				removeAfterStub = editor.on( 'afterPaste', afterPasteStub );
 
-			editor.once( 'afterPaste', function() {
-				resume( function() {
-					bender.assert.beautified.html( expected, bot.editor.getData() );
+				editor.once( 'afterPaste', function() {
+					resume( function() {
+						bender.assert.beautified.html( expected, bot.editor.getData() );
 
-					removeBeforeStub.removeListener();
-					removePasteStub.removeListener();
-					removeAfterStub.removeListener();
+						removeBeforeStub.removeListener();
+						removePasteStub.removeListener();
+						removeAfterStub.removeListener();
 
-					assert.areSame( 1, beforePasteStub.callCount, 'beforePaste even count' );
-					assert.areSame( 1, pasteStub.callCount, 'paste event count' );
-					assert.areSame( 1, afterPasteStub.callCount, 'afterPaste event count' );
-				} );
-			}, null, null, 999 );
+						assert.areSame( 1, beforePasteStub.callCount, 'beforePaste even count' );
+						assert.areSame( 1, pasteStub.callCount, 'paste event count' );
+						assert.areSame( 1, afterPasteStub.callCount, 'afterPaste event count' );
+					} );
+				}, null, null, 999 );
 
-			bender.tools.emulatePaste( editor, CKEDITOR.document.getById( fixture ).getOuterHtml() );
+				bender.tools.emulatePaste( editor, CKEDITOR.document.getById( fixture ).getOuterHtml() );
 
-			wait();
+				wait();
+			} );
 		} );
 	}
 
-	// Tests breaks bender run in Edge 16+ when in reversed order (#1047).
 	var tests = {
-		'test paste flow (non-tabular content)': function( editor, bot ) {
-			testPasteFlow( bot, 'nontabular-paste', 'paragraph' );
+		'test paste flow (tabular content) (replace)': function() {
+			testPasteFlow( 'tabular-paste', '2cells1row', 'replace' );
 		},
-
-		'test paste flow (tabular content)': function( editor, bot ) {
-			testPasteFlow( bot, 'tabular-paste', '2cells1row' );
+		'test paste flow (tabular content) (inline)': function() {
+			testPasteFlow( 'tabular-paste', '2cells1row', 'inline' );
+		},
+		'test paste flow (non-tabular content) (replace)': function() {
+			testPasteFlow( 'nontabular-paste', 'paragraph', 'replace' );
+		},
+		'test paste flow (non-tabular content) (inline)': function() {
+			testPasteFlow( 'nontabular-paste', 'paragraph', 'inline' );
 		}
 	};
-
-	tests = bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests );
 
 	tableSelectionHelpers.ignoreUnsupportedEnvironment( tests );
 
