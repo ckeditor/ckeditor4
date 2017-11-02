c04206a96bd9197fb1ef776e1f6fb7341e148104
Wojciech Wozniak <wwozniak92@gmail.com>
2017-11-02 14:24:19 +0100

    unit test for button wrapping

--- tests/plugins/inlinetoolbar/buttons.js
+++ tests/plugins/inlinetoolbar/buttons.js
@@ -1,5 +1,5 @@
 /* bender-tags: inlinetoolbar */
-/* bender-ckeditor-plugins: inlinetoolbar,button */
+/* bender-ckeditor-plugins: inlinetoolbar,button,richcombo */
 
 ( function() {
 	'use strict';
@@ -16,8 +16,8 @@
 				} )
 			} );
 			assert.isInstanceOf( CKEDITOR.ui.button, panel.getItem( 'cut' ), 'Registered button type.' );
-
 		},
+
 		'test removing buttion': function() {
 			var panel = new CKEDITOR.ui.inlineToolbar( this.editor );
 			panel.addItems( {
@@ -29,7 +29,18 @@
 			assert.isInstanceOf( CKEDITOR.ui.button, panel.getItem( 'cut' ), 'Registered button type.' );
 			panel.deleteItem( 'cut' );
 			assert.isUndefined( panel.getItem( 'cut' ), 'The button should be deleted.' );
+		},
 
+		'test group wrapping': function() {
+			var panel = new CKEDITOR.ui.inlineToolbar( this.editor );
+			panel.addItems( {
+				cut: new CKEDITOR.ui.button( {
+					label: 'test',
+					command: 'cut'
+				} )
+			} );
+			panel._view.renderItems( panel._items );
+			assert.isNotNull( panel._view.parts.content.findOne( '.cke_toolgroup' ), 'Button should be wrapped in group' );
 		}
 	} );
 } )();
