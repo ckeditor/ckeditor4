e0a0c6aab716a728d4546cfd1639a4fb474f366c
Marek Lewandowski <m.lewandowski@cksource.com>
2017-11-05 13:38:38 +0100

    Since #1094 was fixed, there's no longer a need to double check element name in widget upcasts handler.
    Also removed fake elements check, as fake elements are only imgs, not figures.

--- plugins/easyimage/plugin.js
+++ plugins/easyimage/plugin.js
@@ -107,12 +107,7 @@
 
 				upcasts: {
 					figure: function( element ) {
-						// http://dev.ckeditor.com/ticket/11110 Don't initialize on pasted fake objects.
-						if ( element.attributes[ 'data-cke-realelement' ] ) {
-							return;
-						}
-
-						if ( element.name === 'figure' && ( !figureClass || element.hasClass( figureClass ) ) ) {
+						if ( !figureClass || element.hasClass( figureClass ) ) {
 							return element;
 						}
 					}
