260529517d253d50a49c9fc40792dfa3d9906c37
Wojciech Wozniak <wwozniak92@gmail.com>
2017-11-10 14:28:56 +0100

    Inline toolbar context first documentation draft, unit tests for active features, major fixes.

---
+++ tests/plugins/inlinetoolbar/context.js
@@ -0,0 +1,41 @@
+/* bender-tags: inlinetoolbar */
+/* bender-ckeditor-plugins: inlinetoolbar,button,richcombo,basicstyles,format  */
+
+( function() {
+	'use strict';
+
+	bender.editor = {};
+
+	bender.test( {
+		'test adding button': function() {
+			var panel = this.editor.inlineToolbar.context( { buttons: 'Bold' } );
+			assert.isInstanceOf( CKEDITOR.ui.button, panel.getItem( 'Bold' ), 'Registered button type.' );
+			panel.destroy();
+		},
+
+		'test adding rich combo': function() {
+			var panel = this.editor.inlineToolbar.context( { buttons: 'Format' } );
+			assert.isInstanceOf( CKEDITOR.ui.richCombo, panel.getItem( 'Format' ), 'Registered richCombo type.' );
+			panel.destroy();
+		},
+		/*'test context adding': function() {
+
+		},
+
+		'test focus change': function() {
+
+		},
+
+		'test widget adding': function() {
+
+		},
+		'Test widget focusing'*/
+
+		'test context destroy': function() {
+			var stub = sinon.stub( this.editor.inlineToolbar, 'destroy' );
+			bender.editor.destroy();
+			assert.isTrue( stub.called, 'Event show should be fired when editor is destroyed.' );
+			stub.restore();
+		}
+	} );
+} )();
