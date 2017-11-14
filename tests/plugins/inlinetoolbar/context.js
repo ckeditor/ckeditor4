b2b824a5a0c3814e0d569d06bec84b4577517b5d
Marek Lewandowski <m.lewandowski@cksource.com>
2017-11-14 15:00:16 +0100

    Tests: added tests for context.refresh - making sure that a proper elements path is used.

--- tests/plugins/inlinetoolbar/context.js
+++ tests/plugins/inlinetoolbar/context.js
@@ -1,4 +1,4 @@
-/* bender-tags: inlinetoolbar */
+/* bender-tags: inlinetoolbar,context */
 /* bender-ckeditor-plugins: inlinetoolbar,button,richcombo,basicstyles,format */
 
 ( function() {
@@ -38,6 +38,47 @@
 		// 	stub.restore();
 		// },
 
+		'test context.refresh picks up editor element path if none provided': function() {
+			var options = {
+					refresh: sinon.stub()
+				},
+				context = this.editor.plugins.inlinetoolbar.create( options ),
+				elementsPathArgument;
+
+			this.editorBot.setHtmlWithSelection( '<p><strong>Fo^o</strong></p>' );
+
+			context.refresh();
+
+			assert.areSame( 1, options.refresh.callCount, 'options.refresh call count' );
+
+			elementsPathArgument = options.refresh.args[ 0 ][ 1 ];
+
+			assert.isInstanceOf( CKEDITOR.dom.elementPath, elementsPathArgument, 'Elements path argument type' );
+			assert.areSame( 'body,p,strong', this._elementPathSerialize( elementsPathArgument ), 'Elements path used' );
+		},
+
+		'test context.refresh uses custom element path if explicitly provided': function() {
+			var options = {
+					refresh: sinon.stub()
+				},
+				context = this.editor.plugins.inlinetoolbar.create( options ),
+				customElementPath,
+				elementsPathArgument;
+
+			this.editorBot.setHtmlWithSelection( '<p><strong>Fo^o</strong><em>bar</em></p>' );
+
+			customElementPath = new CKEDITOR.dom.elementPath( this.editor.editable().findOne( 'em' ).getFirst(), this.editor.editable() );
+
+			context.refresh( customElementPath );
+
+			assert.areSame( 1, options.refresh.callCount, 'options.refresh call count' );
+
+			elementsPathArgument = options.refresh.args[ 0 ][ 1 ];
+
+			assert.isInstanceOf( CKEDITOR.dom.elementPath, elementsPathArgument, 'Elements path argument type' );
+			assert.areSame( 'body,p,em', this._elementPathSerialize( elementsPathArgument ), 'Elements path used' );
+		},
+
 		'test exposes editor.plugins.inlinetoolbar.create': function() {
 			var ContextTypeStub = sinon.stub( CKEDITOR.plugins.inlinetoolbar, 'context' ),
 				ret = this.editor.plugins.inlinetoolbar.create( {} );
@@ -46,6 +87,13 @@
 
 			assert.isInstanceOf( ContextTypeStub, ret, 'Ret type' );
 			sinon.assert.calledWithExactly( ContextTypeStub, this.editor, {} );
+		},
+
+		// Returns a string with given `elmeentPath` member names, joined with comma, e.g. "body,ul,li,a,strong".
+		_elementPathSerialize: function( elmeentPath ) {
+			return CKEDITOR.tools.array.map( elmeentPath.elements, function( elem ) {
+				return elem.getName();
+			} ).reverse().join( ',' );
 		}
 	} );
 } )();
