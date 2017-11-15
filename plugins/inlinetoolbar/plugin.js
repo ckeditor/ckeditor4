d2fc9ababa1da05cff74b65208e914c5449becfa
Marek Lewandowski <m.lewandowski@cksource.com>
2017-11-15 12:27:52 +0100

    Allow widget definitions to be passed both as a string or an array of strings.

--- plugins/inlinetoolbar/plugin.js
+++ plugins/inlinetoolbar/plugin.js
@@ -247,6 +247,10 @@
 			var widgetNames = this.options.widgets,
 				curWidgetName = this.editor.widgets && this.editor.widgets.focused && this.editor.widgets.focused.name;
 
+			if ( typeof widgetNames === 'string' ) {
+				widgetNames = widgetNames.split( ',' );
+			}
+
 			return CKEDITOR.tools.array.indexOf( widgetNames, curWidgetName ) !== -1;
 		},
 
@@ -364,7 +368,7 @@
 				/**
 				 * @param {Object} options Config object for Inline Toolbar.
 				 * @param {String} [options.elements] ACF selector. If any elements in the path matches against it, the toolbar will be shown.
-				 * @param {String[]} [options.widgets] An array of widget names that should trigger this toolbar.
+				 * @param {String[]/String} [options.widgets] An array of widget names that should trigger this toolbar. Alternatively can be passed as a comma-separated string.
 				 * @param {Function} [options.refresh] A function that determines whether the toolbar should be visible for a given `elementPath`.
 				 *
 				 * It gets following parameters:
