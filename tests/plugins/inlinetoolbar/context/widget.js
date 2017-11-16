b77c8f18b89083b4e8847774ecedeb5e95097090
Marek Lewandowski <m.lewandowski@cksource.com>
2017-11-16 14:35:32 +0100

    Corrected element pointed by the inline toolbar after matching a widget.

--- tests/plugins/inlinetoolbar/context/widget.js
+++ tests/plugins/inlinetoolbar/context/widget.js
@@ -133,6 +133,27 @@
 				} );
 		},
 
+		'test widget toolbar points to a proper element': function() {
+			// Toolbar matched to a widget, should point to a widget element.
+			var editor = this.editor,
+				context = this._getContextStub( [ 'pointing' ] );
+
+			editor.widgets.add( 'pointing' );
+
+			this.editorBot.setData(
+				'<p>foo</p><div data-widget="pointing" id="w1">foo</div></p>',
+				function() {
+					var widget = widgetTestsTools.getWidgetById( editor, 'w1' );
+
+					sinon.stub( context, 'show' );
+
+					widget.focus();
+
+					sinon.assert.calledWithExactly( context.show, widget.element );
+					assert.isTrue( true );
+				} );
+		},
+
 		/*
 		 * Returns a Context instance with toolbar show/hide methods stubbed.
 		 *
