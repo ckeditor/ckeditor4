a48a2652db198b3692ddcd16bf4e8ec05777ab69
Krzysztof Krztoń <k.krzton@cksource.com>
2017-11-17 17:18:15 +0100

    Use '.example' domain.

--- tests/core/ckeditor/basepathglobal.js
+++ tests/core/ckeditor/basepathglobal.js
@@ -10,7 +10,7 @@
 		folderPath = path + testDir.slice( 0, testDir.lastIndexOf( '/' ) ),
 		expectedEditorPath = '/apps/ckeditor/',
 		query = CKEDITOR.timestamp ? '?t=' + CKEDITOR.timestamp : '',
-		secondDomainName = 'sub.ckeditor.custom',
+		secondDomainName = 'sub.ckeditor.example',
 		port = window.location.port;
 
 
