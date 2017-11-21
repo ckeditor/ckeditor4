dbbc2bd3bcfe1682fb7463cfd5db4d9391de6d04
Wojciech Wozniak <wwozniak92@gmail.com>
2017-11-21 17:31:33 +0100

    Inline toolbar proper dictonary initialization.

--- plugins/inlinetoolbar/plugin.js
+++ plugins/inlinetoolbar/plugin.js
@@ -86,9 +86,9 @@
 		 * Menu items added to inline toolbar.
 		 *
 		 * @private
-		 * @property {CKEDITOR.ui.button[]/CKEDITOR.ui.richCombo[]}
+		 * @property {Object.<string, CKEDITOR.ui.button|CKEDITOR.ui.richCombo>}
 		 */
-		this._items = [];
+		this._items = {};
 	};
 
 	/**
