66bc74ed431c2da43bb378def4f03d832ebec78c
Marek Lewandowski <m.lewandowski@cksource.com>
2017-11-22 14:45:51 +0100

    Tests: cleanup code style/typos.

--- tests/plugins/inlinetoolbar/context/cssselector.js
+++ tests/plugins/inlinetoolbar/context/cssselector.js
@@ -75,8 +75,8 @@
 		},
 
 		/*
-			* @param {Boolean} expected What's the expected visibility? If `true` toolbar must be visible.
-			*/
+		 * @param {Boolean} expected What's the expected visibility? If `true` toolbar must be visible.
+		 */
 		_assertToolbarVisible: function( expected, context, msg ) {
 			assert.areSame( expected, context.toolbar._view.parts.panel.isVisible(), msg || 'Toolbar visibility' );
 		}
