a2de34d4c63ec4e0cd2131c7ac7e062456663086
Wojciech Wozniak <wwozniak92@gmail.com>
2017-11-24 10:56:22 +0100

    Unit test for CSS loading in balloon panel.

---
+++ tests/plugins/balloonpanel/cssloading.js
@@ -0,0 +1,17 @@
+/* bender-tags: balloonpanel, bug, 4.8, 1221 */
+/* bender-ckeditor-plugins: balloonpanel */
+
+( function() {
+	'use strict';
+
+	bender.editor = {
+	};
+	CKEDITOR.skinName = 'kama,/apps/ckeditor/skins/kama/';
+	var spy = sinon.spy( CKEDITOR.dom.document.prototype, 'appendStyleSheet' );
+	bender.test( {
+		'test loading css with path': function() {
+			assert.areSame( 'kama,/apps/ckeditor/skins/kama/', CKEDITOR.skinName, 'config skinName should be with path' );
+			sinon.assert.calledWith( spy, 'http://127.0.0.1:1030/apps/ckeditor/plugins/balloonpanel/skins/kama/balloonpanel.css' );
+		}
+	} );
+} )();
