81614201ee5813d669145d639fabc71aa2556909
Marek Lewandowski <m.lewandowski@cksource.com>
2017-11-27 09:24:14 +0100

    Tests: minor code style corrections.

--- tests/plugins/balloonpanel/cssloading.js
+++ tests/plugins/balloonpanel/cssloading.js
@@ -1,13 +1,14 @@
-/* bender-tags: balloonpanel, bug, 4.8, 1221 */
+/* bender-tags: balloonpanel, bug, 4.8.0, 1221 */
 /* bender-ckeditor-plugins: balloonpanel */
 
 ( function() {
 	'use strict';
 
-	bender.editor = {
-	};
+	bender.editor = {};
+
 	CKEDITOR.skinName = 'kama,/apps/ckeditor/skins/kama/';
 	var spy = sinon.spy( CKEDITOR.dom.document.prototype, 'appendStyleSheet' );
+
 	bender.test( {
 		'test loading css with path': function() {
 			assert.areSame( 'kama,/apps/ckeditor/skins/kama/', CKEDITOR.skinName, 'config skinName should be with path' );
