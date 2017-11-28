66346211c232c0d363fdaf3f05086c7de4ec6747
Tomasz Jakut <vepomoc@gmail.com>
2017-11-28 15:56:44 +0100

    Ignore test on built version.

--- tests/plugins/balloonpanel/cssloading.js
+++ tests/plugins/balloonpanel/cssloading.js
@@ -10,6 +10,14 @@
 	var spy = sinon.spy( CKEDITOR.dom.document.prototype, 'appendStyleSheet' );
 
 	bender.test( {
+		_should: {
+			ignore: {
+				// As we don't have straight way to recognise editor's built version,
+				// such trick must be used.
+				'test loading css with path': CKEDITOR.revision !== '%REV%'
+			}
+		},
+
 		'test loading css with path': function() {
 			assert.areSame( 'kama,/apps/ckeditor/skins/kama/', CKEDITOR.skinName, 'config skinName should be with path' );
 			sinon.assert.calledWith( spy, document.location.origin + '/apps/ckeditor/plugins/balloonpanel/skins/kama/balloonpanel.css' );
