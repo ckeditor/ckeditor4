95217dcee885d2743ddd03d0e8a602941527a9cd
Nicholas Hoyt <nhoyt@illinois.edu>
2017-12-07 20:39:10 -0600

    Initial commit

---
+++ custom/a11yfirst.js
@@ -0,0 +1,135 @@
+/**
+* Copyright (c) 2017 University of Illinois - Jon Gunderson and Nicholas Hoyt. All rights reserved.
+* For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.editorConfig = function ( config ) {
+  config.height = 480;
+  config.skin = 'a11yfirst';
+  config.startupFocus = true;
+
+  config.plugins =
+    'a11ychecker,' +
+    'a11yhelp,' +
+    'about,' +
+    'balloonpanel,' +
+    'basicstyles,' +
+    'blockquote,' +
+    'button,' +
+    'clipboard,' +
+    'contextmenu,' +
+    'dialog,' +
+    'dialogui,' +
+    'elementspath,' +
+    'enterkey,' +
+    'entities,' +
+    'fakeobjects,' +
+    'filebrowser,' +
+    'find,' +
+    'floatingspace,' +
+    'floatpanel,' +
+    'horizontalrule,' +
+    'htmlwriter,' +
+    'image,' +
+    'indent,' +
+    'indentlist,' +
+    'language,' +
+    'link,' +
+    'list,' +
+    'liststyle,' +
+    'magicline,' +
+    'maximize,' +
+    'menu,' +
+    'menubutton,' +
+    'notification,' +
+    'panel,' +
+    'pastefromword,' +
+    'pastetext,' +
+    'popup,' +
+    'removeformat,' +
+    'resize,' +
+    'showblocks,' +
+    'showborders,' +
+    'sourcearea,' +
+    'specialchar,' +
+    'tab,' +
+    'table,' +
+    'tableselection,' +
+    'tabletools,' +
+    'toolbar,' +
+    'undo,' +
+    'wysiwygarea,';
+
+  config.extraPlugins =
+    'a11yfirsthelp,' +
+    'a11yformat,' +
+    'a11yheading,' +
+    'a11ystylescombo';
+
+  config.language_list = [
+    'ar:Arabic:rtl',
+    'zh:Chinese',
+    'cs:Czech',
+    'da:Danish',
+    'nl:Dutch',
+    'fi:Finnish',
+    'fr:French',
+    'gd:Gaelic',
+    'de:German',
+    'el:Greek',
+    'he:Hebrew:rtl',
+    'hi:Hindi',
+    'hu:Hungarian',
+    'id:Indonesian',
+    'it:Italian',
+    'ja:Japanese',
+    'ko:Korean',
+    'la:Latin',
+    'no:Norwegian',
+    'fa:Persian:rtl',
+    'pl:Polish',
+    'pt:Portuguese',
+    'ru:Russian',
+    'es:Spanish',
+    'sv:Swedish',
+    'th:Thai',
+    'tr:Turkish',
+    'vi:Vietnamese',
+    'yi:Yiddish'
+  ];
+
+  config.toolbar = [
+    { name: 'heading',        items: [ 'Heading' ] },
+    { name: 'list',           items: [ 'NumberedList', 'BulletedList', 'Indent', 'Outdent' ] },
+    { name: 'link',           items: [ 'Link', 'Unlink', 'Anchor' ] },
+    { name: 'blockformat',    items: [ 'BlockFormat' ] },
+    { name: 'blockquote',     items: [ 'Blockquote', 'ShowBlocks' ] },
+    { name: 'paragraph',      items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight'] },
+    { name: 'misc1',          items: [ 'Image', 'Table' ] },
+    { name: 'a11ychecker',    items: [ 'A11ychecker' ] },
+    { name: 'a11yfirsthelp',  items: [ 'A11yFirstHelp'] },
+    '/',
+    { name: 'undoredo',       items: [ 'Undo', 'Redo'] },
+    { name: 'clipboard',      items: [ 'Cut', 'Copy', 'Paste', 'PasteFromWord' ] },
+    { name: 'search',         items: [ 'Find', 'Replace' ] },
+    { name: 'basicstyles',    items: [ 'Bold', 'Italic'] },
+    { name: 'removeformat',   items: [ 'RemoveFormat' ] },
+    { name: 'inlinestyle',    items: [ 'InlineStyle' ] },
+    { name: 'misc2',          items: [ 'Language', 'SpecialChar' ] }
+  ];
+
+};
+
+CKEDITOR.stylesSet.add ( 'default', [
+  { name: 'Strong',           element: 'strong', overrides: 'b' },
+  { name: 'Emphasis',         element: 'em' , overrides: 'i' },
+  { name: 'Marker',           element: 'span', attributes: { 'class': 'marker' } },
+  { name: 'Inline quotation', element: 'q' },
+  { name: 'Cited work',       element: 'cite' },
+  { name: 'Computer code',    element: 'code' },
+  { name: 'Subscript',        element: 'sub' },
+  { name: 'Superscript',      element: 'sup' },
+  { name: 'Deleted Text',     element: 'del' },
+  { name: 'Inserted Text',    element: 'ins' },
+  { name: 'Strikethrough',    element: 'strike' },
+  { name: 'Underline',        element: 'u' }
+] );
---
+++ custom/ui-library.js
@@ -0,0 +1,16 @@
+/**
+* Copyright (c) 2017 University of Illinois - Jon Gunderson and Nicholas Hoyt. All rights reserved.
+* For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.replace( 'editor', {
+  customConfig: CKEDITOR.basePath + 'custom/a11yfirst.js',
+
+  a11yfirst: {
+    organization: 'University of Illinois Library',
+    a11yPolicyLink: 'http://guides.library.illinois.edu/usersdisabilities/',
+    a11yPolicyLabel: 'Information for Users with Disabilities',
+  },
+
+  headings: 'h1:h4',
+  oneLevel1: true
+} );
