529ae69dc9cfc7c7bc39fbdda86d19c32eeeab05
Nicholas Hoyt <nhoyt@illinois.edu>
2017-12-08 13:09:13 -0600

    Update comments

--- custom/scripts/syncdist.sh
+++ custom/scripts/syncdist.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# This script uses rsync to synchronize (copy or update) the A11yFirst code
+# to the file hierarchy of the CKBuilder output (top-level DISTPATH specified
+# by the first argument) from the development source files in the A11yFirst
+# plugins-dev repository (specified herein as SRC).
+
+# Note: You may need to update the value of the SRC variable to match the
+# location of your plugins-dev checkout.
+
+# For more information on the  distribution build process, see the
+# custom/a11yfirst.js config file.
+
+# custom
+# plugins/a11yfirsthelp
+# plugins/a11yformat
+# plugins/a11yheading
+# plugins/a11ystylescombo
+# plugins/balloonpanel/skins/a11yfirst
+# skins/a11yfirst
+
 if [ $# -eq 0 ]; then
   echo "Syntax:"
   echo "    $0 <distribution-folder-path>"
