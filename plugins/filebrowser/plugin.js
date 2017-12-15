56259abb46d449b4353649252d0d5c3120c1b7f3
Mateusz Samsel <m.samsel@cksource.com>
2017-12-15 11:45:05 +0100

    Change condition to be independent from clipboard plugin.

--- plugins/filebrowser/plugin.js
+++ plugins/filebrowser/plugin.js
@@ -311,7 +311,7 @@
 						if ( uploadFile.call( sender, evt ) ) {
 
 							// Backward compatibility for IE8 and IE9 (https://cksource.tpondemand.com/entity/3117).
-							if ( editor.config.filebrowser_forceSubmit || !( CKEDITOR.plugins.clipboard && CKEDITOR.plugins.clipboard.isFileApiSupported ) ) {
+							if ( editor.config.filebrowser_forceSubmit || CKEDITOR.env.ie && CKEDITOR.env.version <= 9 ) {
 								// Append token preventing CSRF attacks.
 								appendToken( fileInput );
 								return 'form';
