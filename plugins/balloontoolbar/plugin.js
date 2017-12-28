a706711f3f9b9b2cf401e6d81feeb946387b8063
Tomasz Jakut <vepomoc@gmail.com>
2017-12-28 14:18:00 +0100

    Add missing dot.

--- plugins/balloontoolbar/plugin.js
+++ plugins/balloontoolbar/plugin.js
@@ -421,7 +421,7 @@
 				selection = this.editor.getSelection();
 
 				// Shrink the selection so that we're ensured innermost elements are available, so that path for
-				// selection like `foo [<em>bar</em>] baz` also contains `em` element
+				// selection like `foo [<em>bar</em>] baz` also contains `em` element.
 				CKEDITOR.tools.array.forEach( selection.getRanges(), function( range ) {
 					range.shrink( CKEDITOR.SHRINK_ELEMENT, true );
 				} );
