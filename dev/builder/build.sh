11f0f030ba4e78dc178216c1e6c0ea9cb58e8af1
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-01-03 16:59:15 +0100

    Updated license header.

--- dev/builder/build.sh
+++ dev/builder/build.sh
@@ -1,5 +1,5 @@
 ﻿#!/bin/bash
-# Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+# Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
 # For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 
 # Build CKEditor using the default settings (and build.js).
