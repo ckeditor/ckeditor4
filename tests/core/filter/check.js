bc2094a1ea20156fef7c4bd9504b9f0bc4cdf474
Mateusz Samsel <m.samsel@cksource.com>
2018-01-04 15:51:58 +0100

    Adding unit tests.

--- tests/core/filter/check.js
+++ tests/core/filter/check.js
@@ -109,6 +109,7 @@
 			test( true, st( { element: 'p', attributes: { 'class': 'cl1' } } ) );
 			test( true, st( { element: 'a', attributes: { 'class': 'cl1 cl2' } } ) );
 			test( true, st( { element: 'p' } ) );
+			test( true, st( { element: 'a', attributes: { 'class': 'cl2 cl1' } } ) );
 
 			test( false, st( { element: 'p', styles: { height: '10px' } } ) );
 			test( false, st( { element: 'p', attributes: { 'class': 'cl3' } } ) );
