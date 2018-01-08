2531ec6a8b97c9aa3ddf3107e76222e85da800a7
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-01-08 16:15:20 +0100

    Fixed update script.

--- dev/license/updatelicense.sh
+++ dev/license/updatelicense.sh
@@ -1,45 +1,9 @@
-#!/bin/node
+#!/bin/bash
 
-var fs = require( 'fs' ),
-	path = require( 'path' ),
-	destination = process.argv[2],
-	YEAR = new Date().getFullYear(),
-	ACCEPTED_FORMATS = [ '.html', '.txt', '.js', '.md', '.sh', '.css', '.py', '.less', '.php', '.rb' ],
-	EXCLUDED_DIRS = [ '.git' ];
+# Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+# For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 
-if ( !destination ) {
-	console.log( 'Give the path to the directory of the repository.' );
-	process.exit( 0 );
-}
-
-recursivelyUpdateLicenseDate( destination );
-
-function recursivelyUpdateLicenseDate( filepath ) {
-	if ( EXCLUDED_DIRS.indexOf( path.basename( filepath ) ) != -1 ) return;
-
-	var stats = fs.statSync( filepath );
-
-	if ( stats.isDirectory() ) {
-		fs.readdirSync( filepath )
-			.forEach( function( file ) {
-				recursivelyUpdateLicenseDate( path.join( filepath, file ) );
-			} );
-	} else if ( ACCEPTED_FORMATS.indexOf( path.extname( filepath ) ) > -1 ) {
-		updateLicenseDate( filepath );
-	}
-}
-
-function updateLicenseDate( filepath ) {
-	var data = fs.readFileSync( filepath, 'utf8' ),
-		regexp = /(Copyright.*\d{4}.*-.*)\d{4}(.*CKSource)/gi,
-		match = regexp.exec( data ),
-		updated = false;
-
-	while ( match != null ) {
-		updated = true;
-		data = data.replace( match[0], match[1] + YEAR + match[2] );
-		match = regexp.exec( data );
-	}
-
-	if ( updated ) fs.writeFileSync( filepath, data );
-}
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+TOOLPATH="$DIR/updatelicense.js"
+cd $DIR
+node $TOOLPATH
