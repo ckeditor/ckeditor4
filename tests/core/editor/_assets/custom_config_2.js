7627dc478d8d952cbf23bd8e21cc14cfde4590b6
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-01-08 16:19:03 +0100

    Added missing license updates.

--- tests/core/editor/_assets/custom_config_2.js
+++ tests/core/editor/_assets/custom_config_2.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or https://ckeditor.com/legal/ckeditor-oss-license
 */
 
