a3b9aea398bd63a34a3fdbcecbfe9d0ae168cb68
Tomasz Jakut <vepomoc@gmail.com>
2018-01-15 15:23:57 +0100

    Force Edge to run every unit test in new CKEditor's instance.

--- tests/plugins/easyimage/commands.js
+++ tests/plugins/easyimage/commands.js
@@ -18,6 +18,39 @@
 		}
 	};
 
+	// Force Edge to run every test in new CKEditor's instance.
+	function createTestsForEditors( editors, tests ) {
+		var generatedTests = {},
+			test,
+			i = 0;
+
+		function generateTest( name ) {
+			CKEDITOR.tools.array.forEach( editors, function( editor ) {
+				var options = CKEDITOR.tools.object.merge( bender.editors[ editor ], {
+					name: editor + i++
+				} );
+
+				generatedTests[ name + ' (' + editor + ')' ] = function() {
+					bender.editorBot.create( options, function( bot ) {
+						tests[ name ]( bot.editor, bot );
+					} );
+				};
+			} );
+		}
+
+		if ( !CKEDITOR.env.edge ) {
+			return bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests );
+		}
+
+		for ( test in tests ) {
+			if ( test.indexOf( 'test' ) === 0 ) {
+				generateTest( test );
+			}
+		}
+
+		return generatedTests;
+	}
+
 	function assertMenuItemsState( items, asserts ) {
 		CKEDITOR.tools.array.forEach( items, function( item ) {
 			if ( asserts[ item.command ] ) {
@@ -186,6 +219,6 @@
 			}
 		};
 
-	tests = bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests );
+	tests = createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests );
 	bender.test( tests );
 } )();
