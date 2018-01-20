e5db4a672fd4d44d658ef567bbe605eb88725b79
Marek Lewandowski <m.lewandowski@cksource.com>
2018-01-20 19:04:15 +0100

    Moved widget upload feature progress logic back to imagebase. Improved test coverage.

--- tests/plugins/imagebase/features/upload.js
+++ tests/plugins/imagebase/features/upload.js
@@ -49,6 +49,7 @@
 
 			// Array of listeners to be cleared after each TC.
 			this.listeners = [];
+			this.sandbox = sinon.sandbox.create();
 
 			plugin.addImageWidget( editor, imageWidgetDef.name, plugin.addFeature( editor, 'upload', imageWidgetDef ) );
 
@@ -73,9 +74,11 @@
 				listener.removeListener();
 			} );
 
+			this.sandbox.restore();
+			this.editor.widgets.destroyAll( true );
+
 			this.listeners = [];
 			this.editor.uploadRepository.loaders = [];
-			this.editor.widgets.destroyAll( true );
 		},
 
 		setUp: function() {
@@ -83,8 +86,6 @@
 		},
 
 		// To test - test mixed dropped files types (e.g. 1 image, 1 text, 1 unsupported).
-		// To test - progress bar customization.
-		// To test - multiple widgets can use the feature side by side.
 		// To test - edge case: changing mode during upload.
 		// To test - edge case: dropping into readonly editor.
 
@@ -226,6 +227,95 @@
 				callback: function( widgets ) {
 					assert.areSame( 1, widgets.length, 'Widgets count' );
 					assert.areSame( 'testTextWidget', widgets[ 0 ].name, 'Widget name' );
+				}
+			} );
+		},
+
+		'test default progress reporter': function() {
+			var imageBase = CKEDITOR.plugins.imagebase,
+				createForElementSpy = this.sandbox.spy( imageBase.progressBar, 'createForElement' ),
+				bindLoaderSpy = this.sandbox.spy( imageBase.progressBar.prototype, 'bindLoader' ),
+				editor = this.editor;
+
+			this._assertPasteFiles( this.editor, {
+				files: [ bender.tools.getTestPngFile() ],
+				callback: function( widgets ) {
+					assert.areSame( 1, createForElementSpy.callCount, 'ProgressBar.createForElement call count' );
+
+					sinon.assert.calledWithExactly( createForElementSpy, widgets[ 0 ].element );
+
+					sinon.assert.calledWithExactly( bindLoaderSpy, editor.uploadRepository.loaders[ 0 ] );
+				}
+			} );
+		},
+
+		'test progress reporter customization': function() {
+			var CustomProgress = this.sandbox.spy( CKEDITOR.plugins.imagebase, 'progressBar' ),
+				widgetDefinition = this.editor.widgets.registered.testImageWidget,
+				originalProgress = widgetDefinition.progressIndicatorType;
+
+			CustomProgress.createForElement = sinon.spy( function() {
+				return new CustomProgress();
+			} );
+
+			widgetDefinition.progressIndicatorType = CustomProgress;
+
+
+			this._assertPasteFiles( this.editor, {
+				files: [ bender.tools.getTestPngFile() ],
+				callback: function() {
+					widgetDefinition.progressIndicatorType = originalProgress;
+
+					assert.areSame( 1, CustomProgress.callCount, 'ProgressBar.createForElement call count' );
+				}
+			} );
+		},
+
+		'test preventing default progress reporter with event': function() {
+			// This test is a bit tricky, we need to add a new widget so that we can hook to widget.init method because
+			// widget#instanceCreated event is fired too late for this assertion.
+			var imageBase = CKEDITOR.plugins.imagebase,
+				createForElementSpy = this.sandbox.spy( imageBase.progressBar, 'createForElement' ),
+				editor = this.editor,
+				disposableListeners = this.listeners,
+				def = {
+					name: 'progressPrevent',
+					supportedTypes: /text\/html/,
+					init: function() {
+						disposableListeners.push( this.on( 'uploadBegan', function( evt ) {
+							evt.cancel();
+						} ) );
+					}
+				};
+
+			imageBase.addImageWidget( editor, def.name, imageBase.addFeature( editor, 'upload', def ) );
+
+			this._assertPasteFiles( editor, {
+				files: [ getTestHtmlFile() ],
+				callback: function() {
+					// Remove progressPrevent widget, not to affect other test cases.
+					editor.widgets.destroyAll( true );
+					delete editor.widgets.registered.progressPrevent;
+
+					assert.areSame( 0, createForElementSpy.callCount, 'ProgressBar.createForElement call count' );
+				}
+			} );
+		},
+
+		'test preventing default progress reporter with widgetDefinition.progressIndicatorType': function() {
+			var createForElementSpy = this.sandbox.spy( CKEDITOR.plugins.imagebase.progressBar, 'createForElement' ),
+				widgetDefinition = this.editor.widgets.registered.testImageWidget,
+				originalProgress = widgetDefinition.progressIndicatorType;
+
+			// Setting to null will disable loading bar.
+			widgetDefinition.progressIndicatorType = null;
+
+			this._assertPasteFiles( this.editor, {
+				files: [ bender.tools.getTestPngFile() ],
+				callback: function() {
+					widgetDefinition.progressIndicatorType = originalProgress;
+
+					assert.areSame( 0, createForElementSpy.callCount, 'ProgressBar.createForElement call count' );
 				}
 			} );
 		},
