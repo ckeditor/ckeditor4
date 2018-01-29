2dfdd869eab30b39198bb346509df7c45f28ff18
Tomasz Jakut <vepomoc@gmail.com>
2018-01-29 13:53:10 +0100

    Update tests.

--- tests/plugins/easyimage/config.js
+++ tests/plugins/easyimage/config.js
@@ -14,6 +14,7 @@
 				// Widget is identified by id.
 				extraAllowedContent: 'figure[id]',
 				easyimage_class: 'customClass',
+				easyimage_fullClass: 'customFullClass',
 				easyimage_sideClass: 'customSideClass'
 			}
 		},
@@ -52,6 +53,24 @@
 				nameCreated: 'easyimage',
 				html: CKEDITOR.document.getById( 'changedClass' ).getHtml(),
 				bot: this.editorBots.classCustomized
+			} );
+		},
+
+		'test easyimage_fullClass - changed': function() {
+			var bot = this.editorBots.classCustomized;
+
+			bot.setData( CKEDITOR.document.getById( 'changedClass' ).getHtml(), function() {
+				var editor = bot.editor,
+					widgetInstance = widgetTestsTools.getWidgetById( editor, 'customSideId', true );
+
+				widgetInstance.focus();
+
+				// IE11 for some reasons needs to have the command state force refreshed, after focusing the widget with API only.
+				editor.commands.easyimageFull.refresh( editor, editor.elementPath() );
+
+				editor.execCommand( 'easyimageFull' );
+
+				assert.beautified.html( CKEDITOR.document.getById( 'expectedCustomFullClass' ).getHtml(), editor.getData() );
 			} );
 		},
 
