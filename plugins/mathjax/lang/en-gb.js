7a0ed86381a69c0e00b135cfab70c74b8ad6c611
myii <myii@users.noreply.github.com>
2018-02-07 17:09:31 +0000

    Fix typo in MathJax `en-gb.js`

--- plugins/mathjax/lang/en-gb.js
+++ plugins/mathjax/lang/en-gb.js
@@ -5,7 +5,7 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 CKEDITOR.plugins.setLang( 'mathjax', 'en-gb', {
 	title: 'Mathematics in TeX',
 	button: 'Math',
-	dialogInput: 'Write you TeX here',
+	dialogInput: 'Write your TeX here',
 	docUrl: 'http://en.wikibooks.org/wiki/LaTeX/Mathematics',
 	docLabel: 'TeX documentation',
 	loading: 'loading...',
