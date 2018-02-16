a2d31b7fa537a181e31d248132d44b77d2d73e76
Tomasz Jakut <vepomoc@gmail.com>
2018-02-16 17:35:39 +0100

    Update unit test.

--- tests/plugins/imagebase/features/caption.js
+++ tests/plugins/imagebase/features/caption.js
@@ -443,7 +443,6 @@
 			}
 		} ),
 
-
 		// (#1776)
 		'test empty caption placeholder is hidden when blurred': function( editor, bot ) {
 			addTestWidget( editor );
@@ -468,7 +467,7 @@
 			} );
 		},
 
-		// #tp3384
+		// #1646
 		'test caption placeholder integration with basicstyles': createToggleTest( {
 			fixture: 'toggleOneEmpty',
 			initial: false,
@@ -476,8 +475,17 @@
 			blur: false,
 
 			customFocus: function( widget ) {
+				var range = widget.editor.createRange(),
+					caption = widget.parts.caption;
+
 				widget.focus();
-				widget.parts.caption.focus();
+				caption.focus();
+
+				// In Safari and IE11 focus is not enough to move selection.
+				range.setStart( caption.getChild( 0 ), 0 );
+				range.collapse();
+				range.select();
+
 				widget.editor.execCommand( 'bold' );
 			},
 
