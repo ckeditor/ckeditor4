0c2c82979becbb22b0d4a0387a7eb77db508b35b
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-03-05 14:38:44 +0100

    Removed invalid license header.

--- tests/plugins/textwatcher/textwatcher.js
+++ tests/plugins/textwatcher/textwatcher.js
@@ -1,8 +1,3 @@
-/**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
-
 /* bender-tags: editor */
 /* bender-ckeditor-plugins: textwatcher */
 
