6598b6f390dbbe95818db1bc66925f0d338853b9
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-03-08 10:44:01 +0100

    Added unit test for button with custom style and button without icon

--- tests/plugins/button/button.js
+++ tests/plugins/button/button.js
@@ -5,13 +5,21 @@ var customCls = 'my_btn';
 
 bender.editor = {
 	config: {
-		toolbar: [ [ 'custom_btn' ] ],
+		toolbar: [ [ 'custom_btn', 'custom_style_btn', 'iconless_btn' ] ],
 		on: {
 			pluginsLoaded: function( evt ) {
 				var ed = evt.editor;
 				ed.ui.addButton( 'custom_btn', {
 					label: 'button with custom class',
 					className: customCls
+				} );
+				ed.ui.addButton( 'custom_style_btn', {
+					label: 'button with custom style',
+					style: 'background: red'
+				} );
+				ed.ui.addButton( 'iconless_btn', {
+					label: 'button without icon',
+					noicon: true
 				} );
 			}
 		}
@@ -26,5 +34,15 @@ bender.test( {
 		assert.isTrue( btnEl.hasClass( 'cke_button' ), 'check ui type class name' );
 		assert.isTrue( btnEl.hasClass( 'cke_button__custom_btn' ), 'check named ui type class name' );
 		assert.isTrue( btnEl.hasClass( customCls ), 'check ui item custom class name' );
+	},
+	'test button inline style': function() {
+		var btn = this.editor.ui.get( 'custom_style_btn' ),
+			btnEl = CKEDITOR.document.getById( btn._.id );
+		assert.isTrue( btnEl.getStyle( 'background' ) === 'red' );
+	},
+	'test button iconless': function() {
+		var btn = this.editor.ui.get( 'iconless_btn' ),
+			btnEl = CKEDITOR.document.getById( btn._.id );
+		assert.isNull( btnEl.findOne( '.cke_button_icon' ) );
 	}
 } );
