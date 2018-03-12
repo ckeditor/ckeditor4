18a2008c68ad82326a5d53972e7395f58a9842a4
Jon Gunderson <jongund@illinois.edu>
2018-03-12 16:23:55 -0500

    Added period to one of the of one of the messages

--- plugins/a11ylink/lang/en.js
+++ plugins/a11ylink/lang/en.js
@@ -75,6 +75,6 @@ CKEDITOR.plugins.setLang( 'a11ylink', 'en', {
 	msgUrlDisplayText: 'For accessibility reasons the URL is considered a poor choice as the display text for the link.\n\nAre you sure you want to continue?',
 	msgEmailDisplayText: 'For accessibility reasons the e-mail address is considered a poor choice as the display text for the link.\n\nAre you sure you want to continue?',
 	msgEmptyDisplayText: 'Please type the display text for the link',
-	msgNoAnchors: 'No anchors available in the document. The link will be inserted, but will not link to an anchor',
+	msgNoAnchors: 'No anchors available in the document. The link will be inserted, but will not link to an anchor.',
 	msgNoAnchorSelected: 'The link is not associated with an anchor. Do you want to continue?'
 } );
