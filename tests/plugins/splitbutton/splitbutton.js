fb079f2689fdaa6fad09bf67cfa2a9e3a7d39903
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-03-16 11:24:17 +0100

    Improved previous test and added new test case

--- tests/plugins/splitbutton/splitbutton.js
+++ tests/plugins/splitbutton/splitbutton.js
@@ -1,60 +1,116 @@
 /* bender-tags: editor, splitbutton */
 /* bender-ckeditor-plugins: toolbar, splitbutton, basicstyles */
+( function() {
+	'use strict';
 
-bender.editor = {
-	config: {
-		on: {
-			pluginsLoaded: function( evt ) {
-				var editor = evt.editor;
-				editor.ui.add( 'teststylesplit', CKEDITOR.UI_SPLITBUTTON, {
-					label: 'Basic Styles',
-					items: [ {
-						command: 'bold',
-						icon: 'bold',
-						'default': true
-					}, {
-						command: 'italic',
-						icon: 'italic'
-					}, {
-						command: 'underline',
-						icon: 'underline'
-					}, {
-						command: 'strike',
-						icon: 'strike'
-					}, {
-						command: 'subscript',
-						icon: 'subscript'
-					}, {
-						command: 'superscript',
-						icon: 'superscript'
-					} ]
-				} );
+	function assertMenuButtonsState( editor ) {
+		editor.once( 'menuShow', function( evt ) {
+			var editor = evt.editor,
+				menu = editor.ui.instances.teststylesplit._.menu,
+				command,
+				item,
+				key,
+				menuButton;
+
+			for ( key in menu.items ) {
+				item = menu.items[ key ];
+				command = editor.getCommand( item.command );
+				menuButton = menu._.element.findOne( '.' + item.className );
+
+				switch ( command.state ) {
+					case 1:
+						assert.isTrue( menuButton.hasClass( 'cke_menubutton_on', 'Command state is 1, menuButton should have class `cke_menubutton_on`.' ) );
+						break;
+					case 2:
+						assert.isTrue( menuButton.hasClass( 'cke_menubutton_off', 'Command state is 1, menuButton should have class `cke_menubutton_off`.' ) );
+						break;
+				}
+			}
+		} );
+	}
+
+	bender.editor = {
+		config: {
+			on: {
+				pluginsLoaded: function( evt ) {
+					var editor = evt.editor;
+					editor.ui.add( 'teststylesplit', CKEDITOR.UI_SPLITBUTTON, {
+						label: 'Basic Styles',
+						items: [ {
+							command: 'bold',
+							icon: 'bold',
+							'default': true
+						}, {
+							command: 'italic',
+							icon: 'italic'
+						}, {
+							command: 'underline',
+							icon: 'underline'
+						}, {
+							command: 'strike',
+							icon: 'strike'
+						}, {
+							command: 'subscript',
+							icon: 'subscript'
+						}, {
+							command: 'superscript',
+							icon: 'superscript'
+						} ]
+					} );
+				}
 			}
 		}
-	}
-};
+	};
 
-bender.test( {
-	'test splitbutton in toolbar': function() {
-		var splitButton = this.editor.ui.get( 'teststylesplit' ),
-			arrow = CKEDITOR.document.getById( splitButton._.id ),
-			cl,
-			i,
-			item,
-			items = {},
-			key;
+	bender.test( {
+		'test splitbutton rendered in toolbar': function() {
+			var splitButton = this.editor.ui.get( 'teststylesplit' ),
+				arrow = CKEDITOR.document.getById( splitButton._.id ),
+				children = arrow.getParent().$.childNodes,
+				cl,
+				i,
+				item,
+				items = {},
+				key;
 
-		assert.isTrue( arrow.getParent().hasClass( 'cke_splitbutton' ) );
+			// Test if splitbutton is rendered in toolbar.
+			assert.isTrue( arrow.getParent().getParent().hasClass( 'cke_toolgroup' ) );
 
-		var children = arrow.getParent().$.childNodes;
-		for ( i = 0; i < children.length; i++ ) {
-			item = children[ i ];
-			cl = item.classList[ 1 ].substring( 12, item.classList[ 1 ].length );
-			items[ cl ] = cl;
+			// Test if each of splitbutton items are represented by buttons in toolbar
+			for ( i = 0; i < children.length; i++ ) {
+				item = children[ i ];
+				cl = item.classList[ 1 ].substring( 12, item.classList[ 1 ].length );
+				items[ cl ] = cl;
+			}
+			for ( key in splitButton.items ) {
+				key = key.substring( 0, key.length - 1 );
+				assert.isTrue( key in items );
+			}
+		},
+		'test state of splitbutton buttons': function() {
+			var splitButton = this.editor.ui.get( 'teststylesplit' ),
+				arrow = CKEDITOR.document.getById( splitButton._.id ),
+				splitButtonElement = arrow.getParent(),
+				button,
+				key,
+				command;
+
+			for ( key in splitButton.items ) {
+				command = this.editor.getCommand( splitButton.items[ key ].command );
+				button = splitButtonElement.findOne( '.cke_button__' + command.name );
+				assert.isTrue( button.hasClass( 'cke_button_off' ), 'Button should be in `off` state' );
+				assert.isTrue( command.state == CKEDITOR.TRISTATE_OFF, 'Command state should be 2' );
+				splitButton.click( this.editor );
+				splitButton._.menu.show( CKEDITOR.document.getById( splitButton._.id ), 4 );
+
+				assertMenuButtonsState( this.editor );
+
+				this.editor.execCommand( command.name );
+				assert.isTrue( button.hasClass( 'cke_button_on' ), 'Button should be in `on` state' );
+
+				assertMenuButtonsState( this.editor );
+			}
 		}
-		for ( key in splitButton.items ) {
-			key = key.substring( 0, key.length - 1 );
-			assert.isTrue( key in items );
-		}
-	}
-} );
+	} );
+
+} )();
