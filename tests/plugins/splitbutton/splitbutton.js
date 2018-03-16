0fdfbfd4977335fd6b889e36135e310358c68897
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-03-16 14:37:52 +0100

    Codestyle and comments

--- tests/plugins/splitbutton/splitbutton.js
+++ tests/plugins/splitbutton/splitbutton.js
@@ -21,6 +21,7 @@
 					case 1:
 						assert.isTrue( menuButton.hasClass( 'cke_menubutton_on', 'Command state is 1, menuButton should have class `cke_menubutton_on`.' ) );
 						break;
+
 					case 2:
 						assert.isTrue( menuButton.hasClass( 'cke_menubutton_off', 'Command state is 1, menuButton should have class `cke_menubutton_off`.' ) );
 						break;
@@ -63,7 +64,7 @@
 	};
 
 	bender.test( {
-		'test splitbutton rendered in toolbar': function() {
+		'test split button rendered in toolbar': function() {
 			var splitButton = this.editor.ui.get( 'teststylesplit' ),
 				arrow = CKEDITOR.document.getById( splitButton._.id ),
 				children = arrow.getParent().$.childNodes,
@@ -73,10 +74,10 @@
 				items = {},
 				key;
 
-			// Test if splitbutton is rendered in toolbar.
+			// Test if split button is rendered in toolbar.
 			assert.isTrue( arrow.getParent().getParent().hasClass( 'cke_toolgroup' ) );
 
-			// Test if each of splitbutton items are represented by buttons in toolbar
+			// Test if each of split button items are represented by buttons in toolbar
 			for ( i = 0; i < children.length; i++ ) {
 				item = children[ i ];
 				cl = item.classList[ 1 ].substring( 12, item.classList[ 1 ].length );
@@ -87,7 +88,7 @@
 				assert.isTrue( key in items );
 			}
 		},
-		'test state of splitbutton buttons': function() {
+		'test state of split button buttons': function() {
 			var splitButton = this.editor.ui.get( 'teststylesplit' ),
 				arrow = CKEDITOR.document.getById( splitButton._.id ),
 				splitButtonElement = arrow.getParent(),
@@ -98,14 +99,20 @@
 			for ( key in splitButton.items ) {
 				command = this.editor.getCommand( splitButton.items[ key ].command );
 				button = splitButtonElement.findOne( '.cke_button__' + command.name );
+
+				// Test initial state of buttons and commands
 				assert.isTrue( button.hasClass( 'cke_button_off' ), 'Button should be in `off` state' );
 				assert.isTrue( command.state == CKEDITOR.TRISTATE_OFF, 'Command state should be 2' );
+
 				splitButton.click( this.editor );
 				splitButton._.menu.show( CKEDITOR.document.getById( splitButton._.id ), 4 );
 
+				// Test menu button state before executing command
 				assertMenuButtonsState( this.editor );
 
 				this.editor.execCommand( command.name );
+
+				// Test button and menu button state after executing command
 				assert.isTrue( button.hasClass( 'cke_button_on' ), 'Button should be in `on` state' );
 
 				assertMenuButtonsState( this.editor );
