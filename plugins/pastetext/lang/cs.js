70c1453ff408a6bb7be1f272ac6543e415b44824
Ondřej Vacek <o.vacek@gmail.com>
2018-03-19 18:11:05 +0100

    Update cs.js
    Added missing translation

--- plugins/pastetext/lang/cs.js
+++ plugins/pastetext/lang/cs.js
@@ -4,6 +4,6 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
 CKEDITOR.plugins.setLang( 'pastetext', 'cs', {
 	button: 'Vložit jako čistý text',
-	pasteNotification: 'Press %1 to paste. Your browser doesn‘t support pasting with the toolbar button or context menu option.', // MISSING
+	pasteNotification: 'Stiskněte %1 pro vložení. Váš prohlížeč nepodporuje vkládání textu pomocí panelu nástrojů nebo kontextové nabídky.',
 	title: 'Vložit jako čistý text'
 } );
