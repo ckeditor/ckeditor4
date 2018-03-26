3fcc6e52fef11aca4ebc41dbc95046696eb3ee35
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-03-26 12:22:05 +0200

    Moved tests into core/config.

---
+++ tests/core/config/plugin.js
@@ -0,0 +1,44 @@
+/* bender-tags: editor */
+
+( function() {
+
+bender.test( {
+	'test extraPlugins allows whitespaces': function() {
+		bender.editorBot.create( { name: 'editor_extraplugins', config: { extraPlugins: 'basicstyles, image2, toolbar ' } }, function( bot ) {
+			var editor = bot.editor,
+				plugins = CKEDITOR.tools.objectKeys( editor.plugins );
+
+			assert.isTrue( contains( plugins, 'basicstyles' ) );
+			assert.isTrue( contains( plugins, 'image2' ) );
+			assert.isTrue( contains( plugins, 'toolbar' ) );
+		} );
+	},
+
+	'test plugins allows whitespaces': function() {
+		bender.editorBot.create( { name: 'editor_plugins', config: { plugins: 'basicstyles, image2, toolbar ' } }, function( bot ) {
+			var editor = bot.editor,
+				plugins = CKEDITOR.tools.objectKeys( editor.plugins );
+
+			assert.isTrue( contains( plugins, 'basicstyles' ) );
+			assert.isTrue( contains( plugins, 'image2' ) );
+			assert.isTrue( contains( plugins, 'toolbar' ) );
+		} );
+	},
+
+	'test removePlugins allows whitespaces': function() {
+		bender.editorBot.create( { name: 'editor_removePlugins', config: { extraPlugins: 'basicstyles,image2,toolbar', removePlugins: 'basicstyles, image2, toolbar ' } }, function( bot ) {
+			var editor = bot.editor,
+				plugins = CKEDITOR.tools.objectKeys( editor.plugins );
+
+			assert.isFalse( contains( plugins, 'basicstyles' ) );
+			assert.isFalse( contains( plugins, 'image2' ) );
+			assert.isFalse( contains( plugins, 'toolbar' ) );
+		} );
+	}
+} );
+
+function contains( array, value ) {
+	return CKEDITOR.tools.array.indexOf( array, value ) !== -1;
+}
+
+} )();
