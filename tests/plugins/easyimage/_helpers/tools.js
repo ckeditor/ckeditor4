3c7981e88b219940a87e3711cbb1bcf9c58631e6
Marek Lewandowski <m.lewandowski@cksource.com>
2018-04-04 11:43:21 +0200

    Corrected stray line leaked after the merge.

--- tests/plugins/easyimage/_helpers/tools.js
+++ tests/plugins/easyimage/_helpers/tools.js
@@ -29,7 +29,6 @@ var easyImageTools = ( function() {
 	return {
 		CLOUD_SERVICES_UPLOAD_GATEWAY: 'https://33333.cke-cs.com/easyimage/upload/',
 		CLOUD_SERVICES_TOKEN_URL: 'https://33333.cke-cs.com/token/dev/ijrDsqFix838Gh3wGO3F77FSW94BwcLXprJ4APSp3XQ26xsUHTi0jcb1hoBt',
-		createTestsForEditors: createTestsForEditors,
 		assertCommandsState: assertCommandsState,
 		assertMenuItemsState: assertMenuItemsState,
 		isUnsupportedEnvironment: isUnsupportedEnvironment
