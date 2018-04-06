71344137e6957d07896be26771e2cce18d987974
Tomasz Jakut <vepomoc@gmail.com>
2018-04-06 11:43:11 +0200

    Add CKEDITOR.dom.rect abstract class.

---
+++ core/dom/rect.js
@@ -0,0 +1,69 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @fileOverview Defines the "virtual" {@link CKEDITOR.dom.rect} class
+ * which contains the defintion of an element's DOM rectangle. This file is for
+ * documentation purposes only.
+ */
+
+/**
+ * Virtual class that illustrates the {@link CKEDITOR.dom.element} DOM rectangle (dimensions and coordinates
+ * of the area, which element occupies on the page).
+ *
+ * @class CKEDITOR.dom.rect
+ * @abstract
+ * @since 4.10.0
+ */
+
+/**
+ * Element's offset from the top.
+ *
+ * @property {Number} top
+ */
+
+/**
+ * Element's offset from the bottom.
+ *
+ * @property {Number} bottom
+ */
+
+/**
+ * Element's offset from the left.
+ *
+ * @property {Number} left
+ */
+
+/**
+ * Element's offset from the right.
+ *
+ * @property {Number} right
+ */
+
+/**
+ * Element's height.
+ *
+ * @property {Number} height
+ */
+
+/**
+ * Element's width.
+ *
+ * @property {Number} width
+ */
+
+/**
+ * Element's offset from the viewport's left edge.
+ * This property is not available in Internet Explorer or Edge.
+ *
+ * @property {Number} x
+ */
+
+/**
+ * Element's offset from the viewport's top edge.
+ * This property is not available in Internet Explorer or Edge.
+ *
+ * @property {Number} y
+ */
