09eaa753d601f588d6f79e8f280eaf31ba73e6d4
Tomasz Jakut <vepomoc@gmail.com>
2018-04-26 18:46:32 +0200

    Add unit test.

--- tests/core/config/plugins.js
+++ tests/core/config/plugins.js
@@ -52,6 +52,42 @@
 			bender.editorBot.create( { name: 'editor_extraPlugins_undefined', config: { extraPlugins: undefined } }, function() {
 				assert.pass();
 			} );
+		},
+
+		// (#1802)
+		'test extraPlugins allows array': function() {
+			bender.editorBot.create( { name: 'editor_extraPluginsarray', config: { extraPlugins: [ 'basicstyles', 'image2', 'toolbar' ] } }, function( bot ) {
+				var editor = bot.editor,
+					plugins = CKEDITOR.tools.objectKeys( editor.plugins );
+
+				assert.isTrue( contains( plugins, 'basicstyles' ) );
+				assert.isTrue( contains( plugins, 'image2' ) );
+				assert.isTrue( contains( plugins, 'toolbar' ) );
+			} );
+		},
+
+		// (#1802)
+		'test plugins allows array': function() {
+			bender.editorBot.create( { name: 'editor_pluginsarray', config: { plugins: [ 'basicstyles', 'image2', 'toolbar' ] } }, function( bot ) {
+				var editor = bot.editor,
+					plugins = CKEDITOR.tools.objectKeys( editor.plugins );
+
+				assert.isTrue( contains( plugins, 'basicstyles' ) );
+				assert.isTrue( contains( plugins, 'image2' ) );
+				assert.isTrue( contains( plugins, 'toolbar' ) );
+			} );
+		},
+
+		// (#1802)
+		'test removePlugins allows array': function() {
+			bender.editorBot.create( { name: 'editor_removePluginsarray', config: { extraPlugins: 'basicstyles,image2,toolbar', removePlugins: [ 'basicstyles', 'image2', 'toolbar' ] } }, function( bot ) {
+				var editor = bot.editor,
+					plugins = CKEDITOR.tools.objectKeys( editor.plugins );
+
+				assert.isFalse( contains( plugins, 'basicstyles' ) );
+				assert.isFalse( contains( plugins, 'image2' ) );
+				assert.isFalse( contains( plugins, 'toolbar' ) );
+			} );
 		}
 	} );
 
