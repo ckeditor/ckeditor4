878bb276dc806da2279eccdabf4399962aff6b00
Sam Sipasseuth <sam@taotesting.com>
2018-05-18 08:50:48 +0200

    temporarily skip the untab  when the focus in on an empty node

--- plugins/taountab/plugin.js
+++ plugins/taountab/plugin.js
@@ -32,23 +32,25 @@ CKEDITOR.plugins.add('taountab', {
                 var sel = editor.getSelection();
                 var ranges = editor.getSelection().getRanges();
                 var caretPosition = ranges[0].startOffset;
-
+                var selectedText, startIndex, position;
                 //first, select a range of strings to verify if there are enough successive whitespaces, using the native selection is here more reliable than ckeditor's api
                 var focusNode = new CKEDITOR.dom.text(sel._.cache.nativeSel.focusNode);
                 var text = focusNode.getText();
+                if(text === null){
+                    return;//todo fix this: temporarily skip the situation where the focus in on an empty node
+                }
                 ranges[0].setStart(focusNode, Math.max(0, caretPosition - spaceNum));
                 ranges[0].setEnd(focusNode, Math.min(caretPosition + spaceNum, text.length));
                 sel.selectRanges([ranges[0]]);
-                var selectedText = sel.getSelectedText();
-                var startIndex = getSuccessiveSpacesStartIndex(selectedText, spaceNum);
+
+                selectedText = sel.getSelectedText();
+                startIndex = getSuccessiveSpacesStartIndex(selectedText, spaceNum);
 
                 if(startIndex >= 0){
 
                     //replace text node, this method is more reliable than range deletion as the latter would lose the caret position for successive call to this action
-                    var a = focusNode.getText();
-                    var position = Math.max(0, caretPosition - spaceNum) + startIndex;
-                    var output = [a.slice(0, position), a.slice(position + spaceNum)].join('');
-                    focusNode.setText(output);
+                    position = Math.max(0, caretPosition - spaceNum) + startIndex;
+                    focusNode.setText([text.slice(0, position), text.slice(position + spaceNum)].join(''));
 
                     //place caret at the end of the delete string
                     ranges[0].setStart(focusNode, position);
