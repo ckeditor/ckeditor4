09ffd5afd3f831534461ac2aaf3f6ab590b15aa3
Tomasz Jakut <vepomoc@gmail.com>
2018-05-21 17:36:53 +0200

    Fix whitespace issues in unit test.

--- tests/core/config/plugins.js
+++ tests/core/config/plugins.js
@@ -5,7 +5,12 @@
 
 		// (#1712)
 		'test extraPlugins allows whitespaces': function() {
-			bender.editorBot.create( { name: 'editor_extraplugins', config: { extraPlugins: 'basicstyles, image2, toolbar ' } }, function( bot ) {
+			bender.editorBot.create( {
+				name: 'editor_extraplugins',
+				config: {
+					extraPlugins: 'basicstyles, image2, toolbar '
+				}
+			}, function( bot ) {
 				var editor = bot.editor,
 					plugins = CKEDITOR.tools.objectKeys( editor.plugins );
 
@@ -17,7 +22,12 @@
 
 		// (#1712)
 		'test plugins allows whitespaces': function() {
-			bender.editorBot.create( { name: 'editor_plugins', config: { plugins: 'basicstyles, image2, toolbar ' } }, function( bot ) {
+			bender.editorBot.create( {
+				name: 'editor_plugins',
+				config: {
+					plugins: 'basicstyles, image2, toolbar '
+				}
+			}, function( bot ) {
 				var editor = bot.editor,
 					plugins = CKEDITOR.tools.objectKeys( editor.plugins );
 
@@ -29,7 +39,12 @@
 
 		// (#1712)
 		'test removePlugins allows whitespaces': function() {
-			bender.editorBot.create( { name: 'editor_removePlugins', config: { plugins: 'basicstyles,image2,toolbar', removePlugins: 'basicstyles, image2, toolbar ' } }, function( bot ) {
+			bender.editorBot.create( {
+				name: 'editor_removePlugins', config: {
+					plugins: 'basicstyles,image2,toolbar',
+					removePlugins: 'basicstyles, image2, toolbar '
+				}
+			}, function( bot ) {
 				var editor = bot.editor,
 					plugins = CKEDITOR.tools.objectKeys( editor.plugins );
 
@@ -41,22 +56,42 @@
 
 		// (#1712)
 		"test plugins doesn't throw with undefined": function() {
-			bender.editorBot.create( { name: 'editor_plugins_undefined', config: { plugins: undefined } }, function() {
+			bender.editorBot.create( {
+				name: 'editor_plugins_undefined',
+				config: {
+					plugins: undefined
+				}
+			}, function() {
 				assert.pass();
 			} );
 
-			bender.editorBot.create( { name: 'editor_removePlugins_undefined', config: { removePlugins: undefined } }, function() {
+			bender.editorBot.create( {
+				name: 'editor_removePlugins_undefined',
+				config: {
+					removePlugins: undefined
+				}
+			}, function() {
 				assert.pass();
 			} );
 
-			bender.editorBot.create( { name: 'editor_extraPlugins_undefined', config: { extraPlugins: undefined } }, function() {
+			bender.editorBot.create( {
+				name: 'editor_extraPlugins_undefined',
+				config: {
+					extraPlugins: undefined
+				}
+			}, function() {
 				assert.pass();
 			} );
 		},
 
 		// (#1802)
 		'test extraPlugins allows array': function() {
-			bender.editorBot.create( { name: 'editor_extraPluginsarray', config: { extraPlugins: [ 'basicstyles', 'image2', 'toolbar' ] } }, function( bot ) {
+			bender.editorBot.create( {
+				name: 'editor_extraPluginsarray',
+				config: {
+					extraPlugins: [ 'basicstyles', 'image2', 'toolbar' ]
+				}
+			}, function( bot ) {
 				var editor = bot.editor,
 					plugins = CKEDITOR.tools.objectKeys( editor.plugins );
 
@@ -68,7 +103,12 @@
 
 		// (#1802)
 		'test plugins allows array': function() {
-			bender.editorBot.create( { name: 'editor_pluginsarray', config: { plugins: [ 'basicstyles', 'image2', 'toolbar' ] } }, function( bot ) {
+			bender.editorBot.create( {
+				name: 'editor_pluginsarray',
+				config: {
+					plugins: [ 'basicstyles', 'image2', 'toolbar' ]
+				}
+			}, function( bot ) {
 				var editor = bot.editor,
 					plugins = CKEDITOR.tools.objectKeys( editor.plugins );
 
@@ -80,7 +120,13 @@
 
 		// (#1802)
 		'test removePlugins allows array': function() {
-			bender.editorBot.create( { name: 'editor_removePluginsarray', config: { plugins: 'basicstyles,image2,toolbar', removePlugins: [ 'basicstyles', 'image2', 'toolbar' ] } }, function( bot ) {
+			bender.editorBot.create( {
+				name: 'editor_removePluginsarray',
+				config: {
+					plugins: 'basicstyles,image2,toolbar',
+					removePlugins: [ 'basicstyles', 'image2', 'toolbar' ]
+				}
+			}, function( bot ) {
 				var editor = bot.editor,
 					plugins = CKEDITOR.tools.objectKeys( editor.plugins );
 
