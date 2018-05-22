14bfa1c2405cca15d259c6bbb17174fdb57608f8
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-05-22 11:02:05 +0200

    Added throttle function.

--- core/tools.js
+++ core/tools.js
@@ -580,6 +580,90 @@
 		},
 
 		/**
+		 * Throttles `input` events (or any `input` calls)
+		 * and triggers `output` not more often than once per `minInterval`.
+		 *
+		 * After each `input` scheduled `output` will be destroyed and replaced with
+		 * new one. It gives you the opportunity to modify passed parameters into `input` function and get
+		 * different results.
+		 *
+		 *		var buffer = CKEDITOR.tools.throttle( 200, function( message ) {
+		 *			console.log( message );
+		 *		} );
+		 *
+		 *		buffer.input( 'foo!' );
+		 *		// 'foo!' logged immediately.
+		 *		buffer.input( 'buz!' );
+		 *		// Nothing logged.
+		 *		buffer.input( 'bar!' );
+		 *		// Nothing logged.
+		 *		// ... after 200ms a single 'bar!' will be logged.
+		 *
+		 * Can be easily used with events:
+		 *
+		 *		var buffer = CKEDITOR.tools.throttle( 200, function( evt ) {
+		 *			console.log( evt.data.text );
+		 *		} );
+		 *
+		 *		editor.on( 'key', buffer.input );
+		 *		// Note: There is no need to bind buffer as a context.
+		 *
+		 * @since 4.10.0
+		 * @param {Number} minInterval Minimum interval between `output` calls in milliseconds.
+		 * @param {Function} output Function that will be executed as `output`.
+		 * @param {Object} [scopeObj] The object used to scope the listener call (the `this` object).
+		 * @returns {Object}
+		 * @returns {Function} return.input Buffer's input method.
+		 * Accepts parameters which will be directly passed into `output` function.
+		 * @returns {Function} return.reset Resets buffered events &mdash; `output` will not be executed
+		 * until next `input` is triggered.
+		 */
+		throttle: function( minInterval, output, scopeObj ) {
+			var scheduled,
+				lastOutput = 0;
+
+			scopeObj = scopeObj || {};
+
+			return {
+				input: input,
+				reset: reset
+			};
+
+			function input() {
+				var args = Array.prototype.slice.call( arguments );
+
+				if ( scheduled ) {
+					clearTimeout( scheduled );
+					scheduled = 0;
+				}
+
+				var diff = ( new Date() ).getTime() - lastOutput;
+
+				// If less than minInterval passed after last check,
+				// schedule next for minInterval after previous one.
+				if ( diff < minInterval ) {
+					scheduled = setTimeout( triggerOutput, minInterval - diff );
+				} else {
+					triggerOutput();
+				}
+
+				function triggerOutput() {
+					lastOutput = ( new Date() ).getTime();
+					scheduled = false;
+
+					output.apply( scopeObj, args );
+				}
+			}
+
+			function reset() {
+				if ( scheduled ) {
+					clearTimeout( scheduled );
+					scheduled = lastOutput = 0;
+				}
+			}
+		},
+
+		/**
 		 * Removes spaces from the start and the end of a string. The following
 		 * characters are removed: space, tab, line break, line feed.
 		 *
