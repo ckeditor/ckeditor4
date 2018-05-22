1faf7a359f623a0cfc9798b5f7576e5af0c6f1f1
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-05-22 17:13:13 +0200

    Moved templating into autocomplete plugin.

--- plugins/autocomplete/plugin.js
+++ plugins/autocomplete/plugin.js
@@ -155,8 +155,18 @@
 	 * @param {CKEDITOR.plugins.autocomplete.model.item[]} dataCallback.callback.data The suggestion data that should be
 	 * displayed in the autocomplete view for a given query. The data items should implement the
 	 * {@link CKEDITOR.plugins.autocomplete.model.item} interface.
+	 * @param {String} viewTemplate Changes the view template.
+	 * The given template will be used to render matches in the dropdown.
+	 *
+	 * A minimal template should be wrapped with HTML `li` element containing `data-id={id}` attribute.
+	 *
+	 * ```javascript
+	 * autocomplete.setViewTemplate( '<li data-id="{id}" ><img src="{iconSrc}" alt="{name}" /><span>{name}</span></li>' );
+	 * ```
+	 *
+	 * @param {String} outputTemplate See {@link #outputTemplate}
 	 */
-	function Autocomplete( editor, textTestCallback, dataCallback ) {
+	function Autocomplete( editor, textTestCallback, dataCallback, viewTemplate, outputTemplate ) {
 		var configKeystroke = editor.config.autocomplete_commitKeystroke || CKEDITOR.config.autocomplete_commitKeystroke;
 
 		/**
@@ -206,6 +216,23 @@
 		 * @private
 		 */
 		this._listeners = [];
+
+		/**
+		 * Output template used to write selected match into an editor.
+		 * You can use any parameter passed into `dataCallback` parameter.
+		 *
+		 * ```javascript
+		 * autocomplete.setOutputTemplate( '<a href="src" alt="name">{name}</a>' );
+		 * ```
+		 *
+		 * @readonly
+		 * @property {CKEDITOR.template}
+		 */
+		this.outputTemplate = outputTemplate !== undefined ? new CKEDITOR.template( outputTemplate ) : null;
+
+		if ( viewTemplate ) {
+			this.view.itemTemplate = new CKEDITOR.template( viewTemplate );
+		}
 
 		this.attach();
 	}
@@ -330,7 +357,8 @@
 		 * @returns {String} The HTML to insert.
 		 */
 		getHtmlToInsert: function( item ) {
-			return item.name;
+			return this.outputTemplate ? this.outputTemplate.output( item )
+				: item.name;
 		},
 
 		/**
