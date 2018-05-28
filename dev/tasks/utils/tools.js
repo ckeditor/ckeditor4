abb6a4ece43b4369fb8916f6f1052fcdc21e967a
Tomasz Jakut <vepomoc@gmail.com>
2018-05-28 16:06:36 +0200

    Add missing license headers.

--- dev/tasks/utils/tools.js
+++ dev/tasks/utils/tools.js
@@ -1,3 +1,8 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
 /* jshint node: true, es3: false */
 
 'use strict';
