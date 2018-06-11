ee6781a89a9aa1631104ad7fa41cc0056f1eedb4
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-06-11 14:27:35 +0200

    Fixed race condition on Firefox

--- tests/core/editor/initialize_hidden.js
+++ tests/core/editor/initialize_hidden.js
@@ -5,26 +5,38 @@ CKEDITOR.replaceClass = 'ckeditor';
 
 bender.test( {
 	'test auto initilization': function() {
-		CKEDITOR.on( 'instanceReady', function( evt ) {
-			if ( evt.editor.name == 'editor1' ) {
-				resume( function() {
-					assert.areEqual( '<p>ed1</p>', bender.tools.compatHtml( evt.editor.getData() ) );
-				} );
-			}
-		} );
+		var editor = CKEDITOR.instances.editor1;
 
-		wait();
+		// For Firefox 60.0.1+ this code is executed after 'instanceReady' event so listener won't be called.
+		// For that case just assert it right away.
+		if ( editor.instanceReady ) {
+			this._assertEditor( editor );
+		} else {
+			CKEDITOR.on( 'instanceReady', function( evt ) {
+				if ( evt.editor.name == editor.name ) {
+					resume( function() {
+						this._assertEditor( editor );
+					} );
+				}
+			} );
+
+			wait();
+		}
 	},
 
 	'test replace': function() {
 		CKEDITOR.replace( 'editor2', { on: {
 			instanceReady: function( evt ) {
 				resume( function() {
-					assert.areEqual( '<p>ed2</p>', bender.tools.compatHtml( evt.editor.getData() ) );
+					this._assertEditor( evt.editor );
 				} );
 			}
 		} } );
 
 		wait();
+	},
+
+	_assertEditor: function( editor ) {
+		assert.areEqual( '<p>ed' + editor.name[ editor.name.length - 1 ] + '</p>', bender.tools.compatHtml( editor.getData() ) );
 	}
 } );
