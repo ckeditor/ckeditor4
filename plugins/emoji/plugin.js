6367b682feb799f6b40594e0a6a07df8f944b628
Mateusz Samsel <m.samsel@cksource.com>
2018-06-11 16:26:09 +0200

    Basic draft of emoji toolbar icon.

--- plugins/emoji/plugin.js
+++ plugins/emoji/plugin.js
@@ -10,6 +10,7 @@
 
 	CKEDITOR.plugins.add( 'emoji', {
 		requires: 'autocomplete,textmatch,ajax',
+		icons: 'emojipanel',
 		beforeInit: function() {
 			if ( CKEDITOR.env.ie && CKEDITOR.env.version < 11 ) {
 				return;
@@ -21,11 +22,16 @@
 		},
 
 		init: function( editor ) {
-			var emojiListUrl = editor.config.emoji_emojiListUrl || 'plugins/emoji/emoji.json';
+
+			var emojiListUrl = editor.config.emoji_emojiListUrl || 'plugins/emoji/emoji.json',
+				lang = editor.lang.emoji,
+				defaultEmojiList = null;
 
 			CKEDITOR.ajax.load( CKEDITOR.getUrl( emojiListUrl ), function( data ) {
-				if ( data === null ) {
-					return;
+				defaultEmojiList = JSON.parse( data );
+
+				if ( !editor._.emojiList ) {
+					editor._.emojiList = defaultEmojiList;
 				}
 
 				if ( editor._.emoji === undefined ) {
@@ -99,6 +105,56 @@
 					callback( data );
 				}
 			} );
+
+			editor.addCommand( 'insertEmoji', {
+				exec: function( editor, data ) {
+					editor.insertText( data.emojiName );
+				}
+			} );
+
+			// Name is responsible for icon name also.
+			editor.ui.add( 'emojiPanel', CKEDITOR.UI_PANELBUTTON, {
+				label: 'emoji',
+				title: 'Emoji List',
+				modes: { wysiwyg: 1 },
+				editorFocus: 0,
+				toolbar: 'emoji',
+				panel: {
+					css: [ CKEDITOR.skin.getPath( 'editor' ), this.path + 'skins/default.css' ],
+					attributes: { role: 'listbox', 'aria-label': 'Emoji List' }
+				},
+
+				onBlock: function( panel, block ) {
+					block.element.addClass( 'cke_emoji_panel_block' );
+					block.element.setHtml( getEmojiBlock() );
+					panel.element.addClass( 'cke_emoji_panel' );
+
+				}
+
+			} );
+
+			var clickFn = CKEDITOR.tools.addFunction( function( event ) {
+				editor.insertText( ':' + event.target.dataset.ckeEmojiName + ':' );
+			} );
+
+			function getEmojiBlock() {
+				var output = [];
+				// Search Box:
+				output.push( '<input type="search">' );
+				// Result box:
+				var resultTpl = new CKEDITOR.template( '<h2>{langTitle}</h2>' );
+				var emojiTpl = new CKEDITOR.template( '<li data-cke-emoji-name="{id}" class="cke_emoji_item cke_emoji_item_active">{symbol}</li>' );
+				output.push( resultTpl.output( { langTitle: lang ? lang.resultTitle : 'Frequently used' } ) );
+
+				output.push( '<div class="cke_emoji_list"><ul onclick="CKEDITOR.tools.callFunction(', clickFn, ',event);return false;">' );
+				CKEDITOR.tools.array.forEach( defaultEmojiList, function( emoji ) {
+					output.push( emojiTpl.output( { symbol: emoji.symbol, id: emoji.id.replace( /^:|:$/g, '' ) } ) );
+				} );
+				output.push( '</ul></div>' );
+
+				return '<div class="cke_emoji_inner_panel">' + output.join( '' ) + '</div>';
+			}
+
 		}
 	} );
 } )();
