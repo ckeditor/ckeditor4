ae198ef77f7532512178cfdfb32a09629f42bacb
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-06-11 15:58:48 +0200

    Added following space option.

--- plugins/autocomplete/plugin.js
+++ plugins/autocomplete/plugin.js
@@ -175,6 +175,13 @@
 		this.throttle = config.throttle !== undefined ? config.throttle : 20;
 
 		/**
+		 * See {@link CKEDITOR.plugins.autocomplete.configDefinition#followingSpace}.
+		 *
+		 * @property {Boolean} [followingSpace]
+		 */
+		this.throttle = config.followingSpace !== undefined ? config.followingSpace : true;
+
+		/**
 		 * The autocomplete view instance.
 		 *
 		 * @readonly
@@ -424,11 +431,17 @@
 			}
 
 			var item = this.model.getItemById( itemId ),
-				editor = this.editor;
+				editor = this.editor,
+				selection = editor.getSelection();
 
 			editor.fire( 'saveSnapshot' );
-			editor.getSelection().selectRanges( [ this.model.range ] );
+			selection.selectRanges( [ this.model.range ] );
 			editor.insertHtml( this.getHtmlToInsert( item ), 'text' );
+
+			if ( this.followingSpace ) {
+				insertSpaceAfterMatch( editor );
+			}
+
 			editor.fire( 'saveSnapshot' );
 		},
 
@@ -1510,6 +1523,22 @@
 		return editor.window.getFrame().getParent();
 	}
 
+	function insertSpaceAfterMatch( editor ) {
+		var selection = editor.getSelection();
+
+		var nextNode = selection.getRanges()[ 0 ].getNextNode( function( node ) {
+			return Boolean( node.type == CKEDITOR.NODE_TEXT && node.getText() );
+		} );
+
+		if ( nextNode && nextNode.getText().match( /^\s+/ ) ) {
+			var range = editor.createRange();
+			range.setStart( nextNode, 1 );
+			selection.selectRanges( [ range ] );
+		} else {
+			editor.insertHtml( '&nbsp;' );
+		}
+	}
+
 	function encodeItem( item ) {
 		return CKEDITOR.tools.array.reduce( CKEDITOR.tools.object.keys( item ), function( cur, key ) {
 			cur[ key ] = CKEDITOR.tools.htmlEncode( item[ key ] );
@@ -1639,6 +1668,12 @@
 	 * {@link CKEDITOR.plugins.autocomplete.configDefinition#textTestCallback config.textTestCallback}.
 	 *
 	 * @property {String} query
+	 */
+
+	/**
+	 * Indicates if a following space should be added after accepted match.
+	 *
+	 * @property {Boolean} [followingSpace=true]
 	 */
 
 	/**
