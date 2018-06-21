677f8288818d255f2bc36c8e17d5508a1e63c4f5
Marek Lewandowski <m.lewandowski@cksource.com>
2018-06-21 11:57:47 +0200

    Tests: minor adjustments in editor initialization test code.

--- tests/core/editor/initialize_hidden.js
+++ tests/core/editor/initialize_hidden.js
@@ -5,17 +5,18 @@ CKEDITOR.replaceClass = 'ckeditor';
 
 bender.test( {
 	'test auto initilization': function() {
-		var editor = CKEDITOR.instances.editor1;
+		var editor = CKEDITOR.instances.editor1,
+			expected = '<p>ed1</p>';
 
 		// For Firefox 60.0.1+ this code is executed after 'instanceReady' event so listener won't be called.
-		// For that case just assert it right away.
+		// For that case just assert it right away (#2019).
 		if ( editor.status === 'ready' ) {
-			this._assertEditor( editor );
+			this._assertEditor( expected, editor );
 		} else {
-			CKEDITOR.on( 'instanceReady', function( evt ) {
+			editor.once( 'instanceReady', function( evt ) {
 				if ( evt.editor.name == editor.name ) {
 					resume( function() {
-						this._assertEditor( editor );
+						this._assertEditor( expected, editor );
 					} );
 				}
 			} );
@@ -28,7 +29,7 @@ bender.test( {
 		CKEDITOR.replace( 'editor2', { on: {
 			instanceReady: function( evt ) {
 				resume( function() {
-					this._assertEditor( evt.editor );
+					this._assertEditor( '<p>ed2</p>', evt.editor );
 				} );
 			}
 		} } );
@@ -36,7 +37,7 @@ bender.test( {
 		wait();
 	},
 
-	_assertEditor: function( editor ) {
-		assert.areEqual( '<p>ed' + editor.name[ editor.name.length - 1 ] + '</p>', bender.tools.compatHtml( editor.getData() ) );
+	_assertEditor: function( expectedData, editor ) {
+		assert.areEqual( expectedData, bender.tools.compatHtml( editor.getData() ) );
 	}
 } );
