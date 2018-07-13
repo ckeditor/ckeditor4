6a520f47e34af94d0c537be01ebdc1a6cb4d34ed
Jacek Bogdanski <j.bogdanski@cksource.com>
2018-07-13 17:14:42 +0200

    Added tests for continous typing.

--- tests/plugins/autolink/typing.js
+++ tests/plugins/autolink/typing.js
@@ -102,6 +102,24 @@
 		// (#1815)
 		'test created protected mail link (function) on typing': function() {
 			testTyping( this.editors.encodedDefault, 32, 'a@a^', '<p><a href="javascript:void(location.href=\'mailto:\'+String.fromCharCode(97,64,97))">a@a</a></p>' ); // SPACE
+		},
+
+		// (#1815)
+		'test typing after autolinked text': function() {
+			// The issue doesn't exist on Webkit, although enabling this test for Webkit will result with false negative.
+			if ( CKEDITOR.env.webkit ) {
+				assert.ignore();
+			}
+
+			var editor = this.editors.classic;
+
+			bender.tools.setHtmlWithSelection( editor, 'http://example.com^' );
+
+			editor.editable().fire( 'keydown', new CKEDITOR.dom.event( { keyCode: 32 } ) ); // SPACE
+
+			editor.insertHtml( '&nbsp;test', 'text' );
+
+			assert.areEqual( '<p><a href="http://example.com">http://example.com</a>&nbsp;test</p>', editor.getData() );
 		}
 	} );
 
