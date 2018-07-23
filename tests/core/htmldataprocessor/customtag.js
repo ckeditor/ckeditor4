74df405639f8f377040fef3acf92aa38b359c3f3
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-07-23 15:44:42 +0200

    Added test case and refactored test

--- tests/core/htmldataprocessor/customtag.js
+++ tests/core/htmldataprocessor/customtag.js
@@ -13,8 +13,8 @@
 	extraAllowedContent = '';
 
 	CKEDITOR.tools.array.forEach( elements, function( item ) {
-		var customTag = item + 'foo';
-		extraAllowedContent += customTag + '[*]{*}(*);';
+		var customTag = item;
+		extraAllowedContent += customTag + '[*]{*}(*);' + customTag + 'foo' + '[*]{*}(*);';
 	} );
 
 	bender.editor = {
@@ -26,36 +26,63 @@
 
 	bender.test( {
 		// (#988)
-		'test allowed custom tags': function() {
-			var editor = this.editor,
-				bot = this.editorBot,
-				data = '<objectfoo data-foo="bar">objectfoo</objectfoo>' +
-					'<embedfoo data-foo="bar">embedfoo</embedfoo>' +
-					'<paramfoo data-foo="bar">paramfoo</paramfoo>' +
-					'<htmlfoo data-foo="bar">htmlfoo</htmlfoo>' +
-					'<headfoo data-foo="bar">headfoo</headfoo>' +
-					'<bodyfoo data-foo="bar">bodyfoo</bodyfoo>' +
-					'<titlefoo data-foo="bar">titlefoo</titlefoo>';
+		'test allowed custom tags': assertHtmlDataProcessor( {
+			data: '<objectfoo data-foo="bar">objectfoo</objectfoo>' +
+				'<embedfoo data-foo="bar">embedfoo</embedfoo>' +
+				'<paramfoo data-foo="bar">paramfoo</paramfoo>' +
+				'<htmlfoo data-foo="bar">htmlfoo</htmlfoo>' +
+				'<headfoo data-foo="bar">headfoo</headfoo>' +
+				'<bodyfoo data-foo="bar">bodyfoo</bodyfoo>' +
+				'<titlefoo data-foo="bar">titlefoo</titlefoo>',
 
-			bot.setData( data, function() {
-				assert.areEqual( data, editor.getData() );
-			} );
-		},
+			expected: '<objectfoo data-foo="bar">objectfoo</objectfoo>' +
+				'<embedfoo data-foo="bar">embedfoo</embedfoo>' +
+				'<paramfoo data-foo="bar">paramfoo</paramfoo>' +
+				'<htmlfoo data-foo="bar">htmlfoo</htmlfoo>' +
+				'<headfoo data-foo="bar">headfoo</headfoo>' +
+				'<bodyfoo data-foo="bar">bodyfoo</bodyfoo>' +
+				'<titlefoo data-foo="bar">titlefoo</titlefoo>'
+		} ),
+
 		// (#988)
-		'test not allowed custom tags': function() {
-			var editor = this.editor,
-				bot = this.editorBot,
-				data = '<objectbar data-bar="foo">objectbar</objectbar>' +
+		'test not allowed custom tags': assertHtmlDataProcessor( {
+			data: '<objectbar data-bar="foo">objectbar</objectbar>' +
 					'<embedbar data-bar="foo">embedbar</embedbar>' +
 					'<parambar data-bar="foo">parambar</parambar>' +
 					'<htmlbar data-bar="foo">htmlbar</htmlbar>' +
 					'<headbar data-bar="foo">headbar</headbar>' +
 					'<bodybar data-bar="foo">bodybar</bodybar>' +
-					'<titlebar data-bar="foo">titlebar</titlebar>';
+					'<titlebar data-bar="foo">titlebar</titlebar>',
 
-			bot.setData( data, function() {
-				assert.areEqual( 'objectbarembedbarparambarhtmlbarheadbarbodybartitlebar', editor.getData() );
-			} );
-		}
+			expected: 'objectbarembedbarparambarhtmlbarheadbarbodybartitlebar'
+		} ),
+
+		// (#988)
+		'test protected element names': assertHtmlDataProcessor( {
+			data: '<object data-bar="foo">object</object>' +
+				'<embed data-bar="foo"></embed>' +
+				'<param data-bar="foo"/>' +
+				'<html data-bar="foo">html</html>' +
+				'<head data-bar="foo">head</head>' +
+				'<body data-bar="foo">body</body>' +
+				'<title data-bar="foo">title</title>',
+
+			expected: '<object data-bar="foo">object</object>' +
+				'<embed data-bar="foo"></embed>' +
+				'<param data-bar="foo" />' +
+				'htmlheadbody' +
+				'<title data-bar="foo"></title>'
+		} )
 	} );
+
+	function assertHtmlDataProcessor( config ) {
+		return function() {
+			var editor = this.editor,
+				bot = this.editorBot;
+
+			bot.setData( config.data, function() {
+				assert.areEqual( config.expected, editor.getData() );
+			} );
+		};
+	}
 } )();
