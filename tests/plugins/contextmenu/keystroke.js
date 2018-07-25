86a66e84da94f0ea4eb3c7711df90a8e84e85e03
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-07-25 11:01:37 +0200

    Fixing comments

--- tests/plugins/contextmenu/keystroke.js
+++ tests/plugins/contextmenu/keystroke.js
@@ -16,7 +16,7 @@
 
 	var tests = {
 		'test opening context menu with keystroke': function( editor ) {
-			// Test is unstable on Firefox, because context menu is showing asynchronously and in Firefox its after 'contextMenu' event.
+			// Test is unstable on Firefox, because context menu is showing asynchronously after 'contextMenu' event.
 			// Adding longer timeout like 200ms still doesn't guarantee that test will pass on Firefox.
 			if ( CKEDITOR.env.gecko ) {
 				assert.ignore();
@@ -37,7 +37,7 @@
 						var element = evt.data.element,
 							elementRect = element.getClientRect( true );
 
-						// Edge and IE have values differing by small factor, letls ignore that, because it's not important for context menu positioning.
+						// Edge and IE have values differing by small factor, let's ignore that, because it's not important for context menu positioning.
 						assert.isNumberInRange( elementRect.left, selectionRect.right - 0.1, selectionRect.left + 0.1 );
 						assert.isNumberInRange( elementRect.top, selectionRect.bottom - 0.1, selectionRect.bottom + 0.1 );
 					} );
