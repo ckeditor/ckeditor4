83b0e85a322b6fa2c201e375748461dcd621bcbe
Marek Lewandowski <m.lewandowski@cksource.com>
2018-07-25 13:20:38 +0200

    Tests: fixed unit test for IE8.

--- tests/plugins/wysiwygarea/destroy.js
+++ tests/plugins/wysiwygarea/destroy.js
@@ -14,23 +14,22 @@
 				init: init
 			} );
 
-			CKEDITOR.tools.setTimeout( function() {
-				resume( function() {
-					editor.on( 'destroy', function() {
-						// Seemingly redundant resume here, as the plugins are also loaded in a timeout launched during editor creation.
-						setTimeout( function() {
-							resume( function() {
-								assert.isFalse( init.called, 'plugin init called when editor already destroyed' );
-							} );
-						}, 150 );
-					} );
-
-					editor.destroy();
-
-					wait();
-
+			// Note that resumes under the hood causes yet another setTimeout(). If it was wrapped with other timeout (for readability)
+			// it failed on IE8, due to yet another race condition.
+			resume( function() {
+				editor.on( 'destroy', function() {
+					// Seemingly redundant timeout here, as the plugins are also loaded in a timeout launched during editor creation.
+					setTimeout( function() {
+						resume( function() {
+							assert.isFalse( init.called, 'plugin init called when editor already destroyed' );
+						} );
+					}, 150 );
 				} );
-			}, 0 );
+
+				editor.destroy();
+
+				wait();
+			} );
 
 			editor = CKEDITOR.replace( 'destroyed', {
 				plugins: 'test'
