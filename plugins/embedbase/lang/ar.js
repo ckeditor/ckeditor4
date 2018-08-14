715b588d8eedd62f017849c30240ce537a8a667c
Omar Alahmed <ofma99@gmail.com>
2018-08-14 18:15:19 +0300

    Add Arabic language for embedbase plugin.

---
+++ plugins/embedbase/lang/ar.js
@@ -0,0 +1,15 @@
+/*
+Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+CKEDITOR.plugins.setLang( 'embedbase', 'ar', {
+	pathName: 'media object',
+	title: 'ارفق ميديا',
+	button: 'ارفق ميديا',
+	unsupportedUrlGiven: 'لا يتم دعم الرابط المرفق.',
+	unsupportedUrl: 'الرابط {url} غير صحيح أو غير مدعوم.',
+	fetchingFailedGiven: 'فشل في إرفاق المحتوى.',
+	fetchingFailed: 'فشل في إرفاق المحتوى من الرابط {url}.',
+	fetchingOne: 'جلب رد من oEmbed...',
+	fetchingMany: 'جلب رد من oEmbed, تم {current} من {max}'
+} );
