7c2a08674301b7bd345a8078c52085000d9e4be3
Marek Lewandowski <m.lewandowski@cksource.com>
2018-08-16 14:40:56 +0200

    Tests: upstream failure tracking test cleanup.

--- tests/plugins/wysiwygarea/upstream.js
+++ tests/plugins/wysiwygarea/upstream.js
@@ -6,21 +6,22 @@
 	bender.test( {
 		// (#1907)
 		// Tracks upstream bug for https://bugzilla.mozilla.org/show_bug.cgi?id=1327244
-		'test gecko focus hijack': function() {
+		'test gecko dropping a selection from iframe (requires focus)': function() {
+			if ( !CKEDITOR.env.gecko ) {
+				assert.ignore();
+			}
 			// Short note: this test is based on focuses to iframe > body - as listening to iframe focus alone
 			// was not working on FF, as opposed to Chrome.
 			var frame = CKEDITOR.document.getById( 'gecko-focus-hijack-regression' ),
 				focusHost = CKEDITOR.document.getById( 'focus-host' ),
 				frameDoc = frame.getFrameDocument(),
-				rng = new CKEDITOR.dom.range( frameDoc ),
-				eventType = 'focus';
+				frameBody = frameDoc.getBody(),
+				rng = new CKEDITOR.dom.range( frameDoc );
 
-			// frame.once( eventType, function() {
-			frameDoc.getBody().once( eventType, function() {
+			frameBody.once( 'focus', function() {
 				focusHost.once( 'focus', function() {
 					// Once the frame gets focused second time, resume the suite and assert that the selection has not been modified.
-					// frame.once( 'focus', function() {
-					frameDoc.getBody().once( 'focus', function() {
+					frameBody.once( 'focus', function() {
 						resume( function() {
 							// If the following assertion fails that's great news! Maybe it means that FF fixed a bug.
 							assert.areSame( '', frameDoc.getSelection().getSelectedText(), 'Looks like FF team did something about https://bugzilla.mozilla.org/show_bug.cgi?id=1327244 🙀' );
@@ -30,23 +31,21 @@
 						} );
 					} );
 
-					// frame.focus();
-					// frameDoc.getBody().focus();
+					// Firefox won't be happy if it's not given some extra time to switch focus.
 					window.setTimeout( function() {
-						frameDoc.getBody().focus();
+						frameBody.focus();
 					}, 100 );
 				} );
 
 				focusHost.focus();
 			} );
 
-			rng.setStart( frameDoc.getBody().getFirst(), 0 );
-			rng.setEnd( frameDoc.getBody().getFirst(), 3 );
+			rng.setStart( frameBody.getFirst(), 0 );
+			rng.setEnd( frameBody.getFirst(), 3 );
 
 			frameDoc.getSelection().selectRanges( [ rng ] );
 
-			// frame.focus();
-			frameDoc.getBody().focus();
+			frameBody.focus();
 
 			wait();
 		}
