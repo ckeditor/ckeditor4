bca6172076bb760b8e9d16c63b6658898e59430c
Mateusz Samsel <m.samsel@cksource.com>
2018-08-17 10:09:11 +0200

    Revert "Change assert function to compare html, correct description."
    This reverts commit b39ac05c48766c9d093fe08accf0a17508dab940.

--- tests/_benderjs/ckeditor/static/extensions.js
+++ tests/_benderjs/ckeditor/static/extensions.js
@@ -194,13 +194,15 @@
 	 * @param {Boolean} options.collapsed information if selection is collapsed. If it's true, then additional text is inserted to not remove empty span from some browsers.
 	 * @param {Object} options.bot bot instance in current test case
 	 * @param {String} options.resultHtml html which should be present after applying rich combo change
+	 * @param {Function} options.callback function run after assertion.
 	 */
 	bender.assert.assertCombo = function( options ) {
 		var comboName = options.comboName,
 			comboValue = options.comboValue,
 			collapsed = options.collapsed,
 			bot = options.bot,
-			resultHtml = options.resultHtml;
+			resultHtml = options.resultHtml,
+			htmlMatchingOpts = options.htmlMatchingOpts;
 
 		bot.combo( comboName, function( combo ) {
 			combo.onClick( comboValue );
@@ -208,7 +210,7 @@
 			this.wait( function() {
 				// The empty span from collapsed selection is lost on FF and IE8, insert something to prevent that.
 				collapsed && bot.editor.insertText( 'bar' );
-				assert.beautified.html( resultHtml, bot.editor.editable().getHtml() );
+				assert.isInnerHtmlMatching( resultHtml, bot.editor.editable().getHtml(), htmlMatchingOpts );
 			}, 0 );
 		} );
 	};
