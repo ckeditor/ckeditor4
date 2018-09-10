986579b0087c0f82ba3f66dab48efe710a1de8cd
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-09-10 18:04:30 +0200

    Tests: added classic editor.

--- tests/core/editable/focus.js
+++ tests/core/editable/focus.js
@@ -4,20 +4,25 @@
 ( function() {
 	'use strict';
 
-	bender.editor = {
-		creator: 'inline'
+	bender.editors = {
+		classic: {
+			name: 'classic'
+		},
+		inline: {
+			name: 'inline',
+			creator: 'inline'
+		}
 	};
 
-	bender.test( {
+	var tests = {
 		// #748
-		'test scroll position doesn\'t change when focusing editable': function() {
+		'test scroll position doesn\'t change when focusing editable': function( editor ) {
 			// Edge should be ignored until this is fixed:
 			// https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/14721015/
 			if ( CKEDITOR.env.edge ) {
 				assert.ignore();
 			}
-			var editor = this.editor,
-				editable = editor.editable(),
+			var editable = editor.editable(),
 				body = CKEDITOR.document.getBody(),
 				html = body.getParent(),
 				spacer = new CKEDITOR.dom.element( 'div' ),
@@ -34,5 +39,9 @@
 			assert.areEqual( scrollTop.body, body.$.scrollTop, 'Body should be scrolled top' );
 			assert.areEqual( scrollTop.html, html.$.scrollTop, 'Html should be scrolled top' );
 		}
-	} );
+	};
+
+	tests = bender.tools.createTestsForEditors( CKEDITOR.tools.objectKeys( bender.editors ), tests );
+
+	bender.test( tests );
 } )();
