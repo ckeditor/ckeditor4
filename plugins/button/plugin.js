698a7379e65cb2e447c8b937be9a0588fe518029
Jacek Bogdanski <jacekbogd@gmail.com>
2018-09-17 12:45:54 +0200

    Simplified code.

--- plugins/button/plugin.js
+++ plugins/button/plugin.js
@@ -327,23 +327,14 @@
 
 			if ( element ) {
 				element.setState( state, 'cke_button' );
-
-				if ( state == CKEDITOR.TRISTATE_DISABLED ) {
-					element.setAttribute( 'aria-disabled', true );
-				} else {
-					element.removeAttribute( 'aria-disabled' );
-				}
+				element.setAttribute( 'aria-disabled', state == CKEDITOR.TRISTATE_DISABLED );
 
 				if ( !this.hasArrow ) {
 					// Note: aria-pressed attribute should not be added to menuButton instances. (https://dev.ckeditor.com/ticket/11331)
-					if ( state == CKEDITOR.TRISTATE_ON ) {
-						element.setAttribute( 'aria-pressed', true );
-					} else {
-						element.removeAttribute( 'aria-pressed' );
-					}
+					element.setAttribute( 'aria-pressed', state == CKEDITOR.TRISTATE_ON );
 				} else {
+					// Indicates that menu button is opened (#421).
 					element.setAttribute( 'aria-expanded', state == CKEDITOR.TRISTATE_ON );
-					CKEDITOR.document.getById( this._.id + '_label' ).setText( this.label );
 				}
 
 				return true;
