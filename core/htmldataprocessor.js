ee5ac73a02df7132d5e35ac972a9781fb2eabe5a
Jacek Bogdanski <jacekbogd@gmail.com>
2018-09-18 15:58:38 +0200

    Removed redundant comment.

--- core/htmldataprocessor.js
+++ core/htmldataprocessor.js
@@ -869,7 +869,6 @@
 	// Matches `cke:encoded` element in unicode, hexadecimal, HTML-code, or HTML-entity.
 	function createReservedElementsRegex() {
 		return new RegExp( '(' +
-			// Create closed element regex i.e `<cke:encoded>xxx</cke:encoded>`.
 			createEncodedRegex( '<cke:encoded>' ) +
 			'(.*?)' +
 			createEncodedRegex( '</cke:encoded>' ) +
