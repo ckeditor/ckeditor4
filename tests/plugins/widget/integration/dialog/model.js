3b1530d8d2df3803e14eef8bec5ff47537432a17
Marek Lewandowski <m.lewandowski@cksource.com>
2018-09-20 16:40:27 +0200

    Added tests for dialog.isEditing and widget integration.

--- tests/plugins/widget/integration/dialog/model.js
+++ tests/plugins/widget/integration/dialog/model.js
@@ -41,6 +41,8 @@
 			} );
 
 			this.editor.addCommand( 'dialog1', new CKEDITOR.dialogCommand( 'dialog1' ) );
+
+			this._disposableListeners = [];
 		},
 
 		tearDown: function() {
@@ -49,6 +51,11 @@
 			if ( dialog ) {
 				dialog.hide();
 			}
+
+			CKEDITOR.tools.array.filter( this._disposableListeners, function( listener ) {
+				listener.removeListener();
+				return false;
+			} );
 		},
 
 		'test getting model from dialog in edit mode': function() {
@@ -58,13 +65,13 @@
 			bot.setData( '<p data-widget="test1" id="x">foo</p>', function() {
 				var widget = getWidgetById( editor, 'x' );
 
-				editor.once( 'dialogShow', function( evt ) {
+				this._disposableListeners.push( editor.once( 'dialogShow', function( evt ) {
 					resume( function() {
 						var dialog = evt.data;
 
 						assert.areSame( widget, dialog.getModel( editor ), 'Model returned by dialog.getModel().' );
 					} );
-				} );
+				} ) );
 
 				widget.focus();
 				widget.edit();
@@ -78,19 +85,94 @@
 
 			bot.setHtmlWithSelection( '<p>^</p>' );
 
-			this.editor.on( 'dialogShow', function( evt ) {
+			this._disposableListeners.push( this.editor.on( 'dialogShow', function( evt ) {
 				var dialog = evt.data;
 
 				if ( dialog.getName() === 'dialog1' ) {
-					resume( function(  ) {
+					resume( function() {
 						assert.isInstanceOf( CKEDITOR.plugins.widget, dialog.getModel( bot.editor ), 'Model type returned by dialog.getModel().' );
 					} );
 				}
-			} );
+			} ) );
 
 			this.editor.execCommand( 'test1' );
 
 			wait();
+		},
+
+		'test isEditing return value when no widget is edited': function() {
+			var bot = this.editorBot;
+
+			bot.setHtmlWithSelection( '<p>^</p>' );
+
+			this._disposableListeners.push( this.editor.on( 'dialogShow', function( evt ) {
+				var dialog = evt.data;
+
+				if ( dialog.getName() === 'dialog1' ) {
+					resume( function() {
+						assert.isFalse( dialog.isEditing() );
+					} );
+				}
+			} ) );
+
+			this.editor.execCommand( 'test1' );
+
+			wait();
+		},
+
+		'test isEditing return value when a widget is edited': function() {
+			var editor = this.editor,
+				bot = this.editorBot;
+
+			bot.setData( '<p data-widget="test1" id="x">foo</p>', function() {
+				var widget = getWidgetById( editor, 'x' );
+
+				this._disposableListeners.push( this.editor.on( 'dialogShow', function( evt ) {
+					var dialog = evt.data;
+
+					if ( dialog.getName() === 'dialog1' ) {
+						resume( function() {
+							assert.isTrue( dialog.isEditing() );
+						} );
+					}
+				} ) );
+
+				widget.focus();
+				widget.edit();
+
+				wait();
+			} );
+		},
+
+		'test isEditing return value with offline widget': function() {
+			var editor = this.editor,
+				bot = this.editorBot,
+				div = new CKEDITOR.dom.element( 'div' ),
+				widgetElement = new CKEDITOR.dom.element( 'p' );
+
+			div.append( widgetElement );
+
+			bot.setData( '<p></p>', function() {
+				var offlineWidget = new CKEDITOR.plugins.widget( editor.widgets,
+					200, widgetElement, this.editor.widgets.registered.test1 );
+
+				this._disposableListeners.push( this.editor.on( 'dialogShow', function( evt ) {
+					var dialog = evt.data,
+						getModelStub = sinon.stub( dialog, 'getModel' ).returns( offlineWidget );
+
+					if ( dialog.getName() === 'dialog1' ) {
+						resume( function() {
+							getModelStub.restore();
+
+							assert.isFalse( dialog.isEditing() );
+						} );
+					}
+				} ) );
+
+				offlineWidget.edit();
+
+				wait();
+			} );
 		}
 	} );
 } )();
