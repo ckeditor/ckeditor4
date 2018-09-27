378c93a270ac0203e15f8b9bbc1224d7ffb903f9
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-09-27 10:29:46 +0200

    Tests: Removed IE and Edge from ignored env and slightly reworked test.

--- tests/core/editable/focus.js
+++ tests/core/editable/focus.js
@@ -17,17 +17,13 @@
 	var tests = {
 		// #2420
 		'test scroll position doesn\'t change when preventScroll is set to true': function( editor ) {
-			if ( CKEDITOR.env.edge ) {
-				assert.ignore();
-			}
-
 			var editable = editor.editable(),
 				body = CKEDITOR.document.getBody(),
 				html = body.getParent(),
 				spacer = new CKEDITOR.dom.element( 'div' ),
 				scrollTop = {};
 
-			spacer.setStyle( 'height', '100vh' );
+			spacer.setStyle( 'height', '8000px' );
 			spacer.insertBefore( body.getFirst() );
 
 			scrollTop.body = body.$.scrollTop;
@@ -35,8 +31,14 @@
 
 			editable.focus( { preventScroll: true } );
 
-			assert.areEqual( scrollTop.body, body.$.scrollTop, 'Body should be scrolled top' );
-			assert.areEqual( scrollTop.html, html.$.scrollTop, 'Html should be scrolled top' );
+			// Scroll is delayed in IE and Edge.
+			setTimeout( function() {
+				resume( function() {
+					assert.areEqual( scrollTop.body, body.$.scrollTop, 'Body should be scrolled top' );
+					assert.areEqual( scrollTop.html, html.$.scrollTop, 'Html should be scrolled top' );
+				} );
+			}, 50 );
+			wait();
 		}
 	};
 
