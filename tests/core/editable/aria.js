a6a027997bdaeaf8cf7ece662c07db73076f830e
Tomasz Jakut <vepomoc@gmail.com>
2018-09-29 20:53:05 +0200

    Add unit tests.

--- tests/core/editable/aria.js
+++ tests/core/editable/aria.js
@@ -57,6 +57,54 @@
 				assert.areSame( 1, fired, 'event was fired once' );
 				assert.isNull( describedBy, 'editable does not have has aria-describedby attribute' );
 			} );
+		},
+
+		// (#1904)
+		'test initial value of [aria-readonly] attribute (writable editor)': function() {
+			bender.editorBot.create( {
+				name: 'readonly-initial-writable',
+				creator: 'inline'
+			}, function( bot ) {
+				var editor = bot.editor,
+					readonlyAttr = editor.editable().getAttribute( 'aria-readonly' );
+
+				assert.areSame( 'false', readonlyAttr, 'editable has appropriate value of [aria-readonly] attribute' );
+			} );
+		},
+
+		// (#1904)
+		'test initial value of [aria-readonly] attribute (readonly editor)': function() {
+			bender.editorBot.create( {
+				name: 'readonly-initial-readonly',
+				creator: 'inline',
+				config: {
+					readOnly: true
+				}
+			}, function( bot ) {
+				var editor = bot.editor,
+					readonlyAttr = editor.editable().getAttribute( 'aria-readonly' );
+
+				assert.areSame( 'true', readonlyAttr, 'editable has appropriate value of [aria-readonly] attribute' );
+			} );
+		},
+
+		// (#1904)
+		'test update value of [aria-readonly] attribute': function() {
+			bender.editorBot.create( {
+				name: 'readonly-update',
+				creator: 'inline',
+				config: {
+					readOnly: true
+				}
+			}, function( bot ) {
+				var editor = bot.editor,
+					readonlyAttr;
+
+				editor.setReadOnly( false );
+				readonlyAttr = editor.editable().getAttribute( 'aria-readonly' );
+
+				assert.areSame( 'false', readonlyAttr, 'editable has appropriate value of [aria-readonly] attribute' );
+			} );
 		}
 	} );
 } )();
--- tests/plugins/wysiwygarea/aria.js
+++ tests/plugins/wysiwygarea/aria.js
@@ -58,6 +58,65 @@
 				assert.areSame( 1, fired, 'event was fired once' );
 				assert.isNull( describedBy, 'iframe does not have aria-describedby attribute' );
 			} );
+		},
+
+		// (#1904)
+		'test editor role and [aria-multiline] attribute': function() {
+			bender.editorBot.create( {
+				name: 'role-textbox'
+			}, function( bot ) {
+				var editor = bot.editor,
+					frame = editor.editable().getWindow().getFrame();
+
+				assert.areSame( 'textbox', frame.getAttribute( 'role' ), 'editor frame has appropriate role' );
+				assert.areSame( 'true', frame.getAttribute( 'aria-multiline' ),
+					'editor frame has appropriate value of [aria-multiline] attribute' );
+			} );
+		},
+
+		// (#1904)
+		'test initial value of [aria-readonly] attribute (writable editor)': function() {
+			bender.editorBot.create( {
+				name: 'readonly-initial-writable'
+			}, function( bot ) {
+				var editor = bot.editor,
+					readonlyAttr = editor.editable().getWindow().getFrame().getAttribute( 'aria-readonly' );
+
+				assert.areSame( 'false', readonlyAttr, 'editor frame has appropriate value of [aria-readonly] attribute' );
+			} );
+		},
+
+		// (#1904)
+		'test initial value of [aria-readonly] attribute (readonly editor)': function() {
+			bender.editorBot.create( {
+				name: 'readonly-initial-readonly',
+				config: {
+					readOnly: true
+				}
+			}, function( bot ) {
+				var editor = bot.editor,
+					readonlyAttr = editor.editable().getWindow().getFrame().getAttribute( 'aria-readonly' );
+
+				assert.areSame( 'true', readonlyAttr, 'editor frame has appropriate value of [aria-readonly] attribute' );
+			} );
+		},
+
+		// (#1904)
+		'test update value of [aria-readonly] attribute': function() {
+			bender.editorBot.create( {
+				name: 'readonly-update',
+				config: {
+					readOnly: true
+				}
+			}, function( bot ) {
+				var editor = bot.editor,
+					readonlyAttr;
+
+				editor.setReadOnly( false );
+				readonlyAttr = readonlyAttr = editor.editable().getWindow().getFrame().getAttribute( 'aria-readonly' );
+
+				assert.areSame( 'false', readonlyAttr, 'editor frame has appropriate value of [aria-readonly] attribute' );
+			} );
 		}
 	} );
 } )();
