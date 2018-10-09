dff7a45014f00d392ac0f13102c7b51dcbecc774
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-10-09 11:50:31 +0200

    Tests: added unit test.

---
+++ tests/core/dom/element/focusoptions.js
@@ -0,0 +1,22 @@
+/* bender-tags: editor */
+
+( function() {
+	bender.test( {
+		// Purpose of this test is to detect whenever browser implements `focusOptions.preventScroll`.
+		// Failing test means we should update `CKEDITOR.dom.element.prototype.focus` to drop usage of fallback for this browser.
+		// (#2420)
+		'test native focus options': function() {
+			if ( !CKEDITOR.env.gecko && !CKEDITOR.env.safari ) {
+				assert.ignore();
+			}
+			var document = CKEDITOR.document,
+				container = document.findOne( '.container' ),
+				editable = document.findOne( '.editable' );
+
+			editable.$.focus( { preventScroll: true } );
+
+			assert.isFalse( container.$.scrollTop === 0 );
+		}
+	} );
+
+} )();
