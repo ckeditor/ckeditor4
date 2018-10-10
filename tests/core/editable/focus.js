3f4d3cc5a78ff0a552c29fb81056ec5da90cdb73
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-10-10 15:27:39 +0200

    Tests: fixed and simplified.

--- tests/core/editable/focus.js
+++ tests/core/editable/focus.js
@@ -31,29 +31,21 @@
 			spy.restore();
 		},
 		// (#2420)
+		tearDown: function() {
+			CKEDITOR.document.findOne( '.focusable' ).focus();
+		},
 		'test scroll position doesn\'t change when preventScroll is set to true': function( editor ) {
 			var editable = editor.editable(),
 				body = CKEDITOR.document.getBody(),
-				html = body.getParent(),
-				spacer = new CKEDITOR.dom.element( 'div' ),
-				focusable = new CKEDITOR.dom.element( 'textarea' ),
-				scrollTop = {};
-
-			spacer.setStyle( 'height', '8000px' );
-			spacer.insertBefore( body.getFirst() );
-			focusable.insertBefore( spacer );
-			focusable.focus();
-
-			scrollTop.body = body.$.scrollTop;
-			scrollTop.html = html.$.scrollTop;
+				html = body.getParent();
 
 			editable.focus( { preventScroll: true } );
 
 			// Scroll is delayed in IE and Edge.
 			setTimeout( function() {
 				resume( function() {
-					assert.areEqual( scrollTop.body, body.$.scrollTop, 'Body should be scrolled top' );
-					assert.areEqual( scrollTop.html, html.$.scrollTop, 'Html should be scrolled top' );
+					assert.areEqual( 0, body.$.scrollTop, 'Body should be scrolled top' );
+					assert.areEqual( 0, html.$.scrollTop, 'Html should be scrolled top' );
 				} );
 			}, 50 );
 			wait();
