390a3d193b0b18c67101b4e8a947a99dee2a035c
Jacek Bogdanski <jacekbogd@gmail.com>
2018-10-11 15:26:34 +0200

    Restored refactoring which may caused issue with multiple autocomplete instances.

--- plugins/autocomplete/plugin.js
+++ plugins/autocomplete/plugin.js
@@ -279,8 +279,14 @@
 			this._listeners.push( this.view.on( 'click-item', this.onItemClick, this ) );
 
 			// Update view position on viewport change.
-			this._listeners.push( win.on( 'scroll', this.viewRepositionListener, this ) );
-			this._listeners.push( editorScrollableElement.on( 'scroll', this.viewRepositionListener, this ) );
+			// Note: CKEditor's event system has a limitation that one function
+			// cannot be used as listener for the same event more than once. Hence, wrapper functions.
+			this._listeners.push( win.on( 'scroll', function() {
+				this.viewRepositionListener();
+			}, this ) );
+			this._listeners.push( editorScrollableElement.on( 'scroll', function() {
+				this.viewRepositionListener();
+			}, this ) );
 
 			this._listeners.push( editor.on( 'contentDom', onContentDom, this ) );
 
