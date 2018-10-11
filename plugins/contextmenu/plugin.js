391719d45e6521f61aa730ba5fc0baedf91e656e
Jacek Bogdanski <jacekbogd@gmail.com>
2018-10-11 13:40:28 +0200

    Contextmenu allows for custom css.

--- plugins/contextmenu/plugin.js
+++ plugins/contextmenu/plugin.js
@@ -1,5 +1,5 @@
-﻿/**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+﻿/** @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights
+ * reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
@@ -30,6 +30,8 @@ CKEDITOR.plugins.add( 'contextmenu', {
 			$: function( editor ) {
 				this.base.call( this, editor, {
 					panel: {
+						// Allow adding custom CSS (#2202).
+						css: editor.config.contextmenu_contentCss,
 						className: 'cke_menu_panel',
 						attributes: {
 							'aria-label': editor.lang.contextmenu.options
