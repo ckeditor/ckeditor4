c6a6eb3e7968559c50d18f02855030818b4154a2
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-10-12 09:26:55 +0200

    Docs: corrected entry.

--- plugins/richcombo/plugin.js
+++ plugins/richcombo/plugin.js
@@ -110,7 +110,7 @@ CKEDITOR.plugins.add( 'richcombo', {
 			 * Updates state of the combo.
 			 *
 			 * @since 4.11.0
-			 * @param {CKEDITOR.editor} editor The editor instance which this rich combo instance.
+			 * @param {CKEDITOR.editor} editor The editor instance which this rich combo is to be used by.
 			 */
 			updateState: function( editor ) {
 				// Don't change state while richcombo is active (https://dev.ckeditor.com/ticket/11793).
