b76a64326554eb6a7d81b58e95b2a5404b27d5f2
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-11-19 12:48:58 +0100

    Tests: added comment.

--- tests/core/dom/element/focusoptions.js
+++ tests/core/dom/element/focusoptions.js
@@ -4,6 +4,7 @@
 	bender.test( {
 		// Purpose of this test is to detect whenever browser implements `focusOptions.preventScroll`.
 		// Failing test means we should update `CKEDITOR.dom.element.prototype.focus` to drop usage of fallback for this browser.
+		// Note: this test fails when refreshed on Edge, but on first run it passes. It looks like Edge doesn't focus editable element on refresh.
 		// (#2420)
 		'test native focus options': function() {
 			if ( CKEDITOR.env.chrome || CKEDITOR.env.ie && !CKEDITOR.env.edge ) {
