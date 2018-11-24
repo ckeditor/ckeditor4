815edf1df7679730fbcea7dde55ee824ff8abf47
Tomasz Jakut <vepomoc@gmail.com>
2018-11-24 21:02:53 +0100

    Add unit test.

--- tests/plugins/button/button.js
+++ tests/plugins/button/button.js
@@ -5,10 +5,16 @@ var customCls = 'my_btn';
 
 bender.editor = {
 	config: {
-		toolbar: [ [ 'custom_btn', 'expandable_btn' ] ],
+		toolbar: [ [ 'custom_btn', 'expandable_btn', 'clickable_btn' ] ],
 		on: {
 			pluginsLoaded: function( evt ) {
-				var ed = evt.editor;
+				var ed = evt.editor,
+					buttonCmd = new CKEDITOR.command( ed, {
+						exec: function() {}
+					} );
+
+				ed.addCommand( 'buttonCmd', buttonCmd );
+
 				ed.ui.addButton( 'custom_btn', {
 					label: 'button with custom class',
 					className: customCls
@@ -17,6 +23,11 @@ bender.editor = {
 				ed.ui.addButton( 'expandable_btn', {
 					label: 'expandable button',
 					hasArrow: true
+				} );
+
+				ed.ui.addButton( 'clickable_btn', {
+					label: 'clickable button',
+					command: 'buttonCmd'
 				} );
 			}
 		}
@@ -41,5 +52,45 @@ bender.test( {
 		btnEl = CKEDITOR.document.getById( btn._.id );
 
 		assert.isTrue( btnEl.hasClass( 'cke_button_expandable' ), 'check ui item expandable class name' );
+	},
+
+	// (#2565)
+	'test right-clicking button': function() {
+		if ( !CKEDITOR.env.ie ) {
+			assert.ignore();
+		}
+
+		var editor = this.editor,
+			btn = editor.ui.get( 'clickable_btn' ),
+			btnEl = CKEDITOR.document.getById( btn._.id ),
+			buttonCmd = editor.getCommand( 'buttonCmd' ),
+			spy = sinon.spy( buttonCmd, 'exec' );
+
+		dispatchMouseEvent( btnEl, 'mouseup', CKEDITOR.MOUSE_BUTTON_RIGHT );
+
+		assert.areSame( 0, spy.callCount );
 	}
 } );
+
+function dispatchMouseEvent( element, type, button ) {
+	var ie8ButtonMap = {
+			0: 1,
+			1: 4,
+			2: 2
+		},
+		mouseEvent;
+	element = element.$;
+
+	// Thanks to http://help.dottoro.com/ljhlvomw.php
+	if ( document.createEventObject ) {
+		mouseEvent = document.createEventObject();
+
+		mouseEvent.button = ie8ButtonMap[ button ];
+		element.fireEvent( 'on' + type, mouseEvent );
+	} else {
+		mouseEvent = document.createEvent( 'MouseEvent' );
+
+		mouseEvent.initMouseEvent( type, true, true, window, 0, 0, 0, 80, 20, false, false, false, false, button, null );
+		element.dispatchEvent( mouseEvent );
+	}
+}
