87a73bc2c5d56f592f1dc9571647105755cfa199
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-12-04 17:01:05 +0100

    Tests: added test case.

--- tests/plugins/colorbutton/config.js
+++ tests/plugins/colorbutton/config.js
@@ -46,6 +46,36 @@
 			CKEDITOR.tools.array.map( colorOptions.toArray(), function( el, index ) {
 				assert.areSame( expectedLabels[ index ], colorOptions.getItem( index ).getAttribute( 'title' ), 'Title for color at index ' + index );
 			} );
+		},
+
+		// #1478
+		'test config.colorButton_colors applies color': function() {
+			var editor = this.editors.colorLabels,
+				bgColorBtn = editor.ui.get( 'BGColor' ),
+				filter = new CKEDITOR.htmlParser.filter( {
+					text: function( value ) {
+						return value.replace( '{', '[' ).replace( '}', ']' );
+					},
+					elements: {
+						br: function() {
+							return false;
+						}
+					}
+				} ),
+				colorSquare;
+
+			bender.tools.selection.setWithHtml( editor, '<p>[foo]</p>' );
+
+			editor.focus();
+			bgColorBtn.click( editor );
+
+			colorSquare = bgColorBtn._.panel.getBlock( bgColorBtn._.id ).element.findOne( 'a.cke_colorbox[data-value=FF9900]' );
+
+			colorSquare.$.onclick();
+
+			assert.beautified.html( bender.tools.selection.getWithHtml( editor ), '<p><span style="background-color:#ff9900;">[foo]</span></p>', {
+				customFilters: [ filter ]
+			} );
 		}
 	} );
 } )();
