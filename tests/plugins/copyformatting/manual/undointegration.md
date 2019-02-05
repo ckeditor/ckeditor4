71bf1c6e31a6fc56fae0c1dc7450903dc341e781
Mateusz Samsel <m.samsel@cksource.com>
2019-02-05 16:10:52 +0100

    Correct manual test description, add unit test.

--- tests/plugins/copyformatting/manual/undointegration.md
+++ tests/plugins/copyformatting/manual/undointegration.md
@@ -2,4 +2,12 @@
 @bender-tags: bug, copyformatting, 4.11.3, trac16675
 @bender-ckeditor-plugins: copyformatting, toolbar, wysiwygarea, undo, basicstyles
 
-1. Play with undo
+## Test both editors
+1. Add some content to editor (e.g. new line), to "avctivate" undo step.
+2. Start to click around editor to change selection inside editor (20-25 times). Selection have to differ between adjacent steps.
+
+### Expected:
+Undo UI button is active.
+
+### Unexpected:
+Undo UI became disabled.
---
+++ tests/plugins/copyformatting/undointegration.js
@@ -0,0 +1,45 @@
+/* bender-tags: copyformatting */
+/* bender-ckeditor-plugins: wysiwygarea, toolbar, copyformatting, undo */
+/* bender-include: _helpers/tools.js*/
+
+( function() {
+	'use strict';
+
+	bender.editor = {
+		config: {
+			allowedcontent: true
+		}
+	};
+
+	bender.test( {
+		'test undo integration': function() {
+			var editor = this.editor,
+				bot = this.editorBot,
+				sel = editor.getSelection(),
+				el1,
+				el2;
+
+			assert.areSame( 0, editor.undoManager.index, 'There shouldn\'t be any undo steps.' );
+
+			bot.setHtmlWithSelection( '<p><span id="one">foo</span> []bar <span id="two">baz</span></p>' );
+			editor.insertText( '1' );
+
+			assert.areSame( 1, editor.undoManager.index, 'There should be only 1 undo step.' );
+
+			el1 = editor.editable().findOne( '#one' );
+			el2 = editor.editable().findOne( '#two' );
+
+			for ( var i = 0; i < editor.undoManager.limit; i++ ) {
+				sel.selectElement( i % 2 ? el1 : el2 );
+				editor.document.fire( 'mouseup', new CKEDITOR.dom.event( {
+					button: CKEDITOR.MOUSE_BUTTON_LEFT
+				} ) );
+			}
+
+			assert.areSame( 1, editor.undoManager.index, 'There shouldn\'t be new undo steps.' );
+			assert.isTrue( editor.undoManager.undoable(), 'Editor should has possibility to undo.' );
+
+		}
+	} );
+
+} )();
