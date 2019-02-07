c4ab7062ea86fce8d715522c0f5d52c03306f80c
Mateusz Samsel <m.samsel@cksource.com>
2019-02-07 10:56:06 +0100

    Change how comments are added to code.

--- tests/plugins/balloonpanel/cssloading.js
+++ tests/plugins/balloonpanel/cssloading.js
@@ -23,15 +23,13 @@
 
 		'test loading css with path': function() {
 			assert.areSame( 'kama,%BASE_PATH%../../apps/ckeditor/skins/kama/', CKEDITOR.skinName, 'config skinName should be with path' );
-			/*
-			(#2796)
-			'removeDots' is necessary to properly compare URL. We cannot use absolute url, as test might have additional folders in URL.
-			Tests run in bender:
-				* from dashboard have address like: http://tests.ckeditor.test/tests/plugins/balloonpanel/cssloading
-				* from jobs on Travis have address like: http://tests.ckeditor.test/jobs/VY5lgovj70g1AOkv/tests/tests/plugins/balloonpanel/cssloading
-			This is why there have to be relative URL. However 'appendStyleSheet' receive final URL which does not contain dots in address.
-			It's necessary to adapt URL address to be properly compared in assertion.
-			*/
+			// (#2796)
+			// 'removeDots' is necessary to properly compare URL. We cannot use absolute url, as test might have additional folders in URL.
+			// Tests run in bender:
+			// 	* from dashboard have address like: http://tests.ckeditor.test/tests/plugins/balloonpanel/cssloading
+			// 	* from jobs on Travis have address like: http://tests.ckeditor.test/jobs/VY5lgovj70g1AOkv/tests/tests/plugins/balloonpanel/cssloading
+			// This is why there have to be relative URL. However 'appendStyleSheet' receive final URL which does not contain dots in address.
+			// It's necessary to adapt URL address to be properly compared in assertion.
 			sinon.assert.calledWith( spy, balloonTestsTools.getDocumentOrigin() +
 				balloonTestsTools.removeDotsFromUrl( '%BASE_PATH%../../apps/ckeditor/plugins/balloonpanel/skins/kama/balloonpanel.css' ) );
 		}
