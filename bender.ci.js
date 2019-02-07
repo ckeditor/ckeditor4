722031b70226de6460a5a71118353852bb1630f1
Mateusz Samsel <m.samsel@cksource.com>
2019-02-07 11:14:14 +0100

    Force bender to use headless browsers.

--- bender.ci.js
+++ bender.ci.js
@@ -5,5 +5,9 @@ var config = require( './bender' );
 
 config.startBrowser = process.env.BROWSER || 'Chrome';
 config.isTravis = true;
+config.startBrowserOptions = {
+	Chrome: '--headless --disable-gpu',
+	Firefox: '-headless'
+};
 
 module.exports = config;
