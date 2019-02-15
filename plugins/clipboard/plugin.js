ab75de2e46ec3e0aa7b253d93355a943d72a92a4
Mateusz Samsel <m.samsel@cksource.com>
2019-02-15 15:18:22 +0100

    Correct comment description.

--- plugins/clipboard/plugin.js
+++ plugins/clipboard/plugin.js
@@ -1976,7 +1976,7 @@
 			// ...and paste content into the drop position.
 			dropRange = editor.createRange();
 			// Get actual selection with bookmarks if drop's bookmark are not in editable any longer.
-			// This might happen after extracting content from range. (#2292)
+			// This might happen after extracting content from range (#2292).
 			if ( !dropBookmark.startNode.getCommonAncestor( editable ) ) {
 				dropBookmark = editor.getSelection().createBookmarks()[ 0 ];
 			}
