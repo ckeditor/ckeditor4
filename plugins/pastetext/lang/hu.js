d9daf731d172643c04143b94d1197f7a0c67818f
Wiktor Walc <w.walc@cksource.com>
2019-02-20 12:52:43 +0100

    Updated language files.

--- plugins/pastetext/lang/hu.js
+++ plugins/pastetext/lang/hu.js
@@ -4,6 +4,6 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
 CKEDITOR.plugins.setLang( 'pastetext', 'hu', {
 	button: 'Beillesztés formázatlan szövegként',
-	pasteNotification: 'Nyomjd meg a %1 gombot a beillesztéshez. A böngésző nem támogatja a beillesztést az eszköztás gombbal vagy a menüből.',
+	pasteNotification: 'Nyomja meg a %1 gombot a beillesztéshez. A böngésző nem támogatja a beillesztést az eszköztár gombbal vagy a menüből.',
 	title: 'Beillesztés formázatlan szövegként'
 } );
