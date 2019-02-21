0e2592a8c051ad491d62f32f34b6d78d6d906d99
Ragnar Stølsmark <rstolsmark@live.no>
2019-02-21 10:38:28 +0100

    Fixed missing norwegian translation

--- plugins/pastetext/lang/nb.js
+++ plugins/pastetext/lang/nb.js
@@ -4,6 +4,6 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
 CKEDITOR.plugins.setLang( 'pastetext', 'nb', {
 	button: 'Lim inn som ren tekst',
-	pasteNotification: 'Press %1 to paste. Your browser doesn‘t support pasting with the toolbar button or context menu option.', // MISSING
+	pasteNotification: 'Trykk %1 for å lime inn. Nettleseren din støtter ikke å lime inn med knappen i verktøylinjen eller høyreklikkmenyen.',
 	title: 'Lim inn som ren tekst'
 } );
