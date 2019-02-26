ed4e330e18a90e56223f3a798058dd008c608c93
Mateusz Samsel <m.samsel@cksource.com>
2019-02-26 11:05:35 +0100

    Correct comments.

--- tests/_benderjs/ckeditor/static/bot.js
+++ tests/_benderjs/ckeditor/static/bot.js
@@ -114,7 +114,7 @@
 
 	bender.editorBot.createAsync = function( profile ) {
 		return bender.tools.promise( function( resolve, reject ) {
-			// By default this editor, should be surrounded with `promisifyCase`, so wait statements are generated inside this function,
+			// By default this editor, should be surrounded with `promise`, so wait statements are generated inside that function,
 			// and shouldn't be call inside creation of new editor.
 			profile.ignoreEditorWaits = profile.ignoreEditorWaits === undefined ? true : profile.ignoreEditorWaits;
 
