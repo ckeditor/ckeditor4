2dea684236f420fa976e30593fde75181146adfa
Nicholas Hoyt <nhoyt@illinois.edu>
2019-02-27 10:28:11 -0600

    Remove files no longer used

--- custom/ui-library.js
+++
@@ -1,9 +0,0 @@
-/**
-* Copyright (c) 2018 University of Illinois - Jon Gunderson and Nicholas Hoyt. All rights reserved.
-* For licensing, see LICENSE.md or http://ckeditor.com/license
-*/
-CKEDITOR.replace( 'editor', {
-  customConfig: CKEDITOR.basePath + 'custom/config.js',
-  headings: 'h1:h4',
-  oneLevel1: true
-} );
