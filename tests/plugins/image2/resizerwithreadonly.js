1ed6547b642e4182bbe394fe4cb175c21d4b36b2
Dariusz Jarzębski <d.jarzebski@cksource.com>
2019-03-04 12:15:54 +0100

    Rewrite unit test to check if resize handler is created properly.

--- tests/plugins/image2/resizerwithreadonly.js
+++ tests/plugins/image2/resizerwithreadonly.js
@@ -5,7 +5,7 @@
 	'use strict';
 
 	bender.test( {
-		// #719
+		// #719, #2816, #2874
 		'test readOnly set to false': function() {
 			bender.editorBot.create( {
 				name: 'editor1',
@@ -27,8 +27,8 @@
 			} );
 		},
 
-		// #719
-		'test readOnly set to true': function() {
+		// #719, #2874
+		'test readOnly set to true at initialization': function() {
 			bender.editorBot.create( {
 				name: 'editor2',
 				config: {
@@ -42,13 +42,37 @@
 					listener.removeListener();
 
 					resume( function() {
-						assert.areSame( 2, editor.editable().getElementsByTag( 'span' ).$.length );
+						assert.areSame( 3, editor.editable().getElementsByTag( 'span' ).$.length );
 					} );
 				} );
 
 				editor.setData( '<img src="_assets/foo.png" alt="" />' );
 				wait();
 			} );
-		}
+		},
+
+		// #719, #2816
+		'test readOnly set to true after initialization': function() {
+			bender.editorBot.create( {
+				name: 'editor3',
+				config: {
+					readOnly: true
+				}
+			}, function( bot ) {
+				var editor = bot.editor,
+					listener;
+
+				listener = editor.on( 'dataReady', function() {
+					listener.removeListener();
+
+					resume( function() {
+						assert.areSame( 3, editor.editable().getElementsByTag( 'span' ).$.length );
+					} );
+				} );
+
+				editor.setData( '<img src="_assets/foo.png" alt="" />' );
+				wait();
+			} );
+		}		
 	} );
 }() );
