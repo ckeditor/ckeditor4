44cd87cab30a682f5262fba7f74e3cf0e140171b
Dariusz Jarzębski <d.jarzebski@cksource.com>
2019-03-07 11:00:52 +0100

    Refactor unit tests.

--- tests/plugins/image2/resizerwithreadonly.js
+++ tests/plugins/image2/resizerwithreadonly.js
@@ -6,61 +6,33 @@
 
 	bender.test( {
 		// #719, #2816, #2874
-		'test readOnly set to false': function() {
-			bender.editorBot.create( {
-				name: 'editor1'
-			}, function( bot ) {
-				var editor = bot.editor;
-
-				editor.setData( '<img src="_assets/foo.png" alt="" />', function() {
-					resume( function() {
-						var resizer = editor.editable().findOne( '.cke_widget_image .cke_image_resizer' );
-						assert.isTrue( Boolean( resizer ), 'Resizer should be enabled'  );
-					} );
-				} );
-
-				wait();
-			} );
-		},
+		'test readOnly set to false': createResizerTest( { name: 'editor1' } ),
 
 		// #719, #2874
-		'test readOnly set to true at initialization': function() {
-			bender.editorBot.create( {
-				name: 'editor2',
-				config: {
-					readOnly: true
-				}
-			}, function( bot ) {
-				var editor = bot.editor;
-
-				editor.setData( '<img src="_assets/foo.png" alt="" />', function() {
-					resume( function() {
-						var resizer = editor.editable().findOne( '.cke_widget_image .cke_image_resizer' );
-						assert.isTrue( Boolean( resizer ), 'Resizer should be enabled'  );
-					} );
-				} );
-
-				wait();
-			} );
-		},
+		'test readOnly set to true at initialization': createResizerTest( { name: 'editor2', config: { readOnly: true } } ),
 
 		// #719, #2816
-		'test readOnly set to true after initialization': function() {
-			bender.editorBot.create( {
-				name: 'editor3'
-			}, function( bot ) {
-				var editor = bot.editor;
+		'test readOnly set to true after initialization': createResizerTest( { name: 'editor3' }, true )
 
-				editor.setData( '<img src="_assets/foo.png" alt="" />', function() {
-					resume( function() {
-						editor.setReadOnly( true );
-						var resizer = editor.editable().findOne( '.cke_widget_image .cke_image_resizer' );
-						assert.isTrue( Boolean( resizer ), 'Resizer should be enabled'  );
-					} );
-				} );
-
-				wait();
-			} );
-		}
 	} );
 }() );
+
+function createResizerTest( profile, readOnly ) {
+	return function() {
+		bender.editorBot.create( profile, function( bot ) {
+			var editor = bot.editor;
+
+			editor.setData( '<img src="_assets/foo.png" alt="" />', function() {
+				resume( function() {
+					if ( readOnly ) {
+						editor.setReadOnly( readOnly );
+					}
+					var resizer = editor.editable().findOne( '.cke_widget_image .cke_image_resizer' );
+					assert.isTrue( Boolean( resizer ), 'Resizer should be enabled'  );
+				} );
+			} );
+
+			wait();
+		} );
+	};
+}
