e581109f1df21b246546ae6ab7867c05dd2c57db
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-03-11 15:29:14 +0100

    Tests: added unit test.

---
+++ tests/plugins/pastefromword/generated/vertical_margin.js
@@ -0,0 +1,39 @@
+/* bender-tags: clipboard,pastefromword */
+/* jshint ignore:start */
+/* bender-ckeditor-plugins: pastefromword,ajax, */
+/* jshint ignore:end */
+/* bender-include: _lib/q.js,_helpers/promisePasteEvent.js,_helpers/assertWordFilter.js,_helpers/createTestCase.js */
+/* bender-include: _helpers/createTestSuite.js,_helpers/pfwTools.js */
+/* global pfwTools,createTestSuite */
+
+( function() {
+	'use strict';
+
+	var config = {
+		allowedContent: true,
+		language: 'en',
+		removePlugins: pfwTools.defaultConfig.removePlugins,
+		pasteFromWord_keepEmptyMargins: true
+	};
+
+	bender.editor = {
+		config: config
+	};
+
+	bender.test( createTestSuite( {
+		browsers: [
+			'chrome',
+			'edge',
+			'firefox',
+			'ie11'
+		],
+		wordVersions: [
+			'Word2016'
+		],
+		tests: {
+			'Vertical_margin': true
+		},
+
+		ignoreAll: CKEDITOR.env.ie
+	} ) );
+} )();
