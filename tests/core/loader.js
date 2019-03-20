d5bcd3d2ab1edfa79801596cf3397b5a3ca962df
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-03-20 15:36:39 +0100

    Tests: added test for script load listeners.

---
+++ tests/core/loader.js
@@ -0,0 +1,34 @@
+/* bender-tags: editor */
+( function() {
+	'use strict';
+
+	bender.test( {
+		// Script listeners should be removed because it might cause memory leaks (#589).
+		'test script load listeners removed': function() {
+			var loader = CKEDITOR.loader,
+				scriptName = loader.loadedScripts.pop();
+
+			getScript().remove();
+
+			delete loader.loadedScripts[ 's:' + scriptName ];
+
+			loader.load( scriptName, false );
+
+			getScript().on( 'load', function() {
+				var script = this;
+
+				setTimeout( function() {
+					resume( function() {
+						assert.isFalse( !!script.$.onreadystatechange );
+						assert.isFalse( !!script.$.onload );
+					} );
+				}, 50 );
+			} );
+
+			wait();
+			function getScript() {
+				return CKEDITOR.document.findOne( 'script[src="' + CKEDITOR.getUrl( 'core/' + scriptName + '.js' ) + '"]' );
+			}
+		}
+	} );
+} )();
