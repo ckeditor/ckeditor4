18a8fbe577bd93be18ff9c2e8dd39c6420cc1680
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-03-22 15:58:37 +0100

    Tests: ignore on built version.

--- tests/core/loader.js
+++ tests/core/loader.js
@@ -5,8 +5,14 @@
 	bender.test( {
 		// Script listeners should be removed because it might cause memory leaks (#589).
 		'test script load listeners removed': function() {
-			var loader = CKEDITOR.loader,
-				scriptName = loader.loadedScripts.pop();
+			var loader = CKEDITOR.loader;
+
+			if ( !loader ) {
+				// All core modules are bundled in release version, so we can't and don't need to test it there.
+				assert.ignore();
+			}
+
+			var scriptName = loader.loadedScripts.pop();
 
 			getScript().remove();
 
