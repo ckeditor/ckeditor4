1d6f4ed580f682311e538d246efba622bcf78adc
Jacek Bogdanski <jacekbogd@gmail.com>
2019-03-26 17:22:46 +0100

    Updated promise to expose full type.

--- core/promise/promise.js
+++ core/promise/promise.js
@@ -3,28 +3,49 @@
 ( function() {
 	'use strict';
 
-	var isPolyfillNeeded = CKEDITOR.env.ie && !CKEDITOR.env.edge;
+	CKEDITOR.domReady( function() {
+		if ( window.Promise ) {
+			CKEDITOR.tools.promise = Promise;
+		} else {
+			var script = new CKEDITOR.dom.element( 'script' );
 
-	if ( isPolyfillNeeded ) {
-		CKEDITOR.loader.load( 'promise/polyfill' );
-	}
+			script.setAttributes( {
+				type: 'text/javascript',
+				src: CKEDITOR.getUrl( 'core/promise/polyfill.js' )
+			} );
+
+			if ( script.$.onreadystatechange !== undefined ) {
+				script.$.onreadystatechange = function() {
+					var readyState = script.$.readyState;
+
+					if ( readyState === 'loaded' || readyState === 'complete' ) {
+						CKEDITOR.tools.promise = ES6Promise;
+					}
+				};
+			} else {
+				script.$.onload = script.$.onreadystatechange = function() {
+					CKEDITOR.tools.promise = ES6Promise;
+				};
+			}
+
+			CKEDITOR.document.getBody().append( script );
+		}
+	} );
 
 	/**
-	 * Creates [Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)
+	 * Alias for [Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)
 	 * object representing asynchronous operation.
 	 *
-	 * This helper function uses [ES6-Promise](https://github.com/stefanpenner/es6-promise) as a polyfill for Internet Explorer.
+	 * Uses [ES6-Promise](https://github.com/stefanpenner/es6-promise) as a polyfill.
 	 * Note that the polyfill won't be loaded if a browser supports native Promise object.
 	 *
 	 * See [MDN Promise documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise) for
 	 * more details how to work with promises.
 	 *
+	 * @property promise
 	 * @param {Function} resolver
 	 * @member CKEDITOR.tools
 	 * @returns {Promise}
 	 */
-	CKEDITOR.tools.promise = function( resolver ) {
-		return isPolyfillNeeded ? new ES6Promise( resolver ) : new Promise( resolver );
-	};
 
 } )();
