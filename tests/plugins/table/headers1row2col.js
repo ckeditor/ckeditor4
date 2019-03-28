d6ee5d1aacf5809f689d0e2a2777839a0d0035c7
Dariusz Jarzębski <d.jarzebski@cksource.com>
2019-03-28 14:43:41 +0100

    Drop parameter in header functions and use 'replace()' method instead.

--- tests/plugins/table/headers1row2col.js
+++ tests/plugins/table/headers1row2col.js
@@ -11,37 +11,37 @@
 	};
 
 	bender.test( {
-		'1 row, 2 cols, none -> none': assertHeadersCorrectnesssAfterManipulation( headerNone( 'input' ), headerNone(), 'none' ),
+		'1 row, 2 cols, none -> none': assertHeadersCorrectnesssAfterManipulation( headerNone(), headerNone(), 'none' ),
 
-		'1 row, 2 cols, none -> col': assertHeadersCorrectnesssAfterManipulation( headerNone( 'input' ), headerCol(), 'col' ),
+		'1 row, 2 cols, none -> col': assertHeadersCorrectnesssAfterManipulation( headerNone(), headerCol(), 'col' ),
 
-		'1 row, 2 cols, none -> row': assertHeadersCorrectnesssAfterManipulation( headerNone( 'input' ), headerRow(), 'row' ),
+		'1 row, 2 cols, none -> row': assertHeadersCorrectnesssAfterManipulation( headerNone(), headerRow(), 'row' ),
 
-		'1 row, 2 cols, none -> both': assertHeadersCorrectnesssAfterManipulation( headerNone( 'input' ), headerBoth(), 'both' ),
+		'1 row, 2 cols, none -> both': assertHeadersCorrectnesssAfterManipulation( headerNone(), headerBoth(), 'both' ),
 
-		'1 row, 2 cols, col -> none': assertHeadersCorrectnesssAfterManipulation( headerCol( 'input' ), headerNone(), 'none' ),
+		'1 row, 2 cols, col -> none': assertHeadersCorrectnesssAfterManipulation( headerCol(), headerNone(), 'none' ),
 
-		'1 row, 2 cols, col -> col': assertHeadersCorrectnesssAfterManipulation( headerCol( 'input' ), headerCol(), 'col' ),
+		'1 row, 2 cols, col -> col': assertHeadersCorrectnesssAfterManipulation( headerCol(), headerCol(), 'col' ),
 
-		'1 row, 2 cols, col -> row': assertHeadersCorrectnesssAfterManipulation( headerCol( 'input' ), headerRow(), 'row' ),
+		'1 row, 2 cols, col -> row': assertHeadersCorrectnesssAfterManipulation( headerCol(), headerRow(), 'row' ),
 
-		'1 row, 2 cols, col -> both': assertHeadersCorrectnesssAfterManipulation( headerCol( 'input' ), headerBoth(), 'both' ),
+		'1 row, 2 cols, col -> both': assertHeadersCorrectnesssAfterManipulation( headerCol(), headerBoth(), 'both' ),
 
-		'1 row, 2 cols, row -> none': assertHeadersCorrectnesssAfterManipulation( headerRow( 'input' ), headerNone(), 'none' ),
+		'1 row, 2 cols, row -> none': assertHeadersCorrectnesssAfterManipulation( headerRow(), headerNone(), 'none' ),
 
-		'1 row, 2 cols, row -> col': assertHeadersCorrectnesssAfterManipulation( headerRow( 'input' ), headerCol(), 'col' ),
+		'1 row, 2 cols, row -> col': assertHeadersCorrectnesssAfterManipulation( headerRow(), headerCol(), 'col' ),
 
-		'1 row, 2 cols, row -> row': assertHeadersCorrectnesssAfterManipulation( headerRow( 'input' ), headerRow(), 'row' ),
+		'1 row, 2 cols, row -> row': assertHeadersCorrectnesssAfterManipulation( headerRow(), headerRow(), 'row' ),
 
-		'1 row, 2 cols, row -> both': assertHeadersCorrectnesssAfterManipulation( headerRow( 'input' ), headerBoth(), 'both' ),
+		'1 row, 2 cols, row -> both': assertHeadersCorrectnesssAfterManipulation( headerRow(), headerBoth(), 'both' ),
 
-		'1 row, 2 cols, both -> none': assertHeadersCorrectnesssAfterManipulation( headerBoth( 'input' ), headerNone(), 'none' ),
+		'1 row, 2 cols, both -> none': assertHeadersCorrectnesssAfterManipulation( headerBoth(), headerNone(), 'none' ),
 
-		'1 row, 2 cols, both -> col': assertHeadersCorrectnesssAfterManipulation( headerBoth( 'input' ), headerCol(), 'col' ),
+		'1 row, 2 cols, both -> col': assertHeadersCorrectnesssAfterManipulation( headerBoth(), headerCol(), 'col' ),
 
-		'1 row, 2 cols, both -> row': assertHeadersCorrectnesssAfterManipulation( headerBoth( 'input' ), headerRow(), 'row' ),
+		'1 row, 2 cols, both -> row': assertHeadersCorrectnesssAfterManipulation( headerBoth(), headerRow(), 'row' ),
 
-		'1 row, 2 cols, both -> both': assertHeadersCorrectnesssAfterManipulation( headerBoth( 'input' ), headerBoth(), 'both' )
+		'1 row, 2 cols, both -> both': assertHeadersCorrectnesssAfterManipulation( headerBoth(), headerBoth(), 'both' )
 	} );
 
 	function assertHeadersCorrectnesssAfterManipulation( input, expected, headerType ) {
@@ -56,39 +56,39 @@
 				dialog.fire( 'ok' );
 				dialog.hide();
 
-				assert.beautified.html( expected,
+				assert.beautified.html( expected.replace( '^', '' ),
 					dialog.getParentEditor().getData() );
 			} );
 		};
 	}
 
-	function headerNone( role ) {
+	function headerNone() {
 		return '<table border="1" cellpadding="1" cellspacing="1" style="width:500px">' +
 					'<tbody>' +
 						'<tr>' +
-							'<td>' + ( role == 'input' ? '^' : '' ) + '&nbsp;</td>' +
+							'<td>^&nbsp;</td>' +
 							'<td>&nbsp;</td>' +
 						'</tr>' +
 					'</tbody>' +
 				'</table>';
 	}
 
-	function headerCol( role ) {
+	function headerCol() {
 		return '<table border="1" cellpadding="1" cellspacing="1" style="width:500px">' +
 					'<tbody>' +
 						'<tr>' +
-							'<th scope="row">' + ( role == 'input' ? '^' : '' ) + '&nbsp;</th>' +
+							'<th scope="row">^&nbsp;</th>' +
 							'<td>&nbsp;</td>' +
 						'</tr>' +
 					'</tbody>' +
 				'</table>';
 	}
 
-	function headerRow( role ) {
+	function headerRow() {
 		return '<table border="1" cellpadding="1" cellspacing="1" style="width:500px">' +
 					'<thead>' +
 						'<tr>' +
-							'<th scope="col">' + ( role == 'input' ? '^' : '' ) + '&nbsp;</th>' +
+							'<th scope="col">^&nbsp;</th>' +
 							'<th scope="col">&nbsp;</th>' +
 						'</tr>' +
 					'</thead>' +
@@ -97,11 +97,11 @@
 				'</table>';
 	}
 
-	function headerBoth( role ) {
+	function headerBoth() {
 		return '<table border="1" cellpadding="1" cellspacing="1" style="width:500px">' +
 					'<thead>' +
 						'<tr>' +
-							'<th scope="col">' + ( role == 'input' ? '^' : '' ) + '&nbsp;</th>' +
+							'<th scope="col">^&nbsp;</th>' +
 							'<th scope="col">&nbsp;</th>' +
 						'</tr>' +
 					'</thead>' +
--- tests/plugins/table/headers2row1col.js
+++ tests/plugins/table/headers2row1col.js
@@ -11,37 +11,37 @@
 	};
 
 	bender.test( {
-		'2 rows, 1 col, none -> none': assertHeadersCorrectnesssAfterManipulation( headerNone( 'input' ), headerNone(), 'none' ),
+		'2 rows, 1 col, none -> none': assertHeadersCorrectnesssAfterManipulation( headerNone(), headerNone(), 'none' ),
 
-		'2 rows, 1 col, none -> col': assertHeadersCorrectnesssAfterManipulation( headerNone( 'input' ), headerCol(), 'col' ),
+		'2 rows, 1 col, none -> col': assertHeadersCorrectnesssAfterManipulation( headerNone(), headerCol(), 'col' ),
 
-		'2 rows, 1 col, none -> row': assertHeadersCorrectnesssAfterManipulation( headerNone( 'input' ), headerRow(), 'row' ),
+		'2 rows, 1 col, none -> row': assertHeadersCorrectnesssAfterManipulation( headerNone(), headerRow(), 'row' ),
 
-		'2 rows, 1 col, none -> both': assertHeadersCorrectnesssAfterManipulation( headerNone( 'input' ), headerBoth(), 'both' ),
+		'2 rows, 1 col, none -> both': assertHeadersCorrectnesssAfterManipulation( headerNone(), headerBoth(), 'both' ),
 
-		'2 rows, 1 col, col -> none': assertHeadersCorrectnesssAfterManipulation( headerCol( 'input' ), headerNone(), 'none' ),
+		'2 rows, 1 col, col -> none': assertHeadersCorrectnesssAfterManipulation( headerCol(), headerNone(), 'none' ),
 
-		'2 rows, 1 col, col -> col': assertHeadersCorrectnesssAfterManipulation( headerCol( 'input' ), headerCol(), 'col' ),
+		'2 rows, 1 col, col -> col': assertHeadersCorrectnesssAfterManipulation( headerCol(), headerCol(), 'col' ),
 
-		'2 rows, 1 col, col -> row': assertHeadersCorrectnesssAfterManipulation( headerCol( 'input' ), headerRow(), 'row' ),
+		'2 rows, 1 col, col -> row': assertHeadersCorrectnesssAfterManipulation( headerCol(), headerRow(), 'row' ),
 
-		'2 rows, 1 col, col -> both': assertHeadersCorrectnesssAfterManipulation( headerCol( 'input' ), headerBoth(), 'both' ),
+		'2 rows, 1 col, col -> both': assertHeadersCorrectnesssAfterManipulation( headerCol(), headerBoth(), 'both' ),
 
-		'2 rows, 1 col, row -> none': assertHeadersCorrectnesssAfterManipulation( headerRow( 'input' ), headerNone(), 'none' ),
+		'2 rows, 1 col, row -> none': assertHeadersCorrectnesssAfterManipulation( headerRow(), headerNone(), 'none' ),
 
-		'2 rows, 1 col, row -> col': assertHeadersCorrectnesssAfterManipulation( headerRow( 'input' ), headerCol(), 'col' ),
+		'2 rows, 1 col, row -> col': assertHeadersCorrectnesssAfterManipulation( headerRow(), headerCol(), 'col' ),
 
-		'2 rows, 1 col, row -> row': assertHeadersCorrectnesssAfterManipulation( headerRow( 'input' ), headerRow(), 'row' ),
+		'2 rows, 1 col, row -> row': assertHeadersCorrectnesssAfterManipulation( headerRow(), headerRow(), 'row' ),
 
-		'2 rows, 1 col, row -> both': assertHeadersCorrectnesssAfterManipulation( headerRow( 'input' ), headerBoth(), 'both' ),
+		'2 rows, 1 col, row -> both': assertHeadersCorrectnesssAfterManipulation( headerRow(), headerBoth(), 'both' ),
 
-		'2 rows, 1 col, both -> none': assertHeadersCorrectnesssAfterManipulation( headerBoth( 'input' ), headerNone(), 'none' ),
+		'2 rows, 1 col, both -> none': assertHeadersCorrectnesssAfterManipulation( headerBoth(), headerNone(), 'none' ),
 
-		'2 rows, 1 col, both -> col': assertHeadersCorrectnesssAfterManipulation( headerBoth( 'input' ), headerCol(), 'col' ),
+		'2 rows, 1 col, both -> col': assertHeadersCorrectnesssAfterManipulation( headerBoth(), headerCol(), 'col' ),
 
-		'2 rows, 1 col, both -> row': assertHeadersCorrectnesssAfterManipulation( headerBoth( 'input' ), headerRow(), 'row' ),
+		'2 rows, 1 col, both -> row': assertHeadersCorrectnesssAfterManipulation( headerBoth(), headerRow(), 'row' ),
 
-		'2 rows, 1 col, both -> both': assertHeadersCorrectnesssAfterManipulation( headerBoth( 'input' ), headerBoth(), 'both' )
+		'2 rows, 1 col, both -> both': assertHeadersCorrectnesssAfterManipulation( headerBoth(), headerBoth(), 'both' )
 	} );
 
 	function assertHeadersCorrectnesssAfterManipulation( input, expected, headerType ) {
@@ -56,17 +56,17 @@
 				dialog.fire( 'ok' );
 				dialog.hide();
 
-				assert.beautified.html( expected,
+				assert.beautified.html( expected.replace( '^', '' ),
 					dialog.getParentEditor().getData() );
 			} );
 		};
 	}
 
-	function headerNone( role ) {
+	function headerNone() {
 		return '<table border="1" cellpadding="1" cellspacing="1" style="width:500px">' +
 					'<tbody>' +
 						'<tr>' +
-							'<td>' + ( role == 'input' ? '^' : '' ) + '&nbsp;</td>' +
+							'<td>^&nbsp;</td>' +
 						'</tr>' +
 						'<tr>' +
 							'<td>&nbsp;</td>' +
@@ -75,11 +75,11 @@
 				'</table>';
 	}
 
-	function headerCol( role ) {
+	function headerCol() {
 		return '<table border="1" cellpadding="1" cellspacing="1" style="width:500px">' +
 					'<tbody>' +
 						'<tr>' +
-							'<th scope="row">' + ( role == 'input' ? '^' : '' ) + '&nbsp;</th>' +
+							'<th scope="row">^&nbsp;</th>' +
 						'</tr>' +
 						'<tr>' +
 							'<th scope="row">&nbsp;</th>' +
@@ -88,11 +88,11 @@
 				'</table>';
 	}
 
-	function headerRow( role ) {
+	function headerRow() {
 		return '<table border="1" cellpadding="1" cellspacing="1" style="width:500px">' +
 					'<thead>' +
 						'<tr>' +
-							'<th scope="col">' + ( role == 'input' ? '^' : '' ) + '&nbsp;</th>' +
+							'<th scope="col">^&nbsp;</th>' +
 						'</tr>' +
 					'</thead>' +
 					'<tbody>' +
@@ -103,11 +103,11 @@
 				'</table>';
 	}
 
-	function headerBoth( role ) {
+	function headerBoth() {
 		return '<table border="1" cellpadding="1" cellspacing="1" style="width:500px">' +
 					'<thead>' +
 						'<tr>' +
-							'<th scope="col">' + ( role == 'input' ? '^' : '' ) + '&nbsp;</th>' +
+							'<th scope="col">^&nbsp;</th>' +
 						'</tr>' +
 					'</thead>' +
 					'<tbody>' +
