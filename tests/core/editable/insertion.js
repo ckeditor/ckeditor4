17feaad639c24a52cff3c02fe1f844afe4221007
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-04-02 16:05:26 +0200

    Tests: moved into existing file.

--- tests/core/editable/insertion.js
+++ tests/core/editable/insertion.js
@@ -160,6 +160,24 @@
 
 			assert.isInnerHtmlMatching( '<p id="p1">foo</p><p>bam^@</p><p>bar@</p>',
 				bender.tools.selection.getWithHtml( editor ), { compareSelection: true, normalizeSelection: true } );
-		}
+		},
+		// (#2813)
+		'test insertHtml when selection ends at the end of span': assertInsertHtml( '<span>foo{bar}</span>', '<span>foo</span><div>div</div>' ),
+		// (#2813)
+		'test insertHtml when selection starts at the beginning of span': assertInsertHtml( '<span>{foo}bar</span>', '<div>div</div><span>bar</span>' ),
+		// (#2813)
+		'test insertHtml when selection covers span': assertInsertHtml( '<span>{foobar}</span>', '<div>div</div>' )
 	} );
+
+	function assertInsertHtml( initial, expected ) {
+		return function() {
+			var editor = this.editorBot.editor;
+
+			bender.tools.selection.setWithHtml( editor, initial );
+
+			editor.insertHtml( '<div>div</div>' );
+
+			assert.areSame( expected, editor.editable().getHtml().toLowerCase().replace( /(<br>|\s)/g, '' ) );
+		};
+	}
 } )();
