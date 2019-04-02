ff7bc9c6e1b017d1755419088dfa9e9064b71f23
Jacek Bogdanski <jacekbogd@gmail.com>
2019-04-02 17:36:41 +0200

    Fixed failing test.

--- tests/plugins/table/table.js
+++ tests/plugins/table/table.js
@@ -15,19 +15,37 @@
 	};
 
 	bender.test( {
-		// (#2423)
-		'test model for newly created table': function() {
-			var editor = this.editors.editor,
-				bot = this.editorBots.editor;
+		'test create table': function() {
+			var bot = this.editorBots.editor,
+				editable = bot.editor.editable();
 
-			bot.setData( '', function() {
-				bot.dialog( 'tableProperties', function( dialog ) {
+			bot.dialog( 'tableProperties', function( dialog ) {
+				var isSmallViewport = editable.getSize( 'width' ) < 500;
+				// Check defaults.
+				assert.areSame( '3', dialog.getValueOf( 'info', 'txtRows' ) );
+				assert.areSame( '2', dialog.getValueOf( 'info', 'txtCols' ) );
+				// Table width is set either to 100% or 500px depending on the editable size.
+				assert.areSame( isSmallViewport ? '100%' : '500px', dialog.getValueOf( 'info', 'txtWidth' ) );
 
-					assert.isNull( dialog.getModel( editor ) );
-					assert.isFalse( dialog.isEditing( editor ) );
+				// (#2423)
+				assert.isNull( dialog.getModel( bot.editor ) );
+				assert.isFalse( dialog.isEditing( bot.editor ) );
 
-					dialog.hide();
-				} );
+				dialog.fire( 'ok' );
+				dialog.hide();
+
+				wait( function() {
+					// https://dev.ckeditor.com/ticket/8337: check cursor position after hand.
+					var output = bender.tools.getHtmlWithSelection( bot.editor );
+					output = bender.tools.fixHtml( bender.tools.compatHtml( output ) );
+					var expected = bender.tools.compatHtml( bender.tools.getValueAsHtml( 'create-table' ) );
+
+					if ( isSmallViewport ) {
+						expected = expected.replace( /500\s*px/, '100%' );
+					}
+
+					assert.areSame( expected, output );
+				}, 0 );
 			} );
 		},
 
@@ -47,38 +65,6 @@
 
 					dialog.hide();
 				} );
-			} );
-		},
-
-		'test create table': function() {
-			var bot = this.editorBots.editor,
-				editable = bot.editor.editable();
-
-			bot.editor.setData( '' );
-
-			bot.dialog( 'tableProperties', function( dialog ) {
-				var isSmallViewport = editable.getSize( 'width' ) < 500;
-				// Check defaults.
-				assert.areSame( '3', dialog.getValueOf( 'info', 'txtRows' ) );
-				assert.areSame( '2', dialog.getValueOf( 'info', 'txtCols' ) );
-				// Table width is set either to 100% or 500px depending on the editable size.
-				assert.areSame( isSmallViewport ? '100%' : '500px', dialog.getValueOf( 'info', 'txtWidth' ) );
-
-				dialog.fire( 'ok' );
-				dialog.hide();
-
-				wait( function() {
-					// https://dev.ckeditor.com/ticket/8337: check cursor position after hand.
-					var output = bender.tools.getHtmlWithSelection( bot.editor );
-					output = bender.tools.fixHtml( bender.tools.compatHtml( output ) );
-					var expected = bender.tools.compatHtml( bender.tools.getValueAsHtml( 'create-table' ) );
-
-					if ( isSmallViewport ) {
-						expected = expected.replace( /500\s*px/, '100%' );
-					}
-
-					assert.areSame( expected, output );
-				}, 0 );
 			} );
 		},
 
