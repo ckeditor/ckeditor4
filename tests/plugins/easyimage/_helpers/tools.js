e22ca6140e13a04f82019f26b7ec2e502112abf4
Jacek Bogdanski <jacekbogd@gmail.com>
2019-04-09 16:48:35 +0200

    Restored tools for imagebase.

--- tests/plugins/easyimage/_helpers/tools.js
+++ tests/plugins/easyimage/_helpers/tools.js
@@ -21,10 +21,16 @@ var easyImageTools = ( function() {
 		} );
 	}
 
+	function isUnsupportedEnvironment() {
+		return CKEDITOR.env.ie && CKEDITOR.env.version < 11;
+	}
+
+
 	return {
 		CLOUD_SERVICES_UPLOAD_GATEWAY: 'https://33333.cke-cs.com/easyimage/upload/',
 		CLOUD_SERVICES_TOKEN_URL: 'https://33333.cke-cs.com/token/dev/ijrDsqFix838Gh3wGO3F77FSW94BwcLXprJ4APSp3XQ26xsUHTi0jcb1hoBt',
 		assertCommandsState: assertCommandsState,
-		assertMenuItemsState: assertMenuItemsState
+		assertMenuItemsState: assertMenuItemsState,
+		isUnsupportedEnvironment: isUnsupportedEnvironment
 	};
 } )();
