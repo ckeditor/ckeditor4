cbf54a4653adafce0ad1e58db070270ea5377b78
Sestowner <owner@sestolab.pp.ua>
2019-04-10 17:26:30 +0300

    Fix typo

--- plugins/codesnippet/lang/uk.js
+++ plugins/codesnippet/lang/uk.js
@@ -6,7 +6,7 @@
 CKEDITOR.plugins.setLang( 'codesnippet', 'uk', {
 	button: 'Вставити фрагмент коду',
 	codeContents: 'Код',
-	emptySnippetError: 'Фрагмент коду не можи бути порожнім.',
+	emptySnippetError: 'Фрагмент коду не може бути порожнім.',
 	language: 'Мова',
 	title: 'Фрагмент коду',
 	pathName: 'фрагмент коду'
