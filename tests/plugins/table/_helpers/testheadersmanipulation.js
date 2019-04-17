1a1e2c77584d393d3494db144d0941a0ebc54149
Dariusz Jarzębski <d.jarzebski@cksource.com>
2019-04-17 11:02:08 +0200

    Use built-in API instead of carving in stone.

--- tests/plugins/table/_helpers/testheadersmanipulation.js
+++ tests/plugins/table/_helpers/testheadersmanipulation.js
@@ -5,7 +5,7 @@ function testHeadersManipulation( input, expected ) {
 
 	return function() {
 		var bot = bender.editorBots.editor;
-		bot.setHtmlWithSelection( CKEDITOR.document.findOne( '.' + input ).getValue().replace( ';', '' ) );
+		bot.setHtmlWithSelection( CKEDITOR.document.findOne( '.' + input ).getValue() );
 
 		bot.dialog( 'tableProperties', function( dialog ) {
 			dialog.setValueOf( 'info', 'selHeaders', expected );
@@ -13,10 +13,10 @@ function testHeadersManipulation( input, expected ) {
 			dialog.fire( 'ok' );
 			dialog.hide();
 
-			expected = CKEDITOR.document.findOne( '.' + expected ).getValue().replace( ';', '' );
+			expected = CKEDITOR.document.findOne( '.' + expected ).getValue();
 
 			assert.beautified.html( expected.replace( '^', '' ),
-				bender.tools.fixHtml( dialog.getParentEditor().getData() ).replace( ';', '' ) );
+				dialog.getParentEditor().getData(), { fixStyles: true } );
 		} );
 	};
 }
