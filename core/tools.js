03316edc2b1ab3476555344c4c4a514395962d74
Jacek Bogdanski <jacekbogd@gmail.com>
2019-05-22 11:57:59 +0200

    Tools API improvements.

--- core/tools.js
+++ core/tools.js
@@ -209,17 +209,19 @@
 				propertiesList = arguments[ argsLength - 1 ];
 				argsLength -= 2;
 			}
+
 			for ( var i = 1; i < argsLength; i++ ) {
-				var source = arguments[ i ];
-				for ( var propertyName in source ) {
+				var source = arguments[ i ] || {};
+
+				CKEDITOR.tools.object.forEach( source, function( propertyValue, propertyName ) {
 					// Only copy existed fields if in overwrite mode.
 					if ( overwrite === true || target[ propertyName ] == null ) {
-						// Only copy  specified fields if list is provided.
+						// Only copy specified fields if list is provided.
 						if ( !propertiesList || ( propertyName in propertiesList ) )
-							target[ propertyName ] = source[ propertyName ];
-
+							target[ propertyName ] = propertyValue;
 					}
-				}
+
+				} );
 			}
 
 			return target;
@@ -1156,7 +1158,7 @@
 			}
 
 			// Fix don't enum bug for IE < 9 browsers (#3120).
-			if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 ) {
+			if ( CKEDITOR.tools.ie && CKEDITOR.tools.version < 9 ) {
 				for ( var i = 0; i < dontEnums.length; i++ ) {
 					if ( hasOwnProperty.call( obj, dontEnums[ i ] ) )
 						keys.push( dontEnums[ i ] );
@@ -2107,6 +2109,56 @@
 		 * @member CKEDITOR.tools
 		 */
 		object: {
+
+			/**
+			 * Iterates over every property in the `object`.
+			 *
+			 * @param {Object} object An object to be iterated over.
+			 * @param {Function} fn The function called for every `object` property.
+			 * @param {Mixed} fn.value The currently iterated object property value.
+			 * @param {String} fn.key The currently iterated object property key.
+			 * @param {Object} fn.obj The original object passed as an `object` variable.
+			 * @param {Mixed} [thisArg=undefined] The context object for `fn`.
+			 * @member CKEDITOR.tools.object
+			 * @since 4.12.0
+			 */
+			forEach: function( obj, fn, thisArg ) {
+				var keys = CKEDITOR.tools.objectKeys( obj );
+
+				for ( var i = 0; i < keys.length; i++ ) {
+					var key = keys[ i ],
+						value = obj[ key ];
+
+					fn.call( thisArg, value, key, obj );
+				}
+			},
+
+			/**
+			 * Applies a function against each value-key pair in an object storing the result in an accumulator passed to the next iteration.
+			 * Note the order of the parameters.
+			 *
+			 * @param {Object} object An object of value-key pair that `fn` is applied on.
+			 * @param {Function} fn A function with the signature `(accumulator, value, key, object) -> b`.
+			 * @param {Mixed} initial Initial value of the accumulator.
+			 * @param {Mixed} [thisArg=undefined] The context object for `fn`.
+			 * @returns {Mixed} The final value of the accumulator.
+			 * @member CKEDITOR.tools.object
+			 * @since 4.12.0
+			*/
+			reduce: function( obj, fn, initial, thisArg ) {
+				var keys = CKEDITOR.tools.objectKeys( obj ),
+					acc = initial;
+
+				for ( var i = 0; i < keys.length; i++ ) {
+					var key = keys[ i ],
+						value = obj[ key ];
+
+					acc = fn.call( thisArg, acc, value, key, obj );
+				}
+
+				return acc;
+			},
+
 			/**
 			 * Returns the first key from `obj` which has a given `value`.
 			 *
