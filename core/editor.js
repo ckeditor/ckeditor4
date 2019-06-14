a92bcbc4976e8cf0000406098f6bb0a74d24082c
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-06-14 12:56:14 +0200

    Remove 'editor-incorrect-destroy' warning.

--- core/editor.js
+++ core/editor.js
@@ -194,8 +194,6 @@
 		CKEDITOR.tools.setTimeout( function() {
 			if ( !this._shouldPreventInit() ) {
 				initConfig( this, instanceConfig );
-			} else {
-				CKEDITOR.warn( 'editor-incorrect-destroy' );
 			}
 		}, 0, this );
 	}
