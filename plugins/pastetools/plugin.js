bcc947e4ba9003dd18c5d9d5da003a38510081b5
Tomasz Jakut <vepomoc@gmail.com>
2019-06-29 13:03:14 +0200

    Add initial implementation.

--- plugins/pastetools/plugin.js
+++ plugins/pastetools/plugin.js
@@ -8,9 +8,36 @@
  */
 
 ( function() {
+	var PasteTools = CKEDITOR.tools.createClass( {
+		$: function() {
+			this.handlers = [];
+		},
+
+		proto: {
+			register: function( definition ) {
+				this.handlers.push( definition );
+			},
+
+			addPasteListener: function( editor ) {
+				editor.on( 'paste', function( evt ) {
+					var handler = CKEDITOR.tools.array.find( this.handlers, function( handler ) {
+						return handler.canHandle( evt );
+					} );
+
+					if ( handler ) {
+						handler.handle( evt );
+					}
+				}, this, null, 3 );
+			}
+		}
+	} );
+
 	CKEDITOR.plugins.add( 'pastetools', {
 		requires: 'clipboard',
-		init: function() {
+		init: function( editor ) {
+			editor.pasteTools = new PasteTools();
+
+			editor.pasteTools.addPasteListener( editor );
 		}
 	} );
 } )();
