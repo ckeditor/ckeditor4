f0e0d09d9466d766ad1b4205e6f06d2b2c78f33f
Tomasz Jakut <vepomoc@gmail.com>
2019-07-04 13:58:55 +0200

    Add basic external filter loading.

--- plugins/pastetools/plugin.js
+++ plugins/pastetools/plugin.js
@@ -8,29 +8,62 @@
  */
 
 ( function() {
-	var PasteTools = CKEDITOR.tools.createClass( {
-		$: function() {
-			this.handlers = [];
-		},
-
-		proto: {
-			register: function( definition ) {
-				this.handlers.push( definition );
+	var loadedFilters = [],
+		PasteTools = CKEDITOR.tools.createClass( {
+			$: function() {
+				this.handlers = [];
 			},
 
-			addPasteListener: function( editor ) {
-				editor.on( 'paste', function( evt ) {
-					var handler = CKEDITOR.tools.array.find( this.handlers, function( handler ) {
-						return handler.canHandle( evt );
-					} );
+			proto: {
+				register: function( definition ) {
+					this.handlers.push( definition );
+				},
 
-					if ( handler ) {
+				addPasteListener: function( editor ) {
+					editor.on( 'paste', function( evt ) {
+						var handler = CKEDITOR.tools.array.find( this.handlers, function( handler ) {
+							return handler.canHandle( evt );
+						} );
+
+						if ( !handler ) {
+							return;
+						}
+
+						var isLoaded = loadFilters( handler.filters, function() {
+							return editor.fire( 'paste', evt.data );
+						} );
+
+						if ( !isLoaded ) {
+							return evt.cancel();
+						}
+
 						handler.handle( evt );
-					}
-				}, this, null, 3 );
+					}, this, null, 3 );
+				}
 			}
+		} );
+
+	function loadFilters( filters, callback ) {
+		var toLoad;
+
+		if ( !CKEDITOR.tools.array.isArray( filters ) || filters.length === 0 ) {
+			return true;
 		}
-	} );
+
+		toLoad = CKEDITOR.tools.array.filter( filters, function( filter ) {
+			return CKEDITOR.tools.array.indexOf( loadedFilters, filter ) === -1;
+		} );
+
+		if ( toLoad.length > 0 ) {
+			CKEDITOR.scriptLoader.load( toLoad, function( loaded ) {
+				loadedFilters.push.apply( loadedFilters, loaded );
+
+				callback();
+			} );
+		}
+
+		return toLoad.length === 0;
+	}
 
 	CKEDITOR.plugins.add( 'pastetools', {
 		requires: 'clipboard',
