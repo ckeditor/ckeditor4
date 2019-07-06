15ac3dcb2edebfb96f7f92cece169f89b19d2846
Tomasz Jakut <vepomoc@gmail.com>
2019-07-06 16:53:53 +0200

    Add Paste from GDocs plugin.

---
+++ plugins/pastefromgdocs/plugin.js
@@ -0,0 +1,47 @@
+﻿/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+( function() {
+	CKEDITOR.plugins.add( 'pastefromgdocs', {
+		requires: 'pastetools',
+
+		init: function( editor ) {
+			var pasteToolsPath = CKEDITOR.plugins.getPath( 'pastetools' ),
+				path = this.path;
+
+			editor.pasteTools.register( {
+				filters: [
+					CKEDITOR.getUrl( pasteToolsPath + 'filter/common.js' ),
+					CKEDITOR.getUrl(  path + 'filter/default.js' )
+				],
+
+				canHandle: function( evt ) {
+					return evt.data.dataValue.match( /id=(\"|\')docs\-internal\-guid\-/ );
+				},
+
+				handle: function( evt ) {
+					var data = evt.data,
+						dataTransferHtml = CKEDITOR.plugins.clipboard.isCustomDataTypesSupported ?
+							data.dataTransfer.getData( 'text/html', true ) : null,
+						gDocsHtml = dataTransferHtml || data.dataValue;
+
+					// Do not apply paste filter to data filtered by the GDocs filter (https://dev.ckeditor.com/ticket/13093).
+					data.dontFilter = true;
+					data.dataValue = CKEDITOR.pasteFilters.gdocs( gDocsHtml, editor );
+
+					if ( editor.config.forcePasteAsPlainText === true ) {
+						// If `config.forcePasteAsPlainText` set to true, force plain text even on Word content (#1013).
+						data.type = 'text';
+					} else if ( !CKEDITOR.plugins.clipboard.isCustomCopyCutSupported && editor.config.forcePasteAsPlainText === 'allow-word' ) {
+						// In browsers using pastebin when pasting from Word, evt.data.type is 'auto' (not 'html') so it gets converted
+						// by 'pastetext' plugin to 'text'. We need to restore 'html' type (#1013) and (#1638).
+						data.type = 'html';
+					}
+				}
+			} );
+		}
+
+	} );
+} )();
