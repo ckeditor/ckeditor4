6d16a2f043e4c187333a00681e79d86cd764f55b
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2019-07-08 16:29:59 +0200

    Remove normalizeHtml function in favor of assert.isInnerHtmlMatching.

--- tests/core/selectionoptimizer.js
+++ tests/core/selectionoptimizer.js
@@ -17,31 +17,31 @@
 	var tests = {
 		'test selection optimization case 1': testSelection( {
 			initial: '<p>foo</p><p>[bar</p><p>]baz</p>',
-			expected: '<p>foo</p><p>[bar]</p><p>baz</p>'
+			expected: '<p>foo</p><p>[bar]@</p><p>baz</p>'
 		} ),
 		'test selection optimization case 2': testSelection( {
 			initial: '<p>foo[</p><p>bar]</p><p>baz</p>',
-			expected: '<p>foo</p><p>[bar]</p><p>baz</p>'
+			expected: '<p>foo</p><p>[bar]@</p><p>baz</p>'
 		} ),
 		'test selection optimization case 3': testSelection( {
 			initial: '<p>foo[</p><p>bar</p><p>]baz</p>',
-			expected: '<p>foo</p><p>[bar]</p><p>baz</p>'
+			expected: '<p>foo</p><p>[bar]@</p><p>baz</p>'
 		} ),
 		'test selection optimization case 4': testSelection( {
 			initial: '<p>foo</p><p>[bar</p><p>b]az</p>',
-			expected: '<p>foo</p><p>[bar</p><p>b]az</p>'
+			expected: '<p>foo</p><p>[bar@</p><p>b]az</p>'
 		} ),
 		'test selection optimization case 5': testSelection( {
 			initial: '<p>fo[o</p><p>bar]</p><p>baz</p>',
-			expected: '<p>fo[o</p><p>bar]</p><p>baz</p>'
+			expected: '<p>fo[o@</p><p>bar]</p><p>baz</p>'
 		} ),
 		'test selection optimization case 6': testSelection( {
 			initial: '<p>foo</p><p>[bar</p><p>]baz</p>',
-			expected: '<p>foo</p><p>[bar]</p><p>baz</p>'
+			expected: '<p>foo</p><p>[bar]@</p><p>baz</p>'
 		} ),
 		'test selection optimization case 7': testSelection( {
 			initial: '<p>foo</p><p>[]bar</p><p>baz</p>',
-			expected: '<p>foo</p><p>[]bar</p><p>baz</p>'
+			expected: '<p>foo</p><p>^bar@</p><p>baz</p>'
 		} )
 	};
 
@@ -55,14 +55,10 @@
 
 			var actual = bender.tools.selection.getWithHtml( editor );
 
-			assert.beautified.html( options.expected, normalizeHtml( actual ) );
+			assert.isInnerHtmlMatching( options.expected, actual, {
+				compareSelection: true,
+				normalizeSelection: true
+			} );
 		};
-	}
-
-	function normalizeHtml( html ) {
-		return html.toLowerCase()
-			.replace( /(<p>(<br>|&nbsp;)<\/p>|<p><\/p>$|<br>|\s|\u200B)/g, '' )
-			.replace( '{', '[' )
-			.replace( '}', ']' );
 	}
 } )();
