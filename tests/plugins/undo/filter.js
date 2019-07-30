5546c004914bca9581cae95d41edc95f7f9240a6
Krzysztof Krztoń <k.krzton@cksource.com>
2019-07-30 14:09:31 +0200

    Tests: added filter unit tests for multiple replace.

--- tests/plugins/undo/filter.js
+++ tests/plugins/undo/filter.js
@@ -7,6 +7,12 @@
 	bender.editor = {};
 
 	bender.test( {
+
+		setUp: function() {
+			this.editor.undoManager.reset();
+			this.editor.undoManager._filterRules = [];
+		},
+
 		// (#3245)
 		'test filter.addRule': function() {
 			var undoManager = this.editor.undoManager,
@@ -16,6 +22,7 @@
 
 			assert.areSame( rule, undoManager._filterRules.pop() );
 		},
+
 		// (#3245)
 		'test filter.filterData': function() {
 			var editor = this.editor,
@@ -37,6 +44,56 @@
 			editor.getSnapshot.restore();
 
 			assert.areEqual( 'farbazfar', newData );
+		},
+
+		// (#3245)
+		'test filter.filterData (double replace)': function() {
+			var editor = this.editor,
+				undoManager = editor.undoManager,
+				data = 'foobarfoobazfoobar';
+
+			undoManager.addFilterRule( function( data ) {
+				return data.replace( /foo/g, 'ba' );
+			} );
+
+			undoManager.addFilterRule( function( data ) {
+				return data.replace( /bab/g, 'f' );
+			} );
+
+			sinon.stub( editor, 'getSnapshot' ).returns( data );
+
+			var newData = new CKEDITOR.plugins.undo.Image( editor, true ).contents;
+
+			editor.getSnapshot.restore();
+
+			assert.areEqual( 'farfazfar', newData );
+		},
+
+		// (#3245)
+		'test filter.filterData (multiple replace)': function() {
+			var editor = this.editor,
+				undoManager = editor.undoManager,
+				data = 'foobarfoobazfoobar';
+
+			undoManager.addFilterRule( function( data ) {
+				return data.replace( /foo/g, 'bar' );
+			} );
+
+			undoManager.addFilterRule( function( data ) {
+				return data.replace( /bar/g, 'far' );
+			} );
+
+			undoManager.addFilterRule( function( data ) {
+				return data.replace( /farfar/g, '-' );
+			} );
+
+			sinon.stub( editor, 'getSnapshot' ).returns( data );
+
+			var newData = new CKEDITOR.plugins.undo.Image( editor, true ).contents;
+
+			editor.getSnapshot.restore();
+
+			assert.areEqual( '-farbaz-', newData );
 		}
 	} );
 } )();
