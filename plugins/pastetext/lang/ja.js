7b3dac362df7284423f58fc2e662a54abc423c60
Sean Budd <seanbudd123@gmail.com>
2019-08-09 16:11:46 +1000

    remove missing comment

--- plugins/pastetext/lang/ja.js
+++ plugins/pastetext/lang/ja.js
@@ -4,6 +4,6 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
 CKEDITOR.plugins.setLang( 'pastetext', 'ja', {
 	button: 'プレーンテキストとして貼り付け',
-	pasteNotification: '％1 を押して貼り付けます。 ブラウザは、ツールバーボタンまたはコンテキストメニューオプションを使用した貼り付けをサポートしていません。', // MISSING
+	pasteNotification: '％1 を押して貼り付けます。 ブラウザは、ツールバーボタンまたはコンテキストメニューオプションを使用した貼り付けをサポートしていません。',
 	title: 'プレーンテキストとして貼り付け'
 } );
