527d32ab2e1215bf2e408b36ec99c33be9e06c6b
Mateusz Samsel <m.samsel@cksource.com>
2019-09-12 12:13:03 +0200

    Force is detached tests to use new html elements each time.

--- tests/core/dom/element/isdetached.js
+++ tests/core/dom/element/isdetached.js
@@ -1,43 +1,88 @@
 /* bender-tags: editor,dom */
 ( function() {
-	var ancestor = new CKEDITOR.dom.element( 'div' ),
-		parent = new CKEDITOR.dom.element( 'div' ),
-		element = new CKEDITOR.dom.element( 'div' ),
-		body = CKEDITOR.document.getBody();
+	var body = CKEDITOR.document.getBody(),
+		wrapper = new CKEDITOR.dom.element( 'div' );
+
+	body.append( wrapper );
 
 	bender.test( {
+		setUp: function() {
+			wrapper.setHtml( '' );
+		},
+
 		// (#3124)
 		'test isDetached when element isn\'t in DOM': function() {
+			var element = new CKEDITOR.dom.element( 'div' );
+
 			assert.isTrue( element.isDetached() );
+			assert.isFalse( wrapper.isDetached() );
+
+			element.remove();
 		},
+
 		// (#3124)
 		'test isDetached when element is in DOM': function() {
-			body.append( element );
+			var element = new CKEDITOR.dom.element( 'div' );
+
+			wrapper.append( element );
+
 			assert.isFalse( element.isDetached() );
+			assert.isFalse( wrapper.isDetached() );
+
+			element.remove();
 		},
+
 		// (#3124)
 		'test isDetached when elements parent isn\'t in DOM': function() {
-			parent.append( element );
-			assert.isTrue( parent.isDetached() );
+			var outerElement = new CKEDITOR.dom.element( 'div' ),
+				element = new CKEDITOR.dom.element( 'div' );
+
+			outerElement.append( element );
+
+			assert.isTrue( outerElement.isDetached() );
 			assert.isTrue( element.isDetached() );
+			assert.isFalse( wrapper.isDetached() );
+
+			outerElement.remove();
 		},
+
 		// (#3124)
 		'test isDetached when elements parent is in DOM': function() {
-			body.append( parent );
-			assert.isFalse( parent.isDetached() );
+			var outerElement = new CKEDITOR.dom.element( 'div' ),
+				element = new CKEDITOR.dom.element( 'div' );
+
+			outerElement.append( element );
+			wrapper.append( outerElement );
+
+			assert.isFalse( outerElement.isDetached() );
 			assert.isFalse( element.isDetached() );
+			assert.isFalse( wrapper.isDetached() );
+
+			outerElement.remove();
 		},
+
 		// (#3124)
 		'test isDetached when elements ancestor isn\'t in DOM': function() {
-			ancestor.append( element );
-			assert.isTrue( ancestor.isDetached() );
-			assert.isTrue( element.isDetached() );
+			var outerElement = CKEDITOR.dom.element.createFromHtml( '<div id="outer"><div><div><div id="inner"></div></div></div></div>' );
+
+			assert.isTrue( outerElement.isDetached() );
+			assert.isTrue( outerElement.findOne( '#inner' ).isDetached() );
+			assert.isFalse( wrapper.isDetached() );
+
+			outerElement.remove();
 		},
+
 		// (#3124)
 		'test isDetached when elements ancestor is in DOM': function() {
-			body.append( ancestor );
-			assert.isFalse( ancestor.isDetached() );
-			assert.isFalse( element.isDetached() );
+			var outerElement = CKEDITOR.dom.element.createFromHtml( '<div id="outer"><div><div><div id="inner"></div></div></div></div>' );
+
+			wrapper.append( outerElement );
+
+			assert.isFalse( outerElement.isDetached() );
+			assert.isFalse( outerElement.findOne( '#inner' ).isDetached() );
+			assert.isFalse( wrapper.isDetached() );
+
+			outerElement.remove();
 		}
 	} );
 } )();
