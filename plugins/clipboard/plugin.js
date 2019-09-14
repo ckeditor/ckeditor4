23f57fb318459b9adc1944e40cc1cb6455930a3a
Jack Wickham <jackwickham@live.co.uk>
2019-09-14 18:29:32 +0100

    Add tests for #3415

--- plugins/clipboard/plugin.js
+++ plugins/clipboard/plugin.js
@@ -2325,7 +2325,7 @@
 		this._ = {
 			metaRegExp: /^<meta.*?>/i,
 			bodyRegExp: /<body(?:[\s\S]*?)>([\s\S]*)<\/body>/i,
-			fragmentRegExp: /^\s*<!--StartFragment-->|<!--EndFragment-->\s*$/g,
+			fragmentRegExp: /\s*<!--StartFragment-->|<!--EndFragment-->\s*/g,
 
 			data: {},
 			files: [],
