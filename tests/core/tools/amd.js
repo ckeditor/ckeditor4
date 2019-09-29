4c93cbdb267b22cb32a85725cd298858abf6d4b5
Tomasz Jakut <vepomoc@gmail.com>
2019-09-29 19:25:12 +0200

    Wait for iframe to load.

--- tests/core/tools/amd.js
+++ tests/core/tools/amd.js
@@ -7,15 +7,29 @@
 		'test promise implementation is loaded': function() {
 			var iframe = document.createElement( 'iframe' );
 
-			setTimeout( function() {
-				resume( function() {
-					assert.isNotUndefined( iframe.contentWindow.CKEDITOR.tools.promise );
-				} );
-			}, 500 );
+			if ( CKEDITOR.env.ie && CKEDITOR.env.version < 11 ) {
+				iframe.onreadystatechange = function() {
+					if ( iframe.contentDocument.readyState !== 'complete' ) {
+						return;
+					}
+
+					callback();
+				};
+			} else {
+				iframe.onload = callback;
+			}
 
 			iframe.src = '%BASE_PATH%core/tools/_assets/amdtest.html';
 			document.body.appendChild( iframe );
 			wait();
+
+			function callback() {
+				resume( function() {
+					wait( function() {
+						assert.isNotUndefined( iframe.contentWindow.CKEDITOR.tools.promise );
+					}, 500 );
+				} );
+			}
 		}
 	} );
 } )();
