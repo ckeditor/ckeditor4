6a7efa1c51fc406a5c096530501e512b27af12c4
Tomasz Jakut <vepomoc@gmail.com>
2019-10-05 18:25:11 +0200

    Add unit test.

--- tests/plugins/widget/widgetselection.js
+++ tests/plugins/widget/widgetselection.js
@@ -508,6 +508,32 @@
 					assertWidgetsEvents( result, [], [], [], [] );
 				}, true );
 			} );
+		},
+
+		// (#3352)
+		'test refreshing selected widgets on key event': function() {
+			var editor = this.editor;
+
+			editor.widgets.add( 'testkeyrefresh', {} );
+			editor.focus();
+
+			this.editorBot.setData( '<p>foo</p><p><span data-widget="testkeyrefresh" id="x">bar</span></p><p>baz</p>', function() {
+				var widget = getWidgetById( editor, 'x' ),
+					range = editor.createRange(),
+					domEvent = new CKEDITOR.dom.event( { keyCode: 65 } );
+
+				range.selectNodeContents( editor.editable() );
+				range.select();
+
+				setTimeout( function() {
+					resume( function() {
+						arrayAssert.itemsAreSame( [ widget ], editor.widgets.selected );
+					} );
+				}, 11 );
+
+				editor.fire( 'key', { domEvent: domEvent } );
+				wait();
+			} );
 		}
 	} );
 } )();
