8027c76a9e940f3e39497edbcccaa91933315b64
Mateusz Samsel <m.samsel@cksource.com>
2019-10-07 10:36:59 +0200

    Add new data types to unit tests.

--- tests/plugins/easyimage/pasteintegration.js
+++ tests/plugins/easyimage/pasteintegration.js
@@ -9,7 +9,9 @@
 		GIF: '<img src="data:image/gif;base64,R0lGODlhAQABAAAAACw=">',
 		PNG: '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=">',
 		BMP: '<img src="data:image/bmp;base64,foo">',
-		SVG: '<img src="data:image/svg+xml;charset=utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E">'
+		SVG: '<img src="data:image/svg+xml;charset=utf8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E">',
+		FAKE_IMAGE: '<img src="data:image/foobar;base64,foobarbaz">',
+		HTML: '<img src="data:text/html;charset=utf8,%3C!DOCTYPE%20html%3E%0A%3Chtml%3E%0A%3C%2Fhtml%3E">'
 	};
 
 	bender.editor = true;
@@ -86,6 +88,22 @@
 			assertEasyImageUpcast( {
 				editor: this.editor,
 				pastedData: IMG.SVG,
+				shouldUpcast: false
+			} );
+		},
+
+		'test easyimage not upcasts image/foobar': function() {
+			assertEasyImageUpcast( {
+				editor: this.editor,
+				pastedData: IMG.FAKE_IMAGE,
+				shouldUpcast: false
+			} );
+		},
+
+		'test easyimage not upcasts text/html': function() {
+			assertEasyImageUpcast( {
+				editor: this.editor,
+				pastedData: IMG.HTML,
 				shouldUpcast: false
 			} );
 		}
