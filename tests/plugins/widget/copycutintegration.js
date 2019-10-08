f7eb214bb8662a7bf7e244233e29892690243ff8
Mateusz Samsel <m.samsel@cksource.com>
2019-10-08 14:40:02 +0200

    Add unit test checking if selection is collapsed after content extraction.

--- tests/plugins/widget/copycutintegration.js
+++ tests/plugins/widget/copycutintegration.js
@@ -37,15 +37,33 @@
 			range.setEnd( editable.findOne( 'div' ), 0 );
 			range.select();
 
-			editable.once( 'copy', function() {
+			editable.fire( 'copy', new CKEDITOR.dom.event( {} ) );
+			assert.pass();
+		},
+
+		'test cutting partially selected widget remain collapsed selection in editor': function() {
+			var editor = this.editor,
+				editable = this.editor.editable(),
+				range = editor.createRange();
+
+			range.setStart( editable.findOne( 'p' ).getFirst(), 3 );
+			range.setEnd( editable.findOne( 'div' ), 0 );
+			range.select();
+
+			editable.once( 'cut', function() {
 				CKEDITOR.tools.setTimeout( function() {
 					resume( function() {
-						assert.pass();
+						assert.isInnerHtmlMatching(
+							'<p>foo@</p><div data-widget="customwidget">Widget</div>',
+							editor.getData()
+						);
+
+						assert.isTrue( editor.getSelection().isCollapsed() );
 					} );
 				} );
-			}, null, null, 10000 );
+			} );
 
-			editable.fire( 'copy', new CKEDITOR.dom.event( {} ) );
+			editable.fire( 'cut', new CKEDITOR.dom.event( {} ) );
 			wait();
 		}
 	} );
