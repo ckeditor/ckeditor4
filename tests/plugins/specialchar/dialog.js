cb19bc526d8028f50e80f42687098cac6c7f8b01
Mateusz Samsel <m.samsel@cksource.com>
2019-10-10 11:03:04 +0200

    Add unit test checking role of empty table cells in specialchar dialog.

---
+++ tests/plugins/specialchar/dialog.js
@@ -0,0 +1,33 @@
+/* bender-tags: specialchar */
+/* bender-ckeditor-plugins: toolbar,wysiwygarea,specialchar */
+
+( function() {
+	'use strict';
+
+	bender.editor = true;
+
+	bender.test( {
+		tearDown: function() {
+			var dialog;
+
+			while ( ( dialog = CKEDITOR.dialog.getCurrent() ) ) {
+				dialog.hide();
+			}
+		},
+
+		'test specialchardialog should have table cells with role="presentation"': function() {
+			var bot = this.editorBot;
+
+			bot.dialog( 'specialchar', function( dialog ) {
+				var tableWithCharacters = dialog.parts.contents.findOne( 'td.cke_dialog_ui_hbox_first table[role="listbox"]' ),
+					tableCells = tableWithCharacters.find( 'td' ).toArray(),
+					i;
+
+				for ( i = 0; i < tableCells.length; i++ ) {
+					assert.areSame( 'presentation', tableCells[ i ].getAttribute( 'role' ),
+						'Table cell with index: ' + i + ' should have role="presentation". Instead it has following html: ' + tableCells[ i ].getOuterHtml() );
+				}
+			} );
+		}
+	} );
+} )();
