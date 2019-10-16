9fff21a288d188257b3c04be0b6bb8fc02c38e22
Mateusz Samsel <m.samsel@cksource.com>
2019-10-16 14:35:32 +0200

    Add first autogenerated unit test for paste from libre office.

---
+++ tests/plugins/pastefromlibreoffice/generated/generic.js
@@ -0,0 +1,34 @@
+/* bender-tags: clipboard,pastefromlibreoffice */
+/* jshint ignore:start */
+/* bender-ckeditor-plugins: pastefromlibreoffice,ajax,basicstyles,bidi,font,link,toolbar,colorbutton,image */
+/* bender-ckeditor-plugins: list,liststyle,sourcearea,format,justify,table,tableresize,tabletools,indent,indentblock,div,dialog */
+/* jshint ignore:end */
+/* bender-include: ../../pastefromword/generated/_helpers/promisePasteEvent.js,../../pastefromword/generated/_helpers/assertWordFilter.js */
+/* bender-include: ../../pastefromword/generated/_helpers/createTestCase.js,../../pastefromword/generated/_helpers/createTestSuite.js */
+/* bender-include: ../../pastefromword/generated/_helpers/pfwTools.js */
+/* global pfwTools,createTestSuite */
+
+( function() {
+	'use strict';
+
+	bender.editor = {
+		config: pfwTools.defaultConfig
+	};
+
+	bender.test( createTestSuite( {
+		browsers: [
+			'chrome',
+			'firefox',
+			'ie11',
+			'safari',
+			'datatransfer'
+		],
+		wordVersions: [
+			'libreoffice6'
+		],
+		tests: {
+			Simple_text: true
+		},
+		ignoreAll: ( CKEDITOR.env.ie && CKEDITOR.env.version <= 11 ) || bender.tools.env.mobile
+	} ) );
+} )();
