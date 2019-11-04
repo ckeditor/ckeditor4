c744ba46d8394b9ec75908859758c624eb4b1ee3
Mateusz Samsel <m.samsel@cksource.com>
2019-11-04 14:10:17 +0100

    Add unit test with simple image in libre office document.

---
+++ tests/plugins/pastefromlibreoffice/generated/imageswithword.js
@@ -0,0 +1,33 @@
+/* bender-tags: clipboard,pastefromlibreoffice */
+/* jshint ignore:start */
+/* bender-ckeditor-plugins: pastefromlibreoffice,pastefromword,ajax,basicstyles,bidi,font,link,toolbar,colorbutton,image */
+/* bender-ckeditor-plugins: list,liststyle,sourcearea,format,justify,table,tableresize,tabletools,indent,indentblock,div,dialog */
+/* jshint ignore:end */
+/* bender-include: ../../pastefromword/generated/_helpers/promisePasteEvent.js,../../pastefromword/generated/_helpers/assertWordFilter.js */
+/* bender-include: ../../pastefromword/generated/_helpers/createTestCase.js,../../pastefromword/generated/_helpers/createTestSuite.js */
+/* bender-include: ./_helpers/config.js */
+/* global pfloConfig,createTestSuite */
+
+( function() {
+	'use strict';
+
+	bender.editor = {
+		config: pfloConfig
+	};
+
+	bender.test( createTestSuite( {
+		browsers: [
+			'chrome',
+			'firefox',
+			'edge' // v18
+		],
+		wordVersions: [
+			'libreoffice6'
+		],
+		tests: {
+			'Images/Simple_image': true
+		},
+		ignoreAll: ( CKEDITOR.env.ie && CKEDITOR.env.version <= 11 ) || bender.tools.env.mobile,
+		includeRTF: true
+	} ) );
+} )();
