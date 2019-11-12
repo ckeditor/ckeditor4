fa126d5eca4068e42a189eb67b26c6c0ab2c7830
Dariusz Jarzębski <d.jarzebski@cksource.com>
2019-11-12 10:31:11 +0100

    Remove trailing newline character before checking the expected result.

--- tests/core/editable/misc.js
+++ tests/core/editable/misc.js
@@ -92,7 +92,12 @@ bender.test( {
 				target: img.$
 			} ) );
 
-			assert.areSame( 'foo', editor.getSelection().getSelectedText(), 'Selection has not been changed' );
+			// From Firefox 70 it returns trailing '\n' (#3633).
+			if ( CKEDITOR.env.gecko && CKEDITOR.env.version >= 700000 ) {
+				assert.areSame( 'foo', editor.getSelection().getSelectedText().trim(), 'Selection has not been changed' );
+			} else {
+				assert.areSame( 'foo', editor.getSelection().getSelectedText(), 'Selection has not been changed' );
+			}
 		} );
 	},
 
@@ -110,7 +115,12 @@ bender.test( {
 				target: img.$
 			} ) );
 
-			assert.areSame( 'foo', editor.getSelection().getSelectedText(), 'Selection has not been changed' );
+			// From Firefox 70 it returns trailing '\n' (#3633).
+			if ( CKEDITOR.env.gecko && CKEDITOR.env.version >= 700000 ) {
+				assert.areSame( 'foo', editor.getSelection().getSelectedText().trim(), 'Selection has not been changed' );
+			} else {
+				assert.areSame( 'foo', editor.getSelection().getSelectedText(), 'Selection has not been changed' );
+			}
 		} );
 	},
 
