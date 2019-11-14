ac81a3f151ceb32444b1c77c43d9d9f16d481b6a
Jacek Bogdanski <jacekbogd@gmail.com>
2019-11-14 13:20:03 +0100

    Unified plugin definition.

--- plugins/quicktable/plugin.js
+++ plugins/quicktable/plugin.js
@@ -1,8 +1,19 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @fileOverview [Quick Table](https://ckeditor.com/cke4/addon/quicktable) plugin.
+ */
+
 ( function() {
 
 	CKEDITOR.plugins.add( 'quicktable', {
 		requires: 'panelbutton,floatpanel',
-		lang: 'en', // %REMOVE_LINE_CORE%
+		// jscs:disable maximumLineLength
+		lang: 'af,ar,az,bg,bn,bs,ca,cs,cy,da,de,de-ch,el,en,en-au,en-ca,en-gb,eo,es,es-mx,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,oc,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,tt,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
+		// jscs:enable maximumLineLength
 		icons: 'quicktable', // %REMOVE_LINE_CORE%
 		hidpi: true, // %REMOVE_LINE_CORE%
 
@@ -13,6 +24,7 @@
 				label: lang.title,
 				title: lang.title,
 				modes: { wysiwyg: 1 },
+				toolbar: 'insert,10',
 				onBlock: function( panel, block ) {
 					var grid = createGridElement( 10, 10 ),
 						status = new CKEDITOR.dom.element( 'div' );
