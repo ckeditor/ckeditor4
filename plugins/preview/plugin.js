b14fe87ff662c57a02c62787fe7b0ccd53c9a1ad
Tomasz Jakut <vepomoc@gmail.com>
2019-11-20 12:25:00 +0100

    Move preview command logic to CKEDITOR.plugins.preview#createPreview() and make it returning CKEDITOR.dom.window instance.

--- plugins/preview/plugin.js
+++ plugins/preview/plugin.js
@@ -10,10 +10,36 @@
 ( function() {
 	var pluginPath;
 
-	var previewCmd = { modes: { wysiwyg: 1, source: 1 },
-		canUndo: false,
-		readOnly: 1,
-		exec: function( editor ) {
+	var previewCmd;
+
+	var pluginName = 'preview';
+
+	// Register a plugin named "preview".
+	CKEDITOR.plugins.add( pluginName, {
+		// jscs:disable maximumLineLength
+		lang: 'af,ar,az,bg,bn,bs,ca,cs,cy,da,de,de-ch,el,en,en-au,en-ca,en-gb,eo,es,es-mx,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,oc,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,tt,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
+		// jscs:enable maximumLineLength
+		icons: 'preview,preview-rtl', // %REMOVE_LINE_CORE%
+		hidpi: true, // %REMOVE_LINE_CORE%
+		init: function( editor ) {
+
+			// Preview is not used for the inline creator.
+			if ( editor.elementMode == CKEDITOR.ELEMENT_MODE_INLINE )
+				return;
+
+			pluginPath = this.path;
+
+			editor.addCommand( pluginName, previewCmd );
+			editor.ui.addButton && editor.ui.addButton( 'Preview', {
+				label: editor.lang.preview.preview,
+				command: pluginName,
+				toolbar: 'document,40'
+			} );
+		}
+	} );
+
+	CKEDITOR.plugins.preview = {
+		createPreview: function( editor ) {
 			var sHTML,
 				config = editor.config,
 				baseTag = config.baseHref ? '<base href="' + config.baseHref + '"/>' : '',
@@ -101,35 +127,19 @@
 				doc.close();
 			}
 
-			return true;
+			return new CKEDITOR.dom.window( oWindow );
 		}
 	};
 
-	var pluginName = 'preview';
-
-	// Register a plugin named "preview".
-	CKEDITOR.plugins.add( pluginName, {
-		// jscs:disable maximumLineLength
-		lang: 'af,ar,az,bg,bn,bs,ca,cs,cy,da,de,de-ch,el,en,en-au,en-ca,en-gb,eo,es,es-mx,et,eu,fa,fi,fo,fr,fr-ca,gl,gu,he,hi,hr,hu,id,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,oc,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,tt,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
-		// jscs:enable maximumLineLength
-		icons: 'preview,preview-rtl', // %REMOVE_LINE_CORE%
-		hidpi: true, // %REMOVE_LINE_CORE%
-		init: function( editor ) {
-
-			// Preview is not used for the inline creator.
-			if ( editor.elementMode == CKEDITOR.ELEMENT_MODE_INLINE )
-				return;
-
-			pluginPath = this.path;
-
-			editor.addCommand( pluginName, previewCmd );
-			editor.ui.addButton && editor.ui.addButton( 'Preview', {
-				label: editor.lang.preview.preview,
-				command: pluginName,
-				toolbar: 'document,40'
-			} );
-		}
-	} );
+	previewCmd = {
+		modes: {
+			wysiwyg: 1,
+			source: 1
+		},
+		canUndo: false,
+		readOnly: 1,
+		exec: CKEDITOR.plugins.preview.createPreview
+	};
 } )();
 
 /**
