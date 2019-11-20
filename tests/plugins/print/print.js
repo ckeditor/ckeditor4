ef822ea9a7f2898ea9e2054d6145ba7b74d13d71
Tomasz Jakut <vepomoc@gmail.com>
2019-11-20 12:42:27 +0100

    Update unit test for print command.

--- tests/plugins/print/print.js
+++ tests/plugins/print/print.js
@@ -5,28 +5,26 @@ bender.editor = {
 	startupData: '<p>Foo</p>'
 };
 
-var printStub;
-
 bender.test( {
-	setUp: function() {
-		printStub = sinon.stub( this.editor.window.$, 'print' );
-	},
-
-	tearDown: function() {
-		printStub.restore();
-	},
-
 	// (#3661)
-	'test print command fires contentPreview': function() {
+	'test print command uses CKEDITOR.plugins.preview#createPreview()': function() {
 		var editor = this.editor,
-			spy = sinon.spy();
-
-		editor.on( 'contentPreview', spy );
+			createPreviewStub = sinon.stub( CKEDITOR.plugins.preview, 'createPreview', function() {
+				return {
+					$: {
+						print: function() {},
+						document: {
+							execCommand: function() {}
+						}
+					}
+				};
+			} );
 
 		editor.once( 'afterCommandExec', function() {
 			resume( function() {
-				assert.areSame( 1, spy.callCount, 'Event was invoked correctly' );
-				editor.removeListener( 'contentPreview', spy );
+				createPreviewStub.restore();
+
+				assert.areSame( 1, createPreviewStub.callCount );
 			} );
 		} );
 
