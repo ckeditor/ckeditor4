0991d37bc1ad9d841c9aa4de3c7f515b47990654
Mykola Pukhalskyi <notfound.la2.asterios.tm@gmail.com>
2019-11-28 16:57:26 +0200

    Create uk.js
    Ukrainian translation

---
+++ plugins/emoji/lang/uk.js
@@ -0,0 +1,21 @@
+Learn more or give us feedback
+/*
+Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+CKEDITOR.plugins.setLang( 'emoji', 'uk', {
+	searchPlaceholder: 'Пошук емодзі…',
+	searchLabel: 'Поле введення, яке відповідає за пошук та фільтрацію емодзі на панелі',
+	navigationLabel: 'Групова навігація для розділів емодзі.', // Groups navigation for emoji sections.
+	title: 'Список емодзі',
+	groups: {
+		people: 'Люди',
+		nature: 'Природа та тварини',
+		food: 'Їжа та напої',
+		travel: 'Подорожі та місця',
+		activities: 'Спорт та дозвілля',
+		objects: 'Речі та об’єкти',
+		symbols: 'Символи та значки',
+		flags: 'Прапорці'
+	}
+} );
