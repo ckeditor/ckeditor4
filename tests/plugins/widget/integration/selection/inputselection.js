2c2f002c30ef600d472db0e35103d0f271e96572
Mateusz Samsel <m.samsel@cksource.com>
2019-12-16 12:51:46 +0100

    Extract common logic of unit test to a separate function.

--- tests/plugins/widget/integration/selection/inputselection.js
+++ tests/plugins/widget/integration/selection/inputselection.js
@@ -50,73 +50,66 @@
 		}
 	};
 
+	// @param {bot} bot
+	// @param {String} elementName name of used html element. Value should be equal: 'input' or 'textarea'
+	// @param {Function} assertCallback funciton which returns a function to be executed on 'keydown' listener when assert for the tesr might be checked.
+	function prepareEditor( bot, elementName, assertCallback ) {
+		var editor = bot.editor;
+		var htmlElement = elementName === 'textarea' ? '<textarea></textarea>' : '<input type="text">';
+
+		bot.setData( '<div class="testwidget">' + htmlElement + '</div>', function() {
+			var editable = editor.editable(),
+				widget = editor.widgets.getByElement( editable.findOne( 'div.testwidget' ) ),
+				range = editor.createRange(),
+				el = widget.element.findOne( elementName );
+
+			el.focus();
+
+			range.selectNodeContents( el );
+			range.select();
+
+			editable.once( 'keydown', assertCallback( editor, el, widget ), null, null, 100000 );
+
+			editable.fire( 'keydown', new CKEDITOR.dom.event( {
+				keyCode: 66 // B
+			} ) );
+		} );
+
+	}
+
 	bender.test( {
-		'test textarea inside widget should allow on typing': function() {
+		setUp: function() {
 			if ( !CKEDITOR.env.ie || CKEDITOR.env.version < 9 ) {
 				assert.ignore();
 			}
+		},
 
-			var editor = this.editor;
-
-			this.editorBot.setData( '<div class="testwidget"><textarea></textarea></div>', function() {
-				var editable = editor.editable(),
-					widget = editor.widgets.getByElement( editable.findOne( 'div.testwidget' ) ),
-					range = editor.createRange(),
-					textArea = widget.element.findOne( 'textarea' );
-
-				textArea.focus();
-
-				range.selectNodeContents( textArea );
-				range.select();
-
-				editable.once( 'keydown', function() {
+		'test textarea inside widget should allow on typing': function() {
+			prepareEditor( this.editorBot, 'textarea', function( editor, el, widget ) {
+				return function() {
 					var selection = editor.getSelection();
 					// Edge expands selection to inner div. IE keeps selction inside textArea.
 					// Important is to not have focused widget's wrapper.
 					if ( CKEDITOR.env.edge ) {
 						assert.isTrue( selection.getStartElement().equals( widget.element ) );
 					} else {
-						assert.isTrue( selection.getStartElement().equals( textArea ) );
+						assert.isTrue( selection.getStartElement().equals( el ) );
 					}
-				}, null, null, 100000 );
-
-				editable.fire( 'keydown', new CKEDITOR.dom.event( {
-					keyCode: 66 // B
-				} ) );
+				};
 			} );
 		},
 
 		'test input inside widget should allow on typing': function() {
-			if ( !CKEDITOR.env.ie || CKEDITOR.env.version < 9 ) {
-				assert.ignore();
-			}
-
-			var editor = this.editor;
-
-			this.editorBot.setData( '<div class="testwidget2"><input type="text"></div>', function() {
-				var editable = editor.editable(),
-					widget = editor.widgets.getByElement( editable.findOne( 'div.testwidget2' ) ),
-					range = editor.createRange(),
-					inputElement = widget.element.findOne( 'input' );
-
-				inputElement.focus();
-
-				range.selectNodeContents( inputElement );
-				range.select();
-
-				editable.once( 'keydown', function() {
+			prepareEditor( this.editorBot, 'input', function( editor, el ) {
+				return function() {
 					var selection = editor.getSelection();
 					// Edge keeps selection on input element, IE11 moves it to wrapper, however there is still possibility to type in input field.
 					if ( CKEDITOR.env.edge ) {
-						assert.isTrue( selection.getStartElement().equals( inputElement ) );
+						assert.isTrue( selection.getStartElement().equals( el ) );
 					} else {
 						assert.isTrue( CKEDITOR.plugins.widget.isDomWidget( selection.getStartElement() ) );
 					}
-				}, null, null, 100000 );
-
-				editable.fire( 'keydown', new CKEDITOR.dom.event( {
-					keyCode: 66 // B
-				} ) );
+				};
 			} );
 		}
 	} );
