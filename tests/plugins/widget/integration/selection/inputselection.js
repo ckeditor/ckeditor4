a9a9b1fd776c6fdbbf2e8a8619b3fc1d307d803c
Mateusz Samsel <m.samsel@cksource.com>
2019-12-16 14:06:41 +0100

    Extract widget definition builder.

--- tests/plugins/widget/integration/selection/inputselection.js
+++ tests/plugins/widget/integration/selection/inputselection.js
@@ -1,6 +1,8 @@
 /* bender-tags: widgetcore */
 /* bender-ckeditor-plugins: widget,undo */
 /* bender-ckeditor-remove-plugins: tableselection */
+/* bender-include: ../../_helpers/tools.js */
+/* global widgetTestsTools */
 
 ( function() {
 	'use strict';
@@ -12,39 +14,17 @@
 				instanceReady: function( evt ) {
 					var editor = evt.editor;
 
-					editor.widgets.add( 'testwidget', {
-						requiredContent: 'div(testwidget)',
-						allowedContent: 'div(testwidget)',
-						template: '<div class="testwidget">' +
-							'<textarea></textarea>' +
-							'</div>',
-						button: 'Add textarea widget',
-						upcast: function( element ) {
-							return element.name === 'div' && element.hasClass( 'testwidget' );
-						},
-						init: function() {
-							this.on( 'focus', function() {
-								this.element.findOne( 'textarea' ).focus();
-							} );
-						}
-					} );
+					editor.widgets.add( 'testwidget', widgetTestsTools.buildWidgetDefinition( {
+						name: 'testwidget',
+						elementName: 'textarea',
+						innerHtml: '<textarea></textarea>'
+					} ) );
 
-					editor.widgets.add( 'testwidget2', {
-						requiredContent: 'div(testwidget2)',
-						allowedContent: 'div(testwidget2)',
-						template: '<div class="testwidget2">' +
-							'<input type="text">' +
-							'</div>',
-						button: 'Add input widget',
-						upcast: function( element ) {
-							return element.name === 'div' && element.hasClass( 'testwidget2' );
-						},
-						init: function() {
-							this.on( 'focus', function() {
-								this.element.findOne( 'input' ).focus();
-							} );
-						}
-					} );
+					editor.widgets.add( 'testwidget2', widgetTestsTools.buildWidgetDefinition( {
+						name: 'testwidget2',
+						elementName: 'input',
+						innerHtml: '<input type="text">'
+					} ) );
 				}
 			}
 		}
@@ -53,13 +33,17 @@
 	// @param {bot} bot
 	// @param {String} elementName name of used html element. Value should be equal: 'input' or 'textarea'
 	// @param {Function} assertCallback funciton which returns a function to be executed on 'keydown' listener when assert for the tesr might be checked.
-	function prepareEditor( bot, elementName, assertCallback ) {
-		var editor = bot.editor;
-		var htmlElement = elementName === 'textarea' ? '<textarea></textarea>' : '<input type="text">';
+	function assertSelection( config ) {
+		var bot = config.bot,
+			elementName = config.elementName,
+			assertCallback = config.assertCallback,
+			widgetName = config.widgetName,
+			editor = bot.editor,
+			htmlElement = elementName === 'textarea' ? '<textarea></textarea>' : '<input type="text">';
 
-		bot.setData( '<div class="testwidget">' + htmlElement + '</div>', function() {
+		bot.setData( '<div class="' + widgetName + '">' + htmlElement + '</div>', function() {
 			var editable = editor.editable(),
-				widget = editor.widgets.getByElement( editable.findOne( 'div.testwidget' ) ),
+				widget = editor.widgets.getByElement( editable.findOne( 'div.' + widgetName ) ),
 				range = editor.createRange(),
 				el = widget.element.findOne( elementName );
 
@@ -85,31 +69,41 @@
 		},
 
 		'test textarea inside widget should allow on typing': function() {
-			prepareEditor( this.editorBot, 'textarea', function( editor, el, widget ) {
-				return function() {
-					var selection = editor.getSelection();
-					// Edge expands selection to inner div. IE keeps selction inside textArea.
-					// Important is to not have focused widget's wrapper.
-					if ( CKEDITOR.env.edge ) {
-						assert.isTrue( selection.getStartElement().equals( widget.element ) );
-					} else {
-						assert.isTrue( selection.getStartElement().equals( el ) );
-					}
-				};
+			assertSelection( {
+				bot: this.editorBot,
+				elementName: 'textarea',
+				widgetName: 'testwidget',
+				assertCallback: function( editor, el, widget ) {
+					return function() {
+						var selection = editor.getSelection();
+						// Edge expands selection to inner div. IE keeps selction inside textArea.
+						// Important is to not have focused widget's wrapper.
+						if ( CKEDITOR.env.edge ) {
+							assert.isTrue( selection.getStartElement().equals( widget.element ) );
+						} else {
+							assert.isTrue( selection.getStartElement().equals( el ) );
+						}
+					};
+				}
 			} );
 		},
 
 		'test input inside widget should allow on typing': function() {
-			prepareEditor( this.editorBot, 'input', function( editor, el ) {
-				return function() {
-					var selection = editor.getSelection();
-					// Edge keeps selection on input element, IE11 moves it to wrapper, however there is still possibility to type in input field.
-					if ( CKEDITOR.env.edge ) {
-						assert.isTrue( selection.getStartElement().equals( el ) );
-					} else {
-						assert.isTrue( CKEDITOR.plugins.widget.isDomWidget( selection.getStartElement() ) );
-					}
-				};
+			assertSelection( {
+				bot: this.editorBot,
+				elementName: 'input',
+				widgetName: 'testwidget2',
+				assertCallback: function( editor, el ) {
+					return function() {
+						var selection = editor.getSelection();
+						// Edge keeps selection on input element, IE11 moves it to wrapper, however there is still possibility to type in input field.
+						if ( CKEDITOR.env.edge ) {
+							assert.isTrue( selection.getStartElement().equals( el ) );
+						} else {
+							assert.isTrue( CKEDITOR.plugins.widget.isDomWidget( selection.getStartElement() ) );
+						}
+					};
+				}
 			} );
 		}
 	} );
