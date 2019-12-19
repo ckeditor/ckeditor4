26a10623588e3d82312101ec2fa4513b1fd8cdc2
Mateusz Samsel <m.samsel@cksource.com>
2019-12-19 14:55:23 +0100

    Correct unit test, add new unit test checking combintantin of modes and commands inside one panel.

--- tests/plugins/panelbutton/command.js
+++ tests/plugins/panelbutton/command.js
@@ -13,11 +13,23 @@
 
 			editor.ui.add( 'testPanel', CKEDITOR.UI_PANELBUTTON, {
 				command: 'testCommand',
+				panel: {
+					attributes: {}
+				}
+			} );
+
+			editor.addCommand( 'testCommand2', {
+				exec: function() {}
+			} );
+
+			editor.ui.add( 'testPanel2', CKEDITOR.UI_PANELBUTTON, {
+				command: 'testCommand2',
 				modes: { wysiwyg: 1 },
 				panel: {
 					attributes: {}
 				}
 			} );
+
 		}
 	} );
 
@@ -58,6 +70,21 @@
 			assert.areSame( CKEDITOR.TRISTATE_DISABLED, testPanel.getState(), 'Tristate DISABLED' );
 		},
 
+		'test closed panelbutton should use mode state when is available and not react on command state changes': function() {
+			var editor = this.editor,
+				testPanel = editor.ui.get( 'testPanel2' ),
+				testCommand = editor.getCommand( 'testCommand2' );
+
+			testCommand.setState( CKEDITOR.TRISTATE_OFF );
+			assert.areSame( CKEDITOR.TRISTATE_OFF, testPanel.getState(), 'Tristate OFF (1)' );
+
+			testCommand.setState( CKEDITOR.TRISTATE_ON );
+			assert.areSame( CKEDITOR.TRISTATE_OFF, testPanel.getState(), 'Tristate OFF (2)' );
+
+			testCommand.setState( CKEDITOR.TRISTATE_DISABLED );
+			assert.areSame( CKEDITOR.TRISTATE_OFF, testPanel.getState(), 'Tristate OFF (3)' );
+		},
+
 		'test panelbutton should restore command state after panel hide': function() {
 			var editor = this.editor,
 				bot = this.editorBot,
@@ -70,6 +97,21 @@
 				panel.hide();
 
 				assert.areSame( CKEDITOR.TRISTATE_ON, testPanel.getState(), '"testPanel" should have same state as command after closing it' );
+			} );
+		},
+
+		'test panelbutton should restore state from the modes after panel hide and do not use the command state': function() {
+			var editor = this.editor,
+				bot = this.editorBot,
+				testPanel = editor.ui.get( 'testPanel2' ),
+				testCommand = editor.getCommand( 'testCommand2' );
+
+			testCommand.setState( CKEDITOR.TRISTATE_ON );
+
+			bot.panel( 'testPanel', function( panel ) {
+				panel.hide();
+
+				assert.areSame( CKEDITOR.TRISTATE_OFF, testPanel.getState(), '"testPanel" should have state obtaiend from the modes not the command' );
 			} );
 		}
 	} );
