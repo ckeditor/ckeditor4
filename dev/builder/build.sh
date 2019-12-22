393fe2dc887fdc432c0cedd97351149b47404e07
Comandeer <vepomoc@gmail.com>
2019-12-22 18:36:15 +0100

    Update shebang in build script.

--- dev/builder/build.sh
+++ dev/builder/build.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+﻿#!/usr/bin/env bash
 # Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
 # For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 
