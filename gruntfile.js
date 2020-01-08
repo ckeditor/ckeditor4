a2b2ba756b30992c5e4064f43d0a45ff264936f1
Comandeer <vepomoc@gmail.com>
2020-01-08 18:11:51 +0100

    Remove jshint & jscs.

--- gruntfile.js
+++ gruntfile.js
@@ -1,42 +1,9 @@
-/* jshint node: true, browser: false, es3: false */
-
 'use strict';
 
 module.exports = function( grunt ) {
-	// First register the "default" task, so it can be analyzed by other tasks.
-	grunt.registerTask( 'default', [ 'jshint:git', 'jscs:git' ] );
-
-	// Files that will be ignored by the "jscs" and "jshint" tasks.
-	var ignoreFiles = [
-		// Automatically loaded from .gitignore. Add more if necessary.
-
-		'lang/**',
-		'vendor/**',
-		'plugins/*/lib/**',
-		'plugins/**/lang/**',
-		'plugins/uicolor/yui/**',
-		'plugins/htmlwriter/samples/assets/outputforflash/**',
-		'samples/toolbarconfigurator/lib/**',
-		'tests/adapters/jquery/_assets/**',
-		'tests/core/dom/_assets/**',
-		'tests/core/selection/_helpers/rangy.js'
-	];
-
 	// Basic configuration which will be overloaded by the tasks.
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
-
-		jshint: {
-			options: {
-				ignores: ignoreFiles
-			}
-		},
-
-		jscs: {
-			options: {
-				excludeFiles: ignoreFiles
-			}
-		},
 
 		plugin: {
 			externalDir: '../ckeditor-plugins/',
