02cf35f6e604e655feb8f4de057908d4a8396afe
Tomasz Jakut <vepomoc@gmail.com>
2020-02-18 18:10:10 +0100

    Add unit test.

--- tests/core/creators/inline.js
+++ tests/core/creators/inline.js
@@ -12,5 +12,20 @@ bender.test( {
 
 		assert.areSame( editor.name, editor.container.getId() );
 		assert.areSame( editor.name, editor.ui.contentsElement.getId() );
+	},
+
+	// (#3866)
+	'test forcing editable mode': function() {
+		CKEDITOR.inline( 'readOnly', {
+			readOnly: false,
+			on: {
+				instanceReady: function( evt ) {
+					resume( function() {
+						assert.isFalse( evt.editor.readOnly, 'Editor is in read-only mode' );
+					} );
+				}
+			}
+		} );
+		wait();
 	}
 } );
