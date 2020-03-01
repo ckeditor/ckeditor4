036df05a3d0fe6099c3268c5d6962aca787e167d
Tomasz Jakut <vepomoc@gmail.com>
2020-03-01 16:44:05 +0100

    Add wildcard handling for align attribute.

--- plugins/pastefromlibreoffice/filter/default.js
+++ plugins/pastefromlibreoffice/filter/default.js
@@ -68,11 +68,6 @@
 							insertPageBreakBefore( editor, element );
 						}
 
-						if ( element.attributes.align ) {
-							styles[ 'text-align' ] = element.attributes.align;
-							delete element.attributes.align;
-						}
-
 						element.attributes.style = CKEDITOR.tools.writeCssText( styles );
 
 						element.filterChildren( filter );
@@ -178,6 +173,22 @@
 					'style': function( styles, element ) {
 						// Returning false deletes the attribute.
 						return Style.normalizedStyles( element, editor ) || false;
+					},
+
+					// Many elements can have [align] attribute. Let's make it a wildcard
+					// transformation then!
+					'align': function( align, element ) {
+						// Images have their own handling logic.
+						if ( element.name === 'img' ) {
+							return;
+						}
+
+						var styles = CKEDITOR.tools.parseCssText( element.attributes.style );
+
+						styles[ 'text-align' ] = element.attributes.align;
+						element.attributes.style = CKEDITOR.tools.writeCssText( styles );
+
+						return false;
 					},
 
 					'cellspacing': remove,
