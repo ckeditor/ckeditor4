298d0284f7f4da6d19ae26cdf93922fc8c576ab2
Krzysztof Krztoń <k.krzton@cksource.com>
2020-05-07 11:40:06 +0200

    Tests: add unit test covering background color.

--- tests/plugins/colorbutton/config.js
+++ tests/plugins/colorbutton/config.js
@@ -58,6 +58,25 @@
 					} ]
 				}
 			}
+		},
+		colorNameAttributesBackground: {
+			creator: 'inline',
+			name: 'colorNameAttributesBackground',
+			config: {
+				colorButton_colors: 'bg-red/F00,bg-blue/00F',
+				extraAllowedContent: 'span(*)',
+				language: 'en',
+				colorButton_backStyle: {
+					element: 'span',
+					attributes: { 'class': '#(colorName)' },
+					overrides: [ {
+						element: 'span',
+						attributes: {
+							'class': /.text/
+						}
+					} ]
+				}
+			}
 		}
 	};
 
@@ -174,6 +193,37 @@
 			colorSquare.$.onclick();
 
 			assert.beautified.html( bender.tools.selection.getWithHtml( editor ), '<p>foo <span class="text-red">[bar]</span> baz</p>', {
+				customFilters: [ filter ],
+				fixStyles: true
+			} );
+		},
+
+		// (#3490)
+		'test config.colorButton_backStyle with colorName attribute': function() {
+			var editor = this.editors.colorNameAttributesBackground,
+				backColorBtn = editor.ui.get( 'BGColor' ),
+				filter = new CKEDITOR.htmlParser.filter( {
+					text: function( value ) {
+						return value.replace( '{', '[' ).replace( '}', ']' );
+					},
+					elements: {
+						br: function() {
+							return false;
+						}
+					}
+				} ),
+				colorSquare;
+
+			bender.tools.selection.setWithHtml( editor, '<p>foo [bar] baz</p>' );
+
+			editor.focus();
+			backColorBtn.click( editor );
+
+			colorSquare = backColorBtn._.panel.getBlock( backColorBtn._.id ).element.findOne( 'a.cke_colorbox[data-value=F00]' );
+
+			colorSquare.$.onclick();
+
+			assert.beautified.html( bender.tools.selection.getWithHtml( editor ), '<p>foo <span class="bg-red">[bar]</span> baz</p>', {
 				customFilters: [ filter ],
 				fixStyles: true
 			} );
