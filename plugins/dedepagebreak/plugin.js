12cab9d9a2aca8d020c7ab7cf95374d6ea6fe048
llgoer <llgoer@github.com>
2020-05-10 16:44:42 +0800

    增加DedePageBreak插件

---
+++ plugins/dedepagebreak/plugin.js
@@ -0,0 +1,21 @@
+﻿CKEDITOR.plugins.add( 'dedepagebreak', {
+
+	icons: 'dedepagebreak',
+
+	init: function( editor ) {
+
+		editor.addCommand( 'insertDedePageBreak', {
+
+			exec: function( editor ) {
+				var now = new Date();
+				editor.insertHtml( '#p#分页标题#e#' );
+			}
+		});
+
+		editor.ui.addButton( 'DedePageBreak', {
+			label: 'Insert PageBreak',
+			command: 'insertDedePageBreak',
+			toolbar: 'insert'
+		});
+	}
+});
