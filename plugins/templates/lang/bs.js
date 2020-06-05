3e10997a6116b0ef2d5e2b38f648b31dab996f90
Rukma <rukma30@gmail.com>
2020-06-05 13:06:07 +0530

    Updating label to Side-by-Side

--- plugins/templates/lang/bs.js
+++ plugins/templates/lang/bs.js
@@ -3,7 +3,7 @@ Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'templates', 'bs', {
-	button: "Side by Side", // MISSING
+	button: "Side-by-Side", // MISSING
 	emptyListMsg: '(No templates defined)', // MISSING
 	insertOption: 'Replace actual contents', // MISSING
 	options: 'Template Options', // MISSING
