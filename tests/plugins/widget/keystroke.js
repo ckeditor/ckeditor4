d7ce7e447373ed91fe41493056c194b1b28db3d6
Krzysztof Krztoń <k.krzton@cksource.com>
2020-06-09 16:09:49 +0200

    Tests: Check for browser errors on mode change.

--- tests/plugins/widget/keystroke.js
+++ tests/plugins/widget/keystroke.js
@@ -8,33 +8,74 @@
 		editor.editable().fire( 'keydown', new CKEDITOR.dom.event( { keyCode: CKEDITOR.CTRL + 13 } ) );
 	}
 
-	bender.editor = {
-		config: {
-			keystrokes: [
-				[ CKEDITOR.CTRL + 13 /*Enter*/, 'source' ]
-			]
+	var currentError = null;
+	window.onerror = function( error ) {
+		currentError = error;
+	};
+
+	bender.editors = {
+		wysiwyg: {
+			config: {
+				keystrokes: [
+					[ CKEDITOR.CTRL + 13 /*Enter*/, 'source' ]
+				]
+			}
+		},
+		source: {
+			config: {
+				keystrokes: [
+					[ CKEDITOR.CTRL + 13 /*Enter*/, 'source' ]
+				],
+				startupMode: 'source'
+			}
 		}
 	};
 
 	bender.test( {
+		tearDown: function() {
+			currentError = null;
+		},
+
 		// (#3998)
 		'test change wysiwyg mode to source mode by pressing keystroke': function() {
-			var editor = this.editor;
+			var editor = this.editors.wysiwyg;
 
 			assert.areSame( 'wysiwyg', editor.mode, 'Wysiwyg mode is active at start.' );
+
+			editor.on( 'mode', function() {
+				resume( function() {
+					assert.areSame( 'source', editor.mode, 'Source mode is active (changed from wysiwyg mode).' );
+
+					if ( currentError ) {
+						assert.fail( currentError );
+					}
+				} );
+			} );
+
 			pressCtrlEnter( editor );
-			assert.areSame( 'source', editor.mode, 'Source mode is active (changed from wysiwyg mode).' );
+
+			wait();
 		},
 
 		// (#3998)
 		'test change source mode to wysiwyg mode by pressing keystroke': function() {
-			var editor = this.editor;
+			var editor = this.editors.source;
 
 			assert.areSame( 'source', editor.mode, 'Source mode is active at start.' );
+
+			editor.on( 'mode', function() {
+				resume( function() {
+					assert.areSame( 'wysiwyg', editor.mode, 'Wysiwyg mode is active (changed from source mode).' );
+
+					if ( currentError ) {
+						assert.fail( currentError );
+					}
+				} );
+			} );
+
 			pressCtrlEnter( editor );
-			wait( function() {
-				assert.areSame( 'wysiwyg', editor.mode, 'Wysiwyg mode is active (changed from source mode).' );
-			}, 100 );
+
+			wait();
 		}
 	} );
 } )();
