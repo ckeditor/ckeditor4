840e18e4bea08c92de5a71b1aa26ca3ad5588273
Dariusz Jarzębski <d.jarzebski@cksource.com>
2020-06-22 16:46:22 +0200

    Add comment about unobvious behaviour.

--- plugins/colorbutton/plugin.js
+++ plugins/colorbutton/plugin.js
@@ -596,6 +596,10 @@ CKEDITOR.plugins.add( 'colorbutton', {
 						setColor( color && '#' + color, colorName, history );
 					}
 
+					// The colors may be duplicated in both default palette and color history. If user reopens panel
+					// after choosing color without changing selection, the box that was clicked last should be selected.
+					// If selection changes in the meantime, color box from the default palette has the precedence.
+					// See https://github.com/ckeditor/ckeditor4/pull/3784#pullrequestreview-378461341 for details.
 					if ( !colorbox ) {
 						return;
 					}
