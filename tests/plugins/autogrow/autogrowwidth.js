e672426181cb8bc843943c05f5244bc86f00d227
hub33k <hub33k@yahoo.com>
2020-10-06 17:56:21 +0200

    Tests: rework unit test.

--- tests/plugins/autogrow/autogrowwidth.js
+++ tests/plugins/autogrow/autogrowwidth.js
@@ -18,20 +18,36 @@
 			var editor = this.editor,
 				bot = this.editorBot;
 
-			var html = '';
+			var html = '',
+				initialEditorWidth = getEditorSize( editor ).width,
+				initialEditorHeight = getEditorSize( editor ).height;
+
 			for ( var i = 0; i < 6; i++ ) {
 				html += '<p>test ' + i + '</p>';
 			}
 
 			bot.setData( html, function() {
-				setTimeout( function() {
+				editor.once( 'afterCommandExec', function() {
 					resume( function() {
-						var width = parseInt( editor.editable().getComputedStyle( 'width' ), 10 );
-						assert.isTrue( width > 0, true );
+						var editorWidth = getEditorSize( editor ).width,
+							editorHeight = getEditorSize( editor ).height;
+
+						assert.isTrue( editorHeight > initialEditorHeight, true, 'is editor height bigger than initial editor height' );
+						assert.isTrue( editorWidth > 0, true, 'is editor width greater than zero' );
+						assert.areEqual( editorWidth, initialEditorWidth, 'does width of editor changes' );
 					} );
-				}, 1000 );
+				} );
+
+				editor.execCommand( 'autogrow' );
 				wait();
 			} );
 		}
 	} );
+
+	function getEditorSize( editor ) {
+		return {
+			width: parseInt( editor.editable().getComputedStyle( 'width' ), 10 ),
+			height: parseInt( editor.editable().getComputedStyle( 'height' ), 10 )
+		}
+	}
 } )();
