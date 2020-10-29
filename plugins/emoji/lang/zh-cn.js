6fac2afced3b9224f00da6fe9bb11f8db0d0cb2b
Robert Mikołajuk <r.mikolajuk@cksource.com>
2020-10-29 12:10:53 +0100

    Updated language files.

--- plugins/emoji/lang/zh-cn.js
+++ plugins/emoji/lang/zh-cn.js
@@ -8,13 +8,13 @@ CKEDITOR.plugins.setLang( 'emoji', 'zh-cn', {
 	navigationLabel: 'Groups navigation for emoji sections.', // MISSING
 	title: '表情清单',
 	groups: {
-		people: '人物',
+		people: '笑脸和人物',
 		nature: '自然和动物',
 		food: '食物和饮料',
 		travel: '旅游和地方',
-		activities: 'Activities', // MISSING
-		objects: 'Objects', // MISSING
-		symbols: 'Symbols', // MISSING
-		flags: 'Flags' // MISSING
+		activities: '活动和行为',
+		objects: '物品和对象',
+		symbols: '符号和标志',
+		flags: '国旗和旗帜'
 	}
 } );
