e593627e84507c32c6bb4dd99d5ebef48ab5e5f4
Daniel Macbubi <daniel.macbubi@gmail.com>
2020-11-02 23:18:20 +0200

    support active highgligh

--- plugins/highlight/plugin.js
+++ plugins/highlight/plugin.js
@@ -1,5 +1,8 @@
 CKEDITOR.config.allowedContent = true;
 CKEDITOR.config.extraAllowedContent = 'span(highlight)';
+
+CKEDITOR.config.coreStyle = { name: 'highlight', element: 'span', attributes: { class: 'highlight' } };
+
 CKEDITOR.addCss(
     '.highlight {' +
         'background-color: #DCDCDC;' +
@@ -9,28 +12,49 @@ CKEDITOR.plugins.add( 'highlight', {
     icons: 'about',
     lang: 'en',
     init: function( editor ) {
-        editor.addCommand( 'insertHighlight', {
-            exec: function( editor ) {
-              var element = editor.getSelection().getStartElement().$;
-              var selection = editor.getSelection().getSelectedText();
-              if(element.classList.contains('highlight')) {
-                element.classList.remove('highlight');
-              } else if(element.innerText == selection && element.tagName == "SPAN") {
-                element.classList.add('highlight');
-              } else {
-                var range = editor.getSelection().getRanges()[ 0 ];
-                var newElement = new CKEDITOR.dom.element("span");           // Make Span
-                newElement.setAttribute('class', 'highlight');               // Set Highlight class
-                newElement.append( range.cloneContents() );                  // Set text to element
-                editor.insertElement(newElement);                            // Add Element
-              }
-            }
-        });
+
+        // OLD hand written way - lots of bugs (not recommended)
+        // editor.addCommand( 'insertHighlight', {
+        //     exec: function( editor ) {
+        //       var element = editor.getSelection().getStartElement().$;
+        //       var selection = editor.getSelection().getSelectedText();
+        //       if(element.classList.contains('highlight')) {
+        //         element.classList.remove('highlight');
+        //       } else if(element.innerText == selection && element.tagName == "SPAN") {
+        //         element.classList.add('highlight');
+        //       } else {
+        //         var range = editor.getSelection().getRanges()[ 0 ];
+        //         var newElement = new CKEDITOR.dom.element("span");           // Make Span
+        //         newElement.setAttribute('class', 'highlight');               // Set Highlight class
+        //         newElement.append( range.cloneContents() );                  // Set text to element
+        //         editor.insertElement(newElement);                            // Add Element
+        //       }
+        //     }
+        // });
+
+        // Add button
         editor.ui.addButton( 'Highlight', {
             label: 'Insert Highlight',
             command: 'insertHighlight',
             toolbar: 'colors',
             icon: this.path + 'icons/marker1.png'
         });
+
+        // Listen to style changes
+        var style = new CKEDITOR.style(CKEDITOR.config.coreStyle);
+        editor.attachStyleStateChange( style, function( state ) {
+          !editor.readOnly && editor.getCommand( 'insertHighlight' ).setState( state );
+        } );
+
+        // add class command
+        var form = [
+          [ 'span', function( el ) {
+            el.classList.push('highlight');
+            return el.classList.contains('highlight');
+          } ]
+        ];
+        editor.addCommand( 'insertHighlight', new CKEDITOR.styleCommand( style, {
+          contentForms: form
+        } ) );
     }
 });
