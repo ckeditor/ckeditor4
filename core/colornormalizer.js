f09040ad26de05c2c395ab8646ea2e6dff23f762
sculpt0r <b.kalemba@cksource.com>
2020-11-05 10:22:53 +0100

    Fix code style

--- core/colornormalizer.js
+++ core/colornormalizer.js
@@ -1,100 +1,91 @@
 /**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
-
- /**
-  * @fileoverview Defines the {@link CKEDITOR.tools.style.Color} normalizer class
-  * 		that parse color string code other formats.
-  */
-
-//TODO
-// naming convencion for hex with & without '#' -> methods or flag?
-// uper / lower cases?
+* @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+* For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+/**
+* @fileoverview Defines the {@link CKEDITOR.tools.style.Color} normalizer class
+* 		that parse color string code other formats.
+*/
 
 /**
- * HEX 	 -> valid HEX Color Value with # character: #FFF or #FFFFFF
- * 3-HEX -> HEX but with only 3 characters value: #FFF
- * 6-HEX -> HEX but with exactly 6 characters value: #FFFFFF
- */
+*Basic definitions
+*HEX 	 -> valid HEX Color Value with # character: #FFF or #FFFFFF
+*3-HEX -> HEX but with only 3 characters value: #FFF
+*6-HEX -> HEX but with exactly 6 characters value: #FFFFFF
+*/
+CKEDITOR.tools.style.Color = CKEDITOR.tools.createClass( {
+	$: function( colorCode ) {
+		this._.originalColorCode = colorCode;
+		var stringToHex = this._.convertStringToHex( colorCode );
+		var rgbToHex = this._.convertRgbToHex( colorCode );
+		var hexToHex = this._.normalizeHex( colorCode );
 
-  CKEDITOR.tools.style.Color = CKEDITOR.tools.createClass( {
-		$: function (colorCode) {
-			this._.originalColorCode = colorCode;
+		var finalHex  = stringToHex || rgbToHex || hexToHex || this._.defaultHexColorCode;
 
-			var stringToHex = this._.convertStringToHex( colorCode );
-			var rgbToHex = this._.convertRgbToHex( colorCode );
-			var hexToHex = this._.normalizeHex( colorCode );
+		this._.hexColorCode = finalHex;
+	},
 
-			var finalHex  = stringToHex || rgbToHex || hexToHex || this._.defaultHexColorCode;
+	_: {
+		originalColorCode: '',
+		hexColorCode: '',
+		defaultHexColorCode: '#000000',
 
-			this._.hexColorCode = finalHex;
+		/**
+		* Normalizes hexadecimal notation so that the color string is always 6 characters long and lowercase.
+		*
+		* @param {String} hexColorCode String containing hex colors.
+		* @returns {String} The style data with hex colors normalized.
+		*/
+		normalizeHex: function( hexColorCode ) {
+			var hexRegExp = /#(([0-9a-f]{3}){1,2})($|;|\s+)/gi;
+
+			if ( hexColorCode.match( hexRegExp ) ) {
+				return hexColorCode.replace( hexRegExp, function( match, hexColor, hexColorPart, separator ) {
+					var normalizedHexColor = hexColor.toLowerCase();
+
+					if ( normalizedHexColor.length == 3 ) {
+						var parts = normalizedHexColor.split( '' );
+						normalizedHexColor = [ parts[ 0 ], parts[ 0 ], parts[ 1 ], parts[ 1 ], parts[ 2 ], parts[ 2 ] ].join( '' );
+					}
+
+					return '#' + normalizedHexColor + separator;
+				} );
+			} else {
+				return null;
+			}
+
 		},
 
-		_: {
-			originalColorCode: '',
-			hexColorCode: '',
-			defaultHexColorCode: '#000000',
+		convertRgbToHex: function( styleText ) {
+			var rgbRegExp = /(?:rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\))/gi;
 
-			/**
-			 * Normalizes hexadecimal notation so that the color string is always 6 characters long and lowercase.
-			 *
-			 * @param {String} hexColorCode String containing hex colors.
-			 * @returns {String} The style data with hex colors normalized.
-			 */
-			normalizeHex: function(hexColorCode) {
-				var hexRegExp = /#(([0-9a-f]{3}){1,2})($|;|\s+)/gi;
-
-				if(hexColorCode.match(hexRegExp))
-				{
-					return hexColorCode.replace( hexRegExp, function( match, hexColor, hexColorPart, separator ) {
-						var normalizedHexColor = hexColor.toLowerCase();
-
-						if ( normalizedHexColor.length == 3 ) {
-							var parts = normalizedHexColor.split( '' );
-							normalizedHexColor = [ parts[ 0 ], parts[ 0 ], parts[ 1 ], parts[ 1 ], parts[ 2 ], parts[ 2 ] ].join( '' );
-						}
-
-						return '#' + normalizedHexColor + separator;
-					} );
-				}
-				else {
-					return null;
-				}
-
-			},
-
-			convertRgbToHex: function( styleText ) {
-				var rgbRegExp = /(?:rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\))/gi;
-
-				if( styleText.match( rgbRegExp ) ) {
-					return styleText.replace(rgbRegExp, function( match, red, green, blue ) {
-						var color = [ red, green, blue ];
-						// Add padding zeros if the hex value is less than 0x10.
-						for ( var i = 0; i < 3; i++ )
+			if ( styleText.match( rgbRegExp ) ) {
+				return styleText.replace( rgbRegExp, function( match, red, green, blue ) {
+					var color = [ red, green, blue ];
+					// Add padding zeros if the hex value is less than 0x10.
+					for ( var i = 0; i < 3; i++ ) {
 						color[ i ] = ( '0' + parseInt( color[ i ], 10 ).toString( 16 ) ).slice( -2 );
-						return '#' + color.join( '' );
-					} );
-				}else {
-					return null;
-				}
-			},
-
-			convertStringToHex: function( colorCode ) {
-				var colorToHexObject = CKEDITOR.tools.style.parse._colors;
-				var resultCode = colorToHexObject[colorCode] || null;
-
-				return  resultCode;
+					}
+					return '#' + color.join( '' );
+				} );
+			} else {
+				return null;
 			}
 		},
 
-		proto: {
-			getHex: function() {
-				finalColorCode = this._.hexColorCode.toUpperCase();
+		convertStringToHex: function( colorCode ) {
+			var colorToHexObject = CKEDITOR.tools.style.parse._colors;
+			var resultCode = colorToHexObject[ colorCode ] || null;
 
-				return finalColorCode;
-			}
+			return resultCode;
 		}
+	},
 
-	  }
-  );
+	proto: {
+		getHex: function() {
+			var finalColorCode = this._.hexColorCode.toUpperCase();
+
+			return finalColorCode;
+		}
+	}
+} );
--- tests/core/tools/color.js
+++ tests/core/tools/color.js
@@ -4,35 +4,35 @@
 
 	bender.test( {
 		setUp: function() {
-			this.createColor = function(colorCode) {
-				return new CKEDITOR.tools.style.Color(colorCode);
+			this.createColor = function( colorCode ) {
+				return new CKEDITOR.tools.style.Color( colorCode );
 			};
 		},
 
 		'test color object creation': function() {
-			var colorObject = this.createColor('');
+			var colorObject = this.createColor( '' );
 
-			assert.isObject(colorObject);
+			assert.isObject( colorObject );
 		},
 
 		'test color from 6-HEX lower-case string return 6-HEX': function() {
 			var hexCode = '#ffffff';
 			var expectedHex = '#FFFFFF';
-			var colorObject = this.createColor(hexCode);
+			var colorObject = this.createColor( hexCode );
 
 			var resultHex = colorObject.getHex();
 
-			assert.areSame(expectedHex, resultHex);
+			assert.areSame( expectedHex, resultHex );
 		},
 
-		'test color from 3-HEX lower-case string return 6-HEX' : function(){
+		'test color from 3-HEX lower-case string return 6-HEX': function() {
 			var hexCode = '#fff';
 			var expectedHexCode = '#FFFFFF';
-			var colorObject = this.createColor(hexCode);
+			var colorObject = this.createColor( hexCode );
 
 			var resultHex = colorObject.getHex();
 
-			assert.areSame(expectedHexCode, resultHex);
+			assert.areSame( expectedHexCode, resultHex );
 		},
 
 		'test color from valid rgb string return HEX': function() {
@@ -45,9 +45,9 @@
 			assert.areSame( expectedHexCode, resultHex );
 		},
 
-		'test color from valid string color name return HEX': function(){
+		'test color from valid string color name return HEX': function() {
 			var validColorString = 'red';
-			var expectedHexCode ='#FF0000';
+			var expectedHexCode = '#FF0000';
 
 			var colorObject = this.createColor( validColorString );
 
@@ -58,7 +58,7 @@
 
 		'test color from invalid string color name return default color': function() {
 			var validColorString = 'NotValidColorName';
-			var expectedDefaultHexCode ='#000000';
+			var expectedDefaultHexCode = '#000000';
 
 			var colorObject = this.createColor( validColorString );
 
@@ -67,6 +67,6 @@
 			assert.areSame( expectedDefaultHexCode, resultHex );
 		}
 
-	});
+	} );
 
 } )();
