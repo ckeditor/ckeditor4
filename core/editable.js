d3bde9ea08005a496c3f5c290d6b142f9e1ac241
Tomasz Jakut <vepomoc@gmail.com>
2020-11-17 12:49:42 +0100

    Update comments.

--- core/editable.js
+++ core/editable.js
@@ -1650,7 +1650,7 @@
 				dontFilter = false,
 				html = editable.getHtml(),
 				// Instead of getData method, we directly check the HTML
-				// due to the fact that interal getData operates on latest snapshot,
+				// due to the fact that internal getData operates on latest snapshot,
 				// not the current content (#4301).
 				isEmptyEditable = html === '' || html.match( emptyParagraphRegexp );
 
@@ -1691,7 +1691,7 @@
 			prepareRangeToDataInsertion( that );
 
 			// When enter mode is set to div and content wrapped with div is pasted,
-			// we must ensure that no additional divs are created (#2751, #3379).
+			// we must ensure that no additional divs are created (#2751).
 			if ( editor.enterMode === CKEDITOR.ENTER_DIV && isEmptyEditable ) {
 				clearEditable( editable, range );
 			}
