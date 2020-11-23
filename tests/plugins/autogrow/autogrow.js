8bfcbae98e9e4a64935b2197dadcdd89256566fa
sculpt0r <b.kalemba@cksource.com>
2020-11-23 07:05:36 +0100

    Add failing test - due width change

--- tests/plugins/autogrow/autogrow.js
+++ tests/plugins/autogrow/autogrow.js
@@ -8,7 +8,45 @@
 
 	bender.editor = {};
 
+	bender.createEditor = function( width, paragraphs ) {
+		bender.editorBot.create( {
+			name: 'EditorWidth' + width,
+			config: {
+				width: width
+			}
+		}, function(bot){
+			bot.setData( autogrowTools.getTestContent( paragraphs ), function() {
+				var editor = bot.editor;
+				var initialEditorSize = autogrowTools.getEditorSize( editor );
+				editor.once( 'afterCommandExec', function() {
+					resume( function name( params ) {
+						var editorSize = autogrowTools.getEditorSize( editor );
+
+						assert.isTrue( editorSize.height > initialEditorSize.height, 'editor height should increase' );
+						assert.areEqual( editorSize.width, initialEditorSize.width, 'editor width should not change' );
+					} );
+				} );
+
+				editor.execCommand( 'autogrow' );
+
+				wait();
+			} );
+		} );
+	};
+
 	bender.test( {
+		// (#4372)
+		'test autogrow for editor width 200%': function() { bender.createEditor('200%', 6); },
+		// (#4372)
+		'test autogrow for editor width 20em': function() { bender.createEditor('20em', 6); },
+		// (#4372)
+		'test autogrow for editor width 200px': function() { bender.createEditor('200px', 6); },
+		// (#4372)
+		'test autogrow for editor width 200': function() { bender.createEditor(200, 6); },
+		// (#4372)
+		'test autogrow for editor width 0': function() { bender.createEditor(0, 6); },
+		// (#4372)
+		'test autogrow for editor width auto': function() { bender.createEditor('auto', 6); },
 		// (#4286)
 		'test autogrow': function() {
 			if ( bender.env.ie && bender.env.version < 9 ) {
