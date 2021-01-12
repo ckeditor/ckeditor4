2fb8cb990dd673921a297a3917bc7f204573b113
bunglegrind <bunglegrind@libero.it>
2021-01-12 18:11:44 +0100

    added manual test, test widgets with editables, cancel event when the paragraph is deleted

--- tests/plugins/widget/deleteparagraphsclosetowidgets.js
+++ tests/plugins/widget/deleteparagraphsclosetowidgets.js
@@ -10,52 +10,94 @@
 		rightArrow = 39;
 
 	var input = (
-		'<pre><code class="language-javascript">//Hello world!</code></pre><p>&nbsp;</p><pre>' +
-		'<code class="language-javascript">//Hello another world!</code></pre>'
+		'<div class="simplebox">' +
+		'<h2 class="simplebox-title">Title</h2>' +
+		'<div class="simplebox-content"><p>Content...</p></div></div>' +
+		'<p>&nbsp;</p>' +
+		'<pre><code class="language-javascript">//Hello another world!</code></pre>'
 	);
 
 	var notEmptyInput = (
-		'<pre><code class="language-javascript">//Hello world!</code></pre><p>Hello, world!</p><pre>' +
-		'<code class="language-javascript">//Hello another world!</code></pre>'
+		'<div class="simplebox">' +
+		'<h2 class="simplebox-title">Title</h2>' +
+		'<div class="simplebox-content"><p>Content...</p></div></div>' +
+		'<p>Hello, World!</p>' +
+		'<pre><code class="language-javascript">//Hello another world!</code></pre>'
 	);
 
 	var result = (
-		'<pre><code class="language-javascript">//Hello world!</code></pre><pre>' +
-		'<code class="language-javascript">//Hello another world!</code></pre>'
+		'<div class="simplebox">' +
+		'<h2 class="simplebox-title">Title</h2>' +
+		'<div class="simplebox-content"><p>Content...</p></div></div>' +
+		'<pre><code class="language-javascript">//Hello another world!</code></pre>'
 	);
+
+	var simpleBox = {
+		template: '<div class="simplebox">' +
+			'<h2 class="simplebox-title">Title</h2>' +
+			'<div class="simplebox-content"><p>Content...</p></div>' +
+		'</div>',
+		editables: {
+			title: {
+				selector: '.simplebox-title',
+				allowedContent: 'br strong em'
+			},
+			content: {
+				selector: '.simplebox-content',
+				allowedContent: 'p br ul ol li strong em'
+			}
+		},
+		allowedContent: 'div(!simplebox); div(!simplebox-content); h2(!simplebox-title)',
+		requiredContent: 'div(simplebox)',
+
+		upcast: function( element ) {
+			return element.name === 'div' && element.hasClass( 'simplebox' );
+		}
+	};
 
 	function selectEmptyParagraph( editor ) {
 		var range = editor.createRange();
-		range.selectNodeContents( editor.document.getElementsByTag( 'P' ).getItem( 0 ) );
-		range.collapse( true );
+
+		range.moveToPosition( editor.document.getElementsByTag( 'P' ).getItem( 1 ), CKEDITOR.POSITION_AFTER_START );
 		editor.getSelection().selectRanges( [ range ] );
 	}
 
 	bender.test( {
 
-		// https://github.com/ckeditor/ckeditor4/issues/1572
+		// (#1572)
 		'test fire delete to remove an empty paragraph between two widgets': function() {
+			CKEDITOR.plugins.add( 'simplebox', {
+				init: function( editor ) {
+					editor.widgets.add( 'simplebox', simpleBox );
+				}
+			} );
 			bender.editorBot.create( {
 				name: 'editor1',
-				startupData: input
+				startupData: input,
+				config: {
+					extraPlugins: 'simplebox',
+					allowedContent: true
+				}
 			}, function( bot ) {
 				var editor = bot.editor;
-
 				selectEmptyParagraph( editor );
 				editor.editable().fire( 'keydown', new CKEDITOR.dom.event( { keyCode: deleteKey } ) );
 
-				assert.areSame( result, editor.getData(), 'Empty paragraph between two widget should be removed.' );
+				assert.areSame( result, editor.getData(), 'Empty paragraph between two widgets should be removed.' );
 			} );
 		},
 
-		// https://github.com/ckeditor/ckeditor4/issues/1572
+		// (#1572)
 		'test fire backspace to remove an empty paragraph between two widgets': function() {
 			bender.editorBot.create( {
 				name: 'editor2',
-				startupData: input
+				startupData: input,
+				config: {
+					extraPlugins: 'simplebox',
+					allowedContent: true
+				}
 			}, function( bot ) {
 				var editor = bot.editor;
-
 				selectEmptyParagraph( editor );
 				editor.editable().fire( 'keydown', new CKEDITOR.dom.event( { keyCode: backspace } ) );
 
@@ -63,41 +105,53 @@
 			} );
 		},
 
-		// https://github.com/ckeditor/ckeditor4/issues/1572
+		// (#1572)
 		'test fire right arrow must not remove a paragraph between two widgets': function() {
 			bender.editorBot.create( {
 				name: 'editor3',
-				startupData: input
+				startupData: input,
+				config: {
+					extraPlugins: 'simplebox',
+					allowedContent: true
+				}
 			}, function( bot ) {
 				var editor = bot.editor;
 
 				selectEmptyParagraph( editor );
 				editor.editable().fire( 'keydown', new CKEDITOR.dom.event( { keyCode: rightArrow } ) );
 
-				assert.areSame( input, editor.getData(), 'Empty paragraph between two widget should not be removed.' );
+				assert.areSame( input, editor.getData(), 'Empty paragraph between two widgets should not be removed.' );
 			} );
 		},
 
-		// https://github.com/ckeditor/ckeditor4/issues/1572
+		// (#1572)
 		'test fire left arrow must not remove a paragraph between two widgets': function() {
 			bender.editorBot.create( {
 				name: 'editor4',
-				startupData: input
+				startupData: input,
+				config: {
+					extraPlugins: 'simplebox',
+					allowedContent: true
+				}
 			}, function( bot ) {
 				var editor = bot.editor;
 
 				selectEmptyParagraph( editor );
 				editor.editable().fire( 'keydown', new CKEDITOR.dom.event( { keyCode: leftArrow } ) );
 
-				assert.areSame( input, editor.getData(), 'Empty paragraph between two widget should not be removed.' );
+				assert.areSame( input, editor.getData(), 'Empty paragraph between two widgets should not be removed.' );
 			} );
 		},
 
-		// https://github.com/ckeditor/ckeditor4/issues/1572
+		// (#1572)
 		'test delete must not remove a non empty paragraph between two widgets': function() {
 			bender.editorBot.create( {
 				name: 'editor5',
-				startupData: notEmptyInput
+				startupData: notEmptyInput,
+				config: {
+					extraPlugins: 'simplebox',
+					allowedContent: true
+				}
 			}, function( bot ) {
 				var editor = bot.editor;
 				var p;
@@ -112,11 +166,15 @@
 			} );
 		},
 
-		// https://github.com/ckeditor/ckeditor4/issues/1572
+		// (#1572)
 		'test backspace must not remove a non empty paragraph between two widgets': function() {
 			bender.editorBot.create( {
 				name: 'editor6',
-				startupData: notEmptyInput
+				startupData: notEmptyInput,
+				config: {
+					extraPlugins: 'simplebox',
+					allowedContent: true
+				}
 			}, function( bot ) {
 				var editor = bot.editor;
 				var p;
