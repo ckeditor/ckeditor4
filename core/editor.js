f4c19a9660a2ee3b41a0e779a544eadc710aab7d
sculpt0r <b.kalemba@cksource.com>
2021-04-16 14:26:55 +0200

    Change docs variable type to `dom.element`

--- core/editor.js
+++ core/editor.js
@@ -1668,7 +1668,7 @@
 	 * @since 4.17.0
 	 * @static
 	 * @member CKEDITOR.editor
-	 * @param {CKEDITOR.element} element The DOM element on which editor should be initialized.
+	 * @param {CKEDITOR.dom.element} element The DOM element on which editor should be initialized.
 	 * @param {Object} config The specific configuration to apply to the editor instance. Configuration set here will override the global CKEditor settings.
 	 * @param {String} editorCreationMethod Creator function that should be used to initialize editor (inline/replace).
 	 */
@@ -1713,7 +1713,7 @@
 	 * @since 4.17.0
 	 * @static
 	 * @member CKEDITOR.editor
-	 * @param {CKEDITOR.element} element The DOM element on which editor should be initialized.
+	 * @param {CKEDITOR.dom.element} element The DOM element on which editor should be initialized.
 	 * @param {Object} config Editor configuration.
 	 * @returns {Boolean} True if creation should be delayed.
 	 */
