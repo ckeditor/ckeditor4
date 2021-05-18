5ca73bc1d7afc82ca1ed6aeeb03d139381c4a9c2
Tomasz Jakut <vepomoc@gmail.com>
2021-05-18 17:07:30 +0200

    Use built-in support for short hexes.

--- plugins/colorbutton/plugin.js
+++ plugins/colorbutton/plugin.js
@@ -493,20 +493,9 @@
 			getColorName: function( editor, color ) {
 				var names = ColorBox.colorNames( editor ),
 					hex = color.getHex().replace( /^#/g, '' ),
-					shortHex = getShortHex( hex );
+					shortHex = color.getHex( true ).replace( /^#/g, '' );
 
 				return names[ hex ] || names[ shortHex ];
-
-				function getShortHex( hex ) {
-					var isShortenableRegex = /([a-f0-9])\1([a-f0-9])\2([a-f0-9])\3/i,
-						isShortenable = isShortenableRegex.test( hex );
-
-					if ( !isShortenable ) {
-						return null;
-					}
-
-					return hex[ 0 ] + hex[ 2 ] + hex[ 4 ];
-				}
 			},
 
 			colorNames: function( editor ) {
