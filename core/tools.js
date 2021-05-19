edfebce05ae5b5d327595ab2ebb26d8f9f392850
Mingli Li <mingli.li@salesforce.com>
2021-05-19 10:22:11 -0400

    address code review comments

--- core/tools.js
+++ core/tools.js
@@ -41,76 +41,73 @@
 		},
 		// this is the polyill for the CSS.escape for IE.
 		// https://github.com/mathiasbynens/CSS.escape/blob/master/css.escape.js
-		cssEscapeForIE = function( value ) {
-			var string = String(value);
-			var length = string.length;
-			var index = -1;
-			var codeUnit;
-			var result = '';
-			var firstCodeUnit = string.charCodeAt(0);
-			while ( ++index < length ) {
-				codeUnit = string.charCodeAt(index);
-				// Note: there’s no need to special-case astral symbols, surrogate
-				// pairs, or lone surrogates.
+		cssEscapeForIE = function( selector ) {
+			var stringifiedSelector = String(selector),
+				selectorLength = stringifiedSelector.length,
+				index = -1,
+				codeUnit,
+				result = '',
+				firstCodeUnit = stringifiedSelector.charCodeAt(0),
+				UTF16 = {
+					NULL: 0x0000,
+					START_OF_HEADING: 0x0001,
+					UNIT_SEPARATOR: 0x001F,
+					DELETE: 0x007F,
+					DIGIT_ZERO: 0x0030,
+					DIGIT_NINE: 0x0039,
+					HYPHEN_MINUS: 0x002D,
+					LOW_LINE: 0x005F,
+					START_OF_LATIN_1_SUPPLEMENT: 0x0080,
+					CAPITAL_LETTER_A: 0x0041,
+					CAPITAL_LETTER_Z: 0x005A,
+					SMALL_LETTER_A: 0x0061,
+					SMALL_LETTER_Z: 0x007A
+				};
 
-				// If the character is NULL (U+0000), then the REPLACEMENT CHARACTER
-				// (U+FFFD).
-				if ( codeUnit == 0x0000 ) {
+			// helper function to check if value is in the range
+			function isInRange( value, min, max ) {
+				return value >= min && value <= max;
+			}
+
+			while ( ++index < selectorLength ) {
+				codeUnit = stringifiedSelector.charCodeAt(index);
+
+				if ( codeUnit == UTF16.NULL ) {
 					result += '\uFFFD';
 					continue;
 				}
 
 				if (
-					// If the character is in the range [\1-\1F] (U+0001 to U+001F) or is
-					// U+007F, […]
-					(codeUnit >= 0x0001 && codeUnit <= 0x001F) || codeUnit == 0x007F ||
-						// If the character is the first character and is in the range [0-9]
-						// (U+0030 to U+0039), […]
-						(index == 0 && codeUnit >= 0x0030 && codeUnit <= 0x0039) ||
-							// If the character is the second character and is in the range [0-9]
-							// (U+0030 to U+0039) and the first character is a `-` (U+002D), […]
-							(
-								index == 1 &&
-								codeUnit >= 0x0030 && codeUnit <= 0x0039 &&
-								firstCodeUnit == 0x002D
-							)
+					isInRange( codeUnit, UTF16.START_OF_HEADING, UTF16.UNIT_SEPARATOR ) || codeUnit == UTF16.DELETE ||
+						( index == 0 && isInRange( codeUnit, UTF16.DIGIT_ZERO, UTF16.DIGIT_NINE ) ) ||
+							( index == 1 && isInRange( codeUnit, UTF16.DIGIT_ZERO, UTF16.DIGIT_NINE ) && firstCodeUnit == UTF16.HYPHEN_MINUS )
 				) {
 					// https://drafts.csswg.org/cssom/#escape-a-character-as-code-point
 					result += '\\' + codeUnit.toString(16) + ' ';
 					continue;
 				}
 
-				if (
-					// If the character is the first character and is a `-` (U+002D), and
-					// there is no second character, […]
-					index == 0 &&
-					length == 1 &&
-					codeUnit == 0x002D
-				) {
-					result += '\\' + string.charAt(index);
+				if ( index == 0 && selectorLength == 1 && codeUnit == UTF16.HYPHEN_MINUS ) {
+					result += '\\' + stringifiedSelector.charAt(index);
 					continue;
 				}
 
-				// If the character is not handled by one of the above rules and is
-				// greater than or equal to U+0080, is `-` (U+002D) or `_` (U+005F), or
-				// is in one of the ranges [0-9] (U+0030 to U+0039), [A-Z] (U+0041 to
-				// U+005A), or [a-z] (U+0061 to U+007A), […]
 				if (
-					codeUnit >= 0x0080 ||
-					codeUnit == 0x002D ||
-					codeUnit == 0x005F ||
-					codeUnit >= 0x0030 && codeUnit <= 0x0039 ||
-					codeUnit >= 0x0041 && codeUnit <= 0x005A ||
-					codeUnit >= 0x0061 && codeUnit <= 0x007A
+					codeUnit >= UTF16.START_OF_LATIN_1_SUPPLEMENT ||
+					codeUnit == UTF16.HYPHEN_MINUS ||
+					codeUnit == UTF16.LOW_LINE ||
+					isInRange( codeUnit, UTF16.DIGIT_ZERO, UTF16.DIGIT_NINE ) ||
+					isInRange( codeUnit, UTF16.CAPITAL_LETTER_A, UTF16.CAPITAL_LETTER_Z ) ||
+					isInRange( codeUnit, UTF16.SMALL_LETTER_A, UTF16.SMALL_LETTER_Z )
 				) {
 					// the character itself
-					result += string.charAt(index);
+					result += stringifiedSelector.charAt(index);
 					continue;
 				}
 
 				// Otherwise, the escaped character.
 				// https://drafts.csswg.org/cssom/#escape-a-character
-				result += '\\' + string.charAt(index);
+				result += '\\' + stringifiedSelector.charAt(index);
 			}
 			return result;
 		};
