f73b638d62bff0e565b2f3c7e9b75ee3d17b9782
Karol Dawidziuk <k.dawidziuk@cksource.com>
2021-05-27 12:32:36 +0200

    add unit tests

--- tests/plugins/link/anchor.js
+++ tests/plugins/link/anchor.js
@@ -1,5 +1,5 @@
 /* bender-tags: editor */
-/* bender-ckeditor-plugins: link,toolbar */
+/* bender-ckeditor-plugins: link,toolbar,basicstyles */
 
 ( function() {
 	'use strict';
@@ -78,6 +78,46 @@
 					assert.areSame( 'test', dialog.getValueOf( 'info', 'txtName' ) );
 					assert.areSame( anchor, dialog.getModel( editor ) );
 				} );
+			} );
+		},
+
+		// (#3863)
+		'test preserve duplicate anchors after editing word with custom style': function() {
+			var editor = this.editor,
+				bot = this.editorBot,
+				html = '<p><a id="test" name="test"><strong>text</strong></a></p>';
+
+			bot.setData( html, function() {
+				var range = editor.createRange(),
+					anchor = editor.editable().findOne( '#test' ),
+					textNode = anchor.getChild( 0 );
+
+				range.selectNodeContents( textNode );
+				range.select();
+
+				bot.dialog( 'anchor', function( dialog ) {
+					dialog.setValueOf( 'info', 'txtName', 'duplicate-test' );
+					dialog.getButton( 'ok' ).click();
+
+					assert.areSame( anchor, dialog.getModel( editor ) );
+				} );
+
+			} );
+		},
+
+		// (#4728)
+		'test prevent duplicate anchors in selected multi words with styles': function() {
+			var editor = this.editor,
+				bot = this.editorBot,
+				template = '[<p>Simple <a id="multiTest" name="multiTest" data-cke-saved-name="multiTest"><strong>text</strong></a></p>]',
+				expected = '<p><a id="multiTestResult" name="multiTestResult">Simple <strong>text</strong></a></p>';
+
+			bot.setHtmlWithSelection( template );
+			bot.dialog( 'anchor', function( dialog ) {
+				dialog.setValueOf( 'info', 'txtName', 'multiTestResult' );
+				dialog.getButton( 'ok' ).click();
+
+				assert.beautified.html( expected, editor.getData() );
 			} );
 		}
 	} );
