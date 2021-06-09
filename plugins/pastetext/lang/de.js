92d8e94118f66c516fd4c389a82096e6527bf704
Georg Echterling <61650099+GeorgEchterling@users.noreply.github.com>
2021-06-09 14:43:01 +0200

    Fix typo in German translation
    "über dem Knopf" means "above the button". "über den Knopf" means "via the button"

--- plugins/pastetext/lang/de.js
+++ plugins/pastetext/lang/de.js
@@ -4,6 +4,6 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 */
 CKEDITOR.plugins.setLang( 'pastetext', 'de', {
 	button: 'Als Klartext einfügen',
-	pasteNotification: 'Drücken Sie %1 zum Einfügen. Ihr Browser unterstützt nicht das Einfügen über dem Knopf in der Toolbar oder dem Kontextmenü.',
+	pasteNotification: 'Drücken Sie %1 zum Einfügen. Ihr Browser unterstützt nicht das Einfügen über den Knopf in der Toolbar oder dem Kontextmenü.',
 	title: 'Als Klartext einfügen'
 } );
