d81f72e87d7d2d0a173e0affb24592d12fe75124
Karol Dawidziuk <k.dawidziuk@cksource.com>
2021-08-10 15:16:29 +0200

    prevent open new browser tab in unit test

--- tests/plugins/preview/preview.js
+++ tests/plugins/preview/preview.js
@@ -84,20 +84,41 @@ bender.test( {
 
 	// (#4790)
 	'test callback should be called when document.readyState is complete': function() {
+		// It's not possible to overwrite window.open in IE8.
+		if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 ) {
+			assert.ignore();
+		}
+
+		var documentReadyState = 'complete',
+			// We need to prevent the opening of a new as browsers block it and the test will fall.
+			openStub = sinon.stub( window, 'open', function() {
+			return {
+				document: {
+					open: function() {},
+					write: function() {},
+					close: function() {},
+					readyState: documentReadyState
+				}
+			};
+		} );
+
 		bender.editorBot.create( {
 			name: 'callback-execute-test',
-			startupData: '<p>Foo</p>',
 			config: {
 				plugins: 'print,image'
 			}
 		}, function( bot ) {
-			var preview = CKEDITOR.plugins.preview;
+			var editor = bot.editor,
+				preview = CKEDITOR.plugins.preview;
 
-			preview.createPreview( bot.editor, function( previewWindow ) {
+			preview.createPreview( editor, function( previewWindow ) {
 				resume( function() {
 					assert.areSame( 'complete', previewWindow.$.document.readyState, 'callback was not called because document.readyState is other than complete' );
 				} );
 			} );
+
+			openStub.restore();
+
 			wait();
 		} );
 	}
