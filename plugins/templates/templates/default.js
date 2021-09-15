eae7490959ecd035f9488e8f38864816eda9c647
Diana Pankratz <Diana.Pankratz@t-online.de>
2021-09-15 15:43:15 +0200

    Restore default templates and add new templates for htmlFile.

--- plugins/templates/templates/default.js
+++ plugins/templates/templates/default.js
@@ -27,13 +27,74 @@ CKEDITOR.addTemplates( 'default', {
 		title: 'Strange Template',
 		image: 'template2.gif',
 		description: 'A template that defines two columns, each one with a title, and some text.',
-		htmlFile: CKEDITOR.getUrl( 'plugins/templates/templates/html/strange-template.html' )
+		html: '<table cellspacing="0" cellpadding="0" style="width:100%" border="0">' +
+		'<tr>' +
+			'<td style="width:50%">' +
+				'<h3>Title 1</h3>' +
+			'</td>' +
+			'<td></td>' +
+			'<td style="width:50%">' +
+				'<h3>Title 2</h3>' +
+			'</td>' +
+		'</tr>' +
+		'<tr>' +
+			'<td>' +
+				'Text 1' +
+			'</td>' +
+			'<td></td>' +
+			'<td>' +
+				'Text 2' +
+			'</td>' +
+		'</tr>' +
+		'</table>' +
+		'<p>' +
+		'More text goes here.' +
+		'</p>'
 	},
 	{
 		title: 'Text and Table',
 		image: 'template3.gif',
 		description: 'A title with some text and a table.',
-		htmlFile: CKEDITOR.getUrl( 'plugins/templates/templates/html/text-and-table.html' )
+		html: '<div style="width: 80%">' +
+		'<h3>' +
+			'Title goes here' +
+		'</h3>' +
+		'<table style="width:150px;float: right" cellspacing="0" cellpadding="0" border="1">' +
+			'<caption style="border:solid 1px black">' +
+				'<strong>Table title</strong>' +
+			'</caption>' +
+			'<tr>' +
+				'<td>&nbsp;</td>' +
+				'<td>&nbsp;</td>' +
+				'<td>&nbsp;</td>' +
+			'</tr>' +
+			'<tr>' +
+				'<td>&nbsp;</td>' +
+				'<td>&nbsp;</td>' +
+				'<td>&nbsp;</td>' +
+			'</tr>' +
+			'<tr>' +
+				'<td>&nbsp;</td>' +
+				'<td>&nbsp;</td>' +
+				'<td>&nbsp;</td>' +
+			'</tr>' +
+		'</table>' +
+		'<p>' +
+			'Type the text here' +
+		'</p>' +
+		'</div>'
+	},
+	{
+		title: 'Title and Text',
+		image: 'template3.gif',
+		description: 'A title with some text.',
+		htmlFile: CKEDITOR.getUrl( 'plugins/templates/templates/html/title-and-text.html' )
+	},
+	{
+		title: 'Enumeration',
+		image: 'template3.gif',
+		description: 'An enumeration with multiple levels.',
+		htmlFile: CKEDITOR.getUrl( 'plugins/templates/templates/html/enumeration.html' )
 	}
 	]
 });
