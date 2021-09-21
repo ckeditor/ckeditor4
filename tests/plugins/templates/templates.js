0fd1f085a5f5e265c0960b905092e013eddc98c8
Diana Pankratz <Diana.Pankratz@t-online.de>
2021-09-21 11:40:19 +0200

    Add unit tests for template insertion.

--- tests/plugins/templates/templates.js
+++ tests/plugins/templates/templates.js
@@ -1,10 +1,10 @@
 /* bender-tags: editor,dialog */
 /* bender-ckeditor-plugins: wysiwygarea,toolbar,basicstyles,templates,format*/
 
-( function() {
+(function () {
     'use strict';
 
-    bender.test( {
+    bender.test({
         'test aborting template insertion': function() {
             bender.editorBot.create( {
                 startupData: '<p>Lorem ipsum</p>',
@@ -20,7 +20,109 @@
 
                 } );
             } );
+        },
+        'test insert template and replace content': function() {
+            bender.editorBot.create( {
+                startupData: '<p>Lorem ipsum</p>',
+                config: {
+                    templates_files: ['/tests/plugins/templates/_assets/test.js'],
+                    templates: 'test'
+                }
+            }, function( bot ) {
+                bot.dialog( 'templates', function( dialog ) {
+                    dialog.getContentElement( 'selectTpl', 'chkInsertOpt' ).setValue(true);
+                    setTimeout(function () {
+                        dialog.getContentElement( 'selectTpl', 'templatesList' ).getElement().find( 'a' ).$[1].click();
+
+                        setTimeout(function () {
+                            resume(function () {
+                                assert.areEqual( '<h1>^I am a title</h1>', bot.htmlWithSelection(), 'Editor data has been replaced by the template.' );
+                            });
+                        }, 100);
+                    }, 100);
+                    wait();
+                } );
+            } );
+        },
+        'test insert template file and replace content': function() {
+            bender.editorBot.create( {
+                startupData: '<p>Lorem ipsum</p>',
+                config: {
+                    templates_files: ['/tests/plugins/templates/_assets/test.js'],
+                    templates: 'test'
+                }
+            }, function( bot ) {
+                bot.dialog( 'templates', function( dialog ) {
+                    dialog.getContentElement( 'selectTpl', 'chkInsertOpt' ).setValue(true);
+                    setTimeout(function () {
+                        dialog.getContentElement( 'selectTpl', 'templatesList' ).getElement().findOne( 'a' ).$.click();
+
+                        setTimeout(function () {
+                            resume(function () {
+                                assert.areEqual( '<h1>^I am a title</h1><p>I am a text</p>', bot.htmlWithSelection(), 'Editor data has been replaced by the template.' );
+                            });
+                        }, 100);
+                    }, 100);
+                    wait();
+                } );
+            } );
+        },
+        'test insert template file at cursor position': function() {
+            bender.editorBot.create( {
+                startupData: '<p>Lorem ipsum</p>',
+                config: {
+                    templates_files: ['/tests/plugins/templates/_assets/test.js'],
+                    templates: 'test'
+                }
+            }, function( bot ) {
+                setCursorAtTheEnd(bot.editor);
+                bot.dialog( 'templates', function( dialog ) {
+                    dialog.getContentElement( 'selectTpl', 'chkInsertOpt' ).setValue(false);
+                    setTimeout(function () {
+                        dialog.getContentElement( 'selectTpl', 'templatesList' ).getElement().findOne( 'a' ).$.click();
+
+                        setTimeout(function () {
+                            resume(function () {
+                                assert.areEqual( '<p>Lorem ipsum</p><h1>I am a title</h1><p>I am a text^</p>', bot.htmlWithSelection(), 'Template has been inserted.' );
+                            });
+                        }, 100);
+                    }, 100);
+
+                    wait();
+                } );
+            } );
+        },
+        'test insert template at cursor position': function () {
+            bender.editorBot.create({
+                startupData: '<p>Lorem ipsum</p>',
+                config: {
+                    templates_files: ['/tests/plugins/templates/_assets/test.js'],
+                    templates: 'test'
+                }
+            }, function (bot) {
+                setCursorAtTheEnd(bot.editor);
+                bot.dialog('templates', function (dialog) {
+                    dialog.getContentElement('selectTpl', 'chkInsertOpt').setValue(false);
+                    setTimeout(function () {
+                        dialog.getContentElement('selectTpl', 'templatesList').getElement().find('a').$[1].click();
+
+                        setTimeout(function () {
+                            resume(function () {
+                                assert.areEqual('<p>Lorem ipsum</p><p>I am a text</p><p>Here is some more text^</p>', bot.htmlWithSelection(), 'Template has been inserted.');
+                            });
+                        }, 100);
+                    }, 100);
+
+                    wait();
+                });
+            });
         }
 
-    } );
-} )();
+    });
+    function setCursorAtTheEnd(editor) {
+        editor.focus();
+        var range = editor.createRange();
+        range.moveToElementEditEnd(editor.editable());
+        range.select();
+    }
+})();
