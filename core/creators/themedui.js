d4fbea1cc0d775f9d49039416361b2695376ecad
sculpt0r <github@sculpt0r.net>
2021-10-07 15:14:15 +0200

    Remove unnecessary API comment

--- core/creators/themedui.js
+++ core/creators/themedui.js
@@ -262,7 +262,7 @@ CKEDITOR.replaceClass = 'ckeditor';
 	 * @param {Number/String} width The new width. It can be an integer denoting a value
 	 * in pixels or a CSS size value with unit. When null is passed, the value will not be set.
 	 * @param {Number/String} height The new height. It can be an integer denoting a value
-	 * in pixels or a CSS size value with unit. When null is passed, the value will not be set.
+	 * in pixels or a CSS size value with unit.
 	 * @param {Boolean} [isContentHeight] Indicates that the provided height is to
 	 * be applied to the editor content area, and not to the entire editor
 	 * interface. Defaults to `false`.
