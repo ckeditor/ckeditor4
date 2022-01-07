ca89dcbf357c43978904a2fdec88f82eea1c4789
sculpt0r <github@sculpt0r.net>
2022-01-07 18:31:39 +0100

    Code style & new case

--- tests/core/ckeditor/appendtimestamp.js
+++ tests/core/ckeditor/appendtimestamp.js
@@ -4,14 +4,14 @@
 	'use strict';
 
 	bender.test( {
-		'test append timestamp returns the same resource path if there is no timestamp': performTimestampTest(
-				'',
-				'style.css',
-				'style.css',
-				'Resource path was modified without timestamp in CKEDITOR'
+		'test append timestamp returns the same resource path if there is no CKEDITOR.timestamp': performTimestampTest(
+			'',
+			'style.css',
+			'style.css',
+			'Resource path was modified without timestamp in CKEDITOR'
 		),
 
-		'test append timestamp returns resource path with timestamp': performTimestampTest(
+		'test append timestamp returns resource path with timestamp if there is CKEDITOR.timestamp': performTimestampTest(
 			'cke4',
 			'style.css',
 			'style.css?t=cke4',
@@ -25,7 +25,7 @@
 			'Resource path to directory should not be changed'
 		),
 
-		'test append timestamp to resource path with only timestamp does not change it': performTimestampTest(
+		'test append timestamp to resource path, with timestamp as single URL param, does not change it': performTimestampTest(
 			'cke4',
 			'style.css?t=qaz1',
 			'style.css?t=qaz1',
@@ -37,6 +37,13 @@
 			'style.css?q=abc&t=qaz1',
 			'style.css?q=abc&t=qaz1',
 			'Resource path with chained timestamp was affected with new timestamp'
+		),
+
+		'test append timestamp to resource path with URL params adds timestamp as another param': performTimestampTest(
+			'cke4',
+			'style.css?q=abc',
+			'style.css?q=abc&t=cke4',
+			'Resource path with single param has not been affected by timestamp'
 		)
 	} );
 
