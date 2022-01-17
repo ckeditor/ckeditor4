8a92c89a2d6a135fa36a94d1f55bcc1d7e46d551
Tomasz Jakut <vepomoc@gmail.com>
2022-01-17 10:54:51 +0100

    Update license updater.

--- dev/license/updatelicense.js
+++ dev/license/updatelicense.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 
@@ -7,6 +7,8 @@
 
 var fs = require( 'fs' ),
 	path = require( 'path' ),
+	OLD_COMPANY_NAME_REGEXP = /(\[)?CKSource(\]\(.+?\))? - Frederico Knabben/gi,
+	COMPANY_NAME = 'CKSource Holding sp. z o.o',
 	YEAR = new Date().getFullYear(),
 	ACCEPTED_FORMATS = [ '.html', '.txt', '.js', '.md', '.sh', '.css', '.py', '.less', '.php', '.rb' ],
 	EXCLUDED_DIRS = [ '.git', 'node_modules', 'release', 'coverage' ];
@@ -30,20 +32,27 @@ function recursivelyUpdateLicenseDate( filepath ) {
 				recursivelyUpdateLicenseDate( path.join( filepath, file ) );
 			} );
 	} else if ( ACCEPTED_FORMATS.indexOf( path.extname( filepath ) ) > -1 ) {
-		updateLicenseDate( filepath );
+		updateLicenseBanner( filepath );
 	}
 }
 
-function updateLicenseDate( filepath ) {
+function updateLicenseBanner( filepath ) {
 	var data = fs.readFileSync( filepath, 'utf8' ),
-		regexp = /(Copyright.*\d{4}.*-.*)\d{4}(.*CKSource)/gi,
-		match = regexp.exec( data ),
-		updated = false;
+		bannerRegexp = /(Copyright.*\d{4}.*-.*)\d{4}(.*CKSource.*\.)/gi,
+		bannerMatch = bannerRegexp.exec( data ),
+		updated = false,
+		companyNamePart;
 
-	while ( match != null ) {
+	while ( bannerMatch != null ) {
 		updated = true;
-		data = data.replace( match[ 0 ], match[ 1 ] + YEAR + match[ 2 ] );
-		match = regexp.exec( data );
+		companyNamePart = bannerMatch[ 2 ];
+
+		if ( OLD_COMPANY_NAME_REGEXP.test( companyNamePart ) ) {
+			companyNamePart = companyNamePart.replace( OLD_COMPANY_NAME_REGEXP, '$1' + COMPANY_NAME + '$2' );
+		}
+
+		data = data.replace( bannerMatch[ 0 ], bannerMatch[ 1 ] + YEAR + companyNamePart );
+		bannerMatch = bannerRegexp.exec( data );
 	}
 
 	if ( updated ) {
