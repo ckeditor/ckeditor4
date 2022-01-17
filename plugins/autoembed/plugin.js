83ca1de6eb7ac75c46e5a3dac6a0a11c78575d48
Bartłomiej Kalemba <github@sculpt0r.net>
2022-01-17 13:50:50 +0100

    Update plugin.js

--- plugins/autoembed/plugin.js
+++ plugins/autoembed/plugin.js
@@ -2,7 +2,7 @@
  * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-
+//another dummy change
 'use strict';
 
 ( function() {
