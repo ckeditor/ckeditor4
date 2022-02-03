764c193856601681d4bdde8c313bc0c4c098ab14
Karol Dawidziuk <k.dawidziuk@cksource.com>
2022-02-03 16:58:17 +0100

    Improvements manual tests for removing multiple lists

--- tests/core/editable/removemultiplelist.js
+++ tests/core/editable/removemultiplelist.js
@@ -40,7 +40,7 @@
 			'test part mixed lists is removed with the delete key': performInputOutputTest( 'mixed_part_lists', DELETE_KEY, CKEDITOR.env.ie || CKEDITOR.env.gecko ),
 			'test part mixed lists is removed with the backspace key': performInputOutputTest( 'mixed_part_lists', BACKSPACE_KEY, CKEDITOR.env.ie || CKEDITOR.env.gecko ),
 
-			// #5068
+			// (#5068)
 			'test fully selected lists without part of first list item are removed with the delete key': performInputOutputTest( 'without_first_item', DELETE_KEY, CKEDITOR.env.ie || CKEDITOR.env.gecko ),
 			'test fully selected lists without part of first list item are removed with the backspace key': performInputOutputTest( 'without_first_item', BACKSPACE_KEY, CKEDITOR.env.ie || CKEDITOR.env.gecko )
 		};
