36f8bc71d6a371f34b2d3db9d4f9476b7cef4c42
sculpt0r <github@sculpt0r.net>
2022-02-15 23:31:02 +0100

    Trim value in notEmpty validator

--- plugins/dialog/plugin.js
+++ plugins/dialog/plugin.js
@@ -3298,7 +3298,7 @@ CKEDITOR.DIALOG_STATE_BUSY = 2;
 			notEmpty: function( msg ) {
 				return function() {
 					var value = this && this.getValue ? this.getValue() : arguments[ 0 ];
-					return value.length > 0 ? true : msg;
+					return CKEDITOR.tools.trim( value ).length > 0 ? true : msg;
 				};
 			},
 
