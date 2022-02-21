2a9957d3f67cd05f292e70285bac812d3d58e047
sculpt0r <github@sculpt0r.net>
2022-02-21 06:51:44 +0100

    Update validator tests

--- tests/plugins/dialog/validatornotempty.js
+++ tests/plugins/dialog/validatornotempty.js
@@ -1,124 +1,128 @@
 /* bender-tags: editor, dialog */
 /* bender-ckeditor-plugins: dialog */
 
+var validator = {},
+	msg = 'Given string is empty!';
+
 bender.test( {
+
 	setUp: function() {
-		this.msg = 'Given string is empty!';
-		this.notEmptyValidator = CKEDITOR.dialog.validate.notEmpty( this.msg );
+		validator.notEmptyValidator = CKEDITOR.dialog.validate.notEmpty( msg );
 	},
 
 	tearDown: function() {
-		this.notEmptyValidator = null;
-		this.getValue = null;
+		validator.notEmptyValidator = null;
+		validator.getValue = null;
 	},
 
 	'test notEmptyValidator immediate call': function() {
-		assert.isTrue( CKEDITOR.dialog.validate.notEmpty( this.msg )( 'not empty' ) );
+		assert.isTrue( CKEDITOR.dialog.validate.notEmpty( msg )( 'not empty' ) );
 	},
 
 	'test notEmptyValidator immediate call empty': function() {
-		assert.areSame( CKEDITOR.dialog.validate.notEmpty( this.msg )( '' ), this.msg );
+		assert.areSame( CKEDITOR.dialog.validate.notEmpty( msg )( '' ), msg );
 	},
+
 	// Next two tests prove - that value could be received in `getValue` way
 	// and validator treat them differently, not as some hardcoded manner
 	// all edge cases are performed on local value - less code -and we validate the
 	// logic - not the way the value is received
 	'test notEmptyValidator analyze value from inner getter': function() {
-		setupValueGetter( 'not empty value', this );
-		assert.isTrue( this.notEmptyValidator() );
+		setupValueGetter( 'not empty value', validator );
+		assert.isTrue( validator.notEmptyValidator() );
 	},
 
 	'test notEmptyValidator analyze empty value from inner getter': function() {
-		setupValueGetter( '', this );
-		assert.areSame( this.notEmptyValidator(), this.msg );
+		setupValueGetter( '', validator );
+		assert.areSame( validator.notEmptyValidator(), msg );
 	},
 
 	'test notEmptyValidator returns error message for zero length string': function() {
-		assert.areSame( this.notEmptyValidator( '' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for string with spaces only': function() {
-		assert.areSame( this.notEmptyValidator( '   ' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '   ' ), msg );
 	},
 
 	'test notEmptyValidator returns true called multiple times on not empty value': function() {
 		var value = 'a';
-		this.notEmptyValidator( value );
-		assert.isTrue( this.notEmptyValidator( value ) );
+		validator.notEmptyValidator( value );
+		assert.isTrue( validator.notEmptyValidator( value ) );
 	},
 
 	'test notEmptyValidator returns error msg called multiple times on empty value': function() {
 		var value = '   ';
-		this.notEmptyValidator( value );
-		assert.areSame( this.notEmptyValidator( value ), this.msg );
+		validator.notEmptyValidator( value );
+		assert.areSame( validator.notEmptyValidator( value ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u0020 SPACE ': function() {
-		assert.areSame( this.notEmptyValidator( '\u0020' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u0020' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u00A0 NO-BREAK SPACE': function() {
-		assert.areSame( this.notEmptyValidator( '\u00A0' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u00A0' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u1680 OGHAM SPACE MARK': function() {
-		assert.areSame( this.notEmptyValidator( '\u1680' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u1680' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2000 EN QUAD': function() {
-		assert.areSame( this.notEmptyValidator( '\u2000' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2000' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2001 EM QUAD': function() {
-		assert.areSame( this.notEmptyValidator( '\u2001' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2001' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u02002 EN SPACE': function() {
-		assert.areSame( this.notEmptyValidator( '\u2002' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2002' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2003 EM SPACE ': function() {
-		assert.areSame( this.notEmptyValidator( '\u2003' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2003' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2004 THREE-PER-EM SPACE ': function() {
-		assert.areSame( this.notEmptyValidator( '\u2004' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2004' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2005 FOUR-PER-EM SPACE ': function() {
-		assert.areSame( this.notEmptyValidator( '\u2005' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2005' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2006 SIX-PER-EM SPACE ': function() {
-		assert.areSame( this.notEmptyValidator( '\u2006' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2006' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2007 FIGURE SPACE ': function() {
-		assert.areSame( this.notEmptyValidator( '\u2007' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2007' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2008 PUNCTUATION SPACE': function() {
-		assert.areSame( this.notEmptyValidator( '\u2008' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2008' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2009 THIN SPACE ': function() {
-		assert.areSame( this.notEmptyValidator( '\u2009' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u2009' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u2000A HAIR SPACE': function() {
-		assert.areSame( this.notEmptyValidator( '\u200A' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u200A' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u202F NARROW NO-BREAK SPACE': function() {
-		assert.areSame( this.notEmptyValidator( '\u202F' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u202F' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u205F MEDIUM MATHEMATICAL SPACE': function() {
-		assert.areSame( this.notEmptyValidator( '\u205F' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u205F' ), msg );
 	},
 
 	'test notEmptyValidator returns error message for unicode u3000 IDEOGRAPHIC SPACE ': function() {
-		assert.areSame( this.notEmptyValidator( '\u3000' ), this.msg );
+		assert.areSame( validator.notEmptyValidator( '\u3000' ), msg );
 	}
 } );
 
