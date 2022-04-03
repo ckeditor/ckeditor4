639d0ab6438c8685f49b6a4c28669924dff58762
Tomasz Jakut <vepomoc@gmail.com>
2022-04-03 15:48:51 +0200

    Remove ARIA attributes from full-page data.

--- plugins/wysiwygarea/plugin.js
+++ plugins/wysiwygarea/plugin.js
@@ -597,6 +597,11 @@
 					if ( CKEDITOR.env.gecko && config.enterMode != CKEDITOR.ENTER_BR )
 						data = data.replace( /<br>(?=\s*(:?$|<\/body>))/, '' );
 
+					// Remove ARIA attributes during getting data for full-page editing (#4052).
+					if ( fullPage ) {
+						data = data.replace( '<body role="textbox" aria-multiline="true"', '<body ' );
+					}
+
 					data = editor.dataProcessor.toDataFormat( data );
 
 					if ( xmlDeclaration )
