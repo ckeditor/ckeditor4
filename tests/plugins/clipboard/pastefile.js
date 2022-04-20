3e809649753c5688fc6eed02afa427a1aa97be54
Karol Dawidziuk <karl1995000@wp.pl>
2022-04-20 10:32:21 +0200

    Remove unused arguments and parameter from unit test

--- tests/plugins/clipboard/pastefile.js
+++ tests/plugins/clipboard/pastefile.js
@@ -48,34 +48,34 @@
 		},
 
 		'test pasting unsupported file extension shows notification': function( editor ) {
-			assertPasteFile( editor, this, 'application/pdf', 1 );
+			assertPasteFile( editor, 'application/pdf', 1 );
 		},
 
 		'test pasting unsupported file extension do not shows notification when they are disabled': function( editor ) {
 			editor.config.clipboard_disableNotification = true;
 
-			assertPasteFile( editor, this, 'application/zip', 0 );
+			assertPasteFile( editor, 'application/zip', 0 );
 		},
 
 		'test pasting unsupported image extension do not shows notification when notifications are disabled and ignoring notifications for unsupported images is enabled': function( editor ) {
 			editor.config.clipboard_disableNotification = true;
 			editor.config.clipboard_ignoreNotificationsForImages = false;
 
-			assertPasteFile( editor, this, 'application/jp2', 0 );
+			assertPasteFile( editor, 'application/jp2', 0 );
 		},
 
 		'test pasting unsupported file extension do not shows notification when notifications are disabled and ignoring notifications for unsupported non-images is enabled': function( editor ) {
 			editor.config.clipboard_disableNotification = true;
 			editor.config.clipboard_ignoreNotificationsForNonImages = false;
 
-			assertPasteFile( editor, this, 'application/zip', 0 );
+			assertPasteFile( editor, 'application/zip', 0 );
 		},
 
 		'test pasting unsupported file extension do not shows notification when notifications are disabled and ignoring notifications for specific unsupported extensions is enabled': function( editor ) {
 			editor.config.clipboard_disableNotification = true;
 			editor.config.clipboard_ignoreNotificationsForExtensions = [ 'pdf' ];
 
-			assertPasteFile( editor, this, 'application/pdf', 0 );
+			assertPasteFile( editor, 'application/pdf', 0 );
 		},
 
 		// jscs:disable maximumLineLength
@@ -83,7 +83,7 @@
 			editor.config.clipboard_ignoreNotificationsForImages = false;
 			editor.config.clipboard_ignoreNotificationsForNonImages = true;
 
-			assertPasteFile( editor, this, 'image/webp', 1 );
+			assertPasteFile( editor, 'image/webp', 1 );
 		},
 
 		// jscs:disable maximumLineLength
@@ -91,55 +91,55 @@
 			editor.config.clipboard_ignoreNotificationsForImages = true;
 			editor.config.clipboard_ignoreNotificationsForNonImages = false;
 
-			assertPasteFile( editor, this, 'application/pdf', 1 );
+			assertPasteFile( editor, 'application/pdf', 1 );
 		},
 
 		'test pasting unsupported file extension do not shows notification when ignoring notifications for images and non-images are enabled': function( editor ) {
 			editor.config.clipboard_ignoreNotificationsForImages = true;
 			editor.config.clipboard_ignoreNotificationsForNonImages = true;
 
-			assertPasteFile( editor, this, 'application/pdf', 0 );
+			assertPasteFile( editor, 'application/pdf', 0 );
 		},
 
 		'test pasting unsupported image extension do not shows notification when ignoring notifications for images and non-images are enabled': function( editor ) {
 			editor.config.clipboard_ignoreNotificationsForImages = true;
 			editor.config.clipboard_ignoreNotificationsForNonImages = true;
 
-			assertPasteFile( editor, this, 'image/webp', 0 );
+			assertPasteFile( editor, 'image/webp', 0 );
 		},
 
 		'test pasting unsupported image extension do not shows notification when ignoring notifications for images is disabled': function( editor ) {
 			editor.config.clipboard_ignoreNotificationsForImages = true;
 
-			assertPasteFile( editor, this, 'image/webp', 0 );
+			assertPasteFile( editor, 'image/webp', 0 );
 		},
 
 		'test pasting unsupported file extension do not shows notification when ignoring notifications for non-images is disabled': function( editor ) {
 			editor.config.clipboard_ignoreNotificationsForNonImages = true;
 
-			assertPasteFile( editor, this, 'application/pdf', 0 );
+			assertPasteFile( editor, 'application/pdf', 0 );
 		},
 
 		'test pasting unsupported file extension do not shows notification when ignoring notifications for specific extensions is disabled': function( editor ) {
 			editor.config.clipboard_ignoreNotificationsForExtensions = [ 'pdf' ];
 
-			assertPasteFile( editor, this, 'application/pdf', 0 );
+			assertPasteFile( editor, 'application/pdf', 0 );
 		},
 
 		'test pasting unsupported image extension shows notification when ignoring notifications for non-images is disabled': function( editor ) {
 			editor.config.clipboard_ignoreNotificationsForNonImages = false;
 
-			assertPasteFile( editor, this, 'image/webp', 1 );
+			assertPasteFile( editor, 'image/webp', 1 );
 		},
 
 		'test pasting unsupported file extension shows notification when ignoring notifications for images is disabled': function( editor ) {
 			editor.config.clipboard_ignoreNotificationsForImages = false;
 
-			assertPasteFile( editor, this, 'image/webp', 1 );
+			assertPasteFile( editor, 'image/webp', 1 );
 		}
 	};
 
-	function assertPasteFile( editor, bend, fileType, expectedNotificationCount ) {
+	function assertPasteFile( editor, fileType, expectedNotificationCount ) {
 		var notificationSpy = sinon.spy( editor, 'showNotification' ),
 			options = {
 				type: fileType,
