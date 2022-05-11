9d1110f47952cbd9a5f6b4bd12adcec99df3b99c
Tomasz Jakut <vepomoc@gmail.com>
2022-05-11 11:13:39 +0200

    Ignore tests on iOS.

--- tests/plugins/maximize/aria.js
+++ tests/plugins/maximize/aria.js
@@ -11,8 +11,20 @@
 		'Maximize'
 	] );
 
+	tests.setUp = function() {
+		// Maximize plugin is disabled on iOS (https://dev.ckeditor.com/ticket/8307).
+		if ( CKEDITOR.env.iOS ) {
+			assert.ignore();
+		}
+	};
+
 	// (#2444)
 	tests[ 'the label is not changed after maximizing the editor' ] = function() {
+		// For some reason the global setup function does not work for this test.
+		if ( CKEDITOR.env.iOS ) {
+			assert.ignore();
+		}
+
 		var editor = this.editor,
 			button = buttonTools.getUiItem( editor, 'Maximize' ),
 			domButton = buttonTools.getButtonDomElement( button ),
