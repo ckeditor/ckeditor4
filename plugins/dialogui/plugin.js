82167e57c201be30930eab311c70c27d9348c7d8
Jordan Bradford <jordan@brownboots.com>
2022-06-09 11:11:03 -0500

    making checkbox and radio buttons' .setValue() chainable

--- plugins/dialogui/plugin.js
+++ plugins/dialogui/plugin.js
@@ -1201,6 +1201,7 @@ CKEDITOR.plugins.add( 'dialogui', {
 			setValue: function( checked, noChangeEvent ) {
 				this.getInputElement().$.checked = checked;
 				!noChangeEvent && this.fire( 'change', { value: checked } );
+				return this;
 			},
 
 			/**
@@ -1261,6 +1262,7 @@ CKEDITOR.plugins.add( 'dialogui', {
 				( i < children.length ) && ( item = children[ i ] ); i++ )
 					item.getElement().$.checked = ( item.getValue() == value );
 				!noChangeEvent && this.fire( 'change', { value: value } );
+				return this;
 			},
 
 			/**
