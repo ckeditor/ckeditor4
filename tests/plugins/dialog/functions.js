fa1ad5bce4e5637f2420dd0e1722a966e49c39af
sculpt0r <github@sculpt0r.net>
2022-06-22 09:32:54 +0200

    Add failing test as proof of bug

---
+++ tests/plugins/dialog/functions.js
@@ -0,0 +1,28 @@
+/* bender-tags: editor, dialog */
+/* bender-ckeditor-plugins: dialog */
+
+var validator = {};
+
+bender.test( {
+	// (#4449)
+	'test functions allows properly compose validators': function() {
+		var errorMsg = 'error!';
+		validator.notEmptyNumberValidator = CKEDITOR.dialog.validate.functions(
+			CKEDITOR.dialog.validate.notEmpty( 'Value is required.' ),
+			CKEDITOR.dialog.validate.number( 'Value is not a number.' ),
+			errorMsg
+		);
+
+		setupValueGetter( 'not a number', validator );
+
+		var result = validator.notEmptyNumberValidator();
+		assert.areSame( errorMsg, result );
+	}
+} );
+
+
+function setupValueGetter( value, context ) {
+	context.getValue = function() {
+		return value;
+	};
+}
