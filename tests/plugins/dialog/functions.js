971679bebf35ed6c29dc1f31b4670b572155eec0
sculpt0r <github@sculpt0r.net>
2022-06-22 11:03:08 +0200

    Refator tests

--- tests/plugins/dialog/functions.js
+++ tests/plugins/dialog/functions.js
@@ -4,20 +4,95 @@
 var validator = {};
 
 bender.test( {
+	tearDown: function() {
+		validator.composedValidator = null;
+		validator.getValue = null;
+	},
+
+	'test functions invoke all passed validators joined with default VALIDATE_AND with value': function() {
+		var testValue = 'value',
+			stubValidator = sinon.stub().returns( true );
+
+		validator.composedValidator = CKEDITOR.dialog.validate.functions(
+			stubValidator,
+			stubValidator,
+			stubValidator
+		);
+
+		setupValueGetter( testValue, validator );
+		validator.composedValidator();
+
+		assert.areSame( stubValidator.callCount, 3 );
+		assert.isTrue( stubValidator.calledWith( testValue ) );
+	},
+
+	'test functions returns true if all inner validators returns true - joined with default VALIDATE_AND': function() {
+		var stubValidator = sinon.stub().returns( true );
+
+		validator.composedValidator = CKEDITOR.dialog.validate.functions(
+			stubValidator,
+			stubValidator,
+			stubValidator
+		);
+
+		setupValueGetter( 'any value', validator );
+		var result = validator.composedValidator();
+
+		assert.isTrue( result );
+	},
+
 	// (#4449)
-	'test functions returns error message if inner validator fails': function() {
-		var errorMsg = 'error!';
-		validator.notEmptyNumberValidator = CKEDITOR.dialog.validate.functions(
-			CKEDITOR.dialog.validate.notEmpty( 'Value is required.' ),
-			CKEDITOR.dialog.validate.number( 'Value is not a number.' ),
+	'test functions returns error message if any inner validator returns not true - joined with default VALIDATE_AND': function() {
+		var stubTrueValidator = sinon.stub().returns( true ),
+			stubFalseValidator = sinon.stub().returns( 'error message' ),
+			errorMsg = 'error!';
+
+		validator.composedValidator = CKEDITOR.dialog.validate.functions(
+			stubTrueValidator,
+			stubFalseValidator,
 			errorMsg
 		);
 
-		setupValueGetter( 'not a number', validator );
+		setupValueGetter( 'any value', validator );
+		var result = validator.composedValidator();
 
-		var result = validator.notEmptyNumberValidator();
+		assert.areSame( errorMsg, result );
+	},
+
+	'test functions returns true if any inner validator returns true - joined with VALIDATE_OR': function() {
+		var stubTrueValidator = sinon.stub().returns( true ),
+			stubFalseValidator = sinon.stub().returns( false );
+
+		validator.composedValidator = CKEDITOR.dialog.validate.functions(
+			stubTrueValidator,
+			stubFalseValidator,
+			'error message',
+			CKEDITOR.VALIDATE_OR
+		);
+
+		setupValueGetter( 'any value', validator );
+		var result = validator.composedValidator();
+
+		assert.isTrue( result );
+	},
+
+	'test functions returns error message if all inner validator returns not true - joined with VALIDATE_OR': function() {
+		var stubFalseValidator = sinon.stub().returns( 'error message' ),
+			errorMsg = 'error!';
+
+		validator.composedValidator = CKEDITOR.dialog.validate.functions(
+			stubFalseValidator,
+			stubFalseValidator,
+			errorMsg,
+			CKEDITOR.VALIDATE_OR
+		);
+
+		setupValueGetter( 'any value', validator );
+		var result = validator.composedValidator();
+
 		assert.areSame( errorMsg, result );
 	}
+
 } );
 
 
