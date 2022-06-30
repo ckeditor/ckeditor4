01ea043fb77a50ab22282c8bec6b443fe9c19cad
Karol Dawidziuk <karl1995000@wp.pl>
2022-06-30 09:12:11 +0200

    Extract helper function

--- tests/_benderjs/ckeditor/static/tools.js
+++ tests/_benderjs/ckeditor/static/tools.js
@@ -1278,6 +1278,32 @@
 				mouseEvent.initMouseEvent( type, true, true, window, 0, 0, 0, 80, 20, false, false, false, false, button, null );
 				element.dispatchEvent( mouseEvent );
 			}
+		},
+
+		/**
+		 * Fires element event handler attribute e.g.
+		 * ```html <button onkeydown="return customFn( event )">x</button>```
+		 *
+		 * @param {CKEDITOR.dom.element/HTMLElement} element Element with attached event handler attribute.
+		 * @param {String} eventName Event handler attribute name.
+		 * @param {Object} evt Event payload.
+		*/
+		fireElementEventHandler: function( element, eventName, evt ) {
+			if ( element.$ ) {
+				element = element.$;
+			}
+
+			if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 ) {
+				var nativeEvent = CKEDITOR.document.$.createEventObject();
+
+				for ( var key in evt ) {
+					nativeEvent[ key ] = evt[ key ];
+				}
+
+				element.fireEvent( eventName, nativeEvent );
+			} else {
+				element[ eventName ]( evt );
+			}
 		}
 	};
 
