549acf967f16f5b2697e70db2ef430897f79a639
Karol Dawidziuk <karl1995000@wp.pl>
2023-03-02 23:38:42 +0100

    Add unit test

---
+++ tests/plugins/pastefromword/generated/keep_zero_margin_in_lists.js
@@ -0,0 +1,43 @@
+/* bender-tags: clipboard,pastefromword */
+/* jshint ignore:start */
+/* bender-ckeditor-plugins: pastefromword,ajax,list */
+/* jshint ignore:end */
+/* bender-include: _lib/q.js,_helpers/promisePasteEvent.js,_helpers/assertWordFilter.js,_helpers/createTestCase.js */
+/* bender-include: _helpers/createTestSuite.js,_helpers/pfwTools.js */
+/* global pfwTools,createTestSuite */
+
+( function() {
+	'use strict';
+
+	var config = {
+		extraAllowedContent:
+			'p{text-indent,margin,margin-top,margin-bottom};' +
+			'ul{margin,margin-top,margin-bottom};' +
+			'ol{margin,margin-top,margin-bottom}',
+		language: 'en',
+		pasteTools_keepZeroMargins: true
+	};
+
+	bender.editor = {
+		config: config
+	};
+
+	bender.test( createTestSuite( {
+		browsers: [
+			'chrome',
+			'edge',
+			'firefox',
+			'safari'
+		],
+		wordVersions: [ 'word2016' ],
+		tests: {
+			'Keep_zero_margin_in_lists': true
+		},
+
+		ignoreAll: CKEDITOR.env.ie || bender.tools.env.mobile,
+
+		customFilters: [
+			pfwTools.filters.font
+		]
+	} ) );
+} )();
