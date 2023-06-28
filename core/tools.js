2ba8cb118aa5ce5809879f9773d66b9918b11627
Youssef Attia <16466991+youssef-attia@users.noreply.github.com>
2023-06-28 18:57:31 +0000

    [Trusted Types] Adding tooling for Trusted Types compatibility (#1)
    Adds reviewed conversion function TrustedHTML

--- core/tools.js
+++ core/tools.js
@@ -123,6 +123,43 @@
 	 * @singleton
 	 */
 	CKEDITOR.tools = {
+
+		/**
+		 * Claims safety of an html string and returns trusted version.
+		 *
+		 * @param {string} html The safe html string.
+		 * @param {string} justification A short justification for why this
+		 * html string can be verified as safe.
+		 * @returns {TrustedHTML | string} the same html string but as a
+		 * TrustedHTML or a string if TT is not supported.
+		 */
+		htmlSafeByReview: function (html, justification) {
+
+			// If the justification is empty an error is raised. Any input marked as safe must be accompanied by a justification.
+			if (typeof justification !== 'string' || justification.trim() === '') {
+				let errMsg =
+					'Calls to uncheckedconversion functions must go through security review.';
+				errMsg += ' A justification must be provided to capture what security' +
+					' assumptions are being made.';
+				throw new Error(errMsg);
+			}
+
+			if (self.trustedTypes && self.trustedTypes.createPolicy) {
+				const policy = self.trustedTypes.createPolicy(
+					'trusted#htmlSafeByReview',
+					{
+						createHTML: function (html) {
+							// This policy is only to be used for trusted inputs that do not involve unsanitized user inputs.
+							return html;
+						},
+					}
+				);
+				return policy.createHTML(html);
+			} else {
+				return html;
+			}
+		},
+
 		/**
 		 * Compares the elements of two arrays.
 		 *
