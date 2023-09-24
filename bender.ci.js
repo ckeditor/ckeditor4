12010a7f9701eff201e735a54183cd3eb9e249fd
Tomasz Jakut <vepomoc@gmail.com>
2023-09-24 15:36:45 +0200

    Remove `config.isTravis` from Bender CI config.

--- bender.ci.js
+++ bender.ci.js
@@ -4,7 +4,6 @@
 var config = require( './bender' );
 
 config.startBrowser = process.env.BROWSER || 'Chrome';
-config.isTravis = true;
 config.startBrowserOptions = {
 	Chrome: '--headless --disable-gpu',
 	Firefox: '-headless'
