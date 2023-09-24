0dd11276020cdd4b79d88252f8b0e12d94d4c033
Tomasz Jakut <vepomoc@gmail.com>
2023-09-24 15:36:05 +0200

    Remove Travis-oriented scripts.

--- dev/travis/buildpath.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
-# CKEditor 4 LTS ("Long Term Support") is available under the terms of the Extended Support Model.
-
-# Return CKEditor build path.
-
-echo "../ckeditor4-presets/build/$(ls -1t ../ckeditor4-presets/build/ | head -n 1)/full-all/ckeditor/"
