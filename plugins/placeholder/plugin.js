066fed106e81b03b1238aa58df9f4bc5141bce6b
Progyan <Progyan@hackerrank.com>
2024-06-12 11:17:35 +0530

    feat: Add Support for Test Name and Link
    Add more configurability to Placeholder Select plugin.
    
    Signed-off-by: Progyan Bhattacharya <Progyan@hackerrank.com>

--- plugins/placeholder/plugin.js
+++ plugins/placeholder/plugin.js
@@ -10,27 +10,27 @@
 
 'use strict';
 
-( function() {
-  CKEDITOR.plugins.add( 'placeholder', {
+(function () {
+  CKEDITOR.plugins.add('placeholder', {
     requires: 'widget,dialog',
     lang: 'af,ar,az,bg,ca,cs,cy,da,de,de-ch,el,en,en-au,en-gb,eo,es,es-mx,et,eu,fa,fi,fr,fr-ca,gl,he,hr,hu,id,it,ja,km,ko,ku,lv,nb,nl,no,oc,pl,pt,pt-br,ro,ru,si,sk,sl,sq,sr,sr-latn,sv,th,tr,tt,ug,uk,vi,zh,zh-cn', // %REMOVE_LINE_CORE%
     icons: 'placeholder', // %REMOVE_LINE_CORE%
     hidpi: true, // %REMOVE_LINE_CORE%
 
-    onLoad: function() {
+    onLoad: function () {
       // Register styles for placeholder widget frame.
-     CKEDITOR.addCss('.cke_placeholder{background-color:#fff8ca;margin-right:5px;}');
+      CKEDITOR.addCss('.cke_placeholder{background-color:#fff8ca;margin-right:5px;}');
     },
 
-    init: function( editor ) {
+    init: function (editor) {
 
       var lang = editor.lang.placeholder;
 
       // Register dialog.
-      CKEDITOR.dialog.add( 'placeholder', this.path + 'dialogs/placeholder.js' );
+      CKEDITOR.dialog.add('placeholder', this.path + 'dialogs/placeholder.js');
 
       // Put ur init code here.
-      editor.widgets.add( 'placeholder', {
+      editor.widgets.add('placeholder', {
         // Widget code.
         dialog: 'placeholder',
         pathName: lang.pathName,
@@ -38,58 +38,58 @@
         // add dialog.
         template: '<span class="cke_placeholder">[[]]</span>',
 
-        upcast: function(el) {
-          if(el.name !== "span") {
+        upcast: function (el) {
+          if (el.name !== "span") {
             return;
           }
           var isPlaceholder = el.attributes['data-widget-type'] === 'placeholder';
-          if(!isPlaceholder) {
+          if (!isPlaceholder) {
             return;
           }
           el.addClass('cke_placeholder');
           return el;
         },
 
-        downcast: function() {
-          return new CKEDITOR.htmlParser.text( '{{' + this.data.name + '}}' );
+        downcast: function () {
+          return new CKEDITOR.htmlParser.text('{{' + this.data.name + '}}');
         },
 
-        init: function() {
+        init: function () {
           // Note that placeholder markup characters are stripped for the name.
-          this.setData( 'name', this.element.getText().slice( 2, -2 ) );
+          this.setData('name', this.element.getText().slice(2, -2));
         },
 
-        data: function() {
-          this.element.setText( '{{' + this.data.name + '}}' );
+        data: function () {
+          this.element.setText('{{' + this.data.name + '}}');
         },
 
-        getLabel: function() {
-          return this.editor.lang.widget.label.replace( /%1/, this.data.name + ' ' + this.pathName );
+        getLabel: function () {
+          return this.editor.lang.widget.label.replace(/%1/, this.data.name + ' ' + this.pathName);
         }
-      } );
+      });
 
-      editor.ui.addButton && editor.ui.addButton( 'CreatePlaceholder', {
+      editor.ui.addButton && editor.ui.addButton('CreatePlaceholder', {
         label: lang.toolbar,
         command: 'placeholder',
         toolbar: 'insert,100',
         icon: 'placeholder',
         title: 'Using this option, candidate name and recruiter name will be automatically added to the invite'
-      } );
+      });
     },
 
-    afterInit: function( editor ) {
+    afterInit: function (editor) {
       var placeholderReplaceRegex = /\[\[([^\[\]])+\]\]/g;
 
-      editor.dataProcessor.dataFilter.addRules( {
-        text: function( text, node ) {
-          var dtd = node.parent && CKEDITOR.dtd[ node.parent.name ];
+      editor.dataProcessor.dataFilter.addRules({
+        text: function (text, node) {
+          var dtd = node.parent && CKEDITOR.dtd[node.parent.name];
 
           // Skip the case when placeholder is in elements like <title> or <textarea>
           // but upcast placeholder in custom elements (no DTD).
-          if ( dtd && !dtd.span )
+          if (dtd && !dtd.span)
             return;
 
-          return text.replace( placeholderReplaceRegex, function( match ) {
+          return text.replace(placeholderReplaceRegex, function (match) {
             // Creating widget code.
 
             var option = match.match(/^\[\[(.*?)\]\]$/)[1].toLowerCase();
@@ -97,24 +97,33 @@
             * This values needs to be mapped as in backend we have
             * {{ candidate.name }} and {{ recruiter.name }}
             **/
-            var backendMapping = {'[[Candidate Name]]': '{{ candidate.name }}', '[[Recruiter Name]]': '{{ recruiter.name }}'}
+            var backendMapping = {
+              '[[Candidate Name]]': '{{ candidate.name }}',
+              '[[Recruiter Name]]': '{{ recruiter.name }}',
+              '[[Test Name]]': '{{ test.name }}',
+              '[[Test Link]]': '{{ test.link }}'
+            }
 
             var widgetWrapper = null,
               innerElement = new CKEDITOR.htmlParser.element('span', {
-                class: 'cke_placeholder','data-balloon': 'This automatically adds the '+ option+' in the invite','data-balloon-pos':'down', 'data-balloon-length':'medium','data-widget-type':'placeholder'
+                class: 'cke_placeholder',
+                'data-balloon': 'This automatically adds the ' + option + ' in the invite',
+                'data-balloon-pos': 'down',
+                'data-balloon-length': 'medium',
+                'data-widget-type': 'placeholder'
               });
 
             // Adds placeholder identifier as innertext.
-            innerElement.add( new CKEDITOR.htmlParser.text( backendMapping[match] ) );
-            widgetWrapper = editor.widgets.wrapElement( innerElement, 'placeholder' );
+            innerElement.add(new CKEDITOR.htmlParser.text(backendMapping[match]));
+            widgetWrapper = editor.widgets.wrapElement(innerElement, 'placeholder');
 
             // Return outerhtml of widget wrapper so it will be placed
             // as replacement.
             return widgetWrapper.getOuterHtml();
-          } );
+          });
         }
-      } );
+      });
     }
-  } );
+  });
 
-} )();
+})();
--- plugins/placeholder_select/plugin.js
+++ plugins/placeholder_select/plugin.js
@@ -12,59 +12,60 @@
 
 CKEDITOR.plugins.add('placeholder_select',
   {
-    requires : ['richcombo'],
-    init : function( editor )
-	{
-		//  array of placeholders to choose from that'll be inserted into the editor
+    requires: ['richcombo'],
+    init: function (editor) {
+      //  array of placeholders to choose from that'll be inserted into the editor
       var placeholders = [];
 
-		// init the default config - empty placeholders
+      // instances
+      var editorInstance = CKEDITOR.instances["problem-description"];
+
+      // init the default config - empty placeholders
       var defaultConfig = {
+        contentsCss: editorInstance ? editorInstance['config']['contentsCSS'] : [],
+        className: '',
         format: '[[%placeholder%]]',
-        placeholders : []
+        placeholders: [],
+        title: 'Add candidate or recruiter name to the invitation',
       };
 
-		// merge defaults with the passed in items
+      // merge defaults with the passed in items
       var config = CKEDITOR.tools.extend(defaultConfig, editor.config.placeholder_select || {}, true);
 
-		// run through and create the set of items to use
+      // run through and create the set of items to use
       for (var i = 0; i < config.placeholders.length; i++) {
-			// get our potentially custom placeholder format
+        // get our potentially custom placeholder format
         var placeholder = config.format.replace('%placeholder%', config.placeholders[i]);
         placeholders.push([placeholder, config.placeholders[i], config.placeholders[i]]);
       }
 
-		// add the menu to the editor
-      editor.ui.addRichCombo('placeholder_select',
-        {
-          voiceLabel: 'Insert placeholder',
-          className: 	'cke_format',
-          title: 'Add candidate or recruiter name to the invitation',
-          multiSelect:false,
-          panel:
-          {
-            css: [ editor.config.contentsCss, CKEDITOR.skin.getPath('editor'),CKEDITOR.instances["problem-description"]['config']['contentsCss'][0],
-              CKEDITOR.instances["problem-description"]['config']['contentsCss'][1],
+      // add the menu to the editor
+      editor.ui.addRichCombo('placeholder_select', {
+        voiceLabel: 'Insert placeholder',
+        className: ['cke_format', config.className].filter(cls => cls.trim().length > 0).join(' '),
+        title: config.title,
+        multiSelect: false,
+        panel: {
+          css: [
+            editor.config.contentsCss,
+            CKEDITOR.skin.getPath('editor'),
+            ...config.contentsCss,
+          ],
+          voiceLabel: editor.lang.panelVoiceLabel
+        },
 
-            ],
-            voiceLabel: editor.lang.panelVoiceLabel
-          },
-
-          init: function()
-			{
-            for (var i in placeholders)
-				{
-              this.add(placeholders[i][0], placeholders[i][1], placeholders[i][2]);
-            }
-          },
-
-          onClick: function( value )
-			{
-            editor.focus();
-            editor.fire( 'saveSnapshot' );
-            editor.insertHtml(value);
-            editor.fire( 'saveSnapshot' );
+        init: function () {
+          for (var i in placeholders) {
+            this.add(placeholders[i][0], placeholders[i][1], placeholders[i][2]);
           }
-        });
+        },
+
+        onClick: function (value) {
+          editor.focus();
+          editor.fire('saveSnapshot');
+          editor.insertHtml(value);
+          editor.fire('saveSnapshot');
+        }
+      });
     }
   });
